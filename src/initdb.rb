27d8e685503e7c54bd72ec3cb6e0d885a164b411
Tokusei Noborio <t.noborio@gmail.com>
2009-11-09 18:21:37 +0900

    Create schema.

--- src/initdb.rb
+++ src/initdb.rb
@@ -5,3 +5,56 @@ require 'sequel'
 
 require 'models'
 
+DB.create_table? :groups do
+  primary_key :id, :type=>Integer
+  String :name, :unique=>true, :null=>false
+end
+
+DB.create_table? :users do
+  primary_key :id, :type=>Integer
+  String :account, :unique=>true, :null=>false
+  String :password, :null=>false
+  foreign_key :group_id, :Groups, :null=>false
+end
+
+DB.create_table? :virtualmachinespecs do
+  primary_key :id, :type=>Integer
+  Float :cpu_mhz, :null=>false
+  Fixnum :memory, :null=>false
+end
+
+DB.create_table? :imagestorages do
+  primary_key :id, :type=>Integer
+  primary_key :access_id, :auto_increment=>false, :type=>String, :fixed=>true, :size=>8
+  String :storage_url, :null=>false
+end
+
+DB.create_table? :physicalmachines do
+  primary_key :id, :type=>Integer
+  String :cpu_model, :null=>false
+  Float :cpu_mhz, :null=>false
+  Fixnum :memory, :null=>false
+  String :location, :null=>false
+  String :hypervisor_type, :fixed=>true, :size=>8, :null=>false # xen, kvm, ...
+end
+
+DB.create_table? :instances do
+  primary_key :id, :type=>Integer
+  primary_key :access_id, :type=>String, :auto_increment=>false, :fixed=>true, :size=>8
+  foreign_key :user_id, :Users, :null=>false
+  foreign_key :physicalmachine_id, :Physicalmachines, :null=>false
+  foreign_key :imagestorage_id, :Imagestorages, :null=>false
+  foreign_key :virtualmachinespec_id, :Virtualmachinespecs, :null=>false
+end
+
+DB.create_table? :vmcontrollermasters do
+  primary_key :id, :type=>Integer
+  String :access_url, :null=>false
+end
+
+DB.create_table? :vmcontrolleragents do
+  primary_key :id, :type=>Integer
+  foreign_key :vmcontrollermaster_id, :Vmcontrollermasters
+  foreign_key :physicalmachine_id, :Physicalmachines
+end
+
--- src/models.rb
+++ src/models.rb
@@ -1,6 +1,23 @@
+#!/usr/bin/ruby
+
 require 'rubygems'
 require 'sequel'
 
-DB = Sequel.connect('mysql://localhost/wakame_dcmgr?user=wakame_dcmgr&password=<passwd>')
+DB = Sequel.connect('mysql://localhost/wakame_dcmgr?user=wakame_dcmgr&password=passwd')
 
 # models
+class Groups < Sequel::Model; end
+
+class Users < Sequel::Model; end
+
+class Instances < Sequel::Model; end
+
+class ImageStorages < Sequel::Model; end
+
+class VirtualMachineSpecs < Sequel::Model; end
+
+class PhysicalMachines < Sequel::Model; end
+
+class VMControllerMaster < Sequel::Model; end
+
+class VMControllerAgent < Sequel::Model; end
