f4aa9d2a6b46d42622fc11feaa5d36bb31d15348
Tokusei Noborio <t.noborio@gmail.com>
2009-11-12 18:23:32 +0900

    Implement crud code for sinatra.

--- public_crud.rb
+++ public_crud.rb
@@ -3,51 +3,72 @@ def public_crud obj
   public_name = obj.table_name.to_s # instances
   model = obj.to_s # Instances
 
-list = eval(<<END )
+list_instance = eval(<<END )
   proc do
-    #{public_name}_list = #{model}.all
-    erb :"#{public_name}/index"
+    protected!
+    #{public_name}_render(#{model}.all)
   end
 END
   
 get_instance = eval(<<END )
-  proc do
-    #{public_name}_list = #{model}.all
-    erb :"#{public_name}/new"
+  proc do |id|
+    protected!
+    obj = #{model}.find(:id=>id.to_i)
+    #{public_name}_render(obj)
   end
 END
 
 new_instance = eval(<<END )
   proc do
     protected!
-    #{public_name}_list = #{model}.all
-    erb :"#{public_name}/new"
+    req_hash = JSON.parse(request.body.read)
+    obj = #{model}.new
+    obj.set_all(req_hash)
+    obj.save
+    #{public_name}_render(obj)
   end
 END
 
 update_instance = eval(<<END )
-  proc do
+  proc do |id|
     protected!
-    id = params[:id]
-    @id = id
-    erb :"#{public_name}/update"
+    obj = #{model}.find(:id=>id.to_i)
+    req_hash = JSON.parse(request.body.read)
+    req_hash.delete 'id'
+    obj.set_all(req_hash)
+    obj.save
+    #{public_name}_render(obj)
   end
 END
 
-delelete_instance = eval(<<END )
-  proc do
+destroy_instance = eval(<<END )
+  proc do |id|
     protected!
-    erb :"#{public_name}/update"
-    # #{model}.filter(:id=>id).delete
-    # redirect "/#{public_name}"
+    obj = #{model}.find(:id=>id.to_i)
+    obj.destroy
+    #{public_name}_render(obj)
+  end
+END
+  
+eval(<<END )
+  def #{public_name}_render(obj)
+    def obj2hash i
+        Hash[i.keys.collect{ |key| [key, i[key]] }]
+    end
+    
+    if obj.is_a?(Array)
+      JSON.generate(obj.collect{|i| obj2hash(i)})
+    else
+      JSON.generate(obj2hash(obj))
+    end
   end
 END
 
-  get    "/#{public_name}.json"  ,&list 
+  get    "/#{public_name}.json"  ,&list_instance
   
   post   "/#{public_name}.json"  ,&new_instance
   
   get    %r{/#{public_name}/(\d+).json}  ,&get_instance
   put    %r{/#{public_name}/(\d+).json}  ,&update_instance
-  delete %r{/#{public_name}/(\d+).json}  ,&delelete_instance
+  delete %r{/#{public_name}/(\d+).json}  ,&destroy_instance
 end
--- script/get_instances.rb
+++ script/get_instances.rb
@@ -6,10 +6,11 @@ SITE = 'http://user:pass@localhost:9393/'
 class Instance < ActiveResource::Base
   self.site = SITE
   self.format = :json
+  self.logger = Logger.new($stderr)
 end
 
 # new
-instance = Instance.new(:user_id=>1, :physical_hosts_id=>10,
+instance = Instance.new(:user_id=>1, :physicalhost_id=>10,
                         :imagestorage_id=>100,
                         :hvspec_id=>1)
 instance.save
@@ -29,6 +30,6 @@ instance.destroy
 # list
 list = Instance.find(:all)
 for instance in list do
-  print instance.name
+  p instance
 end
 
