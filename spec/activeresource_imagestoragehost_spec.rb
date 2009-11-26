9fa4736d2fefa66a7309bc5244332e55f7acb1f3
Tokusei Noborio <t.noborio@gmail.com>
2009-11-26 18:24:00 +0900

    Add active resource physical host specs.

--- spec/activeresource_imagestoragehost_spec.rb
+++ spec/activeresource_imagestoragehost_spec.rb
@@ -1,1 +1,11 @@
 
+# -*- coding: utf-8 -*-
+
+require 'rubygems'
+require 'activeresource'
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "image storage host access by active resource" do
+  it "should add host"
+  it "should delete host"
+end
