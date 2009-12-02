29e5665adbdfeca106e64131f278d19d0598a6d4
Tokusei Noborio <t.noborio@gmail.com>
2009-12-02 16:12:33 +0900

    Delete unecessary debug report.

--- lib/dcmgr/public_crud.rb
+++ lib/dcmgr/public_crud.rb
@@ -2,7 +2,6 @@
 
 def public_crud model
   model.public_actions.each{|method_name, pattern, actiontag, args|
-    puts "#{method_name} #{pattern}, #{actiontag}, #{args}"
     eval("#{method_name} pattern, &model.get_action(model, actiontag, args)")
   }
 end
