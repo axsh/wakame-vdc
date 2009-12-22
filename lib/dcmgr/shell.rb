388d9985c07b030d51a0544e686ff20cba810755
Tokusei Noborio <t.noborio@gmail.com>
2009-12-22 14:53:43 +0900

    Add bundler environment in shell.rb

--- lib/dcmgr/shell.rb
+++ lib/dcmgr/shell.rb
@@ -1,5 +1,6 @@
 
 $:.unshift 'lib'
+require "#{File.dirname(__FILE__)}/../../vendor/gems/environment"
 require 'dcmgr'
 Dcmgr::Schema.connect 'mysql://localhost/wakame_dcmgr_test?user=dcmgr_test&password=passwd'
 require 'dcmgr/web'
