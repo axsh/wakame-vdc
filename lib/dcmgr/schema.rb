a944a026be2f6624a6d94202ca8199e3716ff509
Tokusei Noborio <t.noborio@gmail.com>
2009-12-25 16:33:05 +0900

    Add cpu cores, mhz, memory size fro instance schema.

--- lib/dcmgr/schema.rb
+++ lib/dcmgr/schema.rb
@@ -84,6 +84,10 @@ module Dcmgr
         Fixnum :user_id, :null=>false
         Fixnum :physical_host_id, :null=>false
         Fixnum :image_storage_id, :null=>false
+        
+        Fixnum :cpus, :null=>false
+        Float :cpu_mhz, :null=>false
+        Fixnum :memory, :null=>false
       end
       
       @db.create_table? :hv_controllers do
