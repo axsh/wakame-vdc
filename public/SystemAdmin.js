b11bdcbbb88b58c1a72fa48574bcee669a05a3ae
Takahisa Kamiya <gatto3@gatto.jp>
2010-01-07 14:49:46 +0900

    add javascripts

---
+++ public/SystemAdmin.js
@@ -0,0 +1,586 @@
+var cardPanel = null;
+var panelMode = 0;
+
+Ext.onReady(function(){
+  Ext.BLANK_IMAGE_URL='./javascripts/ext-js/resources/images/default/s.gif';
+  Ext.QuickTips.init();
+  var centerPanel = new CenterPanel();
+  var westPanel   = new WestPanel();
+  var northPanel  = new NorthPanel('System Administrator Tool');
+  var southPanel  = new SouthPanel();
+  viewport    = new Ext.Viewport({
+    layout: 'border',
+    items:[ northPanel, centerPanel, westPanel, southPanel]
+  });
+});
+
+function ChangePanel(md)
+{
+  if(panelMode != md){
+    panelMode = md;
+    cardPanel.layout.setActiveItem(panelMode);
+  }
+}
+
+WestPanel = function(){
+  var upPanel = new UpPanel();
+  var downPanel = new DownPanel();
+
+  WestPanel.superclass.constructor.call(this,{
+    region: "west", 
+    split: true,
+    header: false,
+    border: false,
+    width: 150,
+    layout: 'border',
+	items: [upPanel,downPanel]
+  });
+}
+Ext.extend(WestPanel, Ext.Panel);
+
+UpPanel = function(){
+
+  var form = new Ext.form.FormPanel({
+      labelWidth: 5, 
+      width: 100,
+      baseCls: 'x-plain',
+      items: [
+        {
+        xtype: 'combo',
+        editable: false,
+        typeAhead: true,
+        width: 120, 
+        triggerAction: 'all',
+        forceSelection:true,
+        mode: 'local',
+        store: new Ext.data.ArrayStore({
+          id: 1,
+          fields: [
+            'myId',
+            'displayText'
+          ],
+          data: [[1, 'AXSH-SOUMU1'], [2, 'AXSH-SOUMU2']]
+        }),
+        valueField: 'myId',
+        value:1,
+        displayField: 'displayText'
+        }
+      ]
+  });
+
+  UpPanel.superclass.constructor.call(this,{
+    region: "north", 
+    split: true,
+    height: 60,
+    border: false,
+    title: 'Account',
+    defaults:{bodyStyle:'padding:5px'},
+    layout: 'fit',
+    items: [form]
+  });
+}
+Ext.extend(UpPanel, Ext.Panel);
+
+DownPanel = function(){
+  DownPanel.superclass.constructor.call(this,{
+    region: "center", 
+    split: true,
+    header: false,
+    border: false,
+    useArrows:true,
+    enableDD:false,
+    width: 150,
+    listeners: {
+      'click': function(node){
+          if(node.id == 'menu01'){
+            ChangePanel(0);
+          }
+          else if(node.id == 'menu02'){
+            ChangePanel(1);
+          }
+          else if(node.id == 'menu03'){
+            ChangePanel(2);
+          }
+          else if(node.id == 'menu04'){
+            ChangePanel(3);
+          }
+      }
+    },
+    rootVisible: false,
+    root:{
+      text:      '',
+      draggable: false,
+      id:        'root',
+      expanded:  true,
+      children:  [
+        {
+          id:       'child1',
+          text:     'Infrastructure',
+          expanded:  true,
+          children:  [
+            {
+              id:       'menu01',
+              text:     'Instance',
+              leaf:     true
+            },
+            {
+              id:       'menu02',
+              text:     'Image',
+              leaf:     true
+            }
+          ]
+        },
+        {
+          id:       'child2',
+          text:     'PlatHome',
+          expanded:  true,
+          children:  [
+            {
+              id:       'menu03',
+              text:     'Cluster',
+              leaf:     true
+            },
+            {
+              id:       'menu04',
+              text:     'Service',
+              leaf:     true
+            }
+          ]
+        }
+      ]
+    }
+  });
+}
+Ext.extend(DownPanel, Ext.tree.TreePanel);
+
+CenterPanel = function(){
+  var adminQuery = new AdminQueryPanel();
+  cardPanel    = new CardPanel();
+
+  CenterPanel.superclass.constructor.call(this, {
+    region: 'center',
+    split: true,
+    header: false,
+    border: false,
+    layout: 'border',
+	items: [adminQuery,cardPanel]
+  });
+}
+Ext.extend(CenterPanel, Ext.Panel);
+
+AdminQueryPanel = function(){
+  AdminQueryPanel.superclass.constructor.call(this, {
+    height:80,
+    region: 'north',
+    frame : true,
+    bodyStyle:'padding:0px 0px 0',
+    items: [{
+      layout:'column',
+      items:[{
+        columnWidth:.8,
+        layout: 'form',
+        items: [{
+            fieldLabel: 'Keyword',
+            xtype: 'textfield',
+            name: 'account-id',
+            anchor: '100%'
+        }]
+      },{
+        labelWidth: 5, 
+        columnWidth:.2,
+        layout: 'form',
+        items: [{
+          xtype: 'combo',
+          editable: false,
+          anchor: '100%',
+          forceSelection:true,
+          mode: 'local',
+          store: new Ext.data.ArrayStore({
+            id: 1,
+            fields: [
+              'myId',
+              'displayText'
+            ],
+            data: [[1,'include'],[2, 'exact']]
+          }),
+          triggerAction: 'all',
+		  value:'1',
+          valueField: 'myId',
+          displayField: 'displayText'
+        }]
+      }]
+    }],
+	buttons: [{
+	  text:'Search',
+	  handler: function(){
+        alert('Search');
+	  },
+	  scope:this
+	}]
+  });
+}
+Ext.extend(AdminQueryPanel, Ext.form.FormPanel);
+
+CardPanel = function(){
+  var instancePanel = new InstancePanel();
+  var imagePanel = new ImagePanel();
+  var clusterPanel = new ClusterPanel();
+  var servicePanel = new ServicePanel();
+
+  CardPanel.superclass.constructor.call(this, {
+    region: 'center',
+    layout:'card',
+	activeItem: 0, 
+	defaults: {
+		border:false
+	},
+	items: [instancePanel,imagePanel,clusterPanel,servicePanel]
+  });
+}
+Ext.extend(CardPanel, Ext.Panel);
+
+ImagePanel = function(){
+  var sm = new Ext.grid.RowSelectionModel({singleSelect:true}); 
+  var store = new Ext.data.SimpleStore({
+
+    fields: [
+      { name: 'wmi-id' },
+      { name: 'name' },
+      { name: 'owner-id' },
+      { name: 'visibility' },
+      { name: 'architecture' },
+      { name: 'image-size' },
+      { name: 'description' }
+    ],
+    data:[
+      [ 'WK-2009', 'Blog',   'O10001', 'public',  'i386',  '0', 'test'],
+      [ 'WK-2010', 'Debian', 'O10001', 'public',  'x86_64','0', ''],
+      [ 'WK-2011', 'SNS',    'O10001', 'private', 'i386',  '0', 'develop']
+    ]
+  });
+  var clmnModel = new Ext.grid.ColumnModel([
+    new Ext.grid.RowNumberer(),
+    { header: "WMI-ID", width: 100, dataIndex: 'wmi-id' },
+    { header: "Name", width: 100, dataIndex: 'name' },
+    { header: "Owner", width: 100, dataIndex: 'owner-id' },
+    { header: "Visibility", width: 100, dataIndex: 'visibility' },
+    { header: "Architecture", width: 100, dataIndex: 'architecture'  },
+    { header: "Image-size", width: 100, dataIndex: 'image-size'  },
+    { header: "Description", width: 100, dataIndex: 'description'  }
+  ]);
+
+  ImagePanel.superclass.constructor.call(this, {
+    title: 'Image',
+    store: store,
+    cm:clmnModel,
+    sm:sm,
+    width: 320,
+    autoHeight: false,
+    stripeRows: true,
+    bbar: new Ext.PagingToolbar({
+      pageSize: 1,
+      store: store,
+      displayInfo: true,
+      displayMsg: 'Displaying data {0} - {1} of {2}',
+      emptyMsg: "No data to display"
+    }),
+    tbar : [
+      { text : 'Launch',handler:function(){}
+      },
+      { text : 'Delete',handler:function(){}
+      }
+    ]
+  });
+}
+Ext.extend(ImagePanel, Ext.grid.GridPanel);
+
+InstancePanel = function(){
+  var sm = new Ext.grid.RowSelectionModel({singleSelect:true});
+  var store = new Ext.data.SimpleStore({
+    fields: [
+      { name: 'instance-id' },
+      { name: 'owner-id' },
+      { name: 'wmi-id' },
+      { name: 'state' },
+      { name: 'public-dns' },
+      { name: 'private-dns' },
+      { name: 'private-ip' },
+      { name: 'type' },
+      { name: 'service' }
+    ],
+    data:[
+      [ 'AA9999995', 'O10001', 'WK-2009', 'running',  'axsh1.com/sssss/', 'xxxx1.com/xxxxx', '192.168.100.10','small','LB'],
+      [ 'AA9999996', 'O10001', 'WK-2009', 'shutdown', '', '', '', 'large',''],
+      [ 'AA9999997', 'O10001', 'WK-2009', 'running', 'axsh2.com/sssss/',  'xxxx2.com/xxxxx', '192.168.100.11','small','Web']
+    ]
+  });
+
+  var clmnModel = new Ext.grid.ColumnModel([
+    new Ext.grid.RowNumberer(),
+    { header: "Instance ID", width: 100, dataIndex: 'instance-id' },
+    { header: "Owner", width: 100, dataIndex: 'owner-id' },
+    { header: "WMI ID", width: 100, dataIndex: 'wmi-id' },
+    { header: "State", width: 80, dataIndex: 'state' },
+    { header: "Public DNS", width: 100, dataIndex: 'public-dns' },
+    { header: "Private DNS", width: 100, dataIndex: 'private-dns' },
+    { header: "Private IP",  width: 100, dataIndex: 'private-ip'  },
+    { header: "type", width: 50, dataIndex: 'type'  },
+    { header: "Service", width: 100, dataIndex: 'service'  }
+  ]);
+
+  InstancePanel.superclass.constructor.call(this, {
+    title: 'Instance',
+    store: store,
+    cm:clmnModel,
+    sm:sm,
+    width: 320,
+    autoHeight: false,
+    stripeRows: true,
+    bbar: new Ext.PagingToolbar({
+      pageSize: 1,
+      store: store,
+      displayInfo: true,
+      displayMsg: 'Displaying data {0} - {1} of {2}',
+      emptyMsg: "No data to display"
+    }),
+    tbar : [
+      { text : 'Reboot',handler:function(){}
+      },
+      { text : 'Terminate',handler:function(){}
+      },
+      { text : 'Save',handler:function(){}
+      }
+    ]
+  });
+}
+Ext.extend(InstancePanel, Ext.grid.GridPanel);
+
+ClusterPanel = function(){
+  var clistPanel = new ClusterListPanel();
+  var cctrlPanel = new ClusterCtrlPanel();
+  ClusterPanel.superclass.constructor.call(this, {
+    title: 'Cluster',
+    width: 320,
+    header: false,
+    border: false,
+    layout: 'border',
+	items: [clistPanel,cctrlPanel]
+  });
+}
+Ext.extend(ClusterPanel, Ext.Panel);
+
+ClusterListPanel = function(){
+  var sm = new Ext.grid.RowSelectionModel({singleSelect:true});
+  var store = new Ext.data.SimpleStore({
+    fields: [
+      { name: 'cluster-id' },
+      { name: 'name' },
+      { name: 'state' },
+      { name: 'public-dns' }
+    ],
+    data:[
+      [ 'AA9999995', 'Blog', 'running',  'axsh1.com/sssss/']
+    ]
+  });
+  var clmnModel = new Ext.grid.ColumnModel([
+    new Ext.grid.RowNumberer(),
+    { header: "Cluster ID", width: 100, dataIndex: 'cluster-id' },
+    { header: "Name", width: 100, dataIndex: 'name' },
+    { header: "State", width: 50, dataIndex: 'state' },
+    { header: "Public DNS", width: 100, dataIndex: 'public-dns' }
+  ]);
+
+  ClusterListPanel.superclass.constructor.call(this, {
+    region: 'center',
+    store: store,
+    cm:clmnModel,
+    sm:sm,
+    width: 320,
+    split: true,
+    autoHeight: false,
+    stripeRows: true,
+    bbar: new Ext.PagingToolbar({
+      pageSize: 1,
+      store: store,
+      displayInfo: true,
+      displayMsg: 'Displaying data {0} - {1} of {2}',
+      emptyMsg: "No data to display"
+    })
+  });
+}
+Ext.extend(ClusterListPanel, Ext.grid.GridPanel);
+
+ClusterCtrlPanel = function(){
+
+  var sm = new Ext.grid.RowSelectionModel({singleSelect:true});
+  var store = new Ext.data.SimpleStore({
+    fields: [
+      { name: 'app-name' },
+      { name: 'state' },
+      { name: 'now' },
+      { name: 'future' },
+      { name: 'setting' }
+    ],
+    data:[
+      [ 'Apache', 'running', '2',  '2', ''],
+      [ 'LB',     'running', '1',  '1', ''],
+      [ 'MySQL',  'running', '1',  '1', '']
+    ]
+  });
+  var clmnModel = new Ext.grid.ColumnModel([
+    new Ext.grid.RowNumberer(),
+    { header: "Application", width: 100, dataIndex: 'app-name' },
+    { header: "state", width: 100, dataIndex: 'state' },
+    { header: "Now", width: 50, dataIndex: 'now' },
+    { header: "Future", width: 50, dataIndex: 'future' },
+    { header: "Setting", width: 50, dataIndex: 'setting' }
+  ]);
+
+  ClusterCtrlPanel.superclass.constructor.call(this, {
+    region: 'east',
+    store: store,
+    cm:clmnModel,
+    sm:sm,
+    width: 380,
+    split: true,
+    autoHeight: true,
+    stripeRows: true,
+    tbar : [
+      { text : 'Reboot',handler:function(){}
+      },
+      { text : 'Terminate',handler:function(){}
+      },
+      { text : 'Backup',handler:function(){}
+      },
+      { text : 'Restore',handler:function(){}
+      }
+    ]
+  });
+}
+Ext.extend(ClusterCtrlPanel, Ext.grid.GridPanel);
+
+ServicePanel = function(){
+  var sm = new Ext.grid.RowSelectionModel({singleSelect:true});
+  var store = new Ext.data.SimpleStore({
+
+    fields: [
+      { name: 'wmi-id' },
+      { name: 'name' },
+      { name: 'owner-id' },
+      { name: 'visibility' },
+      { name: 'architecture' }
+    ],
+    data:[
+      [ 'WK-2009', 'Blog', 'O10001', 'public', 'i386'],
+      [ 'WK-2011', 'SNS', 'O10001', 'public', 'i386']
+    ]
+  });
+
+  var clmnModel = new Ext.grid.ColumnModel([
+    new Ext.grid.RowNumberer(),
+    { header: "WMI-ID", width: 100, dataIndex: 'wmi-id' },
+    { header: "Name", width: 100, dataIndex: 'name' },
+    { header: "Owner", width: 100, dataIndex: 'owner-id' },
+    { header: "Visibility", width: 100, dataIndex: 'visibility' },
+    { header: "Architecture", width: 100, dataIndex: 'architecture'  }
+  ]);
+
+  ServicePanel.superclass.constructor.call(this, {
+    title: 'Service',
+    store: store,
+    cm:clmnModel,
+    sm:sm,
+    width: 320,
+    autoHeight: false,
+    stripeRows: true,
+    bbar: new Ext.PagingToolbar({
+      pageSize: 1,
+      store: store,
+      displayInfo: true,
+      displayMsg: 'Displaying data {0} - {1} of {2}',
+      emptyMsg: "No data to display"
+    }),
+    tbar : [
+      { text : 'Launch',handler:function(){}
+      },
+      { text : 'Delete',handler:function(){}
+      }
+    ]
+  });
+}
+Ext.extend(ServicePanel, Ext.grid.GridPanel);
+
+AddScaleOutWindow = function(){
+    var form = new Ext.form.FormPanel({
+      labelWidth: 70, 
+      width: 150,
+      baseCls: 'x-plain',
+      items: [
+        {
+        fieldLabel: 'Cluster ID',
+        xtype: 'displayfield',
+        name: 'form_textfield',
+        value: 'AAAAAAA',
+        anchor: '100%'
+        }
+        ,{
+        fieldLabel: 'Name',
+        xtype: 'displayfield',
+        name: 'form_textfield',
+        value: 'AAAAAAA',
+        anchor: '100%'
+        }
+        ,{
+        fieldLabel: 'LB',
+        xtype: 'numberfield',
+        name: 'form_textfield',
+        width: 80
+        }
+        ,{
+        fieldLabel: 'Web',
+        xtype: 'numberfield',
+        name: 'form_textfield',
+        width: 80
+        }
+        ,{
+        fieldLabel: 'APP',
+        xtype: 'numberfield',
+        name: 'form_textfield',
+        width: 80
+        }
+        ,{
+        fieldLabel: 'DB',
+        xtype: 'numberfield',
+        name: 'form_textfield',
+        width: 80
+        }
+      ]
+    });
+
+    AddScaleOutWindow.superclass.constructor.call(this, {
+        iconCls: 'icon-panel',
+        collapsible:true,
+        titleCollapse:true,
+        width: 250,
+        height: 250,
+		layout:'fit',
+		closeAction:'hide',
+        title: 'Scale Out/In',
+		modal: true,
+		plain: true,
+        defaults:{bodyStyle:'padding:15px'},
+
+		items: [form],
+		buttons: [{
+			text:'Save',
+		},{
+			text: 'Close',
+			handler: function(){
+				this.hide();
+			},
+			scope:this
+		}]
+
+    });
+}
+Ext.extend(AddScaleOutWindow, Ext.Window);
