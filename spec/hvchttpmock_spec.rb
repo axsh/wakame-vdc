a636bd2aacac5dcd3301468f0bb6f6489ddf1fd1
Tokusei Noborio <t.noborio@gmail.com>
2010-01-13 11:43:52 +0900

    Add hvc http mock spec.

--- spec/hvchttpmock_spec.rb
+++ spec/hvchttpmock_spec.rb
@@ -3,32 +3,52 @@
 require 'dcmgr'
 
 describe Dcmgr::HvcHttpMock do
-  before(:all) do
-    @hvchttp = Dcmgr::HvcHttpMock.new
-    @hvchttp.add_response('hoge.com', 8080, '/', 200, "ok")
-    @hvchttp.add_response('hoge.com', 1080, '/', 500, "ng")
+  before(:each) do
+    @hvchttp = Dcmgr::HvcHttpMock.new('192.168.1.10')
+    @hvchttp.add_hva('192.168.1.20')
+    @hvchttp.add_instance(ip=>'192.168.1.21', status=>'runnning', hva=>'192.168.1.20')
+    @hvchttp.add_instance(ip=>'192.168.1.22', status=>'offline', hva=>'192.168.1.20')
+    @hvchttp.add_hva('192.168.1.30')
+    @hvchttp.add_instance(ip=>'192.168.1.31', status=>'online', hva=>'192.168.1.30')
   end
 
-  it "should open" do
-    @hvchttp.open('hoge.com', 8080) {|http|
-      res = http.get('/')
-      res.respond_to?(:success?).should be_true
+  it "should run instance" do
+    @hvchttp.open('192.168.1.10', 80) {|http|
+      res = http.get('/run_instance?hva_ip=%s&cpus=%s&cpu_mhz=>%s&memory=%s' %
+                     ['192.168.1.20', '1', '1.0', '2.0'])
       res.success?.should be_true
       res.body.should == "ok"
     }
-    
-    @hvchttp.open('hoge.com', 1080) {|http|
-      res = http.get('/')
-      res.respond_to?(:success?).should be_true
-      res.success?.should be_false
-      res.body.should == "ng"
+
+    @hvchttp.hva['192.168.1.20'].instances.length.should == 3
+  end
+
+  it "should run instance" do
+    @hvchttp.open('192.168.1.10', 80) {|http|
+      res = http.get('/terminate_instance?instance_ip=%s' %
+                     ['192.168.1.22'])
+      res.success?.should be_true
+      res.body.should == "ok"
     }
-    
-    @hvchttp.open('fuga.com', 1080) {|http|
+    @hvchttp.hva['192.168.1.20'].instances['192.168.1.22'].status.should == 'offline'
+  end
+  
+  it "should get describe instances" do
+    @hvchttp.open('192.168.1.10', 80) {|http|
+      res = http.get('/terminate_instance?instance_ip=%s' %
+                     ['192.168.1.22'])
+      res.success?.should be_true
+      ret = body # json decode res.body
+      ret.key?('192.168.1.20').should be_true
+      ret['192.168.1.20'].key('status').should be_true
+    }
+  end
+
+  it "should error access" do
+    @hvchttp.open('192.168.1.10', 1080) {|http|
       res = http.get('/')
-      res.respond_to?(:success?).should be_true
       res.success?.should be_false
-      res.body.should == ""
+      res.status.should == 400
     }
   end
 end
