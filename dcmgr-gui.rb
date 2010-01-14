f34257ffc5ec76b8a9e471bf3d5230ee192160eb
Takahisa Kamiya <gatto3@gatto.jp>
2010-01-14 19:28:48 +0900

    update

--- dcmgr-gui.rb
+++ dcmgr-gui.rb
@@ -8,19 +8,6 @@ require 'logger'
 get '/' do
  erb :index
 end
-
-get '/test' do
-  rtn = {'rows' => [
-   { 'id'=>'AAAAAAA', 'price'=>2000},
-   { 'id'=>'BBBBBBB', 'price'=>3000},
-   { 'id'=>'CCCCCCC', 'price'=>500},
-   { 'id'=>'DDDDDDD', 'price'=>1200},
-   { 'id'=>'EEEEEEE', 'price'=>300},
-   { 'id'=>'FFFFFFF', 'price'=>220}
-  ]}
-  content_type :json
-  rtn.to_json
-end
 ##################
 
 post '/account-create' do
@@ -35,7 +22,6 @@ post '/account-create' do
   rtn.to_json
 end
 
-#----------------------------
 get '/account-list' do
   Account.login('staff', 'passwd')
   accountList = Account.find(:all)
@@ -48,6 +34,43 @@ get '/account-list' do
     rows.store('rg',index.created_at)
     rows.store('cn',index.contract_at)
     rows.store('en',index.enable)
+    rtn['rows'].push(rows)
+  }
+  content_type :json
+  rtn.to_json
+end
+
+get '/instance-list' do
+  Instance.login('staff', 'passwd')
+  instanceList = Instance.find(:all)
+  rtn = {
+    'totalCount'=>0,
+    'rows'=>[]
+  }
+  rtn['totalCount'] = instanceList.length
+  instanceList.each{|index|
+    rows = Hash::new
+    rows.store('id',index.id)
+#  rows.store('nm',index.name)
+#  rows.store('rg',index.created_at)
+#  rows.store('cn',index.contract_at)
+#  rows.store('en',index.enable)
+   rtn['rows'].push(rows)
+  }
+  content_type :json
+  rtn.to_json
+end
+
+get '/image-list' do
+  imagelist = ImageStorage.find(:all)
+  rtn = {
+    'totalCount'=>0,
+    'rows'=>[]
+  }
+  rtn['totalCount'] = imagelist.length
+  imagelist.each{|index|
+    rows = Hash::new
+    rows.store('id',index.id)
     rtn['rows'].push(rows)
   }
   content_type :json
