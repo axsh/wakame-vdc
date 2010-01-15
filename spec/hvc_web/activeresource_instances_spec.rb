2c179668ca520d13352e80a2fa02db0d1b4bd9a3
Tokusei Noborio <t.noborio@gmail.com>
2010-01-15 19:20:53 +0900

    Add active resouce instance specs for hvc web.

---
+++ spec/hvc_web/activeresource_instances_spec.rb
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+require 'rubygems'
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe "instance access by active resouce" do
+  it "should change instance status"
+  it "should change instance ip address"
+end
