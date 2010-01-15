2ad05bd40fcaf7adacf8dfeadd1af80108044ae5
Tokusei Noborio <t.noborio@gmail.com>
2010-01-15 14:29:06 +0900

    Change specs, implement for hvc http.

--- lib/dcmgr/hvchttpmock.rb
+++ lib/dcmgr/hvchttpmock.rb
@@ -6,19 +6,41 @@ require 'active_support'
 module Dcmgr
   class HvcHttpMock
     def initialize(host, port=80)
-      @host = host
-      @port = port
-      @hvas = {}
+      if host.is_a? HvController
+        hvc = host
+        @host = hvc.ip
+        @port = port
+        @hvas = Hash[*hvc.hv_agents.map{|hva|
+                       hva_data = Hva.new(hva.ip)
+                       hva.instances.each{|ins|
+                         hva_data.add_instance(ins.ip, ins.id, ins.status)
+                       }
+                       [hva.ip, hva_data]
+                     }.flatten]
+      else
+        @host = host
+        @port = port
+        @hvas = {}
+      end
     end
 
     def add_hva(hva_ip)
       @hvas[hva_ip] = Hva.new(hva_ip)
     end
 
+    def inc_instance_ip
+      @@instance_ip ||= 0
+      @@instance_ip += 1
+    end
+
     def add_instance(instance)
       hva_ip = instance[:hva]
       hva = @hvas[hva_ip]
-      hva.add_instance(instance[:ip], instance[:status])
+      instance_uuid, instance_ip = instance[:uuid], instance[:ip]
+      unless instance_ip
+        instance_ip = '192.168.2.' + inc_instance_ip
+      end
+      hva.add_instance(instance_ip, instance_uuid, instance[:status])
     end
 
     def hva(ip)
@@ -27,7 +49,7 @@ module Dcmgr
 
     attr_accessor :hvas
           
-    def open(host, port, &block)
+    def open(host, port=80, &block)
       if host == @host && port == @port
         hvas = @hvas
       else
@@ -42,11 +64,16 @@ module Dcmgr
         @instances = {}
       end
 
-      def add_instance(ip, status)
-        @instances[ip] = status
+      def add_instance(ip, uuid, status)
+        @instances[ip] = [uuid, status]
       end
 
-      alias :update_instance :add_instance
+      def update_instance(ip, uuid=nil, status=nil)
+        cur_uuid, cur_status = @instances[ip]
+        uuid ||= cur_uuid
+        status ||= cur_status
+        add_instance(ip, uuid, status)
+      end
 
       attr_accessor :instances
     end
@@ -66,14 +93,15 @@ module Dcmgr
       when '/run_instance'
         query = CGI.parse(uri.query)
         hva_ip = query['hva_ip'][0]
-        @hvas[hva_ip].add_instance(hva_ip, :online)
+        @hvas[hva_ip].update_instance(hva_ip, nil, :online)
+        p @hvas
         HvcHttpMockResponse.new(200, "ok")
       when '/terminate_instance'
         query = CGI.parse(uri.query)
         instance_ip = query['instance_ip'][0]
         @hvas.each_value{|hva|
           next unless hva.instances.key? instance_ip
-          hva.update_instance(instance_ip, :offline)
+          hva.update_instance(instance_ip, nil, :offline)
           return HvcHttpMockResponse.new(200, "ok")
         }
         HvcHttpMockResponse.new(404, "not found")
@@ -82,8 +110,9 @@ module Dcmgr
         ret = {}
         @hvas.each{|hva_ip, hva|
           ret_instances = {}
-          hva.instances.each{|ip, status|
-            ret_instances[ip] = {:status=>status.to_s}
+          hva.instances.each{|ip, ret_instance|
+            ret_instances[ip] = {:uuid=>ret_instances[0],
+              :status=>ret_instance[1]}
           }
           ret[hva_ip] = {
             'status'=>:online,
--- spec/hvchttpmock_spec.rb
+++ spec/hvchttpmock_spec.rb
@@ -1,41 +1,50 @@
 # -*- coding: utf-8 -*-
 
-require 'dcmgr'
-
+require 'rubygems'
+DISABLE_TEST_SERVER = true
+require File.dirname(__FILE__) + '/spec_helper'
 
 describe Dcmgr::HvcHttpMock do
   before(:each) do
-    @hvchttp = Dcmgr::HvcHttpMock.new('192.168.1.10')
-    @hvchttp.add_hva('192.168.1.20')
-    @hvchttp.add_instance(:ip=>'192.168.1.21', :status=>:running, :hva=>'192.168.1.20')
-    @hvchttp.add_instance(:ip=>'192.168.1.22', :status=>:offline, :hva=>'192.168.1.20')
-    @hvchttp.add_hva('192.168.1.30')
-    @hvchttp.add_instance(:ip=>'192.168.1.31', :status=>:online, :hva=>'192.168.1.30')
+    
+  end
+
+  def get_hvchttp
+    hvchttp = Dcmgr::HvcHttpMock.new('192.168.1.10')
+    hvchttp.add_hva('192.168.1.20')
+    hvchttp.add_instance(:ip=>'192.168.1.21', :status=>:running, :hva=>'192.168.1.20')
+    hvchttp.add_instance(:ip=>'192.168.1.22', :status=>:offline, :hva=>'192.168.1.20')
+    hvchttp.add_hva('192.168.1.30')
+    hvchttp.add_instance(:ip=>'192.168.1.31', :status=>:online, :hva=>'192.168.1.30')
+    hvchttp
   end
 
   it "should run instance" do
-    @hvchttp.open('192.168.1.10', 80) {|http|
-      res = http.get('/run_instance?hva_ip=%s&cpus=%s&cpu_mhz=%s&memory=%s' %
-                     ['192.168.1.20', '1', '1.0', '2.0'])
+    hvchttp = get_hvchttp
+    hvchttp.open('192.168.1.10', 80) {|http|
+      res = http.get('/run_instance?hva_ip=%s&instance_uuid=%s&cpus=%s&cpu_mhz=%s&memory=%s' %
+                     ['192.168.1.20', 'I-ABCD', '1', '1.0', '2.0'])
       res.success?.should be_true
       res.body.should == "ok"
     }
 
-    @hvchttp.hva('192.168.1.20').instances.length.should == 3
+    hvchttp.hva('192.168.1.20').instances.length.should == 3
   end
 
   it "should run instance" do
-    @hvchttp.open('192.168.1.10', 80) {|http|
+    hvchttp = get_hvchttp
+    hvchttp.open('192.168.1.10', 80) {|http|
       res = http.get('/terminate_instance?instance_ip=%s' %
                      ['192.168.1.21'])
       res.success?.should be_true
       res.body.should == "ok"
     }
-    @hvchttp.hva('192.168.1.20').instances['192.168.1.21'].should == :offline
+    hvchttp.hva('192.168.1.20').instances['192.168.1.21'][1].should == :offline
   end
   
   it "should get describe instances" do
-    @hvchttp.open('192.168.1.10', 80) {|http|
+    hvchttp = get_hvchttp
+    hvchttp.open('192.168.1.10', 80) {|http|
       res = http.get('/describe_instances')
       res.success?.should be_true
       ret = JSON.parse(res.body) # json decode res.body
@@ -49,17 +58,38 @@ describe Dcmgr::HvcHttpMock do
       ret['192.168.1.20']['instances']['192.168.1.21'].key?('status').should be_true
       ret['192.168.1.20']['instances']['192.168.1.21']['status'].should == 'running'
       
+      ret['192.168.1.20']['instances']['192.168.1.21'].key?('uuid').should be_true
+      
       ret['192.168.1.20']['instances'].key?('192.168.1.22').should be_true
       ret['192.168.1.20']['instances']['192.168.1.22'].key?('status').should be_true
       ret['192.168.1.20']['instances']['192.168.1.22']['status'].should == 'offline'
+
+      ret['192.168.1.20']['instances']['192.168.1.22'].key?('uuid').should be_true
     }
   end
 
   it "should error access" do
-    @hvchttp.open('192.168.1.10', 1080) {|http|
+    hvchttp = get_hvchttp
+    hvchttp.open('192.168.1.10', 1080) {|http|
       res = http.get('/')
       res.success?.should be_false
       res.status.should == 404
     }
   end
+
+  it "should load hvc servers" do
+    Dcmgr::Schema.drop!
+    Dcmgr::Schema.create!
+    Dcmgr::Schema.load_data File.dirname(__FILE__) + '/../fixtures/sample_data'
+    
+    hvchttp = Dcmgr::HvcHttpMock.new(HvController[:ip=>'192.168.1.10'])
+
+    hvchttp.hvas.length.should == 3
+    hvchttp.hvas.key?('192.168.1.20').should be_true
+    hvchttp.hvas.key?('192.168.1.30').should be_true
+    hvchttp.hvas.key?('192.168.1.40').should be_true
+
+    hvchttp.hvas['192.168.1.20'].instances.length.should == 1
+  end
+
 end
