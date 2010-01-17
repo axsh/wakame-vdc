554ed846f4e9aeb439ef6ae6ac5e7e6561eee6e2
Tokusei Noborio <t.noborio@gmail.com>
2010-01-17 09:44:20 +0900

    Add specs, creating detail account for active resouce accounts.

--- spec/web/activeresource_accounts_spec.rb
+++ spec/web/activeresource_accounts_spec.rb
@@ -21,11 +21,30 @@ describe "accounts by active resource" do
     real_account.should be_valid
     real_account.uuid.length.should > 0
     real_account.enable.should == 'y'
+    real_account.memo.should be_nil
+    real_account.contract_at.should be_nil
 
     real_account.account_role.index{|i| i.user.uuid== @user.id }.should_not be_nil
   end
 
-  it "should add with memo, contract_at, enable fields"
+  it "should add with memo, contract_at, enable fields" do
+    dt = Time.now
+    account = @class.create(:name=>'test account',
+                            :enable=>'n',
+                            :memo=>'memo \n abc',
+                            :contract_at=>dt)
+    $account = account
+    account.id.length.should > 0
+    
+    real_account = Account[account.id]
+    real_account.should be_valid
+    real_account.uuid.length.should > 0
+    real_account.enable.should == 'n'
+    real_account.memo.should == 'memo \n abc'
+    real_account.contract_at.should be_close(dt, 1)
+
+    real_account.account_role.index{|i| i.user.uuid== @user.id }.should_not be_nil
+  end
 
   it "should get list" do
     list = @class.find(:all)
