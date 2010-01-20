1e0def038e1d2ec867b4eaa05fffac72856997ec
Tokusei Noborio <t.noborio@gmail.com>
2010-01-20 16:26:26 +0900

    Add accont/user field for image strorage model.

--- lib/dcmgr/models.rb
+++ lib/dcmgr/models.rb
@@ -192,6 +192,9 @@ class ImageStorage < Sequel::Model
   def self.prefix_uuid; 'IS'; end
 
   many_to_one :image_storage_host
+
+  many_to_one :account
+  many_to_one :user
   
   many_to_many :tags, :join_table=>:tag_mappings, :left_key=>:target_id, :conditions=>{:target_type=>TagMapping::TYPE_IMAGE_STORAGE}
 end
