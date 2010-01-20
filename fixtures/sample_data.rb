dd1db760fac1e8c7b503fac09ef13568cd11c954
Tokusei Noborio <t.noborio@gmail.com>
2010-01-20 15:56:16 +0900

    Fix sample data.

--- fixtures/sample_data.rb
+++ fixtures/sample_data.rb
@@ -7,15 +7,15 @@ image_storage_host = ImageStorageHost.create
 image_storage = ImageStorage.create(:image_storage_host=>image_storage_host)
 
 physical_host_a = PhysicalHost.create(:cpus=>4, :cpu_mhz=>1.0,
-                                      :memory=>2.0,
+                                      :memory=>2000,
                                       :hypervisor_type=>'xen')
 PhysicalHost[physical_host_a.id].remove_tag(Tag.system_tag(:STANDBY_INSTANCE))
 physical_host_b = PhysicalHost.create(:cpus=>2, :cpu_mhz=>1.6,
-                                      :memory=>1.0,
+                                      :memory=>1000,
                                       :hypervisor_type=>'xen')
 PhysicalHost[physical_host_b.id].remove_tag(Tag.system_tag(:STANDBY_INSTANCE))
 physical_host_c = PhysicalHost.create(:cpus=>1, :cpu_mhz=>2.0,
-                                      :memory=>4.0,
+                                      :memory=>8000,
                                       :hypervisor_type=>'xen')
 PhysicalHost[physical_host_c.id].remove_tag(Tag.system_tag(:STANDBY_INSTANCE))
 
@@ -26,10 +26,10 @@ hv_agent_a = HvAgent.create(:hv_controller=>hv_controller,
                             :physical_host=>physical_host_a,
                             :ip=>'192.168.1.20')
 hv_agent_b = HvAgent.create(:hv_controller=>hv_controller,
-                            :physical_host=>physical_host_a,
+                            :physical_host=>physical_host_b,
                             :ip=>'192.168.1.30')
 hv_agent_c = HvAgent.create(:hv_controller=>hv_controller,
-                            :physical_host=>physical_host_a,
+                            :physical_host=>physical_host_c,
                             :ip=>'192.168.1.40')
 
 instance_a = Instance.create(:status=>0, # offline
@@ -37,8 +37,8 @@ instance_a = Instance.create(:status=>0, # offline
                              :user=>user,
                              :image_storage=>image_storage,
                              :need_cpus=>1,
-                             :need_cpu_mhz=>1.0,
-                             :need_memory=>0.5,
+                             :need_cpu_mhz=>0.5,
+                             :need_memory=>500,
                              :hv_agent=>hv_agent_a,
                              :ip=>'192.168.2.100')
 
