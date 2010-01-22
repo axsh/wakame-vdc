9c791c223086a5cb30dd075bf7ce2fa6467cc56d
Tokusei Noborio <t.noborio@gmail.com>
2010-01-22 11:28:52 +0900

    Fix active resouce accounts spec.

--- spec/web/activeresource_accounts_spec.rb
+++ spec/web/activeresource_accounts_spec.rb
@@ -6,15 +6,17 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe "accounts by active resource" do
   include ActiveResourceHelperMethods
   before(:all) do
-    reset_db
     @class = describe_activeresource_model :Account
     @user_class = describe_activeresource_model :User
+  end
+  
+  before(:each) do
+    reset_db
     @user = @user_class.find(:myself)
   end
 
   it "should add" do
     account = @class.create(:name=>'test account')
-    $account = account
     account.id.length.should > 0
     
     real_account = Account[account.id]
@@ -46,30 +48,61 @@ describe "accounts by active resource" do
   end
 
   it "should find by id" do
-    account = @class.find(Account[1].uuid)
-    account.id.should == Account[1].uuid
-    
     accounts = @class.find(:all, :params=>{:id=>Account[1].uuid})
     accounts.length.should == 1
     accounts[0].id.should == Account[1].uuid
+
+    accounts = @class.find(:all, :params=>{:id=>'A-1234'})
+    accounts.length.should == 0
+    
+    accounts = @class.find(:all, :params=>{:id=>nil})
+    accounts.length.should == 0
+    
+    accounts = @class.find(:all, :params=>{:id=>''})
+    accounts.length.should == 0
   end
 
   it "should find by account name" do
     accounts = @class.find(:all, :params=>{:name=>Account[1].name})
     accounts.length.should == 1
     accounts[0].id.should == Account[1].uuid
+    
+    accounts = @class.find(:all, :params=>{:name=>'*account*'})
+    accounts.length.should == 2
+    accounts[0].id.should == Account[1].uuid
+    accounts[1].id.should == Account[2].uuid
+    
+    accounts = @class.find(:all, :params=>{:name=>'*account*'})
+    accounts.length.should == 2
+    accounts[0].id.should == Account[1].uuid
+    accounts[1].id.should == Account[2].uuid
+    
+    accounts = @class.find(:all, :params=>{:name=>'*account_hoge*'})
+    accounts.length.should == 0
+    
+    accounts = @class.find(:all, :params=>{:name=>'__test_account_*'})
+    accounts.length.should == 1
+    accounts[0].id.should == Account[1].uuid
+    
+    accounts = @class.find(:all, :params=>{:name=>'*_account2__'})
+    accounts.length.should == 1
+    accounts[0].id.should == Account[2].uuid
+    
+    accounts = @class.find(:all, :params=>{:name=>'_account2_'})
+    accounts.length.should == 0
   end
   
   it "should find by enable, and name" do
     accounts = @class.find(:all, :params=>{:enable=>true, :name=>Account[1].name})
     accounts.length.should == 1
     accounts[0].id.should == Account[1].uuid
+    
   end
 
   it "should find by contract date" do
     accounts = @class.find(:all, :params=>{:contract_at=>[Time.now - 3600, Time.now]})
-    accounts.length.should == 2
-    accounts[0].id.should == Account[1].uuid
+    accounts.length.should >= 1
+    accounts.find{|a| a.id.should == Account[1].uuid}.should be_true
   end    
 
   it "should get list" do
@@ -77,15 +110,18 @@ describe "accounts by active resource" do
     list.find{|account| account.id == Account[1].uuid }.should be_true
   end
 
-  it "should get by id"
+  it "should get by id" do
+    account = @class.find(Account[1].uuid)
+    account.id.should == Account[1].uuid
+  end
 
-  it "should update by id"
+  it "should chagnge"
 
   it "should be able to be used, only enable account"
   
   it "should delete" do
-    id = $account.id
-    $account.destroy
+    account = @class.create(:name=>'test account')
+    account.destroy
     Account[id].should be_nil
   end
 
