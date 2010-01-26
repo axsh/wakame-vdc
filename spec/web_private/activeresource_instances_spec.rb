104745083fef17ca1db23943e1e37325e61ec2f2
Tokusei Noborio <t.noborio@gmail.com>
2010-01-26 09:56:57 +0900

    Fix ar instances spec for private server.

--- spec/web_private/activeresource_instances_spec.rb
+++ spec/web_private/activeresource_instances_spec.rb
@@ -3,7 +3,48 @@
 require 'rubygems'
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe "instance access by active resouce" do
-  it "should change instance status"
-  it "should change instance ip address"
+# describe class:
+#   class Instance < ActiveResource::Base
+#     self.site = 'http://localhost:port/'
+#     self.format = :json
+#   end
+#
+# status:
+#   STATUS_TYPE_OFFLINE = 0
+#   STATUS_TYPE_RUNNING = 1
+#   STATUS_TYPE_ONLINE = 2
+#   STATUS_TYPE_TERMINATING = 3
+describe "instance access by active resouce(private mode)" do
+  include ActiveResourceHelperMethods
+  
+  before(:all) do
+    runserver(:private)
+    @c = ar_private_class :Instance
+  end
+  
+  it "should change instance status" do
+    real_instance = Instance[1]
+    real_instance.status = Instance::STATUS_TYPE_RUNNING
+    real_instance.save
+    
+    instance = @c.find(Instance[1].uuid)
+    instance.status = Instance::STATUS_TYPE_ONLINE
+    instance.save
+
+    Instance[1].status.should == Instance::STATUS_TYPE_ONLINE
+    @c.find(Instance[1].uuid)
+  end
+  
+  it "should change instance ip address" do
+    real_instance = Instance[1]
+    real_instance.ip = '192.168.10.1'
+    real_instance.save
+    
+    instance = @c.find(Instance[1].uuid)
+    instance.ip = '192.168.11.11'
+    instance.save
+
+    real_instance.reload
+    real_instance.ip.should == '192.168.11.11'
+  end
 end
