e29e694f918216093be2194fc8cacdc61e34eef7
Takahisa Kamiya <gatto3@gatto.jp>
2010-01-28 16:58:18 +0900

    modified

--- define.rb
+++ define.rb
@@ -5,3 +5,6 @@
 # instance status
 @@inStatus = {'0'=>'offline','1'=>'pending','2'=>'running','3'=>'terminating'}
 
+# instance type
+@@instanceType = {'small'=>[1,1000,256],'large'=>[1,1000,512] }
+
