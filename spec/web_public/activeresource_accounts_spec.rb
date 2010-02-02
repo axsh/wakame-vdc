50beb32b1042185a36f1fd67dd1c69cb40fa2504
Tokusei Noborio <t.noborio@gmail.com>
2010-02-02 23:00:43 +0900

    Fix spec, ar accounts.

--- spec/web_public/activeresource_accounts_spec.rb
+++ spec/web_public/activeresource_accounts_spec.rb
@@ -135,6 +135,7 @@ describe "accounts by active resource" do
   
   it "should delete" do
     account = @class.create(:name=>'test account')
+    id = account.id
     account.destroy
     Account[id].should be_nil
   end
