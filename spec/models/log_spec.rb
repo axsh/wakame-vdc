2a895544faef578335ca3aba13275deb2eb05bcd
Tokusei Noborio <t.noborio@gmail.com>
2010-02-02 21:24:58 +0900

    Change specs for log.

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -4,7 +4,26 @@ require 'rubygems'
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "log" do
-  it "should log user login"
+  include ActiveResourceHelperMethods
+
+  before(:all) do
+    Log.destroy
+  end
+  
+  it "should log user login" do
+    @c = ar_class :Instance
+    @c.find(Instance[1].uuid)
+
+    log = Log.find(:user_id=>User[1].id, :target_uuid=>User[1].uuid)
+    log.should be_true
+    log.target_uuid.should == User[1].uuid
+    log.action.should == "login"
+    log.user.should == User[1]
+    log.created_at.should be_close(Time.now, 2)
+  end
+
   it "should log run instance"
+
+  
   it "should log shutdown instance"
 end
