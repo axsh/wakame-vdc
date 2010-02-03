d9dadbccad7dee792f85219de78840a5e772298e
Tokusei Noborio <t.noborio@gmail.com>
2010-02-03 15:18:10 +0900

    Add specs, run/shutdown instance roles.

--- spec/evaluator_spec.rb
+++ spec/evaluator_spec.rb
@@ -4,11 +4,55 @@ require 'rubygems'
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Dcmgr::RoleExecutor do
-  it "should evaluate run instance"
-  it "should evaluate shutdown instance"
+  include Dcmgr::RoleExecutor
   
-  it "should evaluate create account"
-  it "should evaluate delete account"
+  it "should evaluate run instance" do
+    instance = Instance[1]
+    role = Dcmgr::RoleExecutor[instance, :run]
+    role.should be_true
+    role.class.is_a? Dcmgr::RoleExecutor::RunInstance
+    role.evaluate(User[1]).should be_true
+
+    instance.should_receive(:status=)
+    instance.should_receive(:save)
+    role.execute(User[1]).should be_true
+  end
+
+  it "should evaluate shutdown instance" do
+    instance = Instance[1]
+    role = Dcmgr::RoleExecutor[instance, :shutdown]
+    role.should be_true
+    role.class.is_a? Dcmgr::RoleExecutor::ShutdownInstance
+    role.evaluate(User[1]).should be_true
+
+    instance.should_receive(:status=)
+    instance.should_receive(:save)
+    role.execute(User[1]).should be_true
+  end
+  
+  it "should evaluate create account" do
+    pending
+    account = Account.new
+    role = Dcmgr::RoleExecutor[account, :create]
+    role.should be_true
+    role.class.is_a? Dcmgr::RoleExecutor::CreateAccount
+    role.evaluate(User[1]).should be_true
+
+    account.should_receive(:save)
+    role.execute(User[1]).should be_true
+  end
+  
+  it "should evaluate delete account" do
+    pending
+    account = Account.create
+    role = Dcmgr::RoleExecutor[account, :destroy]
+    role.should be_true
+    role.class.is_a? Dcmgr::RoleExecutor::DestroyAccount
+    role.evaluate(User[1]).should be_true
+
+    account.should_receive(:destroy)
+    role.execute(User[1]).should be_true
+  end    
   
   it "should evaluate put image storage"
   it "should evaluate get image storage"
