15b21069892eebf5ca8d0bf36b2352e28a24f98c
Tokusei Noborio <t.noborio@gmail.com>
2010-02-17 18:02:08 +0900

    Add specs, change user.

--- spec/web_public/activeresource_run_instance_spec.rb
+++ spec/web_public/activeresource_run_instance_spec.rb
@@ -9,19 +9,29 @@ describe "run instance access by active resource" do
   it "should run/shutdown instance(sample code)" do
     reset_db
 
+    user_name = "user_a"
+    password = "pass"
+
     # account
     account = ar_class(:Account).create
     account.should be_valid
 
     # user
-    user = ar_class(:User).create(:name=>"user#1", :password=>"pass")
+    user = ar_class(:User).create(:name=>user_name, :password=>password)
     user.should be_valid
+    user.name.should == user_name
+
+    # option for change user
+    ar_opts = {:user=>user_name, :password=>password}
+
+    # change user
+    user = ar_class(:User, ar_opts).find(:myself)
+    p user
 
     pending
-    # change user
     
     # key pair
-
+    
     # select image
 
     # auth
