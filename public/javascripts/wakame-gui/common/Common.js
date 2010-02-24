f0976c4dbdec6fddabd8e7ccf7d63b75a180aacd
Takahisa Kamiya <gatto3@gatto.jp>
2010-02-24 14:39:41 +0900

    add copyright.

--- public/javascripts/wakame-gui/common/Common.js
+++ public/javascripts/wakame-gui/common/Common.js
@@ -32,3 +32,28 @@ WakameGUI = {
   version : '1.0'
 };
 
+Ext.apply(WakameGUI,{
+  maxZindex : function() {
+    var ret = 0;
+    var els = Ext.select('*');
+    els.each(function(el){
+    var zIndex = el.getStyle('z-index');
+    if(Ext.isNumber(parseInt(zIndex)) && ret < zIndex) {
+      ret = zIndex;
+    }
+  }, this);
+    return ret;
+},
+  getScrollPos: function() {
+    var y = (document.documentElement.scrollTop > 0)
+       ? document.documentElement.scrollTop
+       : document.body.scrollTop;
+    var x = (document.documentElement.scrollLeft > 0)
+       ? document.documentElement.scrollLeft
+        : document.body.scrollLeft;
+    return {
+      x: x,
+      y: y
+    };
+  }
+});
