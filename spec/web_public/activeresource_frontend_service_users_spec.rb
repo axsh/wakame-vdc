6646ab56a5897b5bdfb32b3532ae9380e884277a
Tokusei Noborio <t.noborio@gmail.com>
2010-02-25 17:19:30 +0900

    Add spec, authorize fs user by ip.

--- spec/web_public/activeresource_frontend_service_users_spec.rb
+++ spec/web_public/activeresource_frontend_service_users_spec.rb
@@ -6,7 +6,20 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe "frontend service users access by active resource" do
   include ActiveResourceHelperMethods
 
-  it "should authorize by ip"
+  it "should authorize by ip" do
+    pending
+    
+    Dcmgr.fsuser_auth_type = :ip
+    Dcmgr.fsuser_auth_users =
+      [{:user=>"gui_server", :ip=>"127.0.0.1"},
+       {:user=>"web_api", :ip=>"192.168.1.101"},]
+
+    gui_server_c = ar_class(:FrontendServiceUser,
+                                :user=>"gui")
+    gui_server = gui_server_c.get(:myself)
+    gui_server.should be_valid
+  end
+  
   it "should authorize by basic auth"
 
   it "should authorize user"
