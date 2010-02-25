d86de67400aa5321bdae8025a721a1ab6d9b9ab8
Tokusei Noborio <t.noborio@gmail.com>
2010-02-25 16:58:47 +0900

    Add ar frontend service users spec file.

---
+++ spec/web_public/activeresource_frontend_service_users_spec.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+require 'rubygems'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "frontend service users access by active resource" do
+  include ActiveResourceHelperMethods
+
+  it "should authorize by ip"
+  it "should authorize by basic auth"
+
+  it "should authorize user"
+end
