c53a5febaa8a167987d720a5c900bb7cbd4ccf17
Tokusei Noborio <t.noborio@gmail.com>
2010-02-25 16:32:36 +0900

    Add metadeta access spec file.

---
+++ spec/web_metadata/metadata_access_spec.rb
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+require 'rubygems'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "metadeta access" do
+  include ActiveResourceHelperMethods
+  
+  before(:all) do
+    runserver(:private)
+    @c = ar_class :Instance, :private=>true
+  end
+  
+  it "should get by sh"
+  it "should get by yaml"
+  it "should get by json"
+
+  it "should get latest"
+end
