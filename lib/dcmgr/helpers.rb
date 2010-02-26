f26c1a85e4ef1223f7ab808ac38dd870c433e906
Tokusei Noborio <t.noborio@gmail.com>
2010-02-26 17:56:31 +0900

    Refactaring helpers, move json_request.

--- lib/dcmgr/helpers.rb
+++ lib/dcmgr/helpers.rb
@@ -4,15 +4,35 @@ module Dcmgr
     def logger
       Dcmgr.logger
     end
+
+    def json_request(request)
+      ret = Hash.new
+      
+      request.GET.each{|k,v|
+        ret[:"_get_#{k}"] = v
+      }
+      
+      if request.content_length.to_i > 0
+        body = request.body.read
+        parsed = JSON.parse(body)
+        Dcmgr.logger.debug("request: " + parsed.inspect)
+        
+        parsed.each{|k,v|
+          ret[k.to_sym] = v
+        }
+      end
+      Dcmgr.logger.debug("request: " + ret.inspect)
+      ret        
+    end
   end
 
   module AuthorizeHelpers
     def protected!
-      response['WWW-Authenticate'] = %(Basic realm="Testing HTTP Auth") and \
-      throw(:halt, [401, "Not authorized\n"]) and \
-      return unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="HTTP Auth") and
+        throw(:halt, [401, "Not authorized\n"]) and
+        return unless authorized?
     end
-    
+
     def authorized?
       @auth ||=  Rack::Auth::Basic::Request.new(request.env)
       @auth.provided? && @auth.basic? && authorize(@auth.credentials, @auth.credentials)
