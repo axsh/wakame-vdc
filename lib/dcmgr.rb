5b26e3952fc4e5e2dbe22c9e10d5745884c6c651
Tokusei Noborio <t.noborio@gmail.com>
2010-03-03 15:37:11 +0900

    Change autoload, Dcmgr::KeyPairFactory.

--- lib/dcmgr.rb
+++ lib/dcmgr.rb
@@ -7,7 +7,6 @@ require 'dcmgr/hvchttp'
 require 'dcmgr/hvchttp/mock'
 require 'dcmgr/scheduler'
 require 'dcmgr/ipmanager'
-require 'dcmgr/keypair_factory'
 require 'dcmgr/route'
 
 module Dcmgr
@@ -86,4 +85,5 @@ module Dcmgr
 
   autoload :Schema, 'dcmgr/schema'
   autoload :FsuserAuthorizer, 'dcmgr/fsuser_authorizer'
+  autoload :KeyPairFactory, 'dcmgr/keypair_factory'
 end
