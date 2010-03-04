6d617a62a0dd8b8d6ae75cb50e9070d66536b000
Tokusei Noborio <t.noborio@gmail.com>
2010-03-04 16:12:12 +0900

    Delete require in web.rb.

--- lib/dcmgr/web.rb
+++ lib/dcmgr/web.rb
@@ -2,12 +2,12 @@ require 'rubygems'
 require 'sinatra'
 require 'sequel'
 
-require 'dcmgr/models'
-require 'dcmgr/rest_models'
-require 'dcmgr/helpers'
+#require 'dcmgr/models'
+#require 'dcmgr/rest_models'
+#require 'dcmgr/helpers'
 
-module Dcmgr
-  class Web < Sinatra::Base
+module Dcmgr::Web
+  class Base < Sinatra::Base
     set :logger, false
     helpers { include Dcmgr::Helpers }
     
@@ -21,41 +21,6 @@ module Dcmgr
     not_found do
       logger.debug "not found: #{request.request_method} #{request.path}"
       "not found"
-    end
-  end
-  
-  class PublicWeb < Web
-    helpers { include Dcmgr::UUIDAuthorizeHelpers }
-    
-    public_crud FrontendServiceUser
-    
-    public_crud PublicAccount
-    public_crud PublicUser
-    public_crud PublicKeyPair
-
-    public_crud PublicNameTag
-    public_crud PublicAuthTag
-    public_crud PublicTagAttribute
-    
-    public_crud PublicInstance
-    public_crud PublicPhysicalHost
-    
-    public_crud PublicImageStorage
-    public_crud PublicImageStorageHost
-    
-    public_crud PublicLocationGroup
-
-    get '/' do
-      'startup dcmgr'
-    end
-  end
-  
-  class PrivateWeb < Web
-    helpers { include Dcmgr::NoAuthorizeHelpers }
-    public_crud PrivateInstance
-    
-    get '/' do
-      'startup dcmgr. private mode'
     end
   end
 end
