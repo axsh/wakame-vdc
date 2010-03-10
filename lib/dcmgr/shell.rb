5a236ad1e5e78640a77e33ffa1931b7d199486f2
Tokusei Noborio <t.noborio@gmail.com>
2010-03-10 16:53:03 +0900

    Bug fix shell.rb.

--- lib/dcmgr/shell.rb
+++ lib/dcmgr/shell.rb
@@ -8,8 +8,9 @@ if conf == "-client"
   require 'client/client'
 else
   require 'dcmgr'
-  include Dcmgr::Models
   Dcmgr.configure conf
+  #Dcmgr::Models
+  #include Dcmgr::Models
 end
 
 IRB.start(__FILE__)
