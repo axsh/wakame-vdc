b54042a6d21e082d8f12b694bfe1165580eeec79
Tokusei Noborio <t.noborio@gmail.com>
2010-03-14 14:20:02 +0900

    Implement hvc http for response/request YAML format.

--- lib/dcmgr/hvchttp.rb
+++ lib/dcmgr/hvchttp.rb
@@ -6,30 +6,30 @@ module Dcmgr
   module HvcAccess
     def run_instance(hva_ip, instance_uuid, instance_ip_addresses, instance_mac_addresses,
                      cpus, cpu_mhz, memory, *opts)
-      post(:action=>"run_instance",
-           :hva_ip=>hva_ip,
-           :instance_uuid=>instance_uuid,
-           :instance_ip_addresses=>instance_ip_addresses,
-           :instance_mac_addresses=>instance_mac_addresses,
-           :cpus=>cpus,
-           :cpu_mhz=>cpu_mhz,
-           :memory=>memory,
-           :opts=>opts)
+      _post(:action=>"run_instance",
+            :hva_ip=>hva_ip,
+            :instance_uuid=>instance_uuid,
+            :instance_ip_addresses=>instance_ip_addresses,
+            :instance_mac_addresses=>instance_mac_addresses,
+            :cpus=>cpus,
+            :cpu_mhz=>cpu_mhz,
+            :memory=>memory,
+            :opts=>opts)
     end
-
+    
     def terminate_instance(hva_ip, instance_uuid, *opts)
-      post(:action=>"terminate_instance",
-           :hva_ip=>hva_ip,
-           :instance_uuid=>instance_uuid,
-           :opts=>opts)
+      _post(:action=>"terminate_instance",
+            :hva_ip=>hva_ip,
+            :instance_uuid=>instance_uuid,
+            :opts=>opts)
     end
 
     def describe_instances(*opts)
-      post(:action=>"describe_instances",
-           :opts=>opts)
+      _post(:action=>"describe_instances",
+            :opts=>opts)
     end
 
-    def post(params)
+    def _post(params)
       request = params.reject{|k,v| k == :opts}
       if params[:opts].include? :url_only
         request
@@ -38,12 +38,23 @@ module Dcmgr
       end
     end
   end
+
+  module JsonHvcAccess
+    def get_response(request)
+      res = post('/', YAML.dump(request))
+      if res.code == 200
+        YAML.load(res.body)
+      else
+        nil
+      end
+    end
+  end
   
   class HvcHttp
-    include HvcAccess
     def open(host, port=3000, &block)
       Net::HTTP.start(host, port) {|http|
         http.extend HvcAccess
+        http.extend JsonHvcAccess
         block.call(http)
       }
     end
