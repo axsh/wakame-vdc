2ff3da761b9ce3b4f293c25dbcfe660027ddb2ba
Tokusei Noborio <t.noborio@gmail.com>
2010-03-15 09:48:22 +0900

    Add spec, for account log.

---
+++ spec/models/account_log_spec.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+require 'rubygems'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "account log" do
+  include ActiveResourceHelperMethods
+
+  it "should get instance, account, status, type, minute) by month"
+end
