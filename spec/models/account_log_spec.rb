2d06c726b0c3d6efd1aa58b49b113c24df310988
Tokusei Noborio <t.noborio@gmail.com>
2010-03-23 21:18:27 +0900

    Add specs, check generate account logs.

--- spec/models/account_log_spec.rb
+++ spec/models/account_log_spec.rb
@@ -3,5 +3,19 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe "account log" do
   include ActiveResourceHelperMethods
 
+  it "should genelate logs" do
+    year = 2010; month = 1
+    10.times{
+      l = Log.create(:action=>'run',
+                     :target_uuid=>Instance[1].uuid,
+                     :account=>Account[1],
+                     :user=>User[1])
+      l.update(:created_at=>Time.gm(year, month))
+    }
+    AccountLog.generate(year, month)
+    AccountLog.filter('YEAR(target_date) = ? AND MONTH(target_date) = ?',
+                      year, month).all.length.should == 10
+  end
+
   it "should get instance, account, status, type, minute) by month"
 end
