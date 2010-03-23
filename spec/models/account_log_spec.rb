9d8d863c49ea0a4264f2428427a08c9acbb310c6
Tokusei Noborio <t.noborio@gmail.com>
2010-03-23 23:18:06 +0900

    Fix specs for account log.

--- spec/models/account_log_spec.rb
+++ spec/models/account_log_spec.rb
@@ -5,16 +5,25 @@ describe "account log" do
 
   it "should genelate logs" do
     year = 2010; month = 1
-    10.times{
-      l = Log.create(:action=>'run',
-                     :target_uuid=>Instance[1].uuid,
-                     :account=>Account[1],
-                     :user=>User[1])
-      l.update(:created_at=>Time.gm(year, month))
-    }
+
+    Log.create(:action=>'run',
+               :target_uuid=>'I-00000001',
+               :account=>Account[1],
+               :user=>User[1]).update(:created_at=>Time.gm(year, month, 1))
+
+    Log.create(:action=>'terminate',
+               :target_uuid=>'I-00000001',
+               :account=>Account[1],
+               :user=>User[1]).update(:created_at=>Time.gm(year, month, 10))
+
+    Log.create(:action=>'run',
+               :target_uuid=>'I-00000002',
+               :account=>Account[1],
+               :user=>User[1]).update(:created_at=>Time.gm(year, month, 3))
+
     AccountLog.generate(year, month)
     AccountLog.filter('YEAR(target_date) = ? AND MONTH(target_date) = ?',
-                      year, month).all.length.should == 10
+                      year, month).all.length.should == 2
   end
 
   it "should get instance, account, status, type, minute) by month"
