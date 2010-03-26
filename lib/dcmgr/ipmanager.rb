a98bd9a612b1fff65795268b925dc9dd42930562
Masahiro Fujiwara <m-fujiwara@axsh.net>
2010-03-26 17:35:00 +0900

    change hvc access class to be able to send a hashed data.

--- lib/dcmgr/ipmanager.rb
+++ lib/dcmgr/ipmanager.rb
@@ -18,27 +18,26 @@ module Dcmgr
       @check_assigned = @default_check_assigned
     end
     
-    # return format
-    # Array, element is Hash.
-    # Hash has group_name, ip, mac.
-    def assign_ips(instance=nil)
+
+    # pick each IP from respective groups. The eth0 group only be
+    # looked up for the time being.
+    def assign_ips(instance)
       Dcmgr.db.transaction do
-        ips = IpGroup.map{|group|
-          ip = group.ips.find{|i| assigned?(i)} or
-          raise NoAssignIPError
-          
-          {:group_name=>group.name,
-            :group=>group,
-            :ip=>ip}
-        }.tap{|ips|
-          assigin_ips_to(instance, ips) if instance
+        %w(eth0).each { |i|
+          ds = Models::Ip.find_by_group_name(i).filter('instance_id IS NULL')
+          p ds.sql
+          raise "no available IP in #{i} group" if ds.count < 1
+          assigned_ip = ds.limit(1, rand(ds.count)).first
+          assigned_ip.instance = instance
+          assigned_ip.save
+          Dcmgr::logger.debug "assigned ip: [#{instance.uuid}], mac: #{assigned_ip[:mac]}, ip: #{assigned_ip[:ip]}"
         }
-      end
+      end      
     end
     
     private
 
-    def assigin_ips_to(instance, ips)
+    def assign_ips_to(instance, ips)
       ips.each{|ip|
         ip_obj = ip[:ip]
         ip_obj.instance = instance
@@ -49,5 +48,21 @@ module Dcmgr
     def assigned?(ip)
       @check_assigned.call(ip)
     end
+
+    def assign_ips_old1(instance=nil)
+      Dcmgr.db.transaction do
+        ips = Models::IpGroup.map{|group|
+          ip = group.ips.find{|i| assigned?(i)} or
+          raise NoAssignIPError
+          
+          {:group_name=>group.name,
+            :group=>group,
+            :ip=>ip}
+        }.tap{|ips|
+          assign_ips_to(instance, ips) if instance
+        }
+      end
+    end
+    
   end
 end
