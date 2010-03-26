2ffb46aa3b52859910965af45dfd3b3bda94e9e8
root <root@hvc01.(none)>
2010-03-26 13:59:55 +0900

    add FindFirst host scheduler.

--- lib/dcmgr/scheduler.rb
+++ lib/dcmgr/scheduler.rb
@@ -5,5 +5,6 @@ module Dcmgr
 
     autoload :Algorithm1, 'dcmgr/scheduler/algorithm1'
     autoload :Algorithm2, 'dcmgr/scheduler/algorithm2'
+    autoload :FindFirst, 'dcmgr/scheduler/find_first'
   end
 end
