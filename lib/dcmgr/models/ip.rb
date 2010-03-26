716a0562d872dd47d9df7d4bbd94f5eb63441ce4
Masahiro Fujiwara <m-fujiwara@axsh.net>
2010-03-26 17:30:19 +0900

    add Ip.find_by_group_name().

--- lib/dcmgr/models/ip.rb
+++ lib/dcmgr/models/ip.rb
@@ -6,6 +6,11 @@ module Dcmgr
       many_to_one :ip_group
       many_to_one :instance
 
+      # dataset filtered by group_name
+      def self.find_by_group_name(group_name)
+        filter(:ip_group_id=>IpGroup.find(:name=>group_name).id)
+      end
+
       def to_s
         self.ip
       end
