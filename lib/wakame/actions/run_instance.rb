38e655739bbcac92c0b6585c01479505b47e3fe2
Masahiro Fujiwara <m-fujiwara@axsh.net>
2010-03-31 15:22:11 +0900

    change not to make the thread nest level deeper. and also tell the instance status to dcmgr when finish.

--- lib/wakame/actions/run_instance.rb
+++ lib/wakame/actions/run_instance.rb
@@ -11,22 +11,20 @@ module Wakame
         StatusDB.barrier {
           active_agents = Models::AgentPool.group_active
         }
-        agent_id = active_agents.select {|agent|
-          local_ip = agent.split('-')
+        agent_id = active_agents.find {|a|
+          local_ip = a.split('-')
           @options["hva_ip"] == local_ip[0]
         }
-        agent_id.each{|agent_id|
-          trigger_action{|action|
-            action.actor_request(agent_id, '/xen/run_instance', @options){|req|
-              req.wait
-              Wakame.log.debug("VM Launched")
-            }
-          }
-          flush_subactions
-          instance = Models::Instance.find(@options["instance_uuid"])
-          instance.status = 2
-          instance.save
-        }
+        p agent_id
+        
+        req = actor_request(agent_id, '/xen/run_instance', @options)
+        req.request
+        req.wait
+        Wakame.log.debug("VM Launched")
+        flush_subactions
+        instance = Models::Instance.find(@options["instance_uuid"])
+        instance.status = 2
+        instance.save
       end
 
     end
