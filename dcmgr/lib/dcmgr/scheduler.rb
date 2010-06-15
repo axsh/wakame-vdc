50588810c0f988edb5350036b035cc1c5c35d53b
Masahito Yoshida <masahito@axsh.net>
2010-06-15 18:20:23 +0900

    Add schedulers

--- dcmgr/lib/dcmgr/scheduler.rb
+++ dcmgr/lib/dcmgr/scheduler.rb
@@ -6,5 +6,7 @@ module Dcmgr
     autoload :Algorithm1, 'dcmgr/scheduler/algorithm1'
     autoload :Algorithm2, 'dcmgr/scheduler/algorithm2'
     autoload :FindFirst, 'dcmgr/scheduler/find_first'
+    autoload :FindLast, 'dcmgr/scheduler/find_last'
+    autoload :FindRandom, 'dcmgr/scheduler/find_random'
   end
 end
