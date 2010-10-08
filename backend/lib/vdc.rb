fbffcedc94ea584f6794076c42d7ac0b2e2c5537
t-iwano <t-iwano@axsh.net>
2010-10-08 20:30:57 +0900

    backend: Additional storage functions

---
+++ backend/lib/vdc.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+require 'isono'
+
+module Vdc
+  module NodeModules
+    autoload :StaLoader, 'vdc/node_modules/sta_loader'
+    autoload :StaLocator, 'vdc/node_modules/sta_locator'
+  end
+end
+
