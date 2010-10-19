f923e007c874d4fc69c1db9bb0a6a42c1806ab30
Masahiro Fujiwara <m-fujiwara@axsh.net>
2010-10-19 18:37:31 +0900

    dcmgr: merge mysql connection attributes to config/initializers/sequel.rb in Rakefile. Rakefile is changed to load the same initializer.

--- dcmgr/conf/initializers/sequel.rb
+++ dcmgr/conf/initializers/sequel.rb
@@ -2,6 +2,10 @@
 
 require 'sequel'
 db = Sequel.connect(Dcmgr.conf.database_url)
-
+if db.is_a?(Sequel::MySQL::Database)
+  Sequel::MySQL.default_charset = 'utf8'
+  Sequel::MySQL.default_collate = 'utf8_general_ci'
+  Sequel::MySQL.default_engine = 'InnoDB'
+end
 #require 'logger' 
 #db.loggers << Logger.new(STDOUT)
