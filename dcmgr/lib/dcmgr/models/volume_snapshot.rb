383967233d5c6a742be3698487cc6c4d881e2443
t-iwano <t-iwano@axsh.net>
2010-10-20 23:19:27 +0900

    add method to_hash_document

--- dcmgr/lib/dcmgr/models/volume_snapshot.rb
+++ dcmgr/lib/dcmgr/models/volume_snapshot.rb
@@ -6,12 +6,18 @@ module Dcmgr::Models
 
     inheritable_schema do
       Fixnum :storage_pool_id, :null=>false
-      Fixnum :origin_volume_id, :null=>false
+      String :origin_volume_id, :null=>false
       Fixnum :size, :null=>false
       Fixnum :status, :null=>false, :default=>0
     end
     with_timestamps
 
     many_to_one :storage_pool
+
+    def to_hash_document
+      h = self.values.dup
+      h[:id] = h[:uuid] = self.canonical_uuid
+      h
+    end
   end
 end
