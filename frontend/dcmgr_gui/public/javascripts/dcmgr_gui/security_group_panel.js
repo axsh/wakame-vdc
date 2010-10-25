716c3664e6244b865134ea406c7faa7c1380d157
saicologic <s-mikami@axsh.net>
2010-10-25 18:04:33 +0900

    frontend: Fixed list and show

--- frontend/dcmgr_gui/public/javascripts/dcmgr_gui/security_group_panel.js
+++ frontend/dcmgr_gui/public/javascripts/dcmgr_gui/security_group_panel.js
@@ -1,10 +1,14 @@
 DcmgrGUI.prototype.securityGroupPanel = function(){
-  var list_request = { "url":DcmgrGUI.Util.getPagePath('/security_groups/show/',1) };
+  
+  var maxrow = 10;
+  var page = 1;
+  var list_request = { 
+    "url" : DcmgrGUI.Util.getPagePath('/security_groups/list/',1),
+    "data" : DcmgrGUI.Util.getPagenateData(page,maxrow)
+  };
   
   DcmgrGUI.List.prototype.getEmptyData = function(){
     return [{
-      "id":'',
-      "group_id":'',
       "name":'',
       "description":''
     }]
@@ -13,26 +17,15 @@ DcmgrGUI.prototype.securityGroupPanel = function(){
   
   DcmgrGUI.Detail.prototype.getEmptyData = function(){
     return {
-      "group_id":"-",
       "name" : "-",
       "description" : "-",
-      "config" : "",
+      "rule":''
     }
   }
   
-  DcmgrGUI.Detail.prototype.register_event('dcmgrGUI.configUpdate',function(event,id){
-
-    var initialize_config = ""
-    + "# Format\n"
-    + "# Connection Method,Protocol,From Port,To Port,Source(IP or group)\n"
-    + "#\n"
-    + "# Exmaple:\n"
-    + "# ssh,tcp,22,22,0.0.0.0/0\n";
-    
-    var data = $('#detail').find('#config_'+id).html();
-    if(!data){
-      $('#security_group_config').html(initialize_config);
-    }
+  var c_pagenate = new DcmgrGUI.Pagenate({
+    row:maxrow,
+    total:30 //todo:get total from dcmgr
   });
   
   DcmgrGUI.Detail.prototype.register_event('dcmgrGUI.afterUpdate',function(event,params){
@@ -54,23 +47,21 @@ DcmgrGUI.prototype.securityGroupPanel = function(){
   });
              
   var c_list = new DcmgrGUI.List({
-    element_id:'#display_volumes',
-    template_id:'#securityGroupsListTemplate'
+    element_id:'#display_security_groups',
+    template_id:'#securityGroupsListTemplate',
+    maxrow:maxrow,
+    page:page
   });
   
   c_list.setDetailTemplate({
     template_id:'#securityGroupsDetailTemplate',
-    detail_path:'/security_groups/detail/'
+    detail_path:'/security_groups/show/'
   });
   
   c_list.element.bind('dcmgrGUI.contentChange',function(event,params){
+    c_list.page = c_pagenate.current_page;
     c_list.setData(params.data);
     c_list.singleCheckList(c_list.detail_template);
-  });
-
-  var c_pagenate = new DcmgrGUI.Pagenate({
-    row:10,
-    total:30 //todo:get total from dcmgr
   });
   
   var bt_refresh  = new DcmgrGUI.Refresh();
@@ -78,7 +69,9 @@ DcmgrGUI.prototype.securityGroupPanel = function(){
   bt_refresh.element.bind('dcmgrGUI.refresh',function(){
     
     //Update list element
-    list_request.url = DcmgrGUI.Util.getPagePath('/security_groups/show/',c_pagenate.current_page);
+    c_list.page = c_pagenate.current_page;
+    list_request.url = DcmgrGUI.Util.getPagePath('/security_groups/list/',c_list.page);
+    list_request.data = DcmgrGUI.Util.getPagenateData(c_list.page,c_list.maxrow)
     c_list.element.trigger('dcmgrGUI.updateList',{request:list_request})
     
     $.each(c_list.checked_list,function(check_id,obj){
@@ -87,7 +80,7 @@ DcmgrGUI.prototype.securityGroupPanel = function(){
       
       //All reload detail element
       c_list.checked_list[check_id].c_detail.update({
-        url:DcmgrGUI.Util.getPagePath('/security_groups/detail/',check_id)
+        url:DcmgrGUI.Util.getPagePath('/security_groups/show/',check_id)
       },true);
     });
   });
