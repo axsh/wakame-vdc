af169cb35f246b1c09d5d12aed6de7970bcdaf46
t-iwano <t-iwano@axsh.net>
2010-10-27 21:04:23 +0900

    frontend: create test code volume snapshots

--- frontend/common/test/unit/models/dcmgr_resource/test_volume_snapshot.rb
+++ frontend/common/test/unit/models/dcmgr_resource/test_volume_snapshot.rb
@@ -9,34 +9,40 @@ module Frontend
   class TestVolumeSnapshot < Test::Unit::TestCase
     def setup
       @snapshot = Frontend::Models::DcmgrResource::VolumeSnapshot
+      @snapshot.set_debug
     end
 
     def teardown
     end
     
+    def test_list
+      params = {
+        :start => 0,
+        :limit => 10
+      }
+      p @snapshot.list(params)
+    end
+
     def test_show
-      account_id = 'a-00000000'
-      p @snapshot.show(account_id)
+      snapshot_id = "snap-65ghi5gs"
+      p @snapshot.show(snapshot_id)
     end
     
     def test_create
-      p @snapshot.create
+      params = {
+        :volume_id => "vol-nmxe5cda"
+      }
+      p @snapshot.create(params)
     end
     
     def test_destroy
-      account_id = 'a-00000000'
-      snapshot_id = 'snap-00000000'
-      p @snapshot.destroy(account_id,snapshot_id)
+      snapshot_id = 'snap-w1gz080l'
+      p @snapshot.destroy(snapshot_id)
     end
     
     def test_status
       account_id = 'a-00000000'
       p @snapshot.status(account_id)
-    end
-    
-    def test_detail
-      account_id = 'a-00000000'
-      p @snapshot.detail(account_id)
     end
   end
 end
