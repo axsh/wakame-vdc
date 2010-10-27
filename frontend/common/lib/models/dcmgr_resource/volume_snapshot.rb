19784acb3b08d84eaa78d36ccbc74f8aeccbe895
t-iwano <t-iwano@axsh.net>
2010-10-27 21:03:44 +0900

    frontend: create resource volume_snapshots

--- frontend/common/lib/models/dcmgr_resource/volume_snapshot.rb
+++ frontend/common/lib/models/dcmgr_resource/volume_snapshot.rb
@@ -1,14 +1,29 @@
 module Frontend::Models
   module DcmgrResource
     class VolumeSnapshot < Base
-      def self.show(account_id)
-        self.get(account_id)
+
+      def self.list(params = {})
+        data = self.find(:all, :params => params)
+        results = []
+        data.each { |row|
+          results << row.attributes
+        }
+      end
+
+      def self.show(snapshot_id)
+        self.get(snapshot_id)
       end
     
-      # def self.create
-      # end
+      def self.create(params)
+        snapshot = self.new
+        snapshot.volume_id = params[:volume_id]
+        #todo:storage_pool_id is not implemented because of the fixed value
+        snapshot.storage_pool_id = 'sp-1sx9jeks'
+        snapshot.save
+        snapshot
+      end
     
-      def self.destroy(account_id,snapshot_id)
+      def self.destroy(snapshot_id)
         self.delete(snapshot_id).body
       end
       
@@ -16,14 +31,6 @@ module Frontend::Models
         @collection ||= self.collection_name
         self.collection_name = File.join(@collection,account_id)
         result = self.get(:status)
-        self.collection_name = @collection
-        result
-      end
-
-      def self.detail(account_id)
-        @collection ||= self.collection_name
-        self.collection_name = File.join(@collection,account_id)
-        result = self.get(:detail)
         self.collection_name = @collection
         result
       end
