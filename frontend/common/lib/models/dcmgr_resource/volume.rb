bc14a6d6e21469c2aa4bb3a58234273a8ef1aa5e
saicologic <s-mikami@axsh.net>
2010-11-10 13:57:10 +0900

    frontend: change arguments for attach and detatch actions

--- frontend/common/lib/models/dcmgr_resource/volume.rb
+++ frontend/common/lib/models/dcmgr_resource/volume.rb
@@ -19,18 +19,18 @@ module Frontend::Models
         self.delete(volume_id).body
       end
     
-      def self.attach(account_id,instance_id)
+      def self.attach(volume_id, instance_id)
         @collection ||= self.collection_name
         self.collection_name = File.join(@collection,account_id)
-        result = self.put(:attach)
+        result = self.put(:attach, {:volume_id => volume_id,:instance_id => instance_id})
         self.collection_name = @collection
         result.body
       end
     
-      def self.detach(account_id,instance_id)
+      def self.detach(volume_id, instance_id)
         @collection ||= self.collection_name
         self.collection_name = File.join(@collection,account_id)
-        result = self.put(:detach)
+        result = self.put(:detach, {:volume_id => volume_id,:instance_id => instance_id})
         self.collection_name = @collection
         result.body
       end
