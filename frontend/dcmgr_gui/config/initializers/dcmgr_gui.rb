4f9d36ca93cf167026451b6fa560edd7e87f2af4
saicologic <s-mikami@axsh.net>
2010-11-16 20:33:57 +0900

    frontend: fixed config for active_resource

--- frontend/dcmgr_gui/config/initializers/dcmgr_gui.rb
+++ frontend/dcmgr_gui/config/initializers/dcmgr_gui.rb
@@ -1,4 +1,6 @@
-DcmgrResource::Base.site = "http://api.dcmgr.ubuntu.local/"
+ActiveResource::Base.class_eval do 
+  self.site = 'http://http://your.dcmgr.api.server/'
+end
 
 @dcmgr_config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'database.yml'))).result)[Rails.env]
 Schema.connect "#{@dcmgr_config['adapter']}://#{@dcmgr_config['host']}/#{@dcmgr_config['database']}?user=#{@dcmgr_config['user']}&password=#{@dcmgr_config['password']}"
