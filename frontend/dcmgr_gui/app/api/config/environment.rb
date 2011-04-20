02c021e1bf00dd48a8f69656fc41a143a0616e81
saicologic <s-mikami@axsh.net>
2011-04-20 18:00:20 +0900

    Changed file name.

--- frontend/dcmgr_gui/app/api/config/environment.rb
+++ frontend/dcmgr_gui/app/api/config/environment.rb
@@ -4,6 +4,6 @@ require File.expand_path('../../../../config/application', __FILE__)
 # Initialize the rails application
 DcmgrGui::Application.initialize!
 
-$stdout.reopen(File.expand_path("log/auth.log", Rails.root) , "a")
+$stdout.reopen(File.expand_path("log/auth_server.log", Rails.root) , "a")
 $stdout.sync = true
 $stderr.reopen($stdout)
