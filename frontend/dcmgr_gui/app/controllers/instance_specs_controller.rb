443014a2533d4ab84baf55e1535f1466f7466a53
saicologic <s-mikami@axsh.net>
2011-04-22 22:17:57 +0900

    Deleted unnecessary code.

--- frontend/dcmgr_gui/app/controllers/instance_specs_controller.rb
+++ frontend/dcmgr_gui/app/controllers/instance_specs_controller.rb
@@ -2,16 +2,6 @@ class InstanceSpecsController < ApplicationController
   respond_to :json
   
   def index
-    instance_specs = DcmgrResource::InstanceSpec.list()
-    respond_with(instance_specs[0], :to => [:json])
-  end
-end
-
-
-class InstanceSpecsController < ApplicationController
-  respond_to :json
-  
-  def index
   end
   
   def show_instance_specs
