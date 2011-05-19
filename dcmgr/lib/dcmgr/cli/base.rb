dd9dcea48611c4dbb1ffffaa8586939272f8c83c
Masahiro Fujiwara <m-fujiwara@axsh.net>
2011-05-19 23:43:37 +0900

    wrap CLI dispatcher with Sequel transaction block.

--- dcmgr/lib/dcmgr/cli/base.rb
+++ dcmgr/lib/dcmgr/cli/base.rb
@@ -13,9 +13,11 @@ module Dcmgr::Cli
       public
       # add before/after task hook.
       def invoke_task(task, *args)
-        before_task
-        super(task, *args)
-        after_task
+        Dcmgr::Models::BaseNew.db.transaction do
+          before_task
+          super(task, *args)
+          after_task
+        end
       end
     
       protected
