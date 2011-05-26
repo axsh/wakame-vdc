d2606a87ed1cf51e4de381da96489282ab348800
saicologic <s-mikami@axsh.net>
2011-05-26 21:55:29 +0900

    Change id

--- frontend/dcmgr_gui/public/javascripts/dcmgr_gui/account_panel.js
+++ frontend/dcmgr_gui/public/javascripts/dcmgr_gui/account_panel.js
@@ -1,4 +1,4 @@
 DcmgrGUI.prototype.accountPanel = function(){
-  var last_updated_at = DcmgrGUI.date.parseISO8601($('#last_updated_at').html());  
-  $('#last_updated_at').html(DcmgrGUI.date.getI18n(last_updated_at));
+  var last_login_at = DcmgrGUI.date.parseISO8601($('#last_login_at').html());  
+  $('#last_login_at').html(DcmgrGUI.date.getI18n(last_login_at));
 }
