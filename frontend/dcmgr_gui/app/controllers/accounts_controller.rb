852cf2ecf4456919836f73af3529cf0eed57f44f
saicologic <s-mikami@axsh.net>
2011-05-26 19:29:31 +0900

    Added change password

--- frontend/dcmgr_gui/app/controllers/accounts_controller.rb
+++ frontend/dcmgr_gui/app/controllers/accounts_controller.rb
@@ -12,4 +12,28 @@ class AccountsController < ApplicationController
   def index
     
   end
+
+  def password
+    @error_message = ''
+    if request.get?
+      #nop
+    elsif request.post?
+      password =  params[:password]
+      new_password = params[:new_password]
+      confirm_password = params[:confirm_password]
+      unless new_password == confirm_password
+        @error_message = I18n.t('error_message.change_password1')
+        return true 
+      end
+
+      user = User.find(:uuid => @current_user.uuid, :password => User.encrypt_password(password))
+      if user
+        user.password = User.encrypt_password(new_password)
+        user.save
+        redirect_to :action => 'index'
+      else
+        @error_message = I18n.t('error_message.change_password2')
+      end
+    end
+  end
 end
