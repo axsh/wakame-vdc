db6dd111cb778dec91426c5e175c5e734a9d598a
Masahiro Fujiwara <m-fujiwara@axsh.net>
2011-06-03 16:09:16 +0900

    fix nil check.

--- dcmgr/lib/dcmgr/models/instance.rb
+++ dcmgr/lib/dcmgr/models/instance.rb
@@ -229,7 +229,7 @@ module Dcmgr::Models
       if instance_nic
         instance_nic.each { |n|
           direct_lease_ds = n.direct_ip_lease
-          next if direct_lease_ds.first
+          next if direct_lease_ds.first.nil?
           outside_lease_ds = n.nat_outside_lease
           
           h[:network] << {
