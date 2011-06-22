1a1e6af26706ea3d7a0156ce0adf3192c1d611f5
Metallion <akieckens@axsh.net>
2011-06-22 13:36:29 +0900

    Added del, modify and show to group cli

--- dcmgr/lib/dcmgr/cli/group.rb
+++ dcmgr/lib/dcmgr/cli/group.rb
@@ -5,7 +5,7 @@ module Dcmgr::Cli
     namespace :group
     M = Dcmgr::Models
 
-    desc "add [options]", "Add a new security group."
+    desc "add [options]", "Add a new security group"
     method_option :uuid, :type => :string, :aliases => "-u", :desc => "The UUID for the new security group."
     method_option :account_id, :type => :string, :aliases => "-a", :desc => "The UUID of the account this security group belongs to.", :required => true
     method_option :name, :type => :string, :aliases => "-n", :desc => "The name for this security group.", :required => true
@@ -14,6 +14,42 @@ module Dcmgr::Cli
       UnknownUUIDError.raise(options[:account_id]) if M::Account[options[:account_id]].nil?
       
       puts super(M::NetfilterGroup,options)
+    end
+    
+    desc "del UUID", "Delete a security group"
+    def del(uuid)
+      super(M::NetfilterGroup,uuid)
+    end
+    
+    desc "show [UUID] [options]", "Show security group(s)"
+    def show(uuid=nil)
+      if uuid
+        group = M::NetfilterGroup[uuid] || UnknownUUIDError.raise(uuid)
+        #p group.netfilter_rules
+        puts ERB.new(<<__END, nil, '-').result(binding)
+Group UUID:\t<%= group.canonical_uuid %>
+Account id:\t<%= group.account_id %>
+Description:\t<%= group.description %>
+Rules:
+<%- group.netfilter_rules.each { |rule| -%>
+<%= rule.permission %>
+<%- } -%>
+__END
+      else
+        puts ERB.new(<<__END, nil, '-').result(binding)
+<%- M::NetfilterGroup.all { |row| -%>
+<%= row.canonical_uuid %>\t<%= row.account_id %>\t<%= row.name %>
+<%- } -%>
+__END
+      end
+    end
+    
+    desc "modify UUID [options]", "Modify an existing security group"
+    method_option :account_id, :type => :string, :aliases => "-a", :desc => "The UUID of the account this security group belongs to."
+    method_option :name, :type => :string, :aliases => "-n", :desc => "The name for this security group."
+    method_option :description, :type => :string, :aliases => "-d", :desc => "The description for this new security group."
+    def modify(uuid)
+      super(M::NetfilterGroup,uuid,options)
     end
     
     desc "addrule UUID [options]", "Add a rule to a security group"
