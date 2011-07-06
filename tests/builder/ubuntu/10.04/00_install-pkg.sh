edafa4016fc98296dfd00e1dcfabce48b670e689
Masahito Yoshida <masahito@axsh.net>
2011-07-07 03:29:31 +0900

    Add netfilter packages to a base package installer.

--- tests/builder/ubuntu/10.04/00_install-pkg.sh
+++ tests/builder/ubuntu/10.04/00_install-pkg.sh
@@ -12,6 +12,7 @@ builder_path=${builder_path:?"builder_path needs to be set"}
 
 # core packages
 deb_pkgs="
+ ebtables iptables
  openssh-server openssh-client
  ruby ruby-dev libopenssl-ruby1.8
  rdoc1.8 irb1.8
