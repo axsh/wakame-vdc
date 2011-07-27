5bc15a6dd6e389fe5e9bc185e1a052c6fcb67304
Masahito Yoshida <masahito@axsh.net>
2011-07-27 15:02:36 +0900

    Add ethtool to installation packages.

--- tests/builder/ubuntu/10.04/00_install-pkg.sh
+++ tests/builder/ubuntu/10.04/00_install-pkg.sh
@@ -12,7 +12,7 @@ builder_path=${builder_path:?"builder_path needs to be set"}
 
 # core packages
 deb_pkgs="
- ebtables iptables ipset
+ ebtables iptables ipset ethtool
  openssh-server openssh-client
  ruby ruby-dev libopenssl-ruby1.8
  rdoc1.8 irb1.8
