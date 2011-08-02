730835d36b27fa255de13007e375deccf0c4ba1f
Masahiro Fujiwara <m-fujiwara@axsh.net>
2011-08-02 14:03:54 +0900

    add retry helper for spec.

--- tests/spec/spec_helper.rb
+++ tests/spec/spec_helper.rb
@@ -4,6 +4,8 @@ require 'bundler/setup' rescue nil
 require 'httparty'
 require 'json'
 
+require 'rspec'
+
 class APITest
   include HTTParty
   base_uri 'http://localhost:9001/api'
@@ -13,5 +15,26 @@ class APITest
 
   def self.create(path, params)
     self.post(path, :query=>params, :body=>'')
+  end
+end
+
+module RetryHelper
+  def retry_until(count=10, &blk)
+    lcount = 0
+    count.times { |n|
+      if blk.call
+        break
+      else
+        sleep 2
+      end
+      lcount = n
+    }
+    count <= lcount && abort("All retry failed within #{count*2} sec")
+  end
+
+  def retry_while(count=10, &blk)
+    retry_while do
+      !blk.call
+    end
   end
 end
