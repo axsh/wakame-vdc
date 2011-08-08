753e5e34c577c953c8d4d71d3b91d0b8ba92db06
Masahiro Fujiwara <m-fujiwara@axsh.net>
2011-08-09 06:11:19 +0900

    update spec for ssh key pair.

--- tests/spec/ssh_key_pair_spec.rb
+++ tests/spec/ssh_key_pair_spec.rb
@@ -3,6 +3,9 @@ require File.expand_path('../spec_helper', __FILE__)
 
 describe "/api/ssh_key_pair" do
   it "tests CURD operations for key pair" do
-    res = APITest.post('/ssh_key_pairs.json', :query=>{:name=>'yyy'}, :body=>'')
+    res = APITest.create('/ssh_key_pairs.json', {:name=>'yyy'})
+    res.success?.should be_true
+    APITest.get("/ssh_key_pairs/#{res["id"]}").success?.should be_true
+    APITest.delete("/ssh_key_pairs/#{res["id"]}").success?.should be_true
   end
 end
