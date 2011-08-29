85595203eff8918f7bf0125d2844b29a8c33ca24
Masahito Yoshida <masahito@axsh.net>
2011-08-29 15:31:56 +0900

    Update cp command option to be sparse file which is destination.

--- dcmgr/lib/dcmgr/drivers/raw.rb
+++ dcmgr/lib/dcmgr/drivers/raw.rb
@@ -5,30 +5,33 @@ module Dcmgr
     class Raw < BackingStore
       include Dcmgr::Logger
       include Dcmgr::Helpers::CliHelper
-      
+
       def create_volume(ctx, snap_file = nil)
         @volume_id   = ctx.volume_id
         @volume      = ctx.volume
 
+        logger.info("creating new volume: #{@volume_id}")
+
         if @snapshot
-          sh("/bin/cp -p %s %s",[snap_file, vol_path])
+          cp_sparse(snap_file, vol_path)
           if $?.exitstatus != 0
             raise "failed copy snapshot: #{snap_file}"
           end
         else
           unless File.exist?(vol_path)
             logger.info("creating parent filesystem(size:#{@volume[:size]}): #{vol_path}")
+
             #sh("/bin/dd if=/dev/zero of=#{vol_path} bs=#{@volume[:size]} count=1000000")
             sh("/bin/dd if=/dev/zero of=#{vol_path} bs=1 count=0 seek=#{@volume[:size] * 1024 * 1024}")
-            sh("du -hs #{vol_path}")
-            sh("du -hs --apparent-size #{vol_path}")
+            du_hs(vol_path)
+
             logger.info("create parent filesystem(size:#{@volume[:size]}): #{vol_path}")
           else
             raise "volume already exists: #{@volume_id}"
           end
         end
-        
-        logger.info("created new volume: #{@volume_id}") 
+
+        logger.info("created new volume: #{@volume_id}")
       end
 
       def delete_volume(ctx)
@@ -38,22 +41,33 @@ module Dcmgr
 
       def create_snapshot(ctx, snap_file)
         @volume      = ctx.volume
-        
-        vol_base_path = @volume[:storage_pool][:export_path]
-        vol_account_path = "#{vol_base_path}/#{@volume[:account_id]}"
-        vol_path = "#{vol_account_path}/#{@volume[:uuid]}"
-        
-        sh("/bin/cp -p %s %s",[vol_path, snap_file])
+
+        logger.info("creating new snapshot: #{snap_file}")
+        cp_sparse(vol_path, snap_file)
         if $?.exitstatus != 0
           raise "failed snapshot file : #{snap_file}"
-        end 
+        end
+        du_hs(snap_file)
+
+        logger.info("created new snapshot: #{snap_file}")
       end
 
+      private
       def vol_path
         vol_base_path = @volume[:storage_pool][:export_path]
         vol_account_path = "#{vol_base_path}/#{@volume[:account_id]}"
         "#{vol_account_path}/#{@volume[:uuid]}"
       end
+
+      def cp_sparse(src, dst)
+        sh("/bin/cp -p --sparse=always %s %s",[src, dst])
+      end
+
+      def du_hs(path)
+        sh("du -hs %s", [path])
+        sh("du -hs --apparent-size %s", [path])
+      end
+
     end
   end
 end
