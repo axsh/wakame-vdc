2c3702079a1ad6cfefc31e8e596423e90efac4ce
saicologic <s-mikami@axsh.net>
2011-09-01 17:23:44 +0900

    Update production config

--- frontend/dcmgr_gui/config/environments/production.rb
+++ frontend/dcmgr_gui/config/environments/production.rb
@@ -1,49 +1,48 @@
 DcmgrGui::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
-  # The production environment is meant for finished, "live" apps.
-  # Code is not reloaded between requests
-  config.cache_classes = true
+  # In the development environment your application's code is reloaded on
+  # every request.  This slows down response time but is perfect for development
+  # since you don't have to restart the webserver when you make code changes.
+  config.cache_classes = false 
+  #TODO: set to work with true.
+  #config.cache_classes = true 
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
+
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = false 
+  config.action_view.debug_rjs             = false 
   config.action_controller.perform_caching = true
 
-  # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
 
-  # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
 
-  # If you have no front-end server that supports something like X-Sendfile,
-  # just comment this out and Rails will serve the files
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+  
+  # Config authentication for WebAPI
+  # Setting proxy server
+  config.proxy_host = "127.0.0.1"
+  config.proxy_port = 8080
+  config.proxy_scheme = "http"
+  config.proxy_dcmgr_host = "127.0.0.1"
+  config.proxy_dcmgr_port = 9001
+  config.proxy_root_user = 'root'
+  config.proxy_nginx = '/opt/nginx/sbin/nginx'
 
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
+  # Setting authentication server
+  config.auth_host = "127.0.0.1"
+  config.auth_port = 3000
+  config.auth_root_user = 'root'
 
-  # Use a different logger for distributed setups
-  # config.logger = SyslogLogger.new
-
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
-
-  # Disable Rails's static asset server
-  # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
-
-  # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable threaded mode
-  # config.threadsafe!
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
+  config.i18n.load_path += Dir[Rails.root.join('locales', '*.{rb,yml}').to_s]
+  config.i18n.default_locale = :en
+  #config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  
   config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
 end
