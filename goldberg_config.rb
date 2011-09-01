98e96fa09c7662aefe7a934ad87c218e8f0baa6d
Masahito Yoshida <masahito@axsh.net>
2011-09-01 16:37:40 +0900

    Add "--without_screen" to vdc.sh command in goldberg.

--- goldberg_config.rb
+++ goldberg_config.rb
@@ -1,4 +1,4 @@
 Project.configure do |config|
   config.timeout = 60.minutes
-  config.command = "/bin/sh -c 'cd tests && ./vdc.sh install && ./vdc.sh standalone:ci'"
+  config.command = "/bin/sh -c 'cd tests && ./vdc.sh install && ./vdc.sh standalone:ci --without_screen=1'"
 end
