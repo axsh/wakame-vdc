7bb8796d12b1ee51effb49a1eb5183cc668d5d29
Masahiro Fujiwara <m-fujiwara@axsh.net>
2011-09-07 15:39:01 -0500

    add volume API spec.

---
+++ tests/spec/volume_spec.rb
@@ -0,0 +1,17 @@
+
+require File.expand_path('../spec_helper', __FILE__)
+
+describe "/api/volumes" do
+  include RetryHelper
+  
+  it "create 99MB blank volume and delete" do
+    res = APITest.create("/volumes", {:volume_size=>99})
+    res.success?.should be_true
+    volume_id = res["id"]
+    retry_until do
+      APITest.get("/volumes/#{volume_id}")["state"] == "available"
+    end
+    APITest.get("/volumes/#{volume_id}")["size"].to_i.should == 99
+    APITest.delete("/volumes/#{volume_id}").success?.should be_true
+  end
+end
