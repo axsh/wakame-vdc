46bd9b19a7df98fdb5a9e26b9ed9f1c8d3f80d32
Masahiro Fujiwara <m-fujiwara@axsh.net>
2011-09-09 21:59:11 -0500

    add spec case for volume store boot instance.

--- tests/spec/instance_spec.rb
+++ tests/spec/instance_spec.rb
@@ -14,4 +14,14 @@ describe "/api/instances" do
     APITest.delete("/instances/#{instance_id}").success?.should be_true
     
   end
+
+  it "runs volume store instance" do
+    res = APITest.create("/instances", {:image_id=>'wmi-lucid1', :instance_spec_id=>'is-demospec'})
+    res.success?.should be_true
+    instance_id = res["id"]
+    retry_until do
+      APITest.get("/instances/#{instance_id}")["state"] == "running"
+    end
+    APITest.delete("/instances/#{instance_id}").success?.should be_true
+  end
 end
