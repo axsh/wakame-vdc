2ec5e1b944c0bf4c6b74be0ad46b4a787ba9baef
Masahiro Fujiwara <m-fujiwara@axsh.net>
2011-09-12 21:26:34 -0500

    add test for create volume from local snapshot.

--- tests/spec/volume_spec.rb
+++ tests/spec/volume_spec.rb
@@ -14,4 +14,15 @@ describe "/api/volumes" do
     APITest.get("/volumes/#{volume_id}")["size"].to_i.should == 99
     APITest.delete("/volumes/#{volume_id}").success?.should be_true
   end
+
+  it "create volume from snapshot snap-lucid1 and delete" do
+    res = APITest.create("/volumes", {:snapshot_id=>'snap-lucid1'})
+    res.success?.should be_true
+    volume_id = res["id"]
+    retry_until do
+      APITest.get("/volumes/#{volume_id}")["state"] == "available"
+    end
+    APITest.delete("/volumes/#{volume_id}").success?.should be_true
+  end
+  
 end
