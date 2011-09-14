f6322bc66bb674ef6571ffc474374131df7034b6
Jari Sundell <sundell.software@gmail.com>
2011-09-14 16:53:00 +0900

    Ensure 'enable_ovs.sh' deletes all interfaces from 'br0'.

--- tests/openflow/enable_ovs.sh
+++ tests/openflow/enable_ovs.sh
@@ -2,9 +2,25 @@
 #
 # If remotely connected, use a script to ensure the
 # network gets restarted.
-brctl delif br0 eth0
+
+last_if_name=""
+
+while true; do
+    if_name=`brctl show | grep '^br0' | sed -e 's:[\t][\t]*:\t:g' | cut -f 4`
+    
+    if [ -z "$if_name" ] || [ "$last_if_name" == "$if_name" ]; then break; fi
+
+    echo brctl delif br0 \"$if_name\"
+    brctl delif br0 "$if_name"
+    last_if_name="$if_name"
+done
+
+echo ip link set br0 down
 ip link set br0 down
+echo brctl delbr br0
 brctl delbr br0
 
-service ovs-switch start
+export BRCOMPAT=yes
+
+/etc/init.d/ovs-switch restart
 /etc/init.d/networking restart
