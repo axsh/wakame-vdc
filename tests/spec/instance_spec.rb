d67d51a518ce0ad6d56b4eab3a1c229a790b0db2
Masahito Yoshida <masahito@axsh.net>
2011-09-16 13:08:05 +0900

    Add spec cases for local/volume store boot instance including "wakame-init".

--- tests/spec/instance_spec.rb
+++ tests/spec/instance_spec.rb
@@ -24,4 +24,25 @@ describe "/api/instances" do
     end
     APITest.delete("/instances/#{instance_id}").success?.should be_true
   end
+
+  it "runs new instance then terminate" do
+    res = APITest.create("/instances", {:image_id=>'wmi-lucid5', :instance_spec_id=>'is-demospec'})
+    res.success?.should be_true
+    instance_id = res["id"]
+    retry_until do
+      APITest.get("/instances/#{instance_id}")["state"] == "running"
+    end
+    APITest.delete("/instances/#{instance_id}").success?.should be_true
+    
+  end
+
+  it "runs volume store instance" do
+    res = APITest.create("/instances", {:image_id=>'wmi-lucid6', :instance_spec_id=>'is-demospec'})
+    res.success?.should be_true
+    instance_id = res["id"]
+    retry_until do
+      APITest.get("/instances/#{instance_id}")["state"] == "running"
+    end
+    APITest.delete("/instances/#{instance_id}").success?.should be_true
+  end
 end
