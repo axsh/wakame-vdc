faa24c5f300f4275915cbc6e95e5888e25e52d41
Masahito Yoshida <masahito@axsh.net>
2011-09-21 11:38:12 +0900

    Add spec to show destinations to upload volume snapshot.

--- tests/spec/volume_snapshot_spec.rb
+++ tests/spec/volume_snapshot_spec.rb
@@ -4,7 +4,17 @@ require File.expand_path('../spec_helper', __FILE__)
 describe "/api/volume_snapshots" do
   include RetryHelper
 
-  it "create snapshot from minimum blank volume to local and delete" do
+  it "should show destinations to upload volume snapshot." do
+    res = APITest.get("/volume_snapshots/upload_destination")
+    res.success?.should be_true
+
+    dests = res.first["results"]
+    dests[0]["destination_id"].should == "local"
+    dests[1]["destination_id"].should == "s3"
+    dests[2]["destination_id"].should == "iijgio"
+  end
+
+  it "should create snapshot from minimum blank volume to local and delete" do
     res = APITest.create("/volumes", {:volume_size=>10})
     res.success?.should be_true
     volume_id = res["id"]
