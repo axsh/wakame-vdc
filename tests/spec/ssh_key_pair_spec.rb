5a495294ab08f5d49b98c1414d5fcfefb6ea2d2b
Masahito Yoshida <masahito@axsh.net>
2011-09-22 17:39:29 +0900

    Add spec to show key pair list.

--- tests/spec/ssh_key_pair_spec.rb
+++ tests/spec/ssh_key_pair_spec.rb
@@ -2,10 +2,17 @@
 require File.expand_path('../spec_helper', __FILE__)
 
 describe "/api/ssh_key_pair" do
-  it "tests CURD operations for key pair" do
+
+  it "should show key pair list" do
+    res = APITest.get("/ssh_key_pairs")
+    res.success?.should be_true
+  end
+
+  it "should test CURD operations for key pair" do
     res = APITest.create('/ssh_key_pairs.json', {:name=>'yyy'})
     res.success?.should be_true
     APITest.get("/ssh_key_pairs/#{res["id"]}").success?.should be_true
     APITest.delete("/ssh_key_pairs/#{res["id"]}").success?.should be_true
   end
+
 end
