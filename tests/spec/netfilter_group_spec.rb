846de008bef7b529e5b3d722ad03b2309274b1eb
Masahito Yoshida <masahito@axsh.net>
2011-09-29 17:43:36 +0900

    Fix netfilter rules.

--- tests/spec/netfilter_group_spec.rb
+++ tests/spec/netfilter_group_spec.rb
@@ -5,7 +5,7 @@ require File.expand_path('../spec_helper', __FILE__)
 describe "/api/netfilter_group" do
   it "should test CURD operations for netfilter" do
     # create not duplicated group name
-    res1 = APITest.create('/netfilter_groups.json', {:name=>'g1', :description=>'g1', :rule => "tcp:22,22,ip4:0.0.0.0/24"})
+    res1 = APITest.create('/netfilter_groups.json', {:name=>'g1', :description=>'g1', :rule => "tcp:22,22,ip4:0.0.0.0"})
     res1.success?.should be_true
     APITest.get("/netfilter_groups/#{res1["uuid"]}").success?.should be_true
 
@@ -18,14 +18,14 @@ describe "/api/netfilter_group" do
     APITest.get("/netfilter_groups/#{res3["uuid"]}").success?.should be_true
 
     # create duplicated group name
-    APITest.create('/netfilter_groups.json', {:name=>'g1', :description=>'g1', :rule => "tcp:22,22,ip4:0.0.0.0/24"}).success?.should be_false
-    APITest.create('/netfilter_groups.json', {:name=>'g2', :description=>'g2', :rule => "tcp:22,22,ip4:0.0.0.0/24"}).success?.should be_false
-    APITest.create('/netfilter_groups.json', {:name=>'g3', :description=>'g3', :rule => "tcp:22,22,ip4:0.0.0.0/24"}).success?.should be_false
+    APITest.create('/netfilter_groups.json', {:name=>'g1', :description=>'g1', :rule => "tcp:22,22,ip4:0.0.0.0"}).success?.should be_false
+    APITest.create('/netfilter_groups.json', {:name=>'g2', :description=>'g2', :rule => "tcp:22,22,ip4:0.0.0.0"}).success?.should be_false
+    APITest.create('/netfilter_groups.json', {:name=>'g3', :description=>'g3', :rule => "tcp:22,22,ip4:0.0.0.0"}).success?.should be_false
 
     # update created group name
-    APITest.update("/netfilter_groups/#{res1["uuid"]}", {:description=>'g1(new)', :rule => "icmp:-1,-1,ip4:0.0.0.0/24"}).success?.should be_true
-    APITest.update("/netfilter_groups/#{res2["uuid"]}", {:description=>'g2(new)', :rule => "tcp:80,80,ip4:0.0.0.0/24"}).success?.should be_true
-    APITest.update("/netfilter_groups/#{res3["uuid"]}", {:description=>'g3(new)', :rule => "udp:53,53,ip4:0.0.0.0/24"}).success?.should be_true
+    APITest.update("/netfilter_groups/#{res1["uuid"]}", {:description=>'g1(new)', :rule => "icmp:-1,-1,ip4:0.0.0.0"}).success?.should be_true
+    APITest.update("/netfilter_groups/#{res2["uuid"]}", {:description=>'g2(new)', :rule => "tcp:80,80,ip4:0.0.0.0"}).success?.should be_true
+    APITest.update("/netfilter_groups/#{res3["uuid"]}", {:description=>'g3(new)', :rule => "udp:53,53,ip4:0.0.0.0"}).success?.should be_true
 
     # delete
     APITest.delete("/netfilter_groups/#{res3["uuid"]}").success?.should be_true
