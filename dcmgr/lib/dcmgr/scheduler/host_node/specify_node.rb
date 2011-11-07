352d2b10931197f13f2c9d22d5bcbd5f42d30642
t-iwano <t-iwano@axsh.net>
2011-11-07 14:05:09 +0900

    fix the change for 3f444c6c80b2c993e4044a64ab577e64f5c948cc

--- dcmgr/lib/dcmgr/scheduler/host_node/specify_node.rb
+++ dcmgr/lib/dcmgr/scheduler/host_node/specify_node.rb
@@ -9,7 +9,7 @@ module Dcmgr
         include Dcmgr::Logger
 
         def schedule(instance)
-          host_uuid = instance.request_params['host_id'] || instance.request_params['host_pool_id']
+          host_uuid = instance.request_params[:host_id] || instance.request_params[:host_pool_id]
           ds = Models::HostNode.online_nodes.filter(:uuid=>Models::HostNode.trim_uuid(host_uuid))
 
           host_node = ds.first
