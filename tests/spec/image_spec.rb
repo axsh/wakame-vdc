6f33dc5f604252c1de4c26492fba981dc5769a90
Metallion <akieckens@axsh.net>
2011-11-15 21:01:35 +0900

    removed old image_spec.rb and renamed image_api_spec.rb to image_spec.rb

--- tests/spec/image_spec.rb
+++ tests/spec/image_spec.rb
@@ -1,61 +1,15 @@
 
 require File.expand_path('../spec_helper', __FILE__)
+include Config
 
-cfg = Config::get_config[:images_api_spec]
+if is_enabled? :images_api_spec
+  cfg = get_config[:images_api_spec]
 
-describe "Test the web api for machine images (/api/images)" do
-  include CliHelper
-
-  before :all do
-    init_env
-    cd_dcmgr_dir
-  end
-
-  # volume_min_size
-  it "should show image list" do
-    res = APITest.get("/images")
-    res.success?.should be_true
-  end
-  unless cfg[:local_image_id].nil?
-    it "should describe machine image (#{cfg[:local_image_id]})" do
-      res = APITest.get("/images/#{cfg[:local_image_id]}")
-      res["id"].should == cfg[:local_image_id]
-      res.success?.should be_true
-    end
-
-    it "should delete machine image (wmi-lucid0) and then register with CLI." do
-      image_id = cfg[:local_image_id]
-      res = APITest.get("/images/#{image_id}")
-
-      # TODO: Image should be registerd via API.
-      require 'yaml'
-      vmimage_uri = YAML.load(res["source"])[:uri]
-      vmimage_path = URI.parse(vmimage_uri).path
-      cmd = "./bin/vdc-manage image add local #{vmimage_path} -m #{res["md5sum"]} -a #{res["account_id"]} -u #{res["id"]} -r #{res["arch"]} -d \"#{res["description"]}\" -s init"
-
-      res = APITest.delete("/images/#{image_id}")
-      res.success?.should be_true
-
-      `#{cmd}`
-      $?.exitstatus.should == 0
-    end
-  end
-  
-  unless cfg[:snapshot_image_id].nil?
-    it "should delete machine image (wmi-lucid1) and then register with CLI." do
-      image_id = cfg[:snapshot_image_id]
-      res = APITest.get("/images/#{image_id}")
-
-      # TODO: Image should be registerd via API.
-      require 'yaml'
-      snap_id = YAML.load(res["source"])[:snapshot_id]
-      cmd = "./bin/vdc-manage image add volume #{snap_id} -m #{res["md5sum"]} -a #{res["account_id"]} -u #{res["id"]} -r #{res["arch"]} -d \"#{res["description"]}\" -s init"
-
-      res = APITest.delete("/images/#{image_id}")
-      res.success?.should be_true
-
-      `#{cmd}`
-      $?.exitstatus.should == 0
-    end
+  describe "/api/images" do
+    include CliHelper
+    
+    it_should_behave_like "show_api", "/images", cfg[:local_image_ids] + cfg[:snapshot_image_ids]
+    it_should_behave_like "image_delete_and_register", cfg[:local_image_ids], :local
+    it_should_behave_like "image_delete_and_register", cfg[:snapshot_image_ids], :snapshot
   end
 end
