e70cc5850fbfbdd0a81a4fc775dde153ce665b03
Masahiro Fujiwara <m-fujiwara@axsh.net>
2011-12-09 14:11:12 -0600

    fix wrong symlink to /dev/null in create_ubuntu_image.sh.

--- tests/image_builder/ubuntu/10.04/create_ubuntu_image.sh
+++ tests/image_builder/ubuntu/10.04/create_ubuntu_image.sh
@@ -70,8 +70,8 @@ echo "nameserver 8.8.8.8" > $tmp_mount_dir/etc/resolv.conf
 #Handle mac address
 echo "Unsetting mac address"
 rm -f $tmp_mount_dir/etc/udev/rules.d/70-persistent-net.rules
-ln -s $tmp_mount_dir/dev/null $tmp_mount_dir/etc/udev/rules.d/70-persistent-net.rules
-sed -i 's/HWADDR=.*/#HWADDR=xx:xx:xx:xx:xx:xx/' $tmp_mount_dir/etc/sysconfig/network-scripts/ifcfg-eth0
+ln -s /dev/null $tmp_mount_dir/etc/udev/rules.d/70-persistent-net.rules
+#sed -i 's/HWADDR=.*/#HWADDR=xx:xx:xx:xx:xx:xx/' $tmp_mount_dir/etc/sysconfig/network-scripts/ifcfg-eth0
 
 #Remove SSH host keys
 echo "Removing ssh host keys"
