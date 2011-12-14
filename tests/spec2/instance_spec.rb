d62f19fcf332a49adc8e42f01c4d0c5f3a6d3180
t-iwano <t-iwano@axsh.net>
2011-12-14 16:29:22 +0900

    add vmimage lucid6 to instance spec

--- tests/spec2/instance_spec.rb
+++ tests/spec2/instance_spec.rb
@@ -20,17 +20,23 @@ describe "/api/instances" do
   it "should run instance with ssh_key and nf_group" do
     run_instance_then_reboot_then_terminate({:host_id => 'hn-demo18', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
                                               :ssh_key_id => 'ssh-demo', :nf_group => 'sg-demofgr'})
+    run_instance_then_reboot_then_terminate({:host_id => 'hn-demo19', :image_id=> 'wmi-lucid6', :instance_spec_id=> 'is-demospec',
+                                              :ssh_key_id => 'ssh-demo', :nf_group => 'sg-demofgr'})
   end
 
   # hostname
   it "should run instance with hostname (min length:1)" do
-    run_instance_then_reboot_then_terminate({:host_id => 'hn-demo19', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
+    run_instance_then_reboot_then_terminate({:host_id => 'hn-demo20', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
                                               :nf_group => 'sg-demofgr', :hostname=>'0'})
+    run_instance_then_reboot_then_terminate({:host_id => 'hn-demo18', :image_id=> 'wmi-lucid6', :instance_spec_id=> 'is-demospec',
+                                              :nf_group => 'sg-demofgr', :hostname=>'1'})
   end
 
   it "should run instance with hostname (max length:32)" do
-    run_instance_then_reboot_then_terminate({:host_id => 'hn-demo20', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
+    run_instance_then_reboot_then_terminate({:host_id => 'hn-demo19', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
                                               :nf_group => 'sg-demofgr', :hostname=>'01234567890123456789012345678901'})
+    run_instance_then_reboot_then_terminate({:host_id => 'hn-demo20', :image_id=> 'wmi-lucid6', :instance_spec_id=> 'is-demospec',
+                                              :nf_group => 'sg-demofgr', :hostname=>'12345678901234567890123456789012'})
   end
 
   it "should not run instance with hostname (less than min length:1)" do
@@ -39,33 +45,35 @@ describe "/api/instances" do
   end
 
   it "should not run instance with hostname (more than max length:32)" do
-    APITest.create("/instances", {:host_id => 'hn-demo19', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
+    APITest.create("/instances", {:host_id => 'hn-demo18', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
                      :nf_group => 'sg-demofgr', :hostname=>'012345678901234567890123456789012'}).success?.should_not be_true
   end
 
   it "should not run instance with invalid hostname" do
-    APITest.create("/instances", {:host_id => 'hn-demo20', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
+    APITest.create("/instances", {:host_id => 'hn-demo18', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
                      :nf_group => 'sg-demofgr', :hostname=>'!'}).success?.should_not be_true
     APITest.create("/instances", {:host_id => 'hn-demo18', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
                      :nf_group => 'sg-demofgr', :hostname=>'#'}).success?.should_not be_true
-    APITest.create("/instances", {:host_id => 'hn-demo19', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
+    APITest.create("/instances", {:host_id => 'hn-demo18', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
                      :nf_group => 'sg-demofgr', :hostname=>'_'}).success?.should_not be_true
-    APITest.create("/instances", {:host_id => 'hn-demo20', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
+    APITest.create("/instances", {:host_id => 'hn-demo18', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
                      :nf_group => 'sg-demofgr', :hostname=>'*'}).success?.should_not be_true
   end
 
   # user_data
   it "should run instance with user_data" do
     run_instance_then_reboot_then_terminate({:host_id => 'hn-demo18', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
-                                              :nf_group => 'sg-demofgr', :user_data => "user_data value"})
+                                              :nf_group => 'sg-demofgr', :user_data => "user_data value1"})
+    run_instance_then_reboot_then_terminate({:host_id => 'hn-demo19', :image_id=> 'wmi-lucid6', :instance_spec_id=> 'is-demospec',
+                                              :nf_group => 'sg-demofgr', :user_data => "user_data value2"})
   end
 
   # ha_enabled
   it "should run instance with ha_enabled" do
-    run_instance_then_reboot_then_terminate({:host_id => 'hn-demo19', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
-                                              :nf_group => 'sg-demofgr', :ha_enabled => 'true'})
     run_instance_then_reboot_then_terminate({:host_id => 'hn-demo20', :image_id=> 'wmi-lucid5', :instance_spec_id=> 'is-demospec',
-                                              :nf_group => 'sg-demofgr', :ha_enabled => 'false'})
+                                              :nf_group => 'sg-demofgr', :ha_enabled => 'true'})
+    run_instance_then_reboot_then_terminate({:host_id => 'hn-demo18', :image_id=> 'wmi-lucid6', :instance_spec_id=> 'is-demospec',
+                                              :nf_group => 'sg-demofgr', :ha_enabled => 'true'})
   end
 
   private
