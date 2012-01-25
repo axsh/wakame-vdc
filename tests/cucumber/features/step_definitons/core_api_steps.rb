edd6a5c6735f45d24869e5f965ff740e9993f072
Metallion <akieckens@axsh.net>
2012-01-25 17:37:38 +0900

    Added a bunch of cucumber steps for core_api

--- tests/cucumber/features/step_definitons/core_api_steps.rb
+++ tests/cucumber/features/step_definitons/core_api_steps.rb
@@ -4,6 +4,11 @@ require 'cucumber/formatter/unicode'
 
 require 'rubygems'
 require 'httparty'
+#require 'step_helpers'
+require File.dirname(__FILE__) + "/step_helpers.rb"
+
+include RetryHelper
+include InstanceHelper
 
 Before do
 end
@@ -11,24 +16,31 @@ end
 After do
 end
 
-When /we make a api create calls to (.*) with the following options/ do |suffix,options|
-  @create_results = []
-  options.hashes.each { |option_hash|
-    @create_results << APITest.create("/#{suffix}",option_hash)
-  }
+When /we make an api create call to (.*) with the following options/ do |suffix,options|
+  @create_results = {} if @create_results.nil?
+  @create_results[suffix] = APITest.create("/#{suffix}",options.hashes.first)
 end
 
-Then /the create calls (?:should|should not) be successful/ do |outcome|
-  @create_results.each { |result|
+Then /the create call to the (.*) api (should|should\snot) be successful/ do |suffix,outcome|
+  #@create_results.each { |result|
     case outcome
       when "should"
-        result.success?.should == true
+        @create_results[suffix].success?.should == true
       when "should not"
-        result.success?.should == false
+        @create_results[suffix].success?.should == false
       else
         raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should_not'"
     end
-  }
+  #}
+end
+
+When /we start an instance of (.*) with the created security group and key pair/ do |image|
+  @create_results = {} if @create_results.nil?
+  @create_results["instances"] = APITest.create("/instances",{:image_id=>image,
+                                                    #TODO: get rid of this hard coded thing
+                                                    :instance_spec_id=>"is-demospec",
+                                                    :ssh_key_id=>@create_results["ssh_key_pairs"]["id"],
+                                                    :security_groups=>[@create_results["security_groups"]["id"]]})
 end
 
 When /we make an api show call to (.*)/ do |suffix|
@@ -61,20 +73,94 @@ Then /the results (.*) should contain (.*)/ do |key,value|
   }.nil?.should == false
 end
 
-######################################
-# Helper class
-######################################
-class APITest
-  include HTTParty
-  
-  base_uri "http://localhost:9001/api"
-  headers 'X-VDC-ACCOUNT-UUID' => 'a-shpoolxx'
 
-  def self.create(path, params)
-    self.post(path, :query=>params, :body=>'')
-  end
-
-  def self.update(path, params)
-    self.put(path, :query=>params, :body=>'')
+Then /the created (.+) should reach state (.+) in (\d+) seconds or less/ do |suffix,state,seconds|
+  retry_until(seconds.to_f) do
+    APITest.get("/#{suffix}/#{@create_results[suffix]["id"]}")["state"] == state
   end
 end
+
+Then /we should be able to ping the started instance in (\d+) seconds or less/ do |seconds|
+  #retry_until_network_started(@create_results["instances"]["id"])
+  retry_until(seconds.to_f) do
+    ping(@create_results["instances"]["id"]).exitstatus == 0
+  end
+end
+
+Then /the started instance should start ssh in (\d+) seconds or less/ do |seconds|
+  #retry_until_ssh_started(@create_results["instances"]["id"])
+  ipaddr = APITest.get("/instances/#{@create_results["instances"]["id"]}")["vif"].first["ipv4"]["address"]
+  retry_until(seconds.to_f) do
+    `echo | nc #{ipaddr} 22`
+    $?.exitstatus == 0
+  end
+end
+
+Then /we should be able to log into the started instance with user (.+) in (\d+) seconds or less/ do |user, seconds|
+  retry_until_loggedin(@create_results["instances"]["id"], user, seconds.to_i)
+end
+
+Then /we should be able to log into the started instance in (\d+) seconds or less/ do |seconds|
+  #retry_until_ssh_started(@create_results["instances"]["id"])
+  ipaddr = APITest.get("/instances/#{@create_results["instances"]["id"]}")["vif"].first["ipv4"]["address"]
+  retry_until(seconds.to_f) do
+    `echo | nc #{ipaddr} 22`
+    $?.exitstatus == 0
+  end
+end
+
+When /we (attach|detach) the created volume/ do |operation|
+  @attach_result = APITest.update("/volumes/#{@create_results["volumes"]["id"]}/#{operation}", {:instance_id=>@create_results["instances"]["id"], :volume_id=>@create_results["volumes"]["id"]})
+end
+
+Then /the (attach|detach) api call (should|should\snot) be successful/ do |operation,outcome|
+  case outcome
+    when "should"
+      @attach_result.success?.should == true
+    when "should not"
+      @attach_result.success?.should == false
+    else
+      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should_not'"
+  end
+end
+
+When /we create a snapshot from the created volume/ do
+  @create_results["volume_snapshots"] = APITest.create("/volume_snapshots", {:volume_id=>@create_results["volumes"]["id"], :destination=>"local"})
+end
+
+When /we delete the created (.+)/ do |suffix|
+  @delete_results = {} if @delete_results.nil?
+  @delete_results[suffix] = APITest.delete("/#{suffix}/#{@create_results[suffix]["id"]}")
+end
+
+Then /Then the delete call to the (.+) api (should|should\snot) be successful/ do |suffix,outcome|
+  case outcome
+    when "should"
+      @delete_results[suffix].success?.should == true
+    when "should not"
+      @delete_results[suffix].success?.should == false
+    else
+      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should_not'"
+  end
+end
+
+Then /Then the update call to the (.+) api (should|should\snot) be successful/ do |suffix,outcome|
+  case outcome
+    when "should"
+      @update_results[suffix].success?.should == true
+    when "should not"
+      @update_results[suffix].success?.should == false
+    else
+      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should_not'"
+  end
+end
+
+When /we create a volume from the created snapshot/ do
+  @create_results["volumes"] = APITest.create("/volumes", {:snapshot_id=>@create_results["volume_snapshots"]["id"]})
+end
+
+When /we (reboot|stop|start) the created instance/ do |operation|
+  @update_results = {} if @update_results.nil?
+  @update_results["instances"] = APITest.update("/instances/#{@create_results["instances"]["id"]}/#{operation}", [])
+end
+
---
+++ tests/cucumber/features/step_definitons/step_helpers.rb
@@ -0,0 +1,85 @@
+######################################
+# Helper class
+######################################
+class APITest
+  include HTTParty
+  
+  base_uri "http://localhost:9001/api"
+  headers 'X-VDC-ACCOUNT-UUID' => 'a-shpoolxx'
+
+  def self.create(path, params)
+    self.post(path, :query=>params, :body=>'')
+  end
+
+  def self.update(path, params)
+    self.put(path, :query=>params, :body=>'')
+  end
+end
+
+module RetryHelper
+  #include Config
+  
+  DEFAULT_WAIT_PERIOD=60*5
+  
+  def retry_until(wait_sec=DEFAULT_WAIT_PERIOD, &blk)
+    start_at = Time.now
+    lcount=0
+    loop {
+      if blk.call
+        break
+      else
+        sleep 2
+      end
+      lcount += 1
+      raise("Retry Failure: Exceed #{wait_sec} sec: Retried #{lcount} times") if (Time.now - start_at) > wait_sec
+    }
+  end
+
+  def retry_while(wait_sec=DEFAULT_WAIT_PERIOD, &blk)
+    retry_until(wait_sec) do
+      !blk.call
+    end
+  end
+end
+
+module InstanceHelper
+  include RetryHelper
+
+  def ping(instance_id)
+    ipaddr = APITest.get("/instances/#{instance_id}")["vif"].first["ipv4"]["address"]
+    `ping -c 1 -W 1 #{ipaddr}`
+    $?
+  end
+
+  def ssh_command(instance_id, user, command, seconds)
+    res = APITest.get("/instances/#{instance_id}")
+
+    ssh_key_id = res["ssh_key_pair"]
+    key_pair = APITest.get("/ssh_key_pairs/#{ssh_key_id}")
+
+    suffix = Time.now.strftime("%s")
+    private_key_path = "/tmp/vdc_id_rsa.pem.#{suffix}"
+    open(private_key_path, "w") { |f| f.write(key_pair["private_key"]) }
+    File.chmod(0600, private_key_path)
+    sleep 5
+
+    cmd = "ssh -o 'StrictHostKeyChecking no' -i #{private_key_path} #{user}@#{res["vif"].first["ipv4"]["address"]} '#{command}'"
+
+    if seconds > 0
+      retry_until(seconds) do
+        `#{cmd}`
+        $?.exitstatus == 0
+      end
+    else
+      `#{cmd}`
+    end
+
+    FileUtils.rm(private_key_path)
+    $?
+  end
+
+  def retry_until_loggedin(instance_id, user, seconds = 0)
+    ssh_command(instance_id, user, "hostname; whoami;", seconds)
+  end
+
+end
