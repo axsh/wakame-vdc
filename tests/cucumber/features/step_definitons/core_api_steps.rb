a3e5af26d674028dc36407b4cc790b582e06ab9b
rakshasa <sundell.software@gmail.com>
2012-01-30 18:58:27 +0900

    Cleaned up the steps by using single-line tests for should.

--- tests/cucumber/features/step_definitons/core_api_steps.rb
+++ tests/cucumber/features/step_definitons/core_api_steps.rb
@@ -82,14 +82,7 @@ Then /^from the previous api call save to registry (.+) the value for key (.+)$/
 end
 
 Then /^the previous api call (should|should\snot) be successful$/ do |outcome|
-  case outcome
-    when "should"
-      @api_last_result.success?.should == true
-    when "should not"
-      @api_last_result.success?.should == false
-    else
-      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should not'"
-  end
+  @api_last_result.success?.should == (outcome == 'should not' ? false : true)
 end
 
 Then /^the previous api call should fail with the HTTP code (\d+)$/ do |rescode|
@@ -104,44 +97,20 @@ end
 
 Then /^the (create|update|delete|get|post|put) call to the (.*) api (should|should\snot) be successful$/ do |call,arg_suffix,outcome|
   suffix = evaluate_argument(arg_suffix)
-
-  case outcome
-    when "should"
-      @api_call_results[call][suffix].success?.should == true
-    when "should not"
-      @api_call_results[call][suffix].success?.should == false
-    else
-      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should not'"
-  end
+  @api_call_results[call][suffix].success?.should == (outcome == 'should not' ? false : true)
 end
 
 # This test currently does not verify the correct type, e.g. strings
 # and integers can both match an integer.
 Then /^the previous api call (should|should\snot) have the key (.+) with (.+)$/ do |outcome,key,arg_value|
   value = evaluate_argument(arg_value)
-
-  case outcome
-    when "should"
-      @api_last_result[key].to_s.should == value
-    when "should\snot"
-      @api_last_result[key].to_s.should_not == value
-    else
-      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should not'"
-  end
+  (@api_last_result[key].to_s == value).should == (outcome == 'should not' ? false : true)
 end
 
 Then /^for (create|update|delete|get|post|put) on (.+) there (should|should\snot) be the key (.+) with (.+)$/ do |call,arg_suffix,outcome,key,arg_value|
   suffix = evaluate_argument(arg_suffix)
   value = evaluate_argument(arg_value)
-
-  case outcome
-    when "should"
-      @api_call_results[call][suffix][key].to_s.should == value
-    when "should\snot"
-      @api_call_results[call][suffix][key].to_s.should_not == value
-    else
-      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should not'"
-  end
+  (@api_call_results[call][suffix][key].to_s == value).should == (outcome == 'should not' ? false : true)
 end
 
 # This step is for validating the response from collection (list).
@@ -149,36 +118,16 @@ end
 # GET /ssh_key_pairs
 Then /^the previous api call results (should|should\snot) contain the key (.+) with (.+)$/ do |outcome,key,arg_value|
   value = evaluate_argument(arg_value)
-
-  case outcome
-    when "should"
-      @api_last_result.first["results"].find { |itr|
-         itr[key].to_s == value
-      }.nil?.should == false
-    when "should not"
-      @api_last_result.first["results"].find { |itr|
-         itr[key].to_s == value
-      }.nil?.should == true
-    else
-      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should not'"
-  end
+  @api_last_result.first["results"].find { |itr|
+    itr[key].to_s == value
+  }.nil?.should_not == (outcome == 'should not' ? false : true)
 end
 
 Then /^the previous api call root array (should|should\snot) contain the key (.+) with (.+)$/ do |outcome,key,arg_value|
   value = evaluate_argument(arg_value)
-
-  case outcome
-    when "should"
-      @api_last_result.parsed_response.find { |itr|
-         itr[key].to_s == value
-      }.nil?.should == false
-    when "should not"
-      @api_last_result.parsed_response.find { |itr|
-         itr[key].to_s == value
-      }.nil?.should == true
-    else
-      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should not'"
-  end
+  @api_last_result.parsed_response.find { |itr|
+    itr[key].to_s == value
+  }.nil?.should_not == (outcome == 'should not' ? false : true)
 end
 
 # This step validates the response from individual resource.
@@ -198,47 +147,21 @@ end
 Then /^for (create|update|delete|get|post|put) on (.+) the results (should|should\snot) contain the key (.+) with (.+)$/ do |call,arg_suffix,outcome,key,arg_value|
   suffix = evaluate_argument(arg_suffix)
   value = evaluate_argument(arg_value)
-
-  case outcome
-    when "should"
-      @api_call_results[call][suffix].first["results"].find { |itr|
-         itr[key].to_s == value
-      }.nil?.should == false
-    when "should not"
-      @api_call_results[call][suffix].first["results"].find { |itr|
-         itr[key].to_s == value
-      }.nil?.should == true
-    else
-      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should not'"
-  end
+  @api_call_results[call][suffix].first["results"].find { |itr|
+    itr[key].to_s == value
+  }.nil?.should_not == (outcome == 'should not' ? false : true)
 end
 
 # Verify the number of elements in the returned array.
 Then /^the previous api call root array (should|should\snot) have (.+) entries$/ do |outcome,arg_count|
   count = evaluate_argument(arg_count)
-
-  case outcome
-    when "should"
-      (@api_last_result.parsed_response.size).should == count.to_i
-    when "should not"
-      (@api_last_result.parsed_response.size).should_not == count.to_i
-    else
-      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should not'"
-  end
+  (@api_last_result.parsed_response.size == count.to_i).should == (outcome == 'should not' ? false : true)
 end
 
 # Verify the number of elements in the returned result.
 Then /^the previous api call results (should|should\snot) have (.+) entries$/ do |outcome,arg_count|
   count = evaluate_argument(arg_count)
-
-  case outcome
-    when "should"
-      (@api_last_result.first["results"].size).should == count.to_i
-    when "should not"
-      (@api_last_result.first["results"].size).should_not == count.to_i
-    else
-      raise "Illegal outcome in .feature file: '#{outcome}'. Legal outcomes are 'should' and 'should not'"
-  end
+  (@api_last_result.first["results"].size == count.to_i).should == (outcome == 'should not' ? false : true)
 end
 
 Then /^the created (.+) should reach state (.+) in (\d+) seconds or less$/ do |suffix,state,seconds|
