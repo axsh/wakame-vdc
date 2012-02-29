926f2066aa39eb047679f9f43eeebc2ede6c25cc
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-02-29 22:15:52 +0900

    update build_cd.sh to use new .deb files.

--- tests/cd_builder/build_cd.sh
+++ tests/cd_builder/build_cd.sh
@@ -3,7 +3,6 @@
 set -e
 
 # Get options
-FLAG_WITHOUT_GPG_SIGN=false
 while true; do
   case "$1" in
     --without-gpg-sign )
@@ -12,6 +11,7 @@ while true; do
     * ) break;;
   esac
 done
+export FLAG_WITHOUT_GPG_SIGN
 
 #Function to generate the mount directory later on
 function randdir {
@@ -27,9 +27,6 @@ function checkreq {
     which $exec >> /dev/null
     [[ $? = 0 ]] || abort "Missing dependency ${requirements[$exec]}.\nTry running 'apt-get install ${requirements[@]}' to install all dependencies."
   done
-  
-  which bundle >> /dev/null
-  [[ $? = 0 ]] || abort "Bundler not found.\nTry running 'gem install bundler' to install it.\nAlso make sure it is in the root user's PATH variable."
 }
 
 function abort() {
@@ -56,8 +53,8 @@ apt_dir="${root_dir}/guts/apt-ftparchive"
 cd_mod_dir="${root_dir}/guts/wakame"
 tmp_mount_dir="${tmp_dir}/$(randdir)"
 wakame_version="11.12"
-wakame_deb="${wakame_dir}/../wakame-vdc_${wakame_version}_amd64.deb"
 arch="amd64"
+wakame_debs="wakame-vdc_${wakame_version}_${arch}.deb wakame-vdc-dvd-config_${wakame_version}_${arch}.deb"
 src_image=`readlink -f $1`
 dst_image="${root_dir}/wakame-vdc-${wakame_version}-${arch}.iso"
 images_dir="${cd_dir}/setup/images"
@@ -68,6 +65,8 @@ guts_archive_dir="${root_dir}/guts/archive"
 package_location_axsh="http://dlc.wakame.axsh.jp.s3.amazonaws.com/vdc/11.12.1/package/ubuntu/"
 package_location='axsh'
 
+## Check prerequisties
+
 [[ $UID = 0 ]] || abort "Operation not permitted. Try using sudo."
 
 #Check if the source image exists
@@ -75,70 +74,64 @@ if [ ! -f ${src_image} ]; then
   abort "Couldn't find source image: ${src_image}"
 fi
 
-
 #Check if dependencies are installed
 checkreq
 
-#Make tmp dir if it doesn't exist
-if [ -f ${tmp_dir} ]; then
-  abort "Failed to create ${tmp_dir}. File exists."
-elif [ ! -d ${tmp_dir} ]; then
-  mkdir -p ${tmp_dir}
-fi
+## Clean artifacts and temporary stuff.
 
 #Clean the working directory
-if [ -d ${wakame_dir}/tmp ]; then rm ${wakame_dir}/tmp/*; fi
-if [ -f ${dst_image} ]; then rm -f ${dst_image}; fi
-if [ -f ${apt_dir}/apt-ftparchive-deb.conf ]; then rm -f ${apt_dir}/apt-ftparchive-deb.conf; fi
-if [ -f ${apt_dir}/apt-ftparchive-extras.conf ]; then rm -f ${apt_dir}/apt-ftparchive-extras.conf; fi
-if [ -f ${apt_dir}/apt-ftparchive-udeb.conf ]; then rm -f ${apt_dir}/apt-ftparchive-udeb.conf; fi
-if [ -f ${apt_dir}/release.conf ]; then rm -f ${apt_dir}/release.conf; fi
+[[ -d ${wakame_dir}/tmp ]] && rm ${wakame_dir}/tmp/*
+[[ -f ${dst_image} ]] && rm -f ${dst_image}
+#Delete ftparchive config files
+[[ -f ${apt_dir}/apt-ftparchive-deb.conf ]] && rm -f ${apt_dir}/apt-ftparchive-deb.conf
+[[ -f ${apt_dir}/apt-ftparchive-extras.conf ]] && rm -f ${apt_dir}/apt-ftparchive-extras.conf
+[[ -f ${apt_dir}/apt-ftparchive-udeb.conf ]] && rm -f ${apt_dir}/apt-ftparchive-udeb.conf
+[[ -f ${apt_dir}/release.conf ]] && rm -f ${apt_dir}/release.conf
 rm -rf ${root_dir}/guts/indices/
+rm -rf ${cd_dir}
 
 #Create directory
-[ -d "${cd_mod_dir}/tmp" ] || mkdir "${cd_mod_dir}/tmp"
-[ -d "${cd_mod_dir}/setup/images" ] || mkdir "${cd_mod_dir}/setup/images"
-[ -d "${cd_mod_dir}/pool" ] || mkdir "${cd_mod_dir}/pool"
-[ -d "${cd_mod_dir}/pool/extras" ] || mkdir "${cd_mod_dir}/pool/extras"
-[ -d "${cd_mod_dir}/pool/main" ] || mkdir "${cd_mod_dir}/pool/main"
-[ -d "${cd_mod_dir}/pool/main/u" ] || mkdir "${cd_mod_dir}/pool/main/u"
-[ -d "${cd_mod_dir}/pool/main/u/ubuntu-keyring" ] || mkdir "${cd_mod_dir}/pool/main/u/ubuntu-keyring"
 
-#Add the required gems to the debian package
-echo "Adding required gems to the debian package to be built"
-cd $wakame_dir/dcmgr
-bundle package
-cd $wakame_dir/frontend/dcmgr_gui
-bundle package
-
-[ -d $wakame_dir/debian/gems/ ] || mkdir -p $wakame_dir/debian/gems/
-[ -d $wakame_dir/debian/gems/dcmgr/ ] || mkdir -p $wakame_dir/debian/gems/dcmgr/
-[ -d $wakame_dir/debian/gems/frontend/ ] || mkdir -p $wakame_dir/debian/gems/frontend/
-
-cp $wakame_dir/dcmgr/vendor/cache/*.gem $wakame_dir/debian/gems/dcmgr/
-cp $wakame_dir/frontend/dcmgr_gui/vendor/cache/*.gem $wakame_dir/debian/gems/frontend/
-
-#Put a bundler gem in the wakame package so we can run bundle install after the installation
-BUNDLER=`gem fetch bundler | cut -d ' ' -f2`
-mv $BUNDLER.gem $wakame_dir/debian/bundler.gem
+#Make tmp dir if it doesn't exist
+mkdir -p ${tmp_dir} || :
+(
+  cd "${cd_mod_dir}"
+  cat <<EOF | while read -r i; do [[ -d "${i}" ]] || mkdir -p "${i}"; done
+tmp
+setup/images
+pool/extras
+pool/main
+pool/main/u
+pool/main/u/ubuntu-keyring
+EOF
+)
 
 #Make the debian package
-cd ${wakame_dir}
+(
+  cd ${wakame_dir}
+  
+  # skip building package if all debs exist already.
+  for i in ${wakame_debs}; do
+    [[ -f "../${i}" ]] 
+  done && exit 0
 
-if $FLAG_WITHOUT_GPG_SIGN; then
-  debuild --no-lintian -us -uc
-else
+  if [[ -n "$FLAG_WITHOUT_GPG_SIGN" ]]; then
+    debuild --no-lintian -us -uc
+  else
   #Check if the Axsh key is on the keyring
-  gpg --list-secret-keys | grep -q $gpg_key_id
-  if [ "$?" -ne "0" ]; then
-    abort "Couldn't find Axsh Co. LTD's private gpg key on the keyring. This script needs it to sign the CD."
+    gpg --list-secret-keys | grep -q $gpg_key_id
+    if [ "$?" -ne "0" ]; then
+      abort "Couldn't find Axsh Co. LTD's private gpg key on the keyring. This script needs it to sign the CD."
+    fi
+    
+    debuild --no-lintian
   fi
   
-  debuild --no-lintian
-fi
-
-if [ ! -e ${wakame_deb} ]; then abort "Couldn't find wakame-vdc package: ${wakame_deb}"; fi
-mv ${wakame_deb} ${cd_mod_dir}/pool/extras
+  for i in ${wakame_debs}; do
+    [[ -e "../${i}" ]] || abort "Couldn't find deb package: ${i}"
+    cp -p "../${i}" "${cd_mod_dir}/pool/extras"
+  done
+)
 
 #Extract guts
 function download_package() { 
@@ -184,12 +177,8 @@ mount -o loop ${src_image} ${tmp_mount_dir}
 echo "Copying CD contents"
 rsync -a ${tmp_mount_dir}/ ${cd_dir}
 
-#Remove samba to clear some space
-rm -rf ${cd_dir}/pool/main/s/samba
-
-#Unzip Packages
-cd ${cd_dir}/dists/${base_distro}/main/binary-amd64
-gunzip Packages.gz
+umount ${tmp_mount_dir}
+rmdir ${tmp_mount_dir}
 
 #Wakamize it
 echo "Adding wakame-vdc to it"
@@ -310,16 +299,72 @@ APT::FTPArchive::Release::Components "main restricted extras";
 APT::FTPArchive::Release::Description "Ubuntu ${base_distro_number} LTS";
 EOF
 
+#Remove unused packages in pool/main to clear some spaces.
+(
+  cd ${cd_dir}
+  cat <<EOF | while read -r i; do [[ -d "pool/main/${i}" ]] && rm -rf "pool/main/${i}"; done
+s/samba
+o/openjdk-6
+e/eucalyptus
+e/eucalyptus-commons-ext
+g/gcc-4.4
+g/gwt
+r/ruby1.8
+r/ruby-defaults
+h/hsqldb
+s/squid
+s/squid-langpack
+libg/libgnuinet-java
+libg/libgnumail-java
+libg/libgnujaf-java
+libg/libgoogle-collections-java
+libe/libezmorph-java
+libb/libbsf-java
+libo/liboro-java
+libw/libwoodstox-java
+libj/libjaxen-java
+libj/libjibx-java
+libj/libjdom1-java
+libj/libjson-java
+libj/libjaxp1.3-java
+libc/libcommons-lang-java
+libc/libcommons-fileupload-java
+libc/libcommons-jxpath-java
+libc/libcommons-dbcp-java
+libc/libcommons-logging-java
+libc/libcommons-collections-java
+libc/libcommons-collections3-java
+libc/libcommons-cli-java
+libc/libcommons-discovery-java
+libc/libcommons-codec-java
+liba/libaxiom-java
+libr/libregexp-java
+libh/libhamcrest-java
+libx/libxalan2-java
+libx/libxstream-java
+libx/libxpp2-java
+libx/libxml-security-java
+libx/libxpp3-java
+libx/libxerces2-java
+c/ca-certificates-java
+libs/libslf4j-java
+libm/libmx4j-java
+libp/libproxool-java
+EOF
+)
+
+# generate dists Packages/Release files for pool/main and pool/extra.
+# sign phase is skipped if $FLAG_WITHOUT_GPG_SIGN is set.
+${PWD}/build_repository.sh ${cd_dir}
+
+# the generated files have strange permission -------x so fix them.
+find ${cd_dir}/dists/ -type f \( -name "Packages" -or -name "Packages.gz" \) -exec chmod 444 {} \;
+
 #Create extra repository and sign with gpg key
 echo "Signing extra repository"
 rm -f ${cd_dir}/dists/${base_distro}/Release.gpg
 cd ${apt_dir}
 perl extraoverride.pl < ${cd_dir}/dists/${base_distro}/main/binary-amd64/Packages >> ${root_dir}/guts/indices/override.${base_distro}.extra.main
-
-# Skip gpg sign
-if ! $FLAG_WITHOUT_GPG_SIGN ; then
-  ${PWD}/build_repository.sh ${cd_dir}
-fi
 
 #Compile iso
 mkisofs -r -V "Wakame-vdc ${WAKAME_VERSION}" \
@@ -328,27 +373,6 @@ mkisofs -r -V "Wakame-vdc ${WAKAME_VERSION}" \
             -c isolinux/boot.cat -no-emul-boot \
             -boot-load-size 4 -boot-info-table \
             -o $dst_image $cd_dir
-
-#Clean up
-umount ${tmp_mount_dir}
-rmdir ${tmp_mount_dir}
-rm -rf ${cd_dir}
-
-rm -rf ${root_dir}/guts/indices/
-
-#Delete ftparchive config files
-if [ -f ${apt_dir}/apt-ftparchive-deb.conf ]; then rm -f ${apt_dir}/apt-ftparchive-deb.conf; fi
-if [ -f ${apt_dir}/apt-ftparchive-extras.conf ]; then rm -f ${apt_dir}/apt-ftparchive-extras.conf; fi
-if [ -f ${apt_dir}/apt-ftparchive-udeb.conf ]; then rm -f ${apt_dir}/apt-ftparchive-udeb.conf; fi
-if [ -f ${apt_dir}/release.conf ]; then rm -f ${apt_dir}/release.conf; fi
-
-#Delete debuild output
-cd ${wakame_dir}
-dh_clean
-rm ${wakame_dir}/../wakame-vdc_${wakame_version}_amd64.build
-rm ${wakame_dir}/../wakame-vdc_${wakame_version}.dsc
-rm ${wakame_dir}/../wakame-vdc_${wakame_version}_amd64.changes
-rm ${wakame_dir}/../wakame-vdc_${wakame_version}.tar.gz
 
 echo "Created iso image ${dst_image}"
 exit 0
--- tests/cd_builder/guts/apt-ftparchive/build_repository.sh
+++ tests/cd_builder/guts/apt-ftparchive/build_repository.sh
@@ -9,5 +9,7 @@ apt-ftparchive -c $APTCONF generate ${here}/apt-ftparchive-udeb.conf
 apt-ftparchive -c $APTCONF generate ${here}/apt-ftparchive-extras.conf
 apt-ftparchive -c $APTCONF release $BUILD/dists/$DISTNAME > $BUILD/dists/$DISTNAME/Release
 
-gpg --default-key "DCFFB6BE" --output $BUILD/dists/$DISTNAME/Release.gpg -ba $BUILD/dists/$DISTNAME/Release
-find . -type f -print0 | xargs -0 md5sum > md5sum.txt
+[[ -z "${FLAG_WITHOUT_GPG_SIGN}" ]] || {
+  gpg --default-key "DCFFB6BE" --output $BUILD/dists/$DISTNAME/Release.gpg -ba $BUILD/dists/$DISTNAME/Release
+}
+find . -type f -exec md5sum {} \; > md5sum.txt
