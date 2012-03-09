29e4918bd03c812ca0e227e25737589f48d4e7d6
rakshasa <sundell.software@gmail.com>
2012-03-09 21:27:37 +0900

    Added './openflow/del_ports.sh tunnels' argument to delete lingering virtual tunnel ports.

--- tests/openflow/del_ports.sh
+++ tests/openflow/del_ports.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-for i in `sudo ../ovs/bin/ovs-vsctl list-ports br0`; do
-    if [ "$i" != "eth0" ]; then
-        sudo ../ovs/bin/ovs-vsctl del-port br0 $i
-    fi;
-done
+if [ "$1" = "tunnels" ]; then
+    for i in `sudo ../ovs/bin/ovs-vsctl list-ports br0`; do
+        if [[ "$i" =~ ^t-[a-zA-Z0-9]*-[0-9]*$ ]]; then
+            sudo ../ovs/bin/ovs-vsctl del-port br0 $i
+        fi;
+    done
+
+elif [ -z "$1" ]; then
+    for i in `sudo ../ovs/bin/ovs-vsctl list-ports br0`; do
+        if [ "$i" != "eth0" ]; then
+            sudo ../ovs/bin/ovs-vsctl del-port br0 $i
+        fi;
+    done
+
+else
+    echo "Invalid argument."
+fi
