cefb2817d5909a3c4adf7372c5894b5203c62c97
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-03-13 22:30:13 +0900

    fix issues on Ruby 1.9.2.
    A class variable was refered from different module name. Strangely it worked on 1.8.7.

--- frontend/dcmgr_gui/app/models/dcmgr_resource/base.rb
+++ frontend/dcmgr_gui/app/models/dcmgr_resource/base.rb
@@ -1,4 +1,11 @@
 module DcmgrResource
+
+  @debug = false
+
+  class << self
+    attr_accessor :debug
+  end
+  
   class Base < ActiveResource::Base
 
     # self.site = 'http://your.dcmgr.api.server'
@@ -6,19 +13,11 @@ module DcmgrResource
     self.format = :json
     self.prefix = '/api/'
     
-    @@debug = false
-    
     class << self
       
       def total_resource
         result = self.find(:first,:params => {:start => 0,:limit => 1})
         result.owner_total
-      end
-      # If headers are not defined in a given subclass, then obtain
-      # headers from the superclass.
-      
-      def set_debug(debug = true)
-        @@debug = debug
       end
 
       def get_resource_state_count(resources, state)
@@ -35,30 +34,30 @@ module DcmgrResource
  
     end
     
-    ActiveResource::Connection.class_eval do 
+  end
+end
 
-      private
-      
-      def default_header
-        @default_header = {
-          'X-VDC-ACCOUNT-UUID' => self.class.send(:class_variable_get,:@@vdc_account_uuid)
-        }
-      end
-      
-      def configure_http(http)
-        http = apply_ssl_options(http)
-
-        #add debug 
-        http.set_debug_output($stderr) if @@debug
-
-        # Net::HTTP timeouts default to 60 seconds.
+ActiveResource::Connection.class_eval do 
+  
+  private
+  def default_header
+    @default_header = {
+      'X-VDC-ACCOUNT-UUID' => self.class.send(:class_variable_get,:@@vdc_account_uuid)
+    }
+  end
+  
+  def configure_http(http)
+    http = apply_ssl_options(http)
+    
+    #add debug 
+    http.set_debug_output($stderr) if DcmgrResource.debug
+    
+    # Net::HTTP timeouts default to 60 seconds.
         if @timeout
           http.open_timeout = @timeout
           http.read_timeout = @timeout
         end
-
-        http
-      end
-    end
+    
+    http
   end
 end
