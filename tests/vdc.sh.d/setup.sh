0a32ba98a8ea6960828fb8b69200f115c8284940
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-03-14 17:22:15 +0900

    fix wrong folder name.

--- tests/vdc.sh.d/setup.sh
+++ tests/vdc.sh.d/setup.sh
@@ -9,8 +9,8 @@ hostname | diff /etc/hostname - >/dev/null || hostname > /etc/hostname
 egrep -v '^#' /etc/hosts | egrep -q $(hostname) || echo "127.0.0.1 $(hostname)" >> /etc/hosts
 
 # always overwrite 10-hva-sysctl.conf since it may have updated entries.
-echo "Configuring sysctl.conf parameters ... /etc/sysctl.conf.d/10-hva-sysctl.conf"
-cp ${VDC_ROOT}/debian/config/10-hva-sysctl.conf /etc/sysctl.conf.d/10-hva-sysctl.conf
+echo "Configuring sysctl.conf parameters ... /etc/sysctl.d/10-hva-sysctl.conf"
+cp ${VDC_ROOT}/debian/config/10-hva-sysctl.conf /etc/sysctl.d/
 # reload sysctls
 initctl restart procps
 
