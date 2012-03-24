7e2edc04b7669af7227542f80aa850fd45a5cfb2
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-03-24 23:05:51 +0900

    add response generator for /images.

--- dcmgr/lib/dcmgr/endpoints/12.03/images.rb
+++ dcmgr/lib/dcmgr/endpoints/12.03/images.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+require 'dcmgr/endpoints/12.03/responses/image'
+
 Dcmgr::Endpoints::V1203::CoreAPI.namespace '/images' do
   post do
     # description 'Register new machine image'
@@ -8,18 +10,24 @@ Dcmgr::Endpoints::V1203::CoreAPI.namespace '/images' do
 
   get do
     # description 'Show list of machine images'
-    res = select_index(:Image, {:start => params[:start],
-                         :limit => params[:limit]})
-    response_to(res)
+    ds = M::Image.dataset
+    if params[:account_id]
+      ds = ds.filter(:account_id=>params[:account_id])
+    end
+
+    ds = datetime_range_params_filter(:created, ds)
+    ds = datetime_range_params_filter(:deleted, ds)
+    
+    collection_respond_with(ds) do |paging_ds|
+      R::ImageCollection.new(paging_ds).generate
+    end
   end
 
   get '/:id' do
     # description "Show a machine image details."
     i = find_by_uuid(:Image, params[:id])
-    if !(examine_owner(i) || i.is_public)
-      raise E::OperationNotPermitted
-    end
-    response_to(i.to_api_document(@account.canonical_uuid))
+
+    respond_with(R::Image.new(i).generate)
   end
 
   delete '/:id' do
@@ -30,6 +38,6 @@ Dcmgr::Endpoints::V1203::CoreAPI.namespace '/images' do
     else
       raise E::OperationNotPermitted
     end
-    response_to([i.canonical_uuid])
+    respond_with([i.canonical_uuid])
   end
 end
---
+++ dcmgr/lib/dcmgr/endpoints/12.03/responses/image.rb
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+module Dcmgr::Endpoints::V1203::Responses
+  class Image < Dcmgr::Endpoints::ResponseGenerator
+    def initialize(image)
+      raise ArgumentError if !image.is_a?(Dcmgr::Models::Image)
+      @image = image
+    end
+
+    def generate()
+      @image.instance_exec {
+        to_hash.merge(:id=>canonical_uuid)
+      }
+    end
+  end
+
+  class ImageCollection < Dcmgr::Endpoints::ResponseGenerator
+    def initialize(ds)
+      raise ArgumentError if !ds.is_a?(Sequel::Dataset)
+      @ds = ds
+    end
+
+    def generate()
+      @ds.all.map { |i|
+        Image.new(i).generate
+      }
+    end
+  end
+end
