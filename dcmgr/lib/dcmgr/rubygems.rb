177bcbb361dd81cca2cc729f253b3e24286cffb3
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-04-02 14:42:34 +0900

    support bundler v1.1 and run agents using abs path.
    bundler 1.1 is out and it supports standalone mode. "dcmgr/vender/bundle/bundler/setup.rb"
    is the standalone mode file to be loaded by each agents.

--- dcmgr/lib/dcmgr/rubygems.rb
+++ dcmgr/lib/dcmgr/rubygems.rb
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+setup_rb = File.expand_path('../../../vendor/bundle/bundler/setup.rb', __FILE__)
 begin
   require 'rubygems'
   require 'bundler/setup'
-rescue Exception => e
+  load setup_rb if File.exists?(setup_rb)
+rescue LoadError => e
+  load setup_rb if File.exists?(setup_rb)
 end
