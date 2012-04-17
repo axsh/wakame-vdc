af82c2ac78ac25f4a7012245bfa2685ec5732c59
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-04-17 21:47:07 +0900

    load bundler's setup.rb earlier if exists.

--- dcmgr/lib/dcmgr/rubygems.rb
+++ dcmgr/lib/dcmgr/rubygems.rb
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 setup_rb = File.expand_path('../../../vendor/bundle/bundler/setup.rb', __FILE__)
+
 begin
   require 'rubygems'
-  require 'bundler/setup'
-  load setup_rb if File.exists?(setup_rb)
+  if File.exists?(setup_rb)
+    load setup_rb
+  else
+    require 'bundler/setup'
+  end
 rescue LoadError => e
-  load setup_rb if File.exists?(setup_rb)
 end
