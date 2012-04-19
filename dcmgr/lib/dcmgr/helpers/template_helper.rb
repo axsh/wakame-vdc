e50ab1c05c6ff67224db1620f09115b276ee6876
t-iwano <t-iwano@axsh.net>
2012-04-19 11:02:24 -0400

    add helper template

---
+++ dcmgr/lib/dcmgr/helpers/template_helper.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+require 'erb'
+
+module Dcmgr
+  module Helpers
+    module TemplateHelper
+      def render_template(template_file_path, output_file_path)
+        # generate config file
+        unless File.exists?(output_file_path)
+          erb = ERB.new(File.new(template_file_path).read, nil, '-')
+          output = File.new(output_file_path, 'w+')
+          output.puts(erb.result(binding))
+          output.close
+          logger.debug("output config file #{output_file_path}")
+        end
+      end
+    end
+  end
+end
