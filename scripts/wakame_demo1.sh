9e7b22a911567b2f0304833154da8e74a770c52b
Tomofumi Hayashi <s1061123@gmail.com>
2012-04-20 23:57:49 +0900

    Fix demo scripts.

--- scripts/wakame_demo1.sh
+++ scripts/wakame_demo1.sh
@@ -11,6 +11,7 @@ wakame_root=${wakame_root:-$(cd ${script_path}/../ && pwd)}
 . $script_path/wakame_utils.sh
 
 tmp_path="${wakame_root}/tmp"
+data_path="${wakame_root}/test/vdc.sh.d"
 account_id=${account_id:?"account_id needs to be set"}
 
 hypervisor=${hypervisor:?"hypervisor needs to be set"}
