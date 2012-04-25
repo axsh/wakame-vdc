eae7b4efd39affd545dc6530d31a055328b51b23
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-04-25 19:03:09 +0900

    update gems for dcmgr/. also added mysql2 gem is as mysql gem replacement.

--- dcmgr/config/initializers/sequel.rb
+++ dcmgr/config/initializers/sequel.rb
@@ -2,29 +2,36 @@
 
 require 'sequel'
 if Sequel::DATABASES.first.nil?
-  db = Sequel.connect(Dcmgr.conf.database_url)
+  db = Sequel.connect(Dcmgr.conf.database_url, :after_connect=>proc { |conn|
+                        case conn.class.to_s
+                        when 'Mysql2::Client', 'Mysql'
+                          # send AUTOCOMMIT=0 for every new connections.
+                          conn.query "SET AUTOCOMMIT=0;"
+                          conn.query "COMMIT;"
+                        end
+                      })
 else
   db = Sequel::DATABASES.first
 end
 
 #require 'logger' 
 #db.loggers << Logger.new(STDERR)
-if db.is_a?(Sequel::MySQL::Database)
+case db.adapter_scheme
+when :mysql, :mysql2
   Sequel::MySQL.default_charset = 'utf8'
   Sequel::MySQL.default_collate = 'utf8_general_ci'
   Sequel::MySQL.default_engine = 'InnoDB'
 
-  db << "SET AUTOCOMMIT=0"
-  Dcmgr::Models::BaseNew.default_row_lock_mode = nil
-end
 
-# Disable TEXT to Sequel::SQL::Blob translation.
-# see the thread: MySQL text turning into blobs
-# http://groups.google.com/group/sequel-talk/browse_thread/thread/d0f4c85abe9b3227/9ceaf291f90111e6
-# lib/sequel/adapters/mysql.rb
-[249, 250, 251, 252].each { |v|
-  Sequel::MySQL::MYSQL_TYPES.delete(v)
-}
+  # Disable TEXT to Sequel::SQL::Blob translation.
+  # see the thread: MySQL text turning into blobs
+  # http://groups.google.com/group/sequel-talk/browse_thread/thread/d0f4c85abe9b3227/9ceaf291f90111e6
+  # lib/sequel/adapters/mysql.rb
+  [249, 250, 251, 252].each { |v|
+    Sequel::MySQL::MYSQL_TYPES.delete(v)
+  }
+end
+Dcmgr::Models::BaseNew.default_row_lock_mode = nil
 
 # Set timezone to UTC
 Sequel.default_timezone = :utc
