fdc232fd5b5e8eddba28f21ef944a74dc7d0625d
Masahito Yoshida <masahito@axsh.net>
2012-04-27 03:42:32 +0900

    fix to install 3rd party rpms.

--- tests/vdc.sh.d/rhel/install.sh
+++ tests/vdc.sh.d/rhel/install.sh
@@ -31,15 +31,26 @@ function rpmspec_depends() {
 # if someone use different release, they want to modify this conf manually. so check if it exists.
 
 # 3rd party rpms
-rpm -qi epel-release >/dev/null || {
-  rpm -ivh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm
-}
-rpm -qi rabbitmq-server | egrep ^Version | grep 2.6.1 -q || {
-  rpm -ivh http://www.rabbitmq.com/releases/rabbitmq-server/v2.6.1/rabbitmq-server-2.6.1-1.noarch.rpm
-}
-rpm -qi flog >/dev/null || {
-  rpm -ivh http://cdimage.wakame.jp/packages/rhel/6/flog-1.8-4.$(arch).rpm
-}
+(
+ yum -qi curl >/dev/null || yum install -y curl
+
+ [ -d $VDC_ROOT/tests/vdc.sh.d/rhel/vendor ] || mkdir -p $VDC_ROOT/tests/vdc.sh.d/rhel/vendor
+ cd $VDC_ROOT/tests/vdc.sh.d/rhel/vendor
+
+ [ -f epel-release-6-5.noarch.rpm ] || \
+   curl -O http://ftp.riken.go.jp/pub/Linux/fedora/epel/6/i386/epel-release-6-5.noarch.rpm
+ [ -f rabbitmq-server-2.6.1-1.noarch.rpm ] || \
+   curl -O http://www.rabbitmq.com/releases/rabbitmq-server/v2.6.1/rabbitmq-server-2.6.1-1.noarch.rpm
+ [ -f flog-1.8-4.$(arch).rpm ] || \
+   curl -O http://cdimage.wakame.jp/packages/rhel/6/flog-1.8-4.$(arch).rpm
+
+ rpm -qi epel-release >/dev/null || \
+   yum install -y $VDC_ROOT/tests/vdc.sh.d/rhel/vendor/epel-release-6-5.noarch.rpm
+ rpm -qi rabbitmq-server >/dev/null || \
+   yum install -y $VDC_ROOT/tests/vdc.sh.d/rhel/vendor/rabbitmq-server-2.6.1-1.noarch.rpm
+ rpm -qi flog >/dev/null || \
+   yum install -y $VDC_ROOT/tests/vdc.sh.d/rhel/vendor/flog-1.8-4.$(arch).rpm
+)
 
 yum update -y
 yum upgrade -y
