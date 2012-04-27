f47b677bfa4d80f83bdf9544ede9b38059f1aa03
Masahito Yoshida <masahito@axsh.net>
2012-04-27 19:53:24 +0900

    add test to exist /etc/hostname.

--- tests/vdc.sh.d/setup.d/hostname.sh
+++ tests/vdc.sh.d/setup.d/hostname.sh
@@ -3,7 +3,9 @@
 set -e
 
 # hostname and /etc/hosts configuration
-hostname | diff /etc/hostname - >/dev/null || hostname > /etc/hostname
+[ -f /etc/hostname ] && {
+  hostname | diff /etc/hostname - >/dev/null || hostname > /etc/hostname
+} || :
 egrep -v '^#' /etc/hosts | egrep -q $(hostname) || echo "127.0.0.1 $(hostname)" >> /etc/hosts
 
 exit 0
