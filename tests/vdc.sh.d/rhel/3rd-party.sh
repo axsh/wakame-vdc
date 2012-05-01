19e2d0075dea6508bd450e311e069c826aa798dd
Masahito Yoshida <masahito@axsh.net>
2012-05-01 19:25:41 +0900

    fix the keyword of 3rd party rpm to check install.

--- tests/vdc.sh.d/rhel/3rd-party.sh
+++ tests/vdc.sh.d/rhel/3rd-party.sh
@@ -27,26 +27,26 @@ vendor_dir=${vendor_dir:-${abs_path}/vendor}
 
 function list_3rd_party() {
   cat <<EOS | egrep -v ^#
-# pkg_name                         pkg_dir
-epel-release-6-5.noarch.rpm        http://ftp.riken.go.jp/pub/Linux/fedora/epel/6/i386
-rabbitmq-server-2.6.1-1.noarch.rpm http://www.rabbitmq.com/releases/rabbitmq-server/v2.6.1
-flog-1.8-4.$(arch).rpm             http://cdimage.wakame.jp/packages/rhel/6
+# pkg_name                         pkg_uri
+epel-release-6-5       http://ftp.riken.go.jp/pub/Linux/fedora/epel/6/i386/epel-release-6-5.noarch.rpm
+rabbitmq-server-2.6.1  http://www.rabbitmq.com/releases/rabbitmq-server/v2.6.1/rabbitmq-server-2.6.1-1.noarch.rpm
+flog-1.8-4             http://cdimage.wakame.jp/packages/rhel/6/flog-1.8-4.$(arch).rpm
 EOS
 }
 
 function download_3rd_party() {
   rpm -qi curl >/dev/null || yum install -y curl
-  list_3rd_party | while read pkg_name pkg_dir; do
+  list_3rd_party | while read pkg_name pkg_uri; do
     echo downloading ${pkg_name} ...
-    [ -f ${vendor_dir}/${pkg_name} ] || {
-      curl ${pkg_dir}/${pkg_name} > ${vendor_dir}/${pkg_name}
+    [ -f ${vendor_dir}/$(basename ${pkg_uri}) ] || {
+      curl ${pkg_uri} > ${vendor_dir}/${pkg_name}
     }
   done
 }
 
 function install_3rd_party() {
-  list_3rd_party | while read pkg_name pkg_dir; do
-    rpm -qi ${pkg_name%%.rpm} >/dev/null || yum install -y ${vendor_dir}/${pkg_name}
+  list_3rd_party | while read pkg_name pkg_uri; do
+    rpm -qi ${pkg_name} >/dev/null || yum install -y ${vendor_dir}/$(basename ${pkg_uri})
   done
 }
 
