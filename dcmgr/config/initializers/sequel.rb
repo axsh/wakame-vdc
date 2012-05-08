136898ec57e0e77c6ab96f489edf3fd16ba6dbf5
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-05-08 19:49:35 +0900

    fix uninitialized Sequel::MySQL constant issue when use with mysql2 adapter.

--- dcmgr/config/initializers/sequel.rb
+++ dcmgr/config/initializers/sequel.rb
@@ -22,14 +22,16 @@ when :mysql, :mysql2
   Sequel::MySQL.default_collate = 'utf8_general_ci'
   Sequel::MySQL.default_engine = 'InnoDB'
 
-
-  # Disable TEXT to Sequel::SQL::Blob translation.
-  # see the thread: MySQL text turning into blobs
-  # http://groups.google.com/group/sequel-talk/browse_thread/thread/d0f4c85abe9b3227/9ceaf291f90111e6
-  # lib/sequel/adapters/mysql.rb
-  [249, 250, 251, 252].each { |v|
-    Sequel::MySQL::MYSQL_TYPES.delete(v)
-  }
+  # this is the mysql adapter specific constants. won't work with mysql2.
+  if db.adapter_scheme == :mysql
+    # Disable TEXT to Sequel::SQL::Blob translation.
+    # see the thread: MySQL text turning into blobs
+    # http://groups.google.com/group/sequel-talk/browse_thread/thread/d0f4c85abe9b3227/9ceaf291f90111e6
+    # lib/sequel/adapters/mysql.rb
+    [249, 250, 251, 252].each { |v|
+      Sequel::MySQL::MYSQL_TYPES.delete(v)
+    }
+  end
 end
 Dcmgr::Models::BaseNew.default_row_lock_mode = nil
 
