c23ea880350f24f3a04f8da404630cf24162b739
saicologic <s-mikami@axsh.net>
2012-05-14 10:32:22 +0900

    Fixed host MAC Address.

--- dcmgr/lib/dcmgr/drivers/openvz.rb
+++ dcmgr/lib/dcmgr/drivers/openvz.rb
@@ -102,8 +102,10 @@ module Dcmgr
             ifname = "eth#{vif[:device_index]}"
             mac = vif[:mac_addr].unpack('A2'*6).join(':')
             host_ifname = vif[:uuid]
+            # host_mac become a randomly generated MAC Address.
+            host_mac = nil
             bridge = bridge_if_name(vif[:ipv4][:network][:dc_network])
-            sh("vzctl set %s --netif_add %s,%s,%s,%s,%s --save",[ctid, ifname, mac, host_ifname, mac, bridge])
+            sh("vzctl set %s --netif_add %s,%s,%s,%s,%s --save",[ctid, ifname, mac, host_ifname, host_mac, bridge])
             #
             # NETIF="ifname=eth0,bridge=vzbr0,mac=52:54:00:68:BB:AC,host_ifname=vif-h63jg7pp,host_mac=52:54:00:68:BB:AC"
             #
