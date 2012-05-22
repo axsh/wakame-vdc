0a14fff2ecdbaa7feb20f1391bd695a70997b96c
saicologic <s-mikami@axsh.net>
2012-05-22 11:03:19 +0900

    Modified to use ServiceType inherited class.

--- dcmgr/lib/dcmgr/configurations/dcmgr.rb
+++ dcmgr/lib/dcmgr/configurations/dcmgr.rb
@@ -101,9 +101,10 @@ module Dcmgr
         #   storage_node_scheduler(:LbScheduler1) {}
         #   network_scheduler(:LbScheduler1) {}
         # }
-        def service_type(name, &blk)
+        def service_type(name, class_name, &blk)
+          raise ArgumentError unless Dcmgr.const_get(class_name) < ServiceType
           @config[:service_types] ||= {}
-          @config[:service_types][name] = ServiceType.new(name).parse_dsl(&blk)
+          @config[:service_types][name] = Dcmgr.const_get(class_name).new(name).parse_dsl(&blk)
           self
         end
       end
