5d6986eb0a1b3cf728dc4564cb50ffe860739622
Masahito Yoshida <masahito@axsh.net>
2012-05-24 16:06:27 +0900

    set kernel parameters for openvz.

--- tests/vdc.sh.d/rhel/setup.sh
+++ tests/vdc.sh.d/rhel/setup.sh
@@ -19,6 +19,7 @@ VDC_ROOT=${VDC_ROOT:?"VDC_ROOT needs to be set"}
 #)
 
 ${VDC_ROOT}/rpmbuild/sysctl.sh < ${VDC_ROOT}/contrib/etc/sysctl.d/30-bridge-if.conf
+${VDC_ROOT}/rpmbuild/sysctl.sh < ${VDC_ROOT}/contrib/etc/sysctl.d/30-openvz.conf
 
 # stop system services.
 for i in apparmor dnsmasq tgtd; do
