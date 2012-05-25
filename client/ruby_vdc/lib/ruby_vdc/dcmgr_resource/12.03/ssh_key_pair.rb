cf7e2fb78031022b83ee6410fcd70f8483406d6f
rakshasa <sundell.software@gmail.com>
2012-05-25 15:14:29 +0900

    Fixed methods and class methods for included methods.

--- client/ruby_vdc/lib/ruby_vdc/dcmgr_resource/12.03/ssh_key_pair.rb
+++ client/ruby_vdc/lib/ruby_vdc/dcmgr_resource/12.03/ssh_key_pair.rb
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 module DcmgrResource::V1203
   module SshKeyPairMethods
-    def self.create(params)
-      ssh_key_pair = self.new
-      ssh_key_pair.download_once = params[:download_once]
-      ssh_key_pair.save
-      ssh_key_pair
+    def self.included(base)
+      base.extend(ClassMethods)
     end
     
-    def self.destroy(uuid)
-      self.delete(uuid).body
+    module ClassMethods
+      def create(params)
+        ssh_key_pair = self.new
+        ssh_key_pair.download_once = params[:download_once]
+        ssh_key_pair.save
+        ssh_key_pair
+      end
+      
+      def destroy(uuid)
+        self.delete(uuid).body
+      end
     end
   end
 
