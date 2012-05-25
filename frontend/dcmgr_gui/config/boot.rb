0ee78f6b3ddece51c17886ec4ecb678989a84157
rakshasa <sundell.software@gmail.com>
2012-05-25 15:30:05 +0900

    Use API 12.03 in frontend.

--- frontend/dcmgr_gui/config/boot.rb
+++ frontend/dcmgr_gui/config/boot.rb
@@ -14,13 +14,13 @@ end if File.exist?(gemfile)
 
 require 'ruby_vdc'
 
-DcmgrResource::Account = DcmgrResource::V1112::Account
-DcmgrResource::HostNode = DcmgrResource::V1112::HostNode
-DcmgrResource::Image = DcmgrResource::V1112::Image
-DcmgrResource::Instance = DcmgrResource::V1112::Instance
-DcmgrResource::InstanceSpec = DcmgrResource::V1112::InstanceSpec
-DcmgrResource::SecurityGroup = DcmgrResource::V1112::SecurityGroup
-DcmgrResource::SshKeyPair = DcmgrResource::V1112::SshKeyPair
-DcmgrResource::StorageNode = DcmgrResource::V1112::StorageNode
-DcmgrResource::Volume = DcmgrResource::V1112::Volume
-DcmgrResource::VolumeSnapshot = DcmgrResource::V1112::VolumeSnapshot
+DcmgrResource::Account = DcmgrResource::V1203::Account
+DcmgrResource::HostNode = DcmgrResource::V1203::HostNode
+DcmgrResource::Image = DcmgrResource::V1203::Image
+DcmgrResource::Instance = DcmgrResource::V1203::Instance
+DcmgrResource::InstanceSpec = DcmgrResource::V1203::InstanceSpec
+DcmgrResource::SecurityGroup = DcmgrResource::V1203::SecurityGroup
+DcmgrResource::SshKeyPair = DcmgrResource::V1203::SshKeyPair
+DcmgrResource::StorageNode = DcmgrResource::V1203::StorageNode
+DcmgrResource::Volume = DcmgrResource::V1203::Volume
+DcmgrResource::VolumeSnapshot = DcmgrResource::V1203::VolumeSnapshot
