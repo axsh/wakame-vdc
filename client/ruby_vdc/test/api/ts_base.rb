b787b2c744ed552723cf417dafa0d856af96d974
rakshasa <sundell.software@gmail.com>
2012-05-30 18:54:16 +0900

    Fixed base unit tests for 'total' and 'owner_total'.

--- client/ruby_vdc/test/api/ts_base.rb
+++ client/ruby_vdc/test/api/ts_base.rb
@@ -4,16 +4,11 @@ require 'test/unit'
 
 module TestBaseMethods
   def test_basic_1112
-    ssh_key_pair = api_class(:v1112).find(:first,:params => {:start => 0,:limit => 1})
+    [:v1112, :v1203].each { |api_ver|
+      object = api_class(api_ver).find(:first,:params => {:start => 0,:limit => 1})
 
-    assert_nothing_raised() { ssh_key_pair.total }
-    assert_nothing_raised() { ssh_key_pair.owner_total }
-  end
-
-  def test_basic_1203
-    ssh_key_pair = api_class(:v1203).find(:first,:params => {:start => 0,:limit => 1})
-
-    assert_nothing_raised() { ssh_key_pair.total }
-    assert_nothing_raised() { ssh_key_pair.owner_total }
+      assert_not_nil(object.total)
+      assert_not_nil(object.owner_total)
+    }
   end
 end
