6c046208dd89479e567c19b107c0701f7365cff6
rakshasa <sundell.software@gmail.com>
2012-06-12 16:15:21 +0900

    Removed '11.12' api related autoload statements.

--- client/ruby-hijiki/lib/hijiki.rb
+++ client/ruby-hijiki/lib/hijiki.rb
@@ -10,17 +10,7 @@ module Hijiki
     module V1112
       require 'hijiki/dcmgr_resource/11.12/base'
 
-      autoload :Account,        'hijiki/dcmgr_resource/11.12/account'
-      autoload :HostNode,       'hijiki/dcmgr_resource/11.12/host_node'
-      autoload :Image,          'hijiki/dcmgr_resource/11.12/image'
-      autoload :Instance,       'hijiki/dcmgr_resource/11.12/instance'
       autoload :InstanceSpec,   'hijiki/dcmgr_resource/11.12/instance_spec'
-      autoload :Network,        'hijiki/dcmgr_resource/11.12/network'
-      autoload :SecurityGroup,  'hijiki/dcmgr_resource/11.12/security_group'
-      autoload :SshKeyPair,     'hijiki/dcmgr_resource/11.12/ssh_key_pair'
-      autoload :StorageNode,    'hijiki/dcmgr_resource/11.12/storage_node'
-      autoload :Volume,         'hijiki/dcmgr_resource/11.12/volume'
-      autoload :VolumeSnapshot, 'hijiki/dcmgr_resource/11.12/volume_snapshot'
     end
 
     module V1203
