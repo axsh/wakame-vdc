4e8c3c8435f880c6dc557586cc5f59649e9ec690
saicologic <s-mikami@axsh.net>
2012-06-13 12:36:55 +0900

    added load balancer target model

---
+++ dcmgr/lib/dcmgr/models/load_balancer_target.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+module Dcmgr::Models
+  class LoadBalancerTarget < AccountResource
+    class RequestError < RuntimeError; end
+    
+    # override Sequel::Model#delete not to delete rows but to set
+    # delete flags.
+    def delete
+      self.deleted_at ||= Time.now
+      self.is_deleted = self.id
+      self.save_changes
+    end
+  end
+end
