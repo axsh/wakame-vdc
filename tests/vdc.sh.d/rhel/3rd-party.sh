13835b04fee95f98363a20baa28cbfa0ceab70fd
Masahito Yoshida <masahito@axsh.net>
2012-06-20 16:56:06 +0900

    tests/vdc.sh.d/rhel: update "download" task not to build rpm using env-builder.

--- tests/vdc.sh.d/rhel/3rd-party.sh
+++ tests/vdc.sh.d/rhel/3rd-party.sh
@@ -32,11 +32,9 @@ vendor_dir=${vendor_dir:-${abs_path}/vendor}
 vendor_dir=${vendor_dir}/${basearch}
 [ -d ${vendor_dir} ] || mkdir -p ${vendor_dir}
 
-function list_3rd_party() {
+function list_3rd_party_builder() {
   cat <<EOS | egrep -v ^#
 # pkg_name                         pkg_uri                                                                        pkg_file
-epel-release-6-7       http://ftp.riken.go.jp/pub/Linux/fedora/epel/6/i386/epel-release-6-7.noarch.rpm            epel-release-6-7.noarch.rpm
-rabbitmq-server-2.6.1  http://www.rabbitmq.com/releases/rabbitmq-server/v2.6.1/rabbitmq-server-2.6.1-1.noarch.rpm rabbitmq-server-2.6.1-1.noarch.rpm
 flog                   git://github.com/hansode/env-builder.git                                                   flog-1.8-3.${basearch}.rpm
 openvswitch            git://github.com/hansode/env-builder.git                                                   kmod-openvswitch-1.4.1-1.el6.${arch}.rpm
 openvswitch            git://github.com/hansode/env-builder.git                                                   openvswitch-1.4.1-1.${arch}.rpm
@@ -45,9 +43,23 @@ lxc                    git://github.com/hansode/env-builder.git                 
 EOS
 }
 
+function list_3rd_party() {
+  vdc_current_base_url=http://dlc.wakame.axsh.jp.s3.amazonaws.com/packages/rhel/6/current/${basearch}
+  cat <<EOS | egrep -v ^#
+# pkg_name                pkg_uri
+epel-release-6-7          http://ftp.riken.go.jp/pub/Linux/fedora/epel/6/i386/epel-release-6-7.noarch.rpm
+rabbitmq-server-2.6.1     http://www.rabbitmq.com/releases/rabbitmq-server/v2.6.1/rabbitmq-server-2.6.1-1.noarch.rpm
+flog                      ${vdc_current_base_url}/flog-1.8-3.${basearch}.rpm
+openvswitch               ${vdc_current_base_url}/kmod-openvswitch-1.4.1-1.el6.${arch}.rpm
+openvswitch               ${vdc_current_base_url}/openvswitch-1.4.1-1.${arch}.rpm
+kmod-openvswitch-vzkernel ${vdc_current_base_url}/kmod-openvswitch-vzkernel-1.4.1-1.el6.${arch}.rpm
+lxc                       ${vdc_current_base_url}/lxc-0.7.5-1.${arch}.rpm
+EOS
+}
+
 function prepare_3rd_party() {
   rpm -qi curl >/dev/null || yum install -y curl
-  list_3rd_party | while read pkg_name pkg_uri pkg_file; do
+  list_3rd_party_builder | while read pkg_name pkg_uri pkg_file; do
     echo downloading ${pkg_name} ...
     case ${pkg_uri} in
     git://*)
@@ -62,7 +74,7 @@ function prepare_3rd_party() {
 }
 
 function build_3rd_party() {
-  list_3rd_party | while read pkg_name pkg_uri pkg_file; do
+  list_3rd_party_builder | while read pkg_name pkg_uri pkg_file; do
     case ${pkg_uri} in
     git://*)
       (cd ${vendor_dir}/$(basename ${pkg_uri%%.git})/rhel/6/${pkg_name} && BUILD_ARCH=${arch} make build)
@@ -72,7 +84,7 @@ function build_3rd_party() {
 }
 
 function deploy_3rd_party() {
-  list_3rd_party | while read pkg_name pkg_uri pkg_file; do
+  list_3rd_party_builder | while read pkg_name pkg_uri pkg_file; do
     case ${pkg_name} in
     flog)
       cp ${vendor_dir}/$(basename ${pkg_uri%%.git})/rhel/6/${pkg_name}/${pkg_file} ${vendor_dir}/.
@@ -90,15 +102,16 @@ function deploy_3rd_party() {
 function download_3rd_party() {
   rpm -qi curl >/dev/null || yum install -y curl
   [ -f ${vendor_dir}/openvz.repo ] || {
-    curl http://download.openvz.org/openvz.repo > ${vendor_dir}/openvz.repo
+    curl -R http://download.openvz.org/openvz.repo -o ${vendor_dir}/openvz.repo
   }
 
-  list_3rd_party | while read pkg_name pkg_uri pkg_file; do
+  list_3rd_party | while read pkg_name pkg_uri; do
+    pkg_file=$(basename ${pkg_uri})
     echo downloading ${pkg_name} ...
     case ${pkg_uri} in
     http://*)
       [ -f ${vendor_dir}/${pkg_file} ] || {
-        curl ${pkg_uri} > ${vendor_dir}/${pkg_file}
+        curl -R ${pkg_uri} -o ${vendor_dir}/${pkg_file}
       }
       ;;
     esac
@@ -125,9 +138,9 @@ deploy)
   deploy_3rd_party
   ;;
 download)
-  prepare_3rd_party
-  build_3rd_party
-  deploy_3rd_party
+  # prepare_3rd_party
+  # build_3rd_party
+  # deploy_3rd_party
   download_3rd_party
   ;;
 install)
