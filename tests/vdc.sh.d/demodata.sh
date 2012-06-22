0393c3bb53704144023a3118d5e5dd76432ce734
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-06-22 18:31:44 +0900

    feed vdc-manage subcommands from STDIN.

--- tests/vdc.sh.d/demodata.sh
+++ tests/vdc.sh.d/demodata.sh
@@ -31,70 +31,6 @@ cd ${VDC_ROOT}/dcmgr/
 
 (. $data_path/demodata_images.sh)
 
-# Physical network definitions
-shlog ./bin/vdc-manage network dc add public
-shlog ./bin/vdc-manage network dc add-network-mode public securitygroup
-shlog ./bin/vdc-manage network dc del-network-mode public passthru
-# bridge only closed network
-shlog ./bin/vdc-manage network dc add null1
-shlog ./bin/vdc-manage network dc add-network-mode null1 l2overlay
-shlog ./bin/vdc-manage network dc add null2
-shlog ./bin/vdc-manage network dc add-network-mode null2 l2overlay
-
-# vlan
-#shlog ./bin/vdc-manage vlan    add --tag-idb 1      --uuid vlan-demo1    --account-id ${account_id}
-#shlog ./bin/vdc-manage network add           --uuid   nw-demo1    --ipv4-gw ${ipv4_gw} --prefix ${prefix_len} --domain vdc.local --dns ${dns_server} --dhcp ${dhcp_server} --metadata ${metadata_server} --metadata-port ${metadata_port} --vlan-id 1 --description demo
-# non vlan
-shlog ./bin/vdc-manage network add \
- --uuid nw-demo1 \
- --ipv4-network ${ipv4_gw} \
- --ipv4_gw ${ipv4_gw} \
- --prefix ${prefix_len} \
- --domain vdc.local \
- --dns ${dns_server} \
- --dhcp ${dhcp_server} \
- --metadata ${metadata_server} \
- --metadata-port ${metadata_port} \
- --service-type std \
- --description "'demo'" \
- --display-name "'demo1'"
-shlog ./bin/vdc-manage network add \
- --uuid nw-demo2 --ipv4-network 10.100.0.0 --prefix 24 --domain vdc.local --metric 10 --service-type std --display-name "'demo2'"
-shlog ./bin/vdc-manage network add \
- --uuid nw-demo3 --ipv4-network 10.101.0.0 --prefix 24 --domain vdc.local --metric 10 --service-type std --display-name "'demo3'"
-shlog ./bin/vdc-manage network add \
- --uuid nw-demo4 --ipv4-network 10.100.0.0 --prefix 24 --domain vdc.local --metric 10 --service-type std --display-name "'demo4'"
-shlog ./bin/vdc-manage network add \
- --uuid nw-demo5 --ipv4-network 10.101.0.0 --prefix 24 --domain vdc.local --metric 10 --service-type std --display-name "'demo5'"
-shlog ./bin/vdc-manage network add \
- --uuid nw-demo6 \
- --network-mode l2overlay \
- --ipv4-network 10.102.0.0 \
- --ipv4_gw 10.102.0.1 \
- --prefix 24 \
- --domain vnet6.local \
- --metric 10 \
- --service-type std \
- --display-name "'demo6'"
-shlog ./bin/vdc-manage network add \
- --uuid nw-demo7 \
- --network-mode l2overlay \
- --ipv4-network 10.103.0.0 \
- --ipv4_gw 10.103.0.1 \
- --prefix 24 \
- --domain vnet7.local \
- --metric 10 \
- --service-type std \
- --display-name "'demo7'"
-
-# set forward interface(= physical network) from network
-shlog ./bin/vdc-manage network forward nw-demo1 public
-shlog ./bin/vdc-manage network forward nw-demo2 public
-shlog ./bin/vdc-manage network forward nw-demo3 public
-shlog ./bin/vdc-manage network forward nw-demo4 null1
-shlog ./bin/vdc-manage network forward nw-demo5 null2
-shlog ./bin/vdc-manage network forward nw-demo6 null1
-shlog ./bin/vdc-manage network forward nw-demo7 null1
 
 [ -f /etc/redhat-release ] && {
   # rhel
@@ -108,36 +44,109 @@ shlog ./bin/vdc-manage network forward nw-demo7 null1
   [ -n "${range_end}"   ] || range_end=`ipcalc ${ipv4_gw}/${prefix_len} | awk '$1 == "HostMax:" { print $2 }'`
 }
 
-shlog ./bin/vdc-manage network service dhcp nw-demo6 --ipv4=10.102.0.2
-shlog ./bin/vdc-manage network service dhcp nw-demo7 --ipv4=10.103.0.2
-shlog ./bin/vdc-manage network service dns nw-demo7 --ipv4=10.103.0.2
+cat <<CMDSET | grep -v '^#' | ./bin/vdc-manage
+# Physical network definitions
+network dc add public
+network dc add-network-mode public securitygroup
+network dc del-network-mode public passthru
+# bridge only closed network
+network dc add null1
+network dc add-network-mode null1 l2overlay
+network dc add null2
+network dc add-network-mode null2 l2overlay
 
-shlog ./bin/vdc-manage network dhcp addrange nw-demo1 $range_begin $range_end
-shlog ./bin/vdc-manage network dhcp addrange nw-demo2 10.100.0.61 10.100.0.65
-shlog ./bin/vdc-manage network dhcp addrange nw-demo2 10.100.0.70 10.100.0.75
-shlog ./bin/vdc-manage network dhcp addrange nw-demo2 10.100.0.68 10.100.0.75 # range prepend
-shlog ./bin/vdc-manage network dhcp addrange nw-demo2 10.100.0.72 10.100.0.80 # range append
-shlog ./bin/vdc-manage network dhcp addrange nw-demo2 10.100.0.60 10.100.0.80 # range merge
-shlog ./bin/vdc-manage network dhcp addrange nw-demo3 10.101.0.60 10.101.0.80
-shlog ./bin/vdc-manage network dhcp addrange nw-demo4 10.100.0.100 10.100.0.130
-shlog ./bin/vdc-manage network dhcp addrange nw-demo5 10.101.0.100 10.101.0.130
-shlog ./bin/vdc-manage network dhcp addrange nw-demo6 10.102.0.10 10.102.0.240
-shlog ./bin/vdc-manage network dhcp addrange nw-demo7 10.103.0.10 10.103.0.240
+# vlan
+#vlan    add --tag-idb 1      --uuid vlan-demo1    --account-id ${account_id}
+#network add           --uuid   nw-demo1    --ipv4-gw ${ipv4_gw} --prefix ${prefix_len} --domain vdc.local --dns ${dns_server} --dhcp ${dhcp_server} --metadata ${metadata_server} --metadata-port ${metadata_port} --vlan-id 1 --description demo
+# non vlan
+network add \
+ --uuid nw-demo1 \
+ --ipv4-network ${ipv4_gw} \
+ --ipv4_gw ${ipv4_gw} \
+ --prefix ${prefix_len} \
+ --domain vdc.local \
+ --dns ${dns_server} \
+ --dhcp ${dhcp_server} \
+ --metadata ${metadata_server} \
+ --metadata-port ${metadata_port} \
+ --service-type std \
+ --description "demo" \
+ --display-name "demo1"
+network add \
+ --uuid nw-demo2 --ipv4-network 10.100.0.0 --prefix 24 --domain vdc.local --metric 10 --service-type std --display-name "'demo2'"
+network add \
+ --uuid nw-demo3 --ipv4-network 10.101.0.0 --prefix 24 --domain vdc.local --metric 10 --service-type std --display-name "'demo3'"
+network add \
+ --uuid nw-demo4 --ipv4-network 10.100.0.0 --prefix 24 --domain vdc.local --metric 10 --service-type std --display-name "'demo4'"
+network add \
+ --uuid nw-demo5 --ipv4-network 10.101.0.0 --prefix 24 --domain vdc.local --metric 10 --service-type std --display-name "'demo5'"
+network add \
+ --uuid nw-demo6 \
+ --network-mode l2overlay \
+ --ipv4-network 10.102.0.0 \
+ --ipv4_gw 10.102.0.1 \
+ --prefix 24 \
+ --domain vnet6.local \
+ --metric 10 \
+ --service-type std \
+ --display-name "demo6"
+network add \
+ --uuid nw-demo7 \
+ --network-mode l2overlay \
+ --ipv4-network 10.103.0.0 \
+ --ipv4_gw 10.103.0.1 \
+ --prefix 24 \
+ --domain vnet7.local \
+ --metric 10 \
+ --service-type std \
+ --display-name "demo7"
 
-shlog ./bin/vdc-manage resourcegroup map hng-shhost hn-${node_id}
-shlog ./bin/vdc-manage resourcegroup map sng-shstor sn-${node_id}
-shlog ./bin/vdc-manage resourcegroup map nwg-shnet  nw-demo1
+# set forward interface(= physical network) from network
+network forward nw-demo1 public
+network forward nw-demo2 public
+network forward nw-demo3 public
+network forward nw-demo4 null1
+network forward nw-demo5 null2
+network forward nw-demo6 null1
+network forward nw-demo7 null1
 
-shlog ./bin/vdc-manage network reserve nw-demo1 --ipv4=${ipaddr}
+network service dhcp nw-demo6 --ipv4=10.102.0.2
+network service dhcp nw-demo7 --ipv4=10.103.0.2
+network service dns nw-demo7 --ipv4=10.103.0.2
 
-shlog ./bin/vdc-manage spec  add --uuid is-demospec --account-id ${account_id} --arch ${hva_arch} --hypervisor ${hypervisor} --cpu-cores 1 --memory-size 256 --quota-weight 1
-shlog ./bin/vdc-manage spec  add --uuid is-demo2    --account-id ${account_id} --arch ${hva_arch} --hypervisor ${hypervisor} --cpu-cores 2 --memory-size 256 --quota-weight 1
+network dhcp addrange nw-demo1 $range_begin $range_end
+network dhcp addrange nw-demo2 10.100.0.61 10.100.0.65
+network dhcp addrange nw-demo2 10.100.0.70 10.100.0.75
+# range prepend
+network dhcp addrange nw-demo2 10.100.0.68 10.100.0.75
+# range append
+network dhcp addrange nw-demo2 10.100.0.72 10.100.0.80
+# range merge
+network dhcp addrange nw-demo2 10.100.0.60 10.100.0.80
+network dhcp addrange nw-demo3 10.101.0.60 10.101.0.80
+network dhcp addrange nw-demo4 10.100.0.100 10.100.0.130
+network dhcp addrange nw-demo5 10.101.0.100 10.101.0.130
+network dhcp addrange nw-demo6 10.102.0.10 10.102.0.240
+network dhcp addrange nw-demo7 10.103.0.10 10.103.0.240
+
+resourcegroup map hng-shhost hn-${node_id}
+resourcegroup map sng-shstor sn-${node_id}
+resourcegroup map nwg-shnet  nw-demo1
+
+network reserve nw-demo1 --ipv4=${ipaddr}
+
+spec  add --uuid is-demospec --account-id ${account_id} --arch ${hva_arch} --hypervisor ${hypervisor} --cpu-cores 1 --memory-size 256 --quota-weight 1
+spec  add --uuid is-demo2    --account-id ${account_id} --arch ${hva_arch} --hypervisor ${hypervisor} --cpu-cores 2 --memory-size 256 --quota-weight 1
 # BEGIN: Temporary add below two lines during instance spec migration.
-shlog ./bin/vdc-manage spec  add --uuid is-small --account-id ${account_id} --arch ${hva_arch} --hypervisor ${hypervisor} --cpu-cores 1 --memory-size 256 --quota-weight 1
-shlog ./bin/vdc-manage spec  add --uuid is-large --account-id ${account_id} --arch ${hva_arch} --hypervisor ${hypervisor} --cpu-cores 1 --memory-size 256 --quota-weight 2
+spec  add --uuid is-small --account-id ${account_id} --arch ${hva_arch} --hypervisor ${hypervisor} --cpu-cores 1 --memory-size 256 --quota-weight 1
+spec  add --uuid is-large --account-id ${account_id} --arch ${hva_arch} --hypervisor ${hypervisor} --cpu-cores 1 --memory-size 256 --quota-weight 2
 # END: Temporary add below two lines during instance spec migration.
-shlog ./bin/vdc-manage spec  addvif is-demo2 eth1
-shlog ./bin/vdc-manage spec  addvif is-demo2 eth2
+spec  addvif is-demo2 eth1
+spec  addvif is-demo2 eth2
+
+keypair add --account-id ${account_id} --uuid ssh-demo --private-key=$data_path/pri.pem --public-key=$data_path/pub.pem --description "'demo key1'" --service-type std --display-name "'demo'"
+
+CMDSET
 
 shlog ./bin/vdc-manage securitygroup add --uuid  sg-demofgr --account-id ${account_id} --description demo --service-type std --display-name demo
 shlog ./bin/vdc-manage securitygroup modify sg-demofgr --rule=- <<EOF
@@ -154,8 +163,6 @@ tcp:80,80,ip4:0.0.0.0
 udp:53,53,ip4:0.0.0.0
 icmp:-1,-1,ip4:0.0.0.0
 EOF
-
-shlog ./bin/vdc-manage keypair add --account-id ${account_id} --uuid ssh-demo --private-key=$data_path/pri.pem --public-key=$data_path/pub.pem --description "'demo key1'" --service-type std --display-name "'demo'"
 
 cat <<EOS | mysql -uroot ${dcmgr_dbname}
 INSERT INTO volume_snapshots values
