fab5dfc082e4377118d8dbd8eb62c396b9ade71c
t-iwano <t-iwano@axsh.net>
2012-06-29 21:28:58 +0900

    fix testcase of storage node

--- dcmgr/lib/dcmgr/endpoints/12.03/storage_nodes.rb
+++ dcmgr/lib/dcmgr/endpoints/12.03/storage_nodes.rb
@@ -20,28 +20,47 @@ Dcmgr::Endpoints::V1203::CoreAPI.namespace '/storage_nodes' do
   
   get '/:id' do
     # description 'Show the storage_pool status'
-    # params id, string, required
+    # param :id, :string, :required
     pool_id = params[:id]
     raise E::UndefinedStorageNodeID if pool_id.nil?
-    sn = find_by_uuid(:StorageNode, pool_id)
+    sn = find_by_public_uuid(:StorageNode, pool_id)
     raise E::UnknownStorageNode if sn.nil?
     respond_with(R::StorageNode.new(sn).generate)
   end
   
   post do
+    # description 'Create a new storage node'
+    # param :id, :string, :required
+    # param :export_path, :string, :required
+    # param :offering_disk_space_mb, :int, :required
+    # param :transport_type, :string, :required
+    # param :storage_type, :string, :required
+    # param :ipaddr, :string, :required
+    # param :snapshot_base_path, :string, :required
+    params.delete(:account_id) if params[:account_id]
     sn = M::StorageNode.create(params)
     respond_with(R::StorageNode.new(sn).generate)
   end
   
   delete '/:id' do
-    sn = find_by_uuid(:StorageNode, params[:id])
+    # description 'Delete storage node'
+    # param :id, :string, :required
+    sn = find_by_public_uuid(:StorageNode, params[:id])
     raise E::UnknownStorageNode if sn.nil?
     sn.destroy
     respond_with(R::StorageNode.new(sn).generate)
   end
 
   put '/:id' do
-    sn = find_by_uuid(:StorageNode, params[:id])
+    # description 'Update storage node'
+    # param :id, string, required
+    # param :export_path, string, optional
+    # param :offering_disk_space_mb, int, optional
+    # param :transport_type, string, optional
+    # param :storage_type, string, optional
+    # param :ipaddr, string, optional
+    # param :snapshot_base_path, string, optional
+    sn = find_by_public_uuid(:StorageNode, params[:id])
     raise E::UnknownStorageNode if sn.nil?
 
     changed = {}
