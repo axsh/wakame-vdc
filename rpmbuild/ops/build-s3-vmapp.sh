0ea75d815dd98d4645d6cc076720fd9f4d9b8282
Masahito Yoshida <masahito@axsh.net>
2012-07-06 00:28:51 +0900

    rpmbuild/ops: add --dry-run option so far.

--- rpmbuild/ops/build-s3-vmapp.sh
+++ rpmbuild/ops/build-s3-vmapp.sh
@@ -4,10 +4,12 @@ set -e
 set -x
 
 . ./config_s3.env
+vmapp_dir=pool/vmapp/
+
 ./pickup-vmapp.sh
 
 find ${vmapp_dir} -type f -name "wakame-*" -mtime +5 | sort | while read line; do
   rm -f ${line}
 done
 
-time s3cmd sync ${vmapp_dir} ${s3_repo_uri} --delete-removed --acl-public --check-md5
+time s3cmd sync ${vmapp_dir} ${s3_repo_uri} --acl-public --check-md5 --dry-run
