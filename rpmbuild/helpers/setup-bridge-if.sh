a2fe0c690443658ff27dd1110e294f0798f790d7
Masahito Yoshida <masahito@axsh.net>
2012-07-09 23:03:10 +0900

    rpmbuild/helpers: add setup-bridge-if.sh.

---
+++ rpmbuild/helpers/setup-bridge-if.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# OPTIONS
+#
+#  --brname=br0
+#  --ifname=eth0
+#
+#  --ip=192.0.2.10
+#  --mask=255.255.255.0
+#  --net=192.0.2.0
+#  --bcast=192.0.2.255
+#  --gw=192.0.2.1
+#
+set -e
+#set -x
+
+args=
+while [ $# -gt 0 ]; do
+  arg="$1"
+  case "${arg}" in
+    --*=*)
+      key=${arg%%=*}; key=$(echo ${key##--} | tr - _)
+      value=${arg##--*=}
+      eval "${key}=\"${value}\""
+      ;;
+    *)
+      args="${args} ${arg}"
+      ;;
+  esac
+  shift
+done
+
+#
+brname=${brname:-br0}
+ifname=${ifname:-eth0}
+
+ip=${ip}
+mask=${mask:-}
+net=${net:-}
+bcast=${bcast:-}
+gw=${gw:-}
+
+ifcfg_prefix=/etc/sysconfig/network-scripts/ifcfg
+
+
+
+#
+# ifcfg-brX
+#
+[ -z "${ip}" ] && {
+  BOOTPROTO=dhcp
+} || {
+  BOOTPROTO=static
+}
+
+cat <<EOS > ${ifcfg_prefix}-${brname}
+DEVICE=${brname}
+TYPE=Bridge
+ONBOOT=yes
+
+BOOTPROTO=${BOOTPROTO}
+$([ -z "${ip}"    ] || echo "IPADDR=${ip}")
+$([ -z "${net}"   ] || echo "NETMASK=${net}")
+$([ -z "${bcast}" ] || echo "BROADCAST=${bcast}")
+$([ -z "${gw}"    ] || echo "GATEWAY=${gw}")
+EOS
+echo ">>> ${ifcfg_prefix}-${brname}"
+cat ${ifcfg_prefix}-${brname}
+
+#
+# ifcfg-ethX
+#
+cat <<EOS > ${ifcfg_prefix}-${ifname}
+DEVICE=${ifname}
+ONBOOT=yes
+BOOTPROTO=static
+
+BRIDGE=${brname}
+TYPE=Ethernet
+EOS
+echo ">>> ${ifcfg_prefix}-${ifname}"
+cat ${ifcfg_prefix}-${ifname}
