4a307374ef79bc03d4c08877dc398281482f3946
Masahito Yoshida <masahito@axsh.net>
2012-07-11 02:44:45 +0900

    rpmbuild/ops: add user/rpmbuild to consider sudo. revert 4eda8be.

--- rpmbuild/ops/createrepo-vdc.sh
+++ rpmbuild/ops/createrepo-vdc.sh
@@ -8,17 +8,16 @@ set -x
 
 [ -d ${rpm_dir} ] || mkdir -p ${rpm_dir}
 
-for arch in ${archs} noarch; do
+for arch in ${archs}; do
   case ${arch} in
   i*86)   basearch=i386; arch=i686;;
   x86_64) basearch=${arch};;
-  noarch) basearch=noarch;;
   esac
 
   chroot_dir=$(cd ../../ && pwd)/tmp/vmapp_builder/chroot/dest/centos-6_${arch}
 
   #
-  # arch, basearch, noarch
+  # arch, basearch
   #
   [ -d ${rpm_dir}/${basearch} ] || mkdir -p ${rpm_dir}/${basearch}
   subdirs="
@@ -29,6 +28,19 @@ for arch in ${archs} noarch; do
   for subdir in ${subdirs}; do
     pkg_dir=${chroot_dir}/${subdir}
     bash -c "[ -d ${pkg_dir} ] && rsync -av --exclude=epel-* ${pkg_dir}/*.rpm ${rpm_dir}/${basearch}/ || :"
+  done
+
+  #
+  # noarch
+  #
+  [ -d ${rpm_dir}/noarch ] || mkdir -p ${rpm_dir}/noarch
+  subdirs="
+    root/rpmbuild/RPMS/noarch
+    ${HOME}/rpmbuild/RPMS/noarch
+  "
+  for subdir in ${subdirs}; do
+    pkg_dir=${chroot_dir}/${subdir}
+    bash -c "[ -d ${pkg_dir} ] && rsync -av --exclude=epel-* ${pkg_dir}/*.rpm ${rpm_dir}/noarch/ || :"
   done
 done
 
