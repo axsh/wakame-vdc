ce7c003dbeaf005c4fa26231ee44103de8a87757
Masahito Yoshida <masahito@axsh.net>
2012-07-11 01:09:07 +0900

    pmbuild/ops: cleanup codes.

--- rpmbuild/ops/createrepo-vdc.sh
+++ rpmbuild/ops/createrepo-vdc.sh
@@ -8,37 +8,27 @@ set -x
 
 [ -d ${rpm_dir} ] || mkdir -p ${rpm_dir}
 
-for arch in ${archs}; do
+for arch in ${archs} noarch; do
   case ${arch} in
   i*86)   basearch=i386; arch=i686;;
   x86_64) basearch=${arch};;
+  noarch) basearch=noarch;;
   esac
 
   chroot_dir=$(cd ../../ && pwd)/tmp/vmapp_builder/chroot/dest/centos-6_${arch}
 
   #
-  # arch, basearch
+  # arch, basearch, noarch
   #
   [ -d ${rpm_dir}/${basearch} ] || mkdir -p ${rpm_dir}/${basearch}
   subdirs="
     tmp/wakame-vdc/tests/vdc.sh.d/rhel/vendor/${basearch}
     root/rpmbuild/RPMS/${arch}
+    ${HOME}/rpmbuild/RPMS/${arch}
   "
   for subdir in ${subdirs}; do
     pkg_dir=${chroot_dir}/${subdir}
     bash -c "[ -d ${pkg_dir} ] && rsync -av --exclude=epel-* ${pkg_dir}/*.rpm ${rpm_dir}/${basearch}/ || :"
-  done
-
-  #
-  # noarch
-  #
-  [ -d ${rpm_dir}/noarch ] || mkdir -p ${rpm_dir}/noarch
-  subdirs="
-    root/rpmbuild/RPMS/noarch
-  "
-  for subdir in ${subdirs}; do
-    pkg_dir=${chroot_dir}/${subdir}
-    bash -c "[ -d ${pkg_dir} ] && rsync -av --exclude=epel-* ${pkg_dir}/*.rpm ${rpm_dir}/noarch/ || :"
   done
 done
 
