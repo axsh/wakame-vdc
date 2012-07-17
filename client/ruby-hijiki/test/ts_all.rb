d0703e6aac830e99520ca9b1af7828b99744d738
rakshasa <sundell.software@gmail.com>
2012-07-17 22:05:53 +0900

    Fixed initialization of hijiki test cases.

--- client/ruby-hijiki/test/ts_all.rb
+++ client/ruby-hijiki/test/ts_all.rb
@@ -4,11 +4,28 @@ $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 
 require 'hijiki'
 
-ActiveResource::Base.class_eval do 
-  self.site = 'http://localhost:9001'
+Hijiki.load(File.join(File.dirname(__FILE__), '..', '..', '..', 'frontend', 'dcmgr_gui', 'config', 'instance_spec.yml'))
+
+Hijiki::DcmgrResource.setup_aliases(:V1203)
+
+Hijiki::RequestAttribute.configure do |c|
+  c.service_type = "std"
+  c.quota_header do |req_attr|
+    {}
+  end
 end
 
-ActiveResource::Connection.set_vdc_account_uuid('a-shpoolxx')
+Thread.current[:hijiki_request_attribute] = Hijiki::RequestAttribute.new('a-shpoolxx')
+
+ActiveResource::Base.class_eval do 
+  begin
+    @dcmgr_gui_config = YAML::load(IO.read(File.join(File.dirname(__FILE__), '..', '..', '..', 'frontend', 'dcmgr_gui', 'config', 'dcmgr_gui.yml')))['development']
+  rescue Errno::ENOENT => e
+    p e.message
+    exit 1
+  end
+  self.site = @dcmgr_gui_config['dcmgr_site'] 
+end
 
 require File.join(File.dirname(__FILE__) + '/api/ts_base.rb')
 
@@ -17,3 +34,4 @@ require File.join(File.dirname(__FILE__) + '/api/ts_base.rb')
     require f
   end
 end
+
