b83c41251a308d355d96b0b1c2690c79eb86618b
saicologic <s-mikami@axsh.net>
2012-07-19 15:42:05 +0900

    added to require publish_message

--- dcmgr/lib/dcmgr/endpoints/12.03/core_api.rb
+++ dcmgr/lib/dcmgr/endpoints/12.03/core_api.rb
@@ -6,6 +6,7 @@ require 'sinatra/base'
 require 'sinatra/dcmgr_api_setup'
 require 'sinatra/quota_evaluation'
 require 'sinatra/internal_request'
+require 'sinatra/publish_message'
 
 require 'dcmgr/endpoints/errors'
 require 'dcmgr/endpoints/12.03/quota_definitions'
@@ -16,6 +17,7 @@ module Dcmgr::Endpoints::V1203
     register Sinatra::DcmgrAPISetup
     register Sinatra::InternalRequest
     register Sinatra::QuotaEvaluation
+    register Sinatra::PublishMessage
 
     # To access constants in this namespace
     include Dcmgr::Endpoints
