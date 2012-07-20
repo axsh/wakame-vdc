2ad5ecf5c348807c23e15d60824e774608903166
t-iwano <t-iwano@axsh.net>
2012-07-20 18:52:42 +0900

    Modified so as not to leave detail information when create and delete.

--- frontend/dcmgr_gui/public/javascripts/dcmgr_gui/security_group_panel.js
+++ frontend/dcmgr_gui/public/javascripts/dcmgr_gui/security_group_panel.js
@@ -148,22 +148,15 @@ DcmgrGUI.prototype.securityGroupPanel = function(){
   var bt_refresh  = new DcmgrGUI.Refresh();
 
   bt_refresh.element.bind('dcmgrGUI.refresh',function(){
-    
     //Update list element
     c_list.page = c_pagenate.current_page;
     list_request.url = DcmgrGUI.Util.getPagePath('/security_groups/list/',c_list.page);
     list_request.data = DcmgrGUI.Util.getPagenateData(c_pagenate.start,c_pagenate.row);
     c_list.element.trigger('dcmgrGUI.updateList',{request:list_request})
-    
-    $.each(c_list.checked_list,function(check_id,obj){
-      //All remove detail element
-      $($('#detail').find('#'+check_id)).remove();
-      
-      //All reload detail element
-      c_list.checked_list[check_id].c_detail.update({
-        url:DcmgrGUI.Util.getPagePath('/security_groups/show/',check_id)
-      },true);
-    });
+
+    var check_id = c_list.currentChecked();
+    //remove detail element
+    $($('#detail').find('#'+check_id)).remove();
   });
   
   c_pagenate.element.bind('dcmgrGUI.updatePagenate',function(){
