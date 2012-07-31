f71eebb52e41ed5ff1ad72465a759480bde2d346
rakshasa <sundell.software@gmail.com>
2012-07-31 19:01:27 +0900

    Fixed Hijiki network api calls.

--- client/ruby-hijiki/lib/hijiki/request/12.03/networks.rb
+++ client/ruby-hijiki/lib/hijiki/request/12.03/networks.rb
@@ -7,24 +7,27 @@ module Hijiki::Request::V1203
     include Hijiki::Request::Common::Helpers
     namespace 'networks', '12.03'
 
-    get :get, ''
-    get :find, '/:id'
+    get :get, '.{format}' do |resource|
+      resource.optional :limit, :service_type, :start
+    end
 
-    delete :delete, '/:id'
-    put :put, '/:id'
+    get :find, '/:id.{format}'
 
-    put :dhcp_reserve, '/:id/dhcp/reserve'
-    put :dhcp_release, '/:id/dhcp/release'
+    delete :delete, '/:id.{format}'
+    post :post, '.{format}'
 
-    get :vifs_get, '/:id/vifs'
-    get :vifs_find, '/:id/vifs/:vif_id'
+    put :dhcp_reserve, '/:id/dhcp/reserve.{format}'
+    put :dhcp_release, '/:id/dhcp/release.{format}'
 
-    post :vifs_post, '/:id/vifs'
-    delete :vifs_delete, '/:id/vifs/:vif_id'
+    get :vifs_get, '/:id/vifs.{format}'
+    get :vifs_find, '/:id/vifs/:vif_id.{format}'
 
-    put :vifs_attach, '/:id/vifs/:vif_id/attach'
-    put :vifs_detach, '/:id/vifs/:vif_id/detach'
-    
+    post :vifs_post, '/:id/vifs.{format}'
+    delete :vifs_delete, '/:id/vifs/:vif_id.{format}'
+
+    put :vifs_attach, '/:id/vifs/:vif_id/attach.{format}'
+    put :vifs_detach, '/:id/vifs/:vif_id/detach.{format}'
+
   end
 
 end
