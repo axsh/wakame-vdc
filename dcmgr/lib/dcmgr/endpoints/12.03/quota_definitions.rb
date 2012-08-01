8c7824205650060cfadb92ef8fea86e5c8685ba8
saicologic <s-mikami@axsh.net>
2012-08-01 16:51:40 +0900

    fixed evaluation.

--- dcmgr/lib/dcmgr/endpoints/12.03/quota_definitions.rb
+++ dcmgr/lib/dcmgr/endpoints/12.03/quota_definitions.rb
@@ -4,30 +4,30 @@ require 'sinatra/quota_evaluation'
 
 Sinatra::QuotaEvaluation.evaluators do
   quota_key 'security_group.count' do
-    quota_value.to_i < M::SecurityGroup.filter(:account_id=>@account.canonical_uuid).count
+    quota_value.to_i <= M::SecurityGroup.filter(:account_id=>@account.canonical_uuid).count
   end
   quota_key 'ssh_key_pair.count' do
-    quota_value.to_i < M::SshKeyPair.filter(:account_id=>@account.canonical_uuid).count
+    quota_value.to_i <= M::SshKeyPair.filter(:account_id=>@account.canonical_uuid).count
   end
   quota_key 'instance.quota_weight' do
-    quota_value.to_f < (M::Instance.lives.filter(:account_id=>@account.canonical_uuid).sum(:quota_weight) || 0.0)
+    quota_value.to_f <= (M::Instance.lives.filter(:account_id=>@account.canonical_uuid).sum(:quota_weight) || 0.0)
   end
   quota_key 'instance.count' do
-    quota_value.to_i < M::Instance.lives.filter(:account_id=>@account.canonical_uuid).count
+    quota_value.to_i <= M::Instance.lives.filter(:account_id=>@account.canonical_uuid).count
   end
   quota_key 'volume.count' do
-    quota_value.to_i < M::Volume.filter(:account_id=>@account.canonical_uuid).count
+    quota_value.to_i <= M::Volume.filter(:account_id=>@account.canonical_uuid).count
   end
   quota_key 'volume.size' do
-    quota_value.to_i < (M::Volume.filter(:account_id=>@account.canonical_uuid).sum(:volume_size) || 0)
+    quota_value.to_i <= (M::Volume.filter(:account_id=>@account.canonical_uuid).sum(:volume_size) || 0)
   end
   quota_key 'image.count' do
-    quota_value.to_i < M::Image.filter(:account_id=>@account.canonical_uuid).count
+    quota_value.to_i <= M::Image.filter(:account_id=>@account.canonical_uuid).count
   end
   quota_key 'backup_object.count' do
-    quota_value.to_i < M::BackupObject.filter(:account_id=>@account.canonical_uuid).count
+    quota_value.to_i <= M::BackupObject.filter(:account_id=>@account.canonical_uuid).count
   end
   quota_key 'backup_object.size' do
-    quota_value.to_i < (M::BackupObject.filter(:account_id=>@account.canonical_uuid).sum(:size) || 0)
+    quota_value.to_i <= (M::BackupObject.filter(:account_id=>@account.canonical_uuid).sum(:size) || 0)
   end
 end
