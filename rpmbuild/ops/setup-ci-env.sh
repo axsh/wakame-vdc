d5966309102263a8a1716a291c98ccbe63aaf4f2
Masahito Yoshida <masahito@axsh.net>
2012-08-05 14:09:51 +0900

    rpmbuild/ops/setup-ci-env.sh: delete remote repository dependency.

--- rpmbuild/ops/setup-ci-env.sh
+++ rpmbuild/ops/setup-ci-env.sh
@@ -6,30 +6,42 @@ set -x
 LANG=C
 LC_ALL=C
 
-repo_uri=${repo_uri:-git://github.com/axsh/wakame-vdc.git}
-ci_dir=~/work/ci/
-[ -d ${ci_dir} ] || mkdir -p ${ci_dir}
-cd ${ci_dir}
+abs_path=$(cd $(dirname $0) && pwd)
 
-[ -d wakame-vdc ] || git clone ${repo_uri} wakame-vdc
+function update_repo() {
+  git pull
+}
 
-cd wakame-vdc
-git pull
+function setup_chroot_dir() {
+  cd ${abs_path}/../../
+  [ -d tmp/vmapp_builder/chroot/base ] || mkdir -p tmp/vmapp_builder/chroot/base/
+  cd   tmp/vmapp_builder/chroot/base
 
-[ -d tmp/vmapp_builder/chroot/base ] || mkdir -p tmp/vmapp_builder/chroot/base/
-cd   tmp/vmapp_builder/chroot/base
+  distro_name="centos"
+  distro_relver="6"
+  distro_subver="3"
+  distro_ver="${distro_relver}"
 
-distro_name="centos"
-distro_relver="6"
-distro_subver="3"
-distro_ver="${distro_relver}"
+  distro="${distro_name}-${distro_ver}"
+  distro_detail="${distro_name}-${distro_ver}.${distro_subver}"
 
-distro="${distro_name}-${distro_ver}"
-distro_detail="${distro_name}-${distro_ver}.${distro_subver}"
+  archs="i686 x86_64"
+  for arch in ${archs}; do
+    [ -f ${distro_detail}_${arch}.tar.gz ] || curl -R -O http://dlc.wakame.axsh.jp.s3.amazonaws.com/demo/rootfs-tree/${distro_detail}_${arch}.tar.gz
+    [ -d ${distro_detail}_${arch}        ] || sudo tar zxvpf ${distro_detail}_${arch}.tar.gz
+    [ -d ${distro}_${arch}               ] || sudo mv ${distro_detail}_${arch} ${distro}_${arch}
+  done
+}
 
-archs="i686 x86_64"
-for arch in ${archs}; do
-  [ -f ${distro_detail}_${arch}.tar.gz ] || s3cmd get s3://dlc.wakame.axsh.jp/demo/rootfs-tree/${distro_detail}_${arch}.tar.gz
-  [ -d ${distro_detail}_${arch}        ] || sudo tar zxvpf ${distro_detail}_${arch}.tar.gz
-  [ -d ${distro}_${arch}               ] || sudo mv ${distro_detail}_${arch} ${distro}_${arch}
-done
+case $1 in
+update_repo)
+  $1
+  ;;
+setup_chroot_dir)
+  $1
+  ;;
+*)
+  update_repo
+  setup_chroot_dir
+  ;;
+esac
