cfbc7b3f356069714498d17ba771969081d6b424
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-08-09 20:49:41 +0900

    dcmgr: change quota definition syntax.

--- dcmgr/lib/dcmgr/endpoints/12.03/quota_definitions.rb
+++ dcmgr/lib/dcmgr/endpoints/12.03/quota_definitions.rb
@@ -3,31 +3,94 @@
 require 'sinatra/quota_evaluation'
 
 Sinatra::QuotaEvaluation.evaluators do
-  quota_key 'security_group.count' do
-    quota_value.to_i < M::SecurityGroup.filter(:account_id=>@account.canonical_uuid).count
+  quota_type 'security_group.count' do
+    fetch do
+      M::SecurityGroup.filter(:account_id=>@account.canonical_uuid).count
+    end
+
+    evaluate do |fetch_value|
+      quota_value.to_i <= fetch_value
+    end
   end
-  quota_key 'ssh_key_pair.count' do
-    quota_value.to_i < M::SshKeyPair.filter(:account_id=>@account.canonical_uuid).count
+  quota_type 'ssh_key_pair.count' do
+    fetch do
+      M::SshKeyPair.filter(:account_id=>@account.canonical_uuid).count
+    end
+    
+    evaluate do |fetch_value|
+      quota_value.to_i <= fetch_value
+    end
   end
-  quota_key 'instance.quota_weight' do
-    quota_value.to_f < (M::Instance.lives.filter(:account_id=>@account.canonical_uuid).sum(:quota_weight) || 0.0)
+  quota_type 'instance.quota_weight' do
+    fetch do
+      (M::Instance.lives.filter(:account_id=>@account.canonical_uuid).sum(:quota_weight) || 0.0)
+    end
+    
+    evaluate do |fetch_value|
+      quota_value.to_f <= fetch_value
+    end
   end
-  quota_key 'instance.count' do
-    quota_value.to_i < M::Instance.lives.filter(:account_id=>@account.canonical_uuid).count
+  quota_type 'instance.count' do
+    fetch do
+      M::Instance.lives.filter(:account_id=>@account.canonical_uuid).count
+    end
+    
+    evaluate do |fetch_value|
+      quota_value.to_i <= fetch_value
+    end
   end
-  quota_key 'volume.count' do
-    quota_value.to_i < M::Volume.filter(:account_id=>@account.canonical_uuid).count
+  quota_type 'volume.count' do
+    fetch do
+      M::Volume.filter(:account_id=>@account.canonical_uuid).count
+    end
+    
+    evaluate do |fetch_value|
+      quota_value.to_i <= fetch_value
+    end
   end
-  quota_key 'volume.size' do
-    quota_value.to_i < (M::Volume.filter(:account_id=>@account.canonical_uuid).sum(:volume_size) || 0)
+  quota_type 'volume.size' do
+    fetch do
+      (M::Volume.filter(:account_id=>@account.canonical_uuid).sum(:volume_size) || 0)
+    end
+    
+    evaluate do |fetch_value|
+      quota_value.to_i <= fetch_value
+    end
   end
-  quota_key 'image.count' do
-    quota_value.to_i < M::Image.filter(:account_id=>@account.canonical_uuid).count
+  quota_type 'image.count' do
+    fetch do
+      M::Image.filter(:account_id=>@account.canonical_uuid).count
+    end
+    
+    evaluate do |fetch_value|
+      quota_value.to_i <= fetch_value
+    end
   end
-  quota_key 'backup_object.count' do
-    quota_value.to_i < M::BackupObject.filter(:account_id=>@account.canonical_uuid).count
+  quota_type 'backup_object.count' do
+    fetch do
+      M::BackupObject.filter(:account_id=>@account.canonical_uuid).count
+    end
+
+    evaluate do |fetch_value|
+      quota_value.to_i <= fetch_value
+    end
   end
-  quota_key 'backup_object.size' do
-    quota_value.to_i < (M::BackupObject.filter(:account_id=>@account.canonical_uuid).sum(:size) || 0)
+  quota_type 'backup_object.size_mb' do
+    fetch do
+      ((M::BackupObject.filter(:account_id=>@account.canonical_uuid).sum(:size) || 0) / (1024 * 1024))
+    end
+
+    evaluate do |fetch_value| 
+      quota_value.to_i <= fetch_value
+    end
+  end
+  quota_type 'load_balancer.count' do
+    fetch do
+      M::LoadBalancer.alives.filter(:account_id=>@account.canonical_uuid).count
+    end
+
+    evaluate do |fetch_value| 
+      quota_value.to_i <= fetch_value
+    end
   end
 end
