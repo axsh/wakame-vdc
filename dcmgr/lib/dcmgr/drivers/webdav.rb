f631a12a84b423bfc3491e6e12fd3cb2a001c7d1
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-08-17 16:34:12 +0900

    hva: remove curl option -q.

--- dcmgr/lib/dcmgr/drivers/webdav.rb
+++ dcmgr/lib/dcmgr/drivers/webdav.rb
@@ -17,11 +17,11 @@ module Dcmgr::Drivers
     end
     
     def upload(src_path, dst_bo)
-      sh("curl -q -T %s %s", [src_path, abs_uri(dst_bo)])
+      sh("curl -T %s %s", [src_path, abs_uri(dst_bo)])
     end
 
     def download(src_bo, dst_path)
-      sh("curl -q -o %s %s", [dst_path, abs_uri(src_bo)])
+      sh("curl -o %s %s", [dst_path, abs_uri(src_bo)])
     end
 
     def delete(bo)
