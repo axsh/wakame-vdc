4e585fc081047e1d55ee7af5c8e0f6e3e5eea44c
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-08-20 11:31:51 +0900

    hva: remove shell oneliner suppport from webdav driver.

--- dcmgr/lib/dcmgr/drivers/webdav.rb
+++ dcmgr/lib/dcmgr/drivers/webdav.rb
@@ -6,15 +6,16 @@ module Dcmgr::Drivers
     include Dcmgr::Helpers::Cgroup::CgroupContextProvider
     include Dcmgr::Helpers::CliHelper
 
-    include CommandAPI
-    
-    def upload_command(src_path, dst_bo)
-      ["curl -T %s", [abs_uri(dst_bo)]]
-    end
-
-    def download_command(src_bo, dst_path)
-      ["curl %s", [abs_uri(src_bo)]]
-    end
+    # Note on CommandAPI (shell onliner) support.
+    # 
+    # For PUT operation, most of DAV servers check the Content-Length
+    # header and expect to be sent from the client. The content will
+    # be rejected if the header has wrong value.
+    # It is difficult to send the proper size of the image file from
+    # the shell oneliner without creating temporary file. Because the
+    # data is transformed to different format, i.e. gzip,  then the
+    # generated data size is vary while goes through the shell pipeline.
+    # Therefore, this driver no longer supports shell onliner API.
     
     def upload(src_path, dst_bo)
       sh("curl -T %s %s", [src_path, abs_uri(dst_bo)])
