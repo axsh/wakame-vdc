32ee935eb6a643cf0fcbba6344972fe63c7b2025
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-08-21 21:40:24 +0900

    hva: fix change for 2e8dcb5b21daaad.

--- dcmgr/lib/dcmgr/rpc/hva_handler.rb
+++ dcmgr/lib/dcmgr/rpc/hva_handler.rb
@@ -594,69 +594,6 @@ module Dcmgr
       def event
         @event ||= Isono::NodeModules::EventChannel.new(@node)
       end
-
-      class HvaContext
-
-        def initialize(hvahandler)
-          raise "Invalid Class: #{hvahandler}" unless hvahandler.instance_of?(HvaHandler)
-          @hva = hvahandler
-        end
-
-        def node
-          @hva.instance_variable_get(:@node)
-        end
-
-        def inst_id
-          @hva.instance_variable_get(:@inst_id)
-        end
-
-        def inst
-          @hva.instance_variable_get(:@inst)
-        end
-
-        def os_devpath
-          @hva.instance_variable_get(:@os_devpath)
-        end
-
-        def metadata_img_path
-          File.expand_path('metadata.img', inst_data_dir)
-        end
-
-        def vol
-          @hva.instance_variable_get(:@vol)
-        end
-
-        def rpc
-          @hva.rpc
-        end
-
-        def inst_data_dir
-          File.expand_path("#{inst_id}", Dcmgr.conf.vm_data_dir)
-        end
-
-        def hypervisor_driver_class
-          Drivers::Hypervisor.driver_class(inst[:host_node][:hypervisor])
-        end
-
-        def logger
-          @instance_logger = InstanceLogger.new(self)
-        end
-
-        class InstanceLogger
-          def initialize(hva_context)
-            @hva_context = hva_context
-            @logger = ::Logger.new(Dcmgr::Logger.default_logdev)
-            @logger.progname = 'HvaHandler'
-          end
-
-          ["fatal", "error", "warn", "info", "debug"].each do |level|
-            define_method(level){|msg|
-              @logger.__send__(level, "Instance UUID: #{@hva_context.inst_id}: #{msg}")
-            }
-          end
-        end
-
-      end
     end
   end
 end
