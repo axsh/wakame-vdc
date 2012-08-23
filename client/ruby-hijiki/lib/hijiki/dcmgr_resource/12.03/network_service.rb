c9a1b45e8995358b494fef44d2284b7688794b76
rakshasa <sundell.software@gmail.com>
2012-08-23 16:45:39 +0900

    Allow creation of network service through Hijiki.

--- client/ruby-hijiki/lib/hijiki/dcmgr_resource/12.03/network_service.rb
+++ client/ruby-hijiki/lib/hijiki/dcmgr_resource/12.03/network_service.rb
@@ -16,6 +16,17 @@ module Hijiki::DcmgrResource::V1203
       def find_service(network_id, service_name)
         find(service_name, :params => { :network_id => network_id })
       end
+
+      def create(network_id, params)
+        service = self.new(:network_id => network_id)
+        service.name = params[:name]
+        service.ipv4 = params[:ipv4]
+        service.incoming_port = params[:incoming_port]
+        service.outgoing_port = params[:outgoing_port]
+        
+        service.save
+        service
+      end
     end
 
   end
