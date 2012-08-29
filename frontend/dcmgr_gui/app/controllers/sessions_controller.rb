78319d4cac8e6ee00a8d4d4a490d0c518997f376
t-iwano <t-iwano@axsh.net>
2012-08-29 21:56:50 +0900

    Add spoofing login.

--- frontend/dcmgr_gui/app/controllers/sessions_controller.rb
+++ frontend/dcmgr_gui/app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
+  require 'base64'
+  require 'openssl'
   layout 'login'
   skip_before_filter :login_required
-  
+  before_filter :authenticate, :only => [:new]
+
   def new
     @user =  User.new
   end
@@ -29,9 +32,28 @@ class SessionsController < ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def destroy
     logout_killing_session!
     redirect_back_or_default('/', :notice => "You have been logged out.")
+  end
+
+  private
+  def authenticate
+    if params[:token]
+      if params[:token] == Base64.encode64("#{OpenSSL::HMAC.digest('sha1', params[:user_id], DcmgrGui::Application.config.authorized_token)}")
+        user = User.find(:uuid => User.trim_uuid(params[:user_id]))
+        if user
+          self.current_user = user
+          user.update_last_login
+          redirect_back_or_default('/', :notice => "Logged in successfully")
+        else
+          @error_message = I18n.t('error_message.sign_in')
+        end
+      else
+        render :status=>403, :text=>"Authentication Failed"
+        return false
+      end
+    end
   end
 end
