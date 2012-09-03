7922c1bb2dbb98f27437563ad72b4b9a83450c70
t-iwano <t-iwano@axsh.net>
2012-09-03 10:59:31 +0900

    modify read the config file of spoof auth token

--- frontend/dcmgr_gui/config/initializers/authentication_token.rb
+++ frontend/dcmgr_gui/config/initializers/authentication_token.rb
@@ -4,7 +4,7 @@
 # If you change this key, all old signed token will become invalid!
 # Make sure the token is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AUTHENTICATION_TOKEN='404ff8b3659da8e805e0b67581c91e950b2faccea40ce5f63562a96771f1'
-if defined?(DcmgrGui::Application)
-  DcmgrGui::Application.config.authentication_token= AUTHENTICATION_TOKEN
-end
+require 'spoof_token_authentication'
+sta = SpoofTokenAuthentication.config
+sta.expire = 3600
+sta.authentication_token = '404ff8b3659da8e805e0b67581c91e950b2faccea40ce5f63562a96771f1'
--- frontend/dcmgr_gui/lib/spoof_token_authentication.rb
+++ frontend/dcmgr_gui/lib/spoof_token_authentication.rb
@@ -2,9 +2,15 @@
 require 'base64'
 require 'openssl'
 module SpoofTokenAuthentication
-  # Decrypts the token from the parameter
-  def generate(data)
-    Base64.encode64("#{OpenSSL::HMAC.digest(OpenSSL::Digest::SHA1::new, DcmgrGui::Application.config.authentication_token, data)}")
+  @config = Struct.new("Config", :expire, :authentication_token).new
+
+  def self.config
+    @config
+  end
+
+  # Generate the token from the parameter
+  def self.generate(user_id, timestamp)
+    Base64.encode64("#{OpenSSL::HMAC.digest(OpenSSL::Digest::SHA1::new, @config.authentication_token, "#{user_id}#{timestamp}")}").chomp
   end
 
   # return true or false as a result of checking the token.
@@ -14,14 +20,12 @@ module SpoofTokenAuthentication
   # else
   #   "Processing of a error"
   # end
-  def check_token(token, user_id, timestamp, expire)
+  def self.check_token(token, user_id, timestamp, expire=nil)
+    expire = @config.expire if expire.nil? || expire.empty?
     # Check the expiration date of the token
-    return false if DateTime.parse(timestamp).since(expire.to_i) < DateTime.now
+    return false if Time.parse(timestamp) + expire.to_i < Time.now
     # compare the token that was decrypted with token
-    return false if token != generate("#{user_id}#{timestamp}#{expire}")
+    return false if token != generate(user_id, timestamp)
     true
   end
-
-  module_function :generate, :check_token
 end
-
