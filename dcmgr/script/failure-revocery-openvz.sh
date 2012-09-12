0c1984b6f2cb212120737c0c206e643a5f0ffd52
Metallion <akieckens@axsh.net>
2012-09-12 12:19:18 +0900

    Moved openvz failure recovery scripts from wakame-vdc/scripts to wakame-vdc/dcmgr/script

--- dcmgr/script/failure-revocery-openvz.sh
+++ dcmgr/script/failure-revocery-openvz.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Determine Wakame root directory. This script needs to reside in $wakame_root/scripts
-wakame_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+# Determine Wakame root directory. This script needs to reside in $dcmgr_root/script
+dcmgr_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
 # Determine Wakame's instances tmp directory
-instances_tmp_dir=`grep vm_data_dir $wakame_root/dcmgr/config/hva.conf | sed "s/.*'\(.*\)'[^']*$/\1/"`
+instances_tmp_dir=`grep vm_data_dir $dcmgr_root/config/hva.conf | sed "s/.*'\(.*\)'[^']*$/\1/"`
 echo $instances_tmp_dir | grep -q vm_data_dir
-if [ $? == 0 ]; then instances_tmp_dir=`grep vm_data_dir $wakame_root/dcmgr/config/hva.conf | sed 's/.*"\(.*\)"[^"]*$/\1/'`; fi
+if [ $? == 0 ]; then instances_tmp_dir=`grep vm_data_dir $dcmgr_root/config/hva.conf | sed 's/.*"\(.*\)"[^"]*$/\1/'`; fi
 
 # This one-liner gets us a nice list of the interfaces that are currently on Open vSwitch. There is probably a shorter cleaner way to write it but it works. :P
 for vif_name in `ovs-vsctl show | grep Interface | tr -s " " | cut -d " " -f3 | sed 's/.*"\(.*\)"[^"]*$/\1/'`; do
