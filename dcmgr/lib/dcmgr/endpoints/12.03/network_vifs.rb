8b2632ddfd16206b249a19302080c57586473ab5
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-09-13 22:07:44 +0900

    dcmgr: fix wrong method name for find_by_uuid.

--- dcmgr/lib/dcmgr/endpoints/12.03/network_vifs.rb
+++ dcmgr/lib/dcmgr/endpoints/12.03/network_vifs.rb
@@ -8,7 +8,7 @@ Dcmgr::Endpoints::V1203::CoreAPI.namespace '/network_vifs' do
     # description "Retrieve details about a vif"
     # params id, string, required
     # params vif_id, string, required
-    respond_with(R::NetworkVif.new(find_uuid(params[:vif_id])).generate)
+    respond_with(R::NetworkVif.new(find_by_uuid(params[:vif_id])).generate)
   end
 
 end
