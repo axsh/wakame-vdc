d4a80056601a0298d84ef0aefce553c7e5e04223
saicologic <s-mikami@axsh.net>
2012-09-14 15:48:54 +0900

    improved AMQP cilient connection

--- dcmgr/lib/dcmgr/messaging.rb
+++ dcmgr/lib/dcmgr/messaging.rb
@@ -11,17 +11,16 @@ module Dcmgr
 
     def self.publish(message, params)
       EM.schedule do
-        conn = Dcmgr.messaging.amqp_client
-        channel = AMQP::Channel.new(conn)
-        ex = channel.topic(params[:topic_name], params[:queue_options])
-        begin
-          channel = AMQP::Channel.new(conn)
-          queue = AMQP::Queue.new(channel, params[:queue_name], :exclusive => false, :auto_delete => true)
-          queue.bind(ex)
-          queue.publish(message(params[:name], message))
-          #queue.publish(message)
-        rescue Exception => e
-          logger.error(e)
+        amqp_channel do
+          @channel ||= AMQP::Channel.new(@amqp_connection)
+          ex = @channel.topic(params[:topic_name], params[:queue_options])
+          begin
+            queue = AMQP::Queue.new(@channel, params[:queue_name], :exclusive => false, :auto_delete => true)
+            queue.bind(ex)
+            queue.publish(message(params[:name], message))
+          rescue Exception => e
+            logger.error(e)
+          end
         end
       end
     end
@@ -39,6 +38,26 @@ module Dcmgr
     #
     def self.message(name, contents)
      [Base64.encode64(name).chomp,Base64.encode64(contents).chomp].join(MESSAGE_BOUNDARY)
+    end
+
+    def self.amqp_channel(&blk)
+      if @amqp_connection.nil?
+        uri = AMQP::Client.parse_connection_uri(Dcmgr.conf.amqp_server_uri)
+        default = ::AMQP.settings
+        opts = {:host => uri['host'] || default[:host],
+                :port => uri['port'] || default[:port],
+                :vhost => uri['vhost'] || default[:vhost],
+                :user => uri['user'] || default[:user],
+                :pass => uri['password'] ||default[:pass]
+        }
+        @amqp_connection = AMQP.connect(opts)
+      end
+
+      if @amqp_connection.connected?
+        blk.call
+      else
+        @amqp_connection.callback &blk
+      end
     end
   end
 end
