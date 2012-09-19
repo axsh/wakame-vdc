c833fbd029c8fd91a032d19d165b539a9ae16c99
saicologic <s-mikami@axsh.net>
2012-09-19 20:52:27 +0900

    change to use the url in the DetailCollection

--- frontend/admin/app/assets/javascripts/admin/controllers/notifications/detail_controller.js
+++ frontend/admin/app/assets/javascripts/admin/controllers/notifications/detail_controller.js
@@ -3,9 +3,7 @@
   // Defer initialization until doc ready.
   $(function(){
 
-    app.collections.detailItems = new app.collections.DetailCollection({
-      url: app.info.api_endpoints.admin + '/api/notifications.json',
-    });
+    app.collections.detailItems = new app.collections.DetailCollection({});
 
     app.views.detail = new app.views.DetailView({collection:app.collections.detailItems});
     app.router = new app.Router;
