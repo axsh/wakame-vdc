9ceb890d391853612de94d53bda881d9845606fb
Metallion <akieckens@axsh.net>
2012-09-19 15:59:40 +0900

    Added checks for mac address format to macrange cli. Also added an output line when adding new ranges.

--- dcmgr/lib/dcmgr/cli/mac_range.rb
+++ dcmgr/lib/dcmgr/cli/mac_range.rb
@@ -6,25 +6,42 @@ module Dcmgr::Cli
     M = Dcmgr::Models
 
     no_tasks {
+      DELIMITER = ':'
+
       def mac_to_int(mac)
-        mac.split('::').join.hex
+        check_mac_format(mac)
+
+        mac.split(DELIMITER).join.hex
       end
-      
+
       def int_to_mac(int)
         mac = int.to_s(16)
         while mac.length < 6
           mac.insert(0,'0')
         end
-        mac.insert(2,'::')
-        mac.insert(6,'::')
+        mac.insert(2, DELIMITER)
+        mac.insert(4 + DELIMITER.length, DELIMITER)
+      end
+
+      def check_mac_format(mac)
+        delimited = /^([0-9a-fA-F]{1,2}#{DELIMITER}){1,2}([0-9a-fA-F]{1,2})$/
+        non_delimited = /^[0-9a-fA-F]{1,6}$/
+
+        #puts "delimited: #{not (mac =~ delimited).nil?}"
+        #puts "non_delimited: #{not (mac =~ non_delimited).nil?}"
+
+        Error.raise "Invalid mac address syntax #{mac}",100 if  (mac =~ delimited).nil? && (mac =~ non_delimited).nil?
       end
     }
+
+    #~ desc "k","k"
+    #~ def check(mac)
+      #~ check_mac_format(mac)
+    #~ end
 
     desc "add VENDOR_ID BEGIN END", "Create a new mac address range"
     method_option :description, :type => :string,  :desc => "Description for the mac address range"
     def add(vendor_id,r_begin,r_end)
-      #TODO: Check hex format of all variables
-
       # Convert hex format to ints
       int_vendor_id = mac_to_int vendor_id
       int_begin = mac_to_int r_begin
@@ -37,12 +54,29 @@ module Dcmgr::Cli
         :description => options[:description]
       }
 
-      M::MacRange.create(fields)
+      r = M::MacRange.create(fields)
+
+      print ERB.new(<<__END, nil, '-').result(binding)
+<%= int_to_mac(r.vendor_id) %><%= DELIMITER %><%= int_to_mac(r.range_begin) %> - <%= int_to_mac(r.vendor_id) %><%= DELIMITER %><%= int_to_mac(r.range_end) %> <%= r.description.nil? ? "" : ( r.description ) %>
+__END
+    end
+
+    desc "modify VENDOR_ID BEGIN END [OPTIONS]", "Modify a mac address range"
+    method_option :description, :type => :string,  :desc => "Description for the mac address range"
+    def modify(vendor_id,r_begin,r_end)
+      # Convert hex format to ints
+      int_vendor_id = mac_to_int vendor_id
+      int_begin = mac_to_int r_begin
+      int_end = mac_to_int r_end
+
+      range = M::MacRange.find(:vendor_id => int_vendor_id, :range_begin => int_begin, :range_end => int_end)
+
+      range.description = options[:description]
+      range.save
     end
 
     desc "del VENDOR_ID [BEGIN] [END]", "Delete an existing mac address range"
     def del(vendor_id, r_begin=nil, r_end=nil)
-      #TODO: Check hex format for vendor id
       # Convert vendor id hex format to int
       int_vendor_id = mac_to_int vendor_id
       if r_begin.nil? && r_end.nil?
@@ -50,7 +84,6 @@ module Dcmgr::Cli
         Error.raise("Vendor id '#{vendor_id}' does not exist",100) if ranges.empty?
       else
         #TODO: raise error if either r_begin or r_end is nil
-        #TODO: Check hex format for ranges
 
         # Convert ranges hex format to int
         int_begin = mac_to_int r_begin
@@ -68,11 +101,9 @@ module Dcmgr::Cli
     desc "show [VENDOR_ID]", "Show existing mac address range(s)"
     def show(vendor_id=nil)
       if vendor_id
-        #TODO: Check hex format for vendor id
-        
         # Convert vendor id hex format to int
         int_vendor_id = mac_to_int(vendor_id)
-        
+
         # Show only the ranges for this vendor id
         ranges = M::MacRange.filter(:vendor_id => int_vendor_id).all
         Error.raise("Vendor id '#{vendor_id}' does not exist",100) if ranges.empty?
@@ -80,11 +111,11 @@ module Dcmgr::Cli
         # Show all ranges for all vendor ids
         ranges = M::MacRange.order(:vendor_id).all
       end
-      
+
       print ERB.new(<<__END, nil, '-').result(binding)
 Dynamic MAC Address Range:
 <%- ranges.each { |r| -%>
-  <%= int_to_mac(r.vendor_id) %>::<%= int_to_mac(r.range_begin) %> - <%= int_to_mac(r.vendor_id) %>::<%= int_to_mac(r.range_end) %>
+  <%= int_to_mac(r.vendor_id) %><%= DELIMITER %><%= int_to_mac(r.range_begin) %> - <%= int_to_mac(r.vendor_id) %><%= DELIMITER %><%= int_to_mac(r.range_end) %> <%= r.description.nil? ? "" : ( r.description ) %>
 <%- } -%>
 __END
     end
