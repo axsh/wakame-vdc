c7b0d1695322a9900ffb870d74324182c9147283
t-iwano <t-iwano@axsh.net>
2012-09-20 20:50:45 +0900

    admin: added host_nodes list

---
+++ frontend/admin/app/assets/javascripts/admin/controllers/host_nodes/search_controller.js
@@ -0,0 +1,24 @@
+(function(){
+  // Defer initialization until doc ready.
+  $(function(){
+     app.collections.paginatedItems = new app.collections.PaginatedCollection({
+
+       model: app.models.Item.extend({
+         urlRoot: app.info.api_endpoints.dcmgr + '/api/12.03/host_nodes.json',
+       }),
+
+       server_api: {
+       },
+
+       paginator_core: {
+         url: app.info.api_endpoints.dcmgr + '/api/12.03/host_nodes.json'
+       }
+     });
+
+     app.views.pagination = new app.views.PaginatedView({collection:app.collections.paginatedItems});
+     app.views.list = new app.views.ListView({collection: app.collections.paginatedItems});
+
+     app.router = new app.Router;
+     Backbone.history.start();
+  });
+})();
