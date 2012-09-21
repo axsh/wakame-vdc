9f4796a191a4791903d4f51a1b21c3025bdacb6a
saicologic <s-mikami@axsh.net>
2012-09-21 16:11:42 +0900

    add create/save/destroy before_hook

--- frontend/admin/models/notification.rb
+++ frontend/admin/models/notification.rb
@@ -3,4 +3,20 @@ class Notification < Sequel::Model
     super
     errors.add(:title, 'cannot be empty') if !title || title.empty?
   end
+
+  def before_create
+    return false if super == false
+    self.created_at = Time.now
+  end
+
+  def before_save
+    return false if super == false
+    self.updated_at = Time.now
+  end
+
+  def before_destroy
+    return false if super == false
+    self.deleted_at = Time.now
+  end
+
 end
