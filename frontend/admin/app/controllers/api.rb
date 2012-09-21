09cc9da88bfdc6b3ea06d89f678d849db583edd7
saicologic <s-mikami@axsh.net>
2012-09-21 16:15:05 +0900

    add CRUD API

--- frontend/admin/app/controllers/api.rb
+++ frontend/admin/app/controllers/api.rb
@@ -5,7 +5,7 @@ DcmgrAdmin.controllers :api do
   get :notifications, :provides => :json do
     limit = params['limit'].nil? ? 10 : params['limit'].to_i
     start = params['start'].nil? ? 0 : params['start'].to_i
-    @notifications = Notification.limit(limit, start).all
+    @notifications = Notification.limit(limit, start).alives.all
     results = []
     @notifications.each {|n|
       results << {
@@ -25,30 +25,92 @@ DcmgrAdmin.controllers :api do
 
   get '/notifications/:id', :provides => :json do
 
-    @notification = Notification.find(:id => params[:id])
-
-    result = {
-      :id => @notification.id,
-      :title => @notification.title,
-      :publish_date_from => @notification.publish_date_from,
-      :publish_date_to => @notification.publish_date_to,
-      :users => @notification.users,
-      :article => @notification.article
-    }
+    @notification = Notification.filter(:id => params[:id]).alives.first
+    if @notification.nil?
+      h = { :result => '' }
+      return render h
+    end
 
     h = {
       :count => 1,
-      :result => result
+      :result => @notification.to_hash
     }
     render h
   end
 
-  delete :notifications, :provides => :json do
-    @notification = Notification.find({:id => params[:id]})
-    @notification.destroy
+  delete '/notifications/:id', :provides => :json do
+    @notification = Notification.filter({:id => params[:id]}).alives.first
+    if @notification.nil?
+      h = { :result => '' }
+      return render h
+    end
+
+    @notification.deleted_at = Time.now
+    @notification.save_changes
+
     h = {
-      :results => true
+      :result => @notification.to_hash
     }
+    render h
+  end
+
+  post :notifications, :provides => :json do
+    @notification = Notification.new
+    @notification.users = params[:users]
+    @notification.title = params[:title]
+    @notification.publish_date_to = Time.iso8601(params[:publish_date_to])
+    @notification.publish_date_from = Time.iso8601(params[:publish_date_from])
+    @notification.article = params[:article]
+    if @notification.valid?
+      @notification.save
+      h = {
+       :result => @notification.to_hash
+      }
+    else
+      h = {
+       :result => {}
+      }
+    end
+    render h
+  end
+
+  put '/notifications/:id', :provides => :json do
+
+    @notification = Notification.filter(:id => params[:id]).alives.first
+    if @notification.nil?
+      h = { :result => '' }
+      return render h
+    end
+
+    if params[:title]
+      @notification.title = params[:title]
+    end
+
+    if params[:users]
+      @notification.users = params[:users]
+    end
+
+
+    if params[:publish_date_to]
+      @notification.publish_date_to = Time.iso8601(params[:publish_date_to])
+    end
+
+    if params[:publish_date_from]
+      @notification.publish_date_from = Time.iso8601(params[:publish_date_from])
+    end
+
+    if params[:article]
+      @notification.article = params[:article]
+    end
+
+    if @notification.valid?
+      @notification.save_changes
+    end
+
+    h = {
+      :result => @notification.to_hash
+    }
+
     render h
   end
 
