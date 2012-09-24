7a7b4a1fccdfa0029bb54607afb2f271199f49d0
saicologic <s-mikami@axsh.net>
2012-09-24 11:41:27 +0900

    added to use of super().

--- frontend/admin/models/notification.rb
+++ frontend/admin/models/notification.rb
@@ -12,16 +12,19 @@ class Notification < Sequel::Model
   end
 
   def before_create
+    super
     return false if super == false
     self.created_at = Time.now
   end
 
   def before_save
+    super
     return false if super == false
     self.updated_at = Time.now
   end
 
   def before_destroy
+    super
     return false if super == false
     self.deleted_at = Time.now
   end
