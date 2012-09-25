e21dc59664611876e7cfdc1cf912fbd2fe13b0ca
t-iwano <t-iwano@axsh.net>
2012-09-25 17:24:03 +0900

    admin: change the date format

--- frontend/admin/app/assets/javascripts/admin/helpers.js
+++ frontend/admin/app/assets/javascripts/admin/helpers.js
@@ -7,6 +7,14 @@
         d.local();
         return d.format('YYYY/MM/DD HH:mm:ss');
       }
+    },
+
+    date_ja: {
+      parse: function(date) {
+	var d = moment.utc(date);
+	d.local();
+	return d.format('YYYY年MM月DD日 HH:mm:ss');
+      }
     }
   };
 
