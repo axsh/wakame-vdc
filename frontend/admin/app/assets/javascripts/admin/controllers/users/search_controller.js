9ba3ea11b1710e18855c2fadcb244f415e23492b
t-iwano <t-iwano@axsh.net>
2012-09-25 15:09:37 +0900

    admin: added search of the users.

--- frontend/admin/app/assets/javascripts/admin/controllers/users/search_controller.js
+++ frontend/admin/app/assets/javascripts/admin/controllers/users/search_controller.js
@@ -1,28 +1,25 @@
 (function(){
+  // Defer initialization until doc ready.
+  $(function(){
+    app.collections.paginatedItems = new app.collections.PaginatedCollection({
+      model: app.models.Item.extend({
+	urlRoot: app.info.api_endpoints.dcmgr_gui + '/api/users.json',
+      }),
 
-    // Defer initialization until doc ready.
-    $(function(){
+      server_api: {
+	id: app.utils.parsedSearch('q'),
+      },
 
-	    app.collections.paginatedItems = new app.collections.PaginatedCollection({
+      paginator_core: {
+	url: app.info.api_endpoints.dcmgr_gui + '/api/users.json'
+      }
 
-		    model: app.models.Item.extend({
-			    urlRoot: app.info.api_endpoints.dcmgr_gui + '/api/users.json',
-			}),
+    });
 
-		    server_api: {
-		    },
+    app.views.pagination = new app.views.PaginatedView({collection:app.collections.paginatedItems});
+    app.views.list = new app.views.ListView({collection: app.collections.paginatedItems});
 
-		    paginator_core: {
-			url: app.info.api_endpoints.dcmgr_gui + '/api/users.json'
-		    }
-
-		});
-
-	    app.views.pagination = new app.views.PaginatedView({collection:app.collections.paginatedItems});
-	    app.views.list = new app.views.ListView({collection: app.collections.paginatedItems});
-
-	    app.router = new app.Router;
-	    Backbone.history.start();
-	});
-
+    app.router = new app.Router;
+    Backbone.history.start();
+  });
 })();
