a75885ba6c43bb801220c0b3938f07531336f35c
Masahiro Fujiwara <m-fujiwara@axsh.net>
2012-10-02 21:15:45 +0900

    nwmongw: add new parameters to the configuration.

--- dcmgr/lib/dcmgr/configurations/nwmongw.rb
+++ dcmgr/lib/dcmgr/configurations/nwmongw.rb
@@ -8,12 +8,37 @@ module Dcmgr
       # AMQP broker to be connected.
       param :amqp_server_uri
 
+      DSL do
+        def driver(driver_name, &blk)
+          @config[:driver_class] = klass = ::Dcmgr::Drivers::NetworkMonitoring.driver_class(driver_name)
+          @config[:driver] = klass::Configuration.new(@subject).parse_dsl(&blk)
+        end
+
+        def network(nwuuid)
+          @config[:networks] << nwuuid
+        end
+      end
+
+      def after_initialize
+        super
+
+        @config[:networks] = []
+      end
+
       def validate(errors)
         unless self.database_uri
           errors << "Unknown database_uri: #{self.database_uri}"
         end
         unless self.amqp_server_uri
           errors << "Unknown amqp_server_uri: #{self.amqp_server_uri}"
+        end
+
+        unless self.driver
+          errors << "driver is unset"
+        end
+
+        if self.networks.empty?
+          errors << "None of Network UUID is set"
         end
       end
     end
