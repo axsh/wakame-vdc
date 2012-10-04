869eb2b90245c04e4ac6accb417a1e2ee790f2a3
Metallion <akieckens@axsh.net>
2012-10-04 15:08:19 +0900

    Added instance show command to vdc-manage

--- dcmgr/lib/dcmgr/cli/instance.rb
+++ dcmgr/lib/dcmgr/cli/instance.rb
@@ -6,14 +6,50 @@ module Dcmgr::Cli
     M = Dcmgr::Models
     include Dcmgr::Constants::Instance
 
+    no_tasks {
+      def show_single_instance(inst)
+        # Placeholder code
+        show_instances_list([inst])
+      end
+
+      def show_instances_list(inst_dataset)
+        puts ERB.new(<<__END, nil, '-').result(binding)
+<%- inst_dataset.each { |row| -%>
+<%= row.canonical_uuid %>\t<%= row.host_node.canonical_uuid %>\t<%= row.state %>
+<%- } -%>
+__END
+      end
+    }
+
     desc "force-state UUID STATE", "Force an instance's state to chance in the database without any other action taken by Wakame. Use only if you know what you're doing!"
     def force_state(uuid,state)
       raise "Invalid state: #{state} possible states are: [#{STATES.join(',')}]" unless STATES.member?(state)
       modify(M::Instance,uuid,{:state => state})
     end
 
-    desc "show [UUID] [options]", "Show onstance(s)"
+    desc "show [UUID] [options]", "Show instance(s)"
+    method_option :show_terminated, :type => :boolean, :default => false, :desc => "Will include terminated instances in the results"
     def show(uuid=nil)
+      insts = options[:show_terminated] ? M::Instance.dataset : M::Instance.alives
+      if uuid
+        # We get these constants loaded so they're searchable
+        # by the Taggable#find method.
+        M::Instance
+        M::HostNode
+
+        resource = M::Taggable.find(uuid)
+        case resource
+        when M::Instance
+          show_single_instance(resource)
+        when M::HostNode
+          show_instances_list(insts.filter(:host_node => resource))
+        else
+          UnknownUUIDError.raise(uuid)
+        end
+      else
+        show_instances_list(insts)
+      end
+
     end
 
   end
