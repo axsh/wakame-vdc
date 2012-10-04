3ea6c52a4c1bbf14c3c6b9effd47cad340aa4bd9
Metallion <akieckens@axsh.net>
2012-10-04 16:59:35 +0900

    Cleaned up a whole bunch of dirty whitespace in the cli classes

--- dcmgr/lib/dcmgr/cli/base.rb
+++ dcmgr/lib/dcmgr/cli/base.rb
@@ -17,11 +17,11 @@ module Dcmgr::Cli
         super(task, *args)
         after_task
       end
-    
+
       protected
       def before_task
       end
-      
+
       def after_task
       end
     }
@@ -29,16 +29,16 @@ module Dcmgr::Cli
     no_tasks {
       class MapperDSL
         attr_reader :defs
-        
+
         def initialize()
           @defs = {}
         end
-      
+
         def option(key, map_key=nil, &blk)
           map_key ||= key
           @defs[key]=proc { |h,v| h[map_key]=blk.call(v) }
         end
-        
+
         def map(key, map_key)
           @defs[key]=proc { |h, v| h[map_key] = v}
         end
@@ -66,39 +66,39 @@ module Dcmgr::Cli
         #TODO: Make this check a little tighter by checking that the model is either from the wakame backend or frontend
         #UnknownModelError.raise(model) unless model < Dcmgr::Models::BaseNew
         UnknownModelError.raise(model) unless model < Sequel::Model
-        
+
         fields = options.dup
-        
+
         if fields.has_key?("uuid") || fields.has_key?(:uuid)
           fields[:uuid] = model.trim_uuid(fields[:uuid]) if model.check_uuid_format(fields[:uuid])
           Error.raise("UUID syntax invalid: #{fields[:uuid]}",100) unless model.check_trimmed_uuid_format(fields[:uuid])
         end
-        
+
         #Create database fields
         new_record = model.create(fields)
-        
+
         #Return uuid if there is one
         new_record.canonical_uuid #if model.respond_to? "canonical_uuid"
       end
-      
+
       def del(model,uuid)
         #UnknownModelError.raise(model) unless model < Dcmgr::Models::BaseNew
         UnknownModelError.raise(model) unless model < Sequel::Model
         to_delete = model[uuid] || UnknownUUIDError.raise(uuid)
         to_delete.destroy
       end
-      
+
       def modify(model,uuid,fields)
         #UnknownModelError.raise(model) unless model < Dcmgr::Models::BaseNew
         UnknownModelError.raise(model) unless model < Sequel::Model
         raise ArgumentError, "fields must be a Hash" unless fields.is_a? Hash
         to_modify = model[uuid] || UnknownUUIDError.raise(uuid)
-        
+
         #Use a copy of the fields hash so this method can work with frozen hashes
         fields_nonil = fields.merge({})
         #Don't update empty fields
         fields_nonil.delete_if {|key,value| value.nil?}
-        
+
         to_modify.set(fields_nonil)
         to_modify.save_changes
       end
