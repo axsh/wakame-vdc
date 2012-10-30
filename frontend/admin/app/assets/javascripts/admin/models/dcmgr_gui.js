1dbf6dfc7b6dc171c14e5b5a6fa322c2f640905f
t-iwano <t-iwano@axsh.net>
2012-10-30 21:11:21 +0900

    admin: added the check of the format of the date.

--- frontend/admin/app/assets/javascripts/admin/models/dcmgr_gui.js
+++ frontend/admin/app/assets/javascripts/admin/models/dcmgr_gui.js
@@ -34,6 +34,22 @@
         this.errors['display_date'] = "掲載期間が未入力です。";
       }
 
+      if(!attrs.display_begin_at == '') {
+        if(_.isNull(attrs.display_begin_at.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/))){
+          if(_.isNull(attrs.display_begin_at.match(/^\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}:\d{2}$/))) {
+            this.errors['display_date'] = "掲載期間の書式が違います。";
+          }
+        }
+      }
+
+      if(!attrs.display_end_at == '') {
+        if(_.isNull(attrs.display_end_at.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/))){
+          if(_.isNull(attrs.display_end_at.match(/^\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}:\d{2}$/))) {
+            this.errors['display_date'] = "掲載期間の書式が違います。";
+          }
+        }
+      }
+
       if(Date.parse(attrs.display_begin_at) > Date.parse(attrs.display_end_at)) {
         this.errors['display_date'] = "無効な掲載期間です。";
       }
