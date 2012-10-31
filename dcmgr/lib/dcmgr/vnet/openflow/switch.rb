5ce556d99ab6e33511e944ab557f493cb1ea7582
rakshasa <sundell.software@gmail.com>
2012-10-31 22:28:34 +0900

    Check if switch.bridge_ipv4 is set before trying to make the IP anti-spoof flow.

--- dcmgr/lib/dcmgr/vnet/openflow/switch.rb
+++ dcmgr/lib/dcmgr/vnet/openflow/switch.rb
@@ -27,6 +27,8 @@ module Dcmgr::VNet::OpenFlow
     end
 
     def update_bridge_ipv4
+      @bridge_ipv4 = nil
+
       ip = case `/bin/uname -s`.rstrip
            when 'Linux'
              `/sbin/ip addr show #{self.switch_name} | awk '$1 == "inet" { print $2 }'`.split('/')[0]
@@ -48,6 +50,7 @@ module Dcmgr::VNet::OpenFlow
 
     def switch_ready
       logger.info "switch_ready: name:#{self.switch_name} datapath_id:%#x ipv4:#{self.bridge_ipv4}." % datapath.datapath_id
+      logger.info "switch_ready: name:#{self.switch_name} error:'bridge_ipv4 not set'" unless self.bridge_ipv4
 
       # There's a short period of time between the switch being
       # activated and features_reply installing flow.
@@ -126,7 +129,7 @@ module Dcmgr::VNet::OpenFlow
       # with the local mac and ip address, so drop those.
       flows << Flow.new(TABLE_LOAD_SRC, 6, {:in_port => OpenFlowController::OFPP_LOCAL}, {:output_reg0 => nil})
       flows << Flow.new(TABLE_LOAD_SRC, 5, {:dl_src => local_hw.to_s}, {:drop => nil})
-      flows << Flow.new(TABLE_LOAD_SRC, 5, {:ip => nil, :nw_src => self.bridge_ipv4}, {:drop =>nil})
+      flows << Flow.new(TABLE_LOAD_SRC, 5, {:ip => nil, :nw_src => self.bridge_ipv4}, {:drop =>nil}) if self.bridge_ipv4
 
       #
       # ARP routing table
@@ -186,13 +189,13 @@ module Dcmgr::VNet::OpenFlow
       end
 
       if message.arp?
-        logger.debug "Got ARP packet; port:#{message.in_port} network:#{port.networks.empty? ? 'nil' : port.networks.first.id} oper:#{message.arp_oper} source:#{message.arp_sha.to_s}/#{message.arp_spa.to_s} dest:#{message.arp_tha.to_s}/#{message.arp_tpa.to_s}."
+        logger.debug "Got ARP packet; switch_name:#{self.switch_name} port:#{message.in_port} network:#{port.networks.empty? ? 'nil' : port.networks.first.id} oper:#{message.arp_oper} source:#{message.arp_sha.to_s}/#{message.arp_spa.to_s} dest:#{message.arp_tha.to_s}/#{message.arp_tpa.to_s}."
       elsif message.ipv4? and message.tcp?
-        logger.debug "Got IPv4/TCP packet; port:#{message.in_port} network:#{port.networks.empty? ? 'nil' : port.networks.first.id} source:#{message.ipv4_saddr.to_s}:#{message.tcp_src_port} dest:#{message.ipv4_daddr.to_s}:#{message.tcp_dst_port}."
+        logger.debug "Got IPv4/TCP packet; switch_name:#{self.switch_name} port:#{message.in_port} network:#{port.networks.empty? ? 'nil' : port.networks.first.id} source:#{message.ipv4_saddr.to_s}:#{message.tcp_src_port} dest:#{message.ipv4_daddr.to_s}:#{message.tcp_dst_port}."
       elsif message.ipv4? and message.udp?
-        logger.debug "Got IPv4/UDP packet; port:#{message.in_port} source:#{message.ipv4_saddr.to_s}:#{message.udp_src_port} dest:#{message.ipv4_daddr.to_s}:#{message.udp_dst_port}."
+        logger.debug "Got IPv4/UDP packet; switch_name:#{self.switch_name} port:#{message.in_port} source:#{message.ipv4_saddr.to_s}:#{message.udp_src_port} dest:#{message.ipv4_daddr.to_s}:#{message.udp_dst_port}."
       else
-        logger.debug "Got Unknown packet; port:#{message.in_port} source:#{message.macsa.to_s} dest:#{message.macda.to_s}."
+        logger.debug "Got Unknown packet; switch_name:#{self.switch_name} port:#{message.in_port} source:#{message.macsa.to_s} dest:#{message.macda.to_s}."
       end
 
       port.networks.each { |network|
