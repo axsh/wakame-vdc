axsh/wakame-vdc

commit f4e3db3e58da44c9f975452371590b2f4e25ae11

fix the bug failing to clear all email addresses from the edit instance dialog.

dcmgr/lib/dcmgr/endpoints/12.03/instances.rb
@@ -266,23 +266,23 @@ Dcmgr::Endpoints::V1203::CoreAPI.namespace '/instances' do
     # Note that the keys should use string for sub hash.
     if params['monitoring'].is_a?(Hash)
       instance.instance_monitor_attr.enabled = (params['monitoring']['enabled'] == 'true')
-      case params['monitoring']['mail_address']
-      when String
-        instance.instance_monitor_attr.recipients << {:mail_address=>params['monitoring']['mail_address']}
-      when Array
-        params['monitoring']['mail_address'].each { |v|
-          instance.instance_monitor_attr.recipients << {:mail_address=>v}
-        }
-      when Hash
-        params['monitoring']['mail_address'].each { |k, v|
-          instance.instance_monitor_attr.recipients << {:mail_address=>v}
-        }
-      when nil
-        # do nothing
-      else
-        raise "Invalid mail address"
+      if params['monitoring'].has_key?('mail_address')
+        case params['monitoring']['mail_address']
+        when ""
+          instance.instance_monitor_attr.recipients = []
+        when Array
+          params['monitoring']['mail_address'].each { |v|
+            instance.instance_monitor_attr.recipients << {:mail_address=>v}
+          }
+        when Hash
+          params['monitoring']['mail_address'].each { |k, v|
+            instance.instance_monitor_attr.recipients << {:mail_address=>v}
+          }
+        else
+          raise "Invalid mail address"
+        end
+        instance.instance_monitor_attr.changed_columns << :recipients
       end
-      instance.instance_monitor_attr.changed_columns << :recipients
       instance.instance_monitor_attr.save_changes
     end
 
@@ -440,27 +440,28 @@ Dcmgr::Endpoints::V1203::CoreAPI.namespace '/instances' do
       if params['monitoring']['enabled']
         instance.instance_monitor_attr.enabled = (params['monitoring']['enabled'] == 'true')
       end
-      case params['monitoring']['mail_address']
-      when String
-        instance.instance_monitor_attr.tap { |o|
-          o.recipients.clear
-          o.recipients << {:mail_address=>params['monitoring']['mail_address']}
-        }
-      when Array
-        instance.instance_monitor_attr.tap { |o|
-          o.recipients = params['monitoring']['mail_address'].map {|v| {:mail_address=>v}}
-        }
-      when Hash
-        instance.instance_monitor_attr.tap { |o|
-          o.recipients = params['monitoring']['mail_address'].map {|k,v| {:mail_address=>v}}
-        }
-      else
-        raise "Invalid monitoring recipient: #{params['monitoring']['mail_address']}"
+      if params['monitoring'].has_key?('mail_address')
+        case params['monitoring']['mail_address']
+        when ""
+          # empty string indicates to clear the recipients list.
+          instance.instance_monitor_attr.recipients.clear
+        when Array
+          instance.instance_monitor_attr.tap { |o|
+            o.recipients = params['monitoring']['mail_address'].map {|v| {:mail_address=>v}}
+          }
+        when Hash
+          instance.instance_monitor_attr.tap { |o|
+            o.recipients = params['monitoring']['mail_address'].map {|k,v| {:mail_address=>v}}
+          }
+        else
+          raise "Invalid monitoring recipient: #{params['monitoring']['mail_address']}"
+        end
+        instance.instance_monitor_attr.changed_columns << :recipients
       end
-      instance.instance_monitor_attr.changed_columns << :recipients
+      
       instance.instance_monitor_attr.save_changes
     end
-
+    
     instance.display_name = params[:display_name] if params[:display_name]
     instance.save_changes
 

client/ruby-hijiki/lib/hijiki/dcmgr_resource/12.03/instance.rb
@@ -105,15 +105,22 @@ module Hijiki::DcmgrResource::V1203
       def validate_monitoring_params(params)
         validate_mailaddress = lambda { |addr|
           unless addr.to_s =~ /^[^@]+@[A-Za-z0-9][A-Za-z0-9\-\.]+$/
-            raise "Mail address validation error: #{params[:monitoring][:mail_address]}"
+            raise "Mail address validation error: #{addr}"
           end
         }
         
-        if params[:monitoring].is_a?(Hash) && params[:monitoring][:enabled] == 'true'
+        if params[:monitoring].is_a?(Hash)
           case params[:monitoring][:mail_address]
           when String
-            validate_mailaddress.call(params[:monitoring][:mail_address])
-          when Array, Hash
+            # empty string is accepted becase it is the list clear command.
+            if params[:monitoring][:mail_address] != ""
+              raise "String is not accepted."
+            end
+          when Hash
+            params[:monitoring][:mail_address].values.each { |v|
+              validate_mailaddress.call(v)
+            }
+          when Array
             params[:monitoring][:mail_address].each { |v|
               validate_mailaddress.call(v)
             }

frontend/dcmgr_gui/app/controllers/instances_controller.rb
@@ -172,9 +172,14 @@ class InstancesController < ApplicationController
       }
       if params['monitoring']
         data[:monitoring][:enabled] = (params['monitoring']['enabled'] == 'true')
-        if params['monitoring']['mail_address']
-          data[:monitoring][:mail_address] = params['monitoring']['mail_address']
-        end
+        # Indicates Hijiki to clear mail address list when the
+        # mail_address parameter does not exist.
+        data[:monitoring][:mail_address] = if params['monitoring']['mail_address'].is_a?(Array)
+                                             params['monitoring']['mail_address']
+                                           else
+                                             ""
+                                           end
+          
       end
       instance = Hijiki::DcmgrResource::Instance.update(instance_id,data)
       render :json => instance

commit bdb67c078ea95b8032c8bf1f5de4a1487cd8ef36

dcmgr: fix nil check bug introduced from f4e3db3e58da.

dcmgr/lib/dcmgr/endpoints/12.03/instances.rb
@@ -268,7 +268,8 @@ Dcmgr::Endpoints::V1203::CoreAPI.namespace '/instances' do
       instance.instance_monitor_attr.enabled = (params['monitoring']['enabled'] == 'true')
       if params['monitoring'].has_key?('mail_address')
         case params['monitoring']['mail_address']
-        when ""
+        when "", nil
+          # Indicates to clear the recipients.
           instance.instance_monitor_attr.recipients = []
         when Array
           params['monitoring']['mail_address'].each { |v|
@@ -440,10 +441,12 @@ Dcmgr::Endpoints::V1203::CoreAPI.namespace '/instances' do
       if params['monitoring']['enabled']
         instance.instance_monitor_attr.enabled = (params['monitoring']['enabled'] == 'true')
       end
+      # Do not add mail_address key when you don't want to change
+      # existing recipient list.
       if params['monitoring'].has_key?('mail_address')
         case params['monitoring']['mail_address']
-        when ""
-          # empty string indicates to clear the recipients list.
+        when "", nil
+          # Indicates to clear the recipients.
           instance.instance_monitor_attr.recipients.clear
         when Array
           instance.instance_monitor_attr.tap { |o|

frontend/dcmgr_gui/public/javascripts/dcmgr_gui/instance_panel.js
@@ -88,11 +88,16 @@ DcmgrGUI.prototype.instancePanel = function(){
         query.push("security_groups[]="+ $(this).val());
       });
 
+      orig_len = query.length;
       _.each(['#mailaddr_0', '#mailaddr_1', '#mailaddr_2'], function(id){
         if( _.isString($(self).find(id).val()) && $(self).find(id).val() != ""){
           query.push("monitoring[mail_address][]="+$(self).find(id).val());
         }
       });
+      if(query.length == orig_len){
+        // Clear recipient list
+        query.push("monitoring[mail_address]=");
+      }
 
       if( !bt_edit_instance.monitor_selector.validate() ){
         return false;