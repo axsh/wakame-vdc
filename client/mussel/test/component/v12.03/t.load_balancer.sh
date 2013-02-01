5dd191b614f1ec50223f0f516bfa0ceaa8e538b6
Masahito Yoshida <masahito@axsh.net>
2013-02-01 16:54:54 +0900

    mussel,test: add component tests for v12.03 load_balancer.

---
+++ client/mussel/test/component/v12.03/t.load_balancer.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. $(cd ${BASH_SOURCE[0]%/*} && pwd)/helper_shunit2.sh
+
+## variables
+
+declare namespace=load_balancer
+
+## functions
+
+function setUp() {
+  xquery=
+  state=
+  uuid=asdf
+}
+
+### index
+
+function test_load_balancer_index_stateless() {
+  local cmd=index
+  assertEquals "$(cli_wrapper ${namespace} ${cmd})" \
+               "curl -X GET ${base_uri}/${namespace}s.${format}?"
+}
+
+function test_load_balancer_index_stateful() {
+  local cmd=index
+  local state=running
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} --state=${state})" \
+               "curl -X GET ${base_uri}/${namespace}s.${format}?state=${state}"
+}
+
+### show
+
+function test_load_balancer_show() {
+  local cmd=show
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${uuid})" \
+               "curl -X GET ${base_uri}/${namespace}s/${uuid}.${format}"
+}
+
+### destroy
+
+function test_load_balancer_destroy() {
+  local cmd=destroy
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${uuid})" \
+               "curl -X DELETE ${base_uri}/${namespace}s/${uuid}.${format}"
+}
+
+### create
+
+function test_load_balancer_create_no_opts() {
+  local cmd=create
+
+  local protocol=http
+  local balancer_port=80
+  local instance_port=80
+  local balance_algorithm=leastconn
+  local max_connection=1000
+
+  local display_name=
+  local cookie_name=
+  local private_key=
+  local public_key=
+
+  local opts=""
+
+  local params="
+    --data-urlencode display_name=${display_name}
+    --data-urlencode protocol=${protocol}
+    --data-urlencode port=${balancer_port}
+    --data-urlencode instance_port=${instance_port}
+    --data-urlencode balance_algorithm=${balance_algorithm}
+    --data-urlencode engine=haproxy
+    --data-urlencode cookie_name=${cookie_name}
+    --data-urlencode private_key=${private_key}
+    --data-urlencode public_key=${public_key}
+    --data-urlencode engine=haproxy
+    --data-urlencode max_connection=${max_connection}
+   "
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${opts})" \
+               "curl -X POST $(echo ${params}) ${base_uri}/${namespace}s.${format}"
+}
+
+function test_load_balancer_create_opts() {
+  local cmd=create
+
+  local protocol=http
+  local balancer_port=80
+  local instance_port=80
+  local balance_algorithm=leastconn
+  local max_connection=1000
+
+  local display_name=
+  local cookie_name=
+  local private_key=
+  local public_key=
+
+  local opts="
+    --protocol=${protocol}
+    --balancer-port=${balancer_port}
+    --instance-port=${instance_port}
+    --balance-algorithm=${balance_algorithm}
+    --max-connection=${max_connection}
+    --display-name=${display_name}
+    --cookie-name=${cookie_name}
+    --private-key=${private_key}
+  "
+
+  local params="
+    --data-urlencode display_name=${display_name}
+    --data-urlencode protocol=${protocol}
+    --data-urlencode port=${balancer_port}
+    --data-urlencode instance_port=${instance_port}
+    --data-urlencode balance_algorithm=${balance_algorithm}
+    --data-urlencode engine=haproxy
+    --data-urlencode cookie_name=${cookie_name}
+    --data-urlencode private_key=${private_key}
+    --data-urlencode public_key=${public_key}
+    --data-urlencode engine=haproxy
+    --data-urlencode max_connection=${max_connection}
+   "
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${opts})" \
+               "curl -X POST $(echo ${params}) ${base_uri}/${namespace}s.${format}"
+}
+
+### xcreate
+
+function test_load_balancer_xcreate() {
+  local cmd=xcreate
+
+  local protocol=http
+  local balancer_port=80
+  local instance_port=80
+  local balance_algorithm=leastconn
+  local max_connection=1000
+
+  local display_name=
+  local cookie_name=
+  local private_key=
+  local public_key=
+
+  local MUSSEL_CUSTOM_DATA="
+    --data-urlencode display_name=${display_name}
+    --data-urlencode protocol=${protocol}
+    --data-urlencode port=${balancer_port}
+    --data-urlencode instance_port=${instance_port}
+    --data-urlencode balance_algorithm=${balance_algorithm}
+    --data-urlencode engine=haproxy
+    --data-urlencode cookie_name=${cookie_name}
+    --data-urlencode private_key=${private_key}
+    --data-urlencode public_key=${public_key}
+    --data-urlencode engine=haproxy
+    --data-urlencode max_connection=${max_connection}
+  "
+
+  assertEquals "$(MUSSEL_CUSTOM_DATA=${MUSSEL_CUSTOM_DATA} cli_wrapper ${namespace} ${cmd})" \
+               "curl -X POST $(echo ${MUSSEL_CUSTOM_DATA}) ${base_uri}/${namespace}s.${format}"
+}
+
+### poweron
+
+function test_load_balancer_poweron() {
+  local cmd=poweron
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${uuid})" \
+               "curl -X PUT -d ${base_uri}/${namespace}s/${uuid}/${cmd}.${format}"
+}
+
+### poweroff
+
+function test_load_balancer_poweroff() {
+  local cmd=poweroff
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${uuid})" \
+               "curl -X PUT -d ${base_uri}/${namespace}s/${uuid}/${cmd}.${format}"
+}
+
+## shunit2
+
+. ${shunit2_file}
