fbf1e951009d28cf66561626c687c11a7b3d5a93
Masahito Yoshida <masahito@axsh.net>
2013-02-01 17:08:34 +0900

    mussel,test: add component tests for v12.03.

---
+++ client/mussel/test/component/v12.03/t.host_node.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. $(cd ${BASH_SOURCE[0]%/*} && pwd)/helper_shunit2.sh
+
+## variables
+
+declare namespace=host_node
+
+## functions
+
+function setUp() {
+  xquery=
+  state=
+  uuid=asdf
+}
+
+### index
+
+function test_host_node_index() {
+  local cmd=index
+  assertEquals "$(cli_wrapper ${namespace} ${cmd})" \
+               "curl -X GET ${base_uri}/${namespace}s.${format}?"
+}
+
+### show
+
+function test_host_node_show() {
+  local cmd=show
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${uuid})" \
+               "curl -X GET ${base_uri}/${namespace}s/${uuid}.${format}"
+}
+
+### xcreate
+
+function test_host_node_xcreate() {
+  local cmd=xcreate
+
+  local MUSSEL_CUSTOM_DATA="
+    --data-urlencode name=shunit2
+  "
+
+  assertEquals "$(MUSSEL_CUSTOM_DATA=${MUSSEL_CUSTOM_DATA} cli_wrapper ${namespace} ${cmd})" \
+               "curl -X POST $(echo ${MUSSEL_CUSTOM_DATA}) ${base_uri}/${namespace}s.${format}"
+}
+
+## shunit2
+
+. ${shunit2_file}
---
+++ client/mussel/test/component/v12.03/t.image.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. $(cd ${BASH_SOURCE[0]%/*} && pwd)/helper_shunit2.sh
+
+## variables
+
+declare namespace=image
+
+## functions
+
+function setUp() {
+  xquery=
+  state=
+  uuid=asdf
+}
+
+### index
+
+function test_image_index() {
+  local cmd=index
+  assertEquals "$(cli_wrapper ${namespace} ${cmd})" \
+               "curl -X GET ${base_uri}/${namespace}s.${format}?"
+}
+
+### show
+
+function test_image_show() {
+  local cmd=show
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${uuid})" \
+               "curl -X GET ${base_uri}/${namespace}s/${uuid}.${format}"
+}
+
+### destroy
+
+function test_image_destroy() {
+  local cmd=destroy
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${uuid})" \
+               "curl -X DELETE ${base_uri}/${namespace}s/${uuid}.${format}"
+}
+
+### xcreate
+
+function test_image_xcreate() {
+  local cmd=xcreate
+
+  local MUSSEL_CUSTOM_DATA="
+    --data-urlencode name=shunit2
+  "
+
+  assertEquals "$(MUSSEL_CUSTOM_DATA=${MUSSEL_CUSTOM_DATA} cli_wrapper ${namespace} ${cmd})" \
+               "curl -X POST $(echo ${MUSSEL_CUSTOM_DATA}) ${base_uri}/${namespace}s.${format}"
+}
+
+## shunit2
+
+. ${shunit2_file}
