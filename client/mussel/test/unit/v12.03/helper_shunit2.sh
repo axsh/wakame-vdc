03b988e35ba424d80975d120798ed2f813664d9d
Masahito Yoshida <masahito@axsh.net>
2013-02-01 14:37:07 +0900

    mussel,test: set fixed params.

--- client/mussel/test/unit/v12.03/helper_shunit2.sh
+++ client/mussel/test/unit/v12.03/helper_shunit2.sh
@@ -16,13 +16,14 @@ readonly shunit2_file=${abs_dirname}/../../shunit2
 
 ## group variables
 
-api_version=${api_version:-12.03}
-host=${host:-localhost}
-port=${port:-9001}
-base_uri=${base_uri:-http://${host}:${port}/api/${api_version}}
-account_id=${account_id:-a-shpoolxx}
-format=${format:-yml}
-declare http_header=X_VDC_ACCOUNT_UUID:${account_id}
+api_version=12.03
+host=localhost
+port=9001
+base_uri=http://${host}:${port}/api/${api_version}
+account_id=a-shpoolxx
+format=yml
+http_header=X_VDC_ACCOUNT_UUID:${account_id}
+
 dry_run=yes
 
 ## group functions
