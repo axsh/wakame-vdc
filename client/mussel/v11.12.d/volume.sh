aa33ee4be2d98b0c5b5a7581f1f342e400df748a
Masahito Yoshida <masahito@axsh.net>
2013-02-02 11:03:19 +0900

    mussel,test: fix to add volume.sh for v11.12.

---
+++ client/mussel/v11.12.d/volume.sh
@@ -0,0 +1,24 @@
+# -*-Shell-script-*-
+#
+# 11.12
+#
+
+case "${cmd}" in
+help)    cmd_help    ${namespace} "index|show|create|attach|detach|destroy" ;;
+index)   cmd_index   $* ;;
+show)    cmd_show    $* ;;
+destroy) cmd_destroy $* ;;
+create)
+  volume_size=${3:-10}
+  call_api -X POST \
+   --data-urlencode "volume_size=${volume_size}" \
+   ${base_uri}/${namespace}s.${format}
+  ;;
+attach|detach)
+  uuid=$3
+  [[ $# = 4 ]] || { echo "${namespace} ${cmd} [vol-id] [inst-id]" >&2; return 1; }
+  call_api -X PUT -d "''" \
+   "${base_uri}/${namespace}s/${uuid}/${cmd}.${format}?instance_id"
+  ;;
+*)       cmd_default $* ;;
+esac
