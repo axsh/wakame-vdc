ee54c70df772d2bd96f55f183bfede01c847c7e8
Masahito Yoshida <masahito@axsh.net>
2013-02-02 16:33:52 +0900

    mussel,test: add invoke_task().

---
+++ client/mussel/test/unit/functions/t.invoke_task.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. $(cd ${BASH_SOURCE[0]%/*} && pwd)/helper_shunit2.sh
+
+## variables
+
+## functions
+
+function setUp() {
+  function task_test() { :; }
+}
+
+function test_invoke_task_no_opts() {
+  invoke_task 2>/dev/null
+  assertNotEquals $? 0
+}
+
+function test_invoke_task_namespace() {
+  invoke_task shunit2 2>/dev/null
+  assertNotEquals $? 0
+}
+
+function test_invoke_task_namespace_cmd() {
+  invoke_task shunit2 testing 2>/dev/null
+  assertNotEquals $? 0
+}
+
+function test_invoke_task_undefined_task() {
+  invoke_task shunit2 testing 2>/dev/null
+  assertNotEquals $? 0
+}
+
+function test_invoke_task_defined_task() {
+  invoke_task shunit2 test
+  assertEquals $? 0
+}
+
+## shunit2
+
+. ${shunit2_file}
