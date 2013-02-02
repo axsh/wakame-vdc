9f9628fd9246460a08ea2ec87824c3838fe3e30f
Masahito Yoshida <masahito@axsh.net>
2013-02-02 09:58:56 +0900

    mussel,test: remove pwd dependency.

--- client/mussel/mussel.sh
+++ client/mussel/mussel.sh
@@ -11,13 +11,9 @@ LC_ALL=C
 
 set -e
 
-### read-only variables
-
-readonly abs_dirname=$(cd ${BASH_SOURCE[0]%/*} && pwd)
-
 ### include files
 
-. ${abs_dirname}/functions
+. ${BASH_SOURCE[0]%/*}/functions
 
 ### prepare
 
@@ -36,9 +32,9 @@ http_header=X_VDC_ACCOUNT_UUID:${account_id}
 
 # include version
 case "${api_version}" in
-11.12) . ${abs_dirname}/_v11.12 ;;
-12.03) . ${abs_dirname}/_v12.03 ;;
-*)     . ${abs_dirname}/_v12.03 ;;
+11.12) . ${BASH_SOURCE[0]%/*}/_v11.12 ;;
+12.03) . ${BASH_SOURCE[0]%/*}/_v12.03 ;;
+*)     . ${BASH_SOURCE[0]%/*}/_v12.03 ;;
 esac
 
 run_cmd ${MUSSEL_ARGS}
