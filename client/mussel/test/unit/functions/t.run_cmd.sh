99c79331b92d9459d27aa39f49b8eebdc7d9cb80
Masahito Yoshida <masahito@axsh.net>
2013-02-02 12:14:58 +0900

    mussel,test: move run_cmd inplementation from each version front file to function.

--- client/mussel/test/unit/functions/t.run_cmd.sh
+++ client/mussel/test/unit/functions/t.run_cmd.sh
@@ -12,9 +12,13 @@
 
 ## functions
 
-function test_cmd_default() {
-  run_cmd
-  assertEquals $? 0
+function test_run_cmd_no_opts() {
+  run_cmd 2>/dev/null
+  assertNotEquals $? 0
+}
+
+function test_run_cmd_opts() {
+  assertEquals "$(run_cmd shunit2 testing 2>&1)" "$0 [namespace] [cmd]"
 }
 
 ## shunit2
