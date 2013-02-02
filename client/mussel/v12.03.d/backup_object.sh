5f66a707ae142895f837d3bddff9a1054bcf06e9
Masahito Yoshida <masahito@axsh.net>
2013-02-02 11:02:14 +0900

    mussel,test: remove symlink and then make real file.

--- client/mussel/v12.03.d/backup_object.sh
+++ client/mussel/v12.03.d/backup_object.sh
@@ -1,1 +1,13 @@
-image.sh
+# -*-Shell-script-*-
+#
+# 12.03
+#
+
+case "${cmd}" in
+help)    cmd_help    ${namespace} "index|show|xcreate|destroy" ;;
+index)   cmd_index   $* ;;
+show)    cmd_show    $* ;;
+destroy) cmd_destroy $* ;;
+xcreate) cmd_xcreate ${namespace} ;;
+*)       cmd_default $* ;;
+esac
