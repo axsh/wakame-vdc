fc39b195472e0cfc15fac400fc136e0030db9045
Masahito Yoshida <masahito@axsh.net>
2013-02-03 14:04:32 +0900

    mussel,test: add dc_network and volume for v12.03.

---
+++ client/mussel/test/component/v12.03/t.dc_network.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. $(cd ${BASH_SOURCE[0]%/*} && pwd)/helper_shunit2.sh
+
+## variables
+
+declare namespace=dc_network
+
+## functions
+
+function setUp() {
+  xquery=
+  state=
+  uuid=asdf
+}
+
+### index
+
+function test_dc_network_index() {
+  local cmd=index
+  assertEquals "$(cli_wrapper ${namespace} ${cmd})" \
+               "curl -X GET ${base_uri}/${namespace}s.${format}?"
+}
+
+### show
+
+function test_dc_network_show() {
+  local cmd=show
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${uuid})" \
+               "curl -X GET ${base_uri}/${namespace}s/${uuid}.${format}"
+}
+
+### destroy
+
+function test_dc_network_destroy() {
+  local cmd=destroy
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${uuid})" \
+               "curl -X DELETE ${base_uri}/${namespace}s/${uuid}.${format}"
+}
+
+### xcreate
+
+function test_dc_network_xcreate() {
+  local cmd=xcreate
+
+  local MUSSEL_CUSTOM_DATA="
+    name=shunit2
+  "
+
+  assertEquals "$(MUSSEL_CUSTOM_DATA=$(urlencode_data ${MUSSEL_CUSTOM_DATA}) cli_wrapper ${namespace} ${cmd})" \
+               "curl -X POST $(urlencode_data ${MUSSEL_CUSTOM_DATA}) ${base_uri}/${namespace}s.${format}"
+}
+
+## shunit2
+
+. ${shunit2_file}
