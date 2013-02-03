3064a34c9cccd5a8a2f409efaf7250fc81b19672
Masahito Yoshida <masahito@axsh.net>
2013-02-03 14:11:28 +0900

    mussel,test: update security_group unit test.

--- client/mussel/test/unit/v12.03/t.security_group.sh
+++ client/mussel/test/unit/v12.03/t.security_group.sh
@@ -29,7 +29,7 @@ function test_security_group_help_stderr_to_devnull_success() {
 function test_security_group_help_stderr_to_stdout_success() {
   extract_args ${namespace} help
   res=$(run_cmd  ${MUSSEL_ARGS} 2>&1)
-  assertEquals "${res}" "$0 ${namespace} [help|destroy|index|show|xcreate]"
+  assertEquals "${res}" "$0 ${namespace} [help|create|destroy|index|show|xcreate]"
 }
 
 ### index
