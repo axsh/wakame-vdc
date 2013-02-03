a8e40067a95b41221572840e33330a45bbba3ffd
Masahito Yoshida <masahito@axsh.net>
2013-02-04 01:56:00 +0900

    mussel,test: enable to set file path to public-key.

--- client/mussel/test/component/v12.03/t.ssh_key_pair.sh
+++ client/mussel/test/component/v12.03/t.ssh_key_pair.sh
@@ -11,8 +11,20 @@
 ## variables
 
 declare namespace=ssh_key_pair
+declare public_key_file=${BASH_SOURCE[0]%/*}/public_key_file.$$.txt
 
 ## functions
+
+function setUp() {
+  cat <<-EOS > ${public_key_file}
+	ASDFASDF
+	ASDFASDF
+	EOS
+}
+
+function tearDown() {
+  rm -f ${public_key_file}
+}
 
 ### create
 
@@ -57,6 +69,32 @@ function test_ssh_key_pair_create_opts() {
     display_name=${display_name}
     download_once=${download_once}
     public_key=${public_key}
+  "
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${opts})" \
+               "curl -X POST $(urlencode_data ${params}) ${base_uri}/${namespace}s.${format}"
+}
+
+function test_ssh_key_pair_create_opts_public_key_file() {
+  local cmd=create
+
+  local description=shunit2
+  local display_name=foo
+  local download_once=
+  local public_key=${public_key_file}
+
+  local opts="
+    --description=${description}
+    --display-name=${display_name}
+    --download-once=${download_once}
+    --public-key=${public_key}
+  "
+
+  local params="
+    description=${description}
+    display_name=${display_name}
+    download_once=${download_once}
+    public_key@${public_key}
   "
 
   assertEquals "$(cli_wrapper ${namespace} ${cmd} ${opts})" \
--- client/mussel/v12.03.d/ssh_key_pair.sh
+++ client/mussel/v12.03.d/ssh_key_pair.sh
@@ -10,7 +10,13 @@ task_create() {
     description=${description:-} \
     display_name=${display_name:-} \
     download_once=${download_once:-} \
-    public_key=${public_key:-} \
+    $(
+      if [[ -f "${public_key}" ]]; then
+        echo public_key@${public_key}
+      else
+        echo public_key=${public_key:-}
+      fi
+    ) \
     ) \
    ${base_uri}/${1}s.${format}
 }
