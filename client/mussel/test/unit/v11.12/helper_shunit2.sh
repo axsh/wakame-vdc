e479ff79f030ec3422c04f58cd900cca62ee0b14
Masahito Yoshida <masahito@axsh.net>
2013-02-03 23:37:53 +0900

    mussel,test: use strict bash syntax to define variables in helper_shunit2.

--- client/mussel/test/unit/v11.12/helper_shunit2.sh
+++ client/mussel/test/unit/v11.12/helper_shunit2.sh
@@ -14,13 +14,13 @@ readonly shunit2_file=${BASH_SOURCE[0]%/*}/../../shunit2
 
 ## group variables
 
-api_version=11.12
-host=localhost
-port=9001
-base_uri=http://${host}:${port}/api/${api_version}
-account_id=a-shpoolxx
-format=yml
-http_header=X_VDC_ACCOUNT_UUID:${account_id}
+declare api_version=11.12
+declare host=localhost
+declare port=9001
+declare base_uri=http://${host}:${port}/api/${api_version}
+declare account_id=a-shpoolxx
+declare format=yml
+declare http_header=X_VDC_ACCOUNT_UUID:${account_id}
 
 dry_run=yes
 
--- client/mussel/test/unit/v12.03/helper_shunit2.sh
+++ client/mussel/test/unit/v12.03/helper_shunit2.sh
@@ -14,13 +14,13 @@ readonly shunit2_file=${BASH_SOURCE[0]%/*}/../../shunit2
 
 ## group variables
 
-api_version=12.03
-host=localhost
-port=9001
-base_uri=http://${host}:${port}/api/${api_version}
-account_id=a-shpoolxx
-format=yml
-http_header=X_VDC_ACCOUNT_UUID:${account_id}
+declare api_version=12.03
+declare host=localhost
+declare port=9001
+declare base_uri=http://${host}:${port}/api/${api_version}
+declare account_id=a-shpoolxx
+declare format=yml
+declare http_header=X_VDC_ACCOUNT_UUID:${account_id}
 
 dry_run=yes
 
