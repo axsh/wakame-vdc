43c0e6e30438e88bf1083c0afdc3d9aef126db50
Masahito Yoshida <masahito@axsh.net>
2013-02-06 03:57:50 +0900

    mussel,test: add basic integration tests for rw.

---
+++ client/mussel/test/integration/v12.03/rw/t.ssh_key_pair.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. ${BASH_SOURCE[0]%/*}/helper_shunit2.sh
+
+## variables
+
+declare namespace=$(namespace ${BASH_SOURCE[0]})
+
+## functions
+
+###
+
+function test_crud() {
+  local uuid
+
+  uuid=$(run_cmd ${namespace} create | awk '$1 == ":id:" {print $2}')
+  assertEquals $? 0
+
+  run_cmd ${namespace} show ${uuid}
+  assertEquals $? 0
+
+  run_cmd ${namespace} destroy ${uuid}
+  assertEquals $? 0
+}
+
+## shunit2
+
+. ${shunit2_file}
