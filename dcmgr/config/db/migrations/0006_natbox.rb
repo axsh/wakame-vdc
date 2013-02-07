12ae9ac6546e47689d506d9ba46596b7cd3ec707
rakshasa <sundell.software@gmail.com>
2013-02-07 21:45:21 +0900

    Renamed routes table to network_routes, and shortened the column names.

--- dcmgr/config/db/migrations/0006_natbox.rb
+++ dcmgr/config/db/migrations/0006_natbox.rb
@@ -1,24 +1,24 @@
 Sequel.migration do
   up do
-    create_table(:routes) do
+    create_table(:network_routes) do
       column :route_type, "int(11)", :default=>0, :null=>false
 
-      column :inner_network_id, "int(11)", :null=>false
-      column :inner_network_vif_id, "int(11)"
-      column :inner_ipv4, "int(11)", :null=>false, :unsigned=>true
+      column :i_network_id, "int(11)", :null=>false
+      column :i_network_vif_id, "int(11)"
+      column :i_ipv4, "int(11)", :null=>false, :unsigned=>true
 
-      column :outer_network_id, "int(11)", :null=>false
-      column :outer_network_vif_id, "int(11)"
-      column :outer_ipv4, "int(11)", :null=>false, :unsigned=>true
+      column :o_network_id, "int(11)", :null=>false
+      column :o_network_vif_id, "int(11)"
+      column :o_ipv4, "int(11)", :null=>false, :unsigned=>true
 
       # If false, delete when vif is disconnected from network.
       column :is_permanent, "int(11)", :default=>false, :null=>false
 
-      index [:inner_network_id, :outer_network_id, :inner_ipv4, :outer_ipv4], :unique=>true
-      index [:inner_network_id]
-      index [:inner_network_vif_id]
-      index [:outer_network_id]
-      index [:outer_network_vif_id]
+      index [:i_network_id, :o_network_id, :i_ipv4, :o_ipv4], :unique=>true
+      index [:i_network_id]
+      index [:i_network_vif_id]
+      index [:o_network_id]
+      index [:o_network_vif_id]
     end
 
     alter_table(:dhcp_ranges) do
@@ -28,7 +28,7 @@ Sequel.migration do
   end
 
   down do
-    drop_table(:routes)
+    drop_table(:network_routes)
 
     alter_table(:dhcp_ranges) do
       drop_column :group
