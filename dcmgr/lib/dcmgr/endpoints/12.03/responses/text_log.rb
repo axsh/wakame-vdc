205983a4986420e811075ff5a15bd41abc846984
saicologic <s-mikami@axsh.net>
2013-02-08 17:09:58 +0900

    Added text log api

--- dcmgr/lib/dcmgr/endpoints/12.03/responses/text_log.rb
+++ dcmgr/lib/dcmgr/endpoints/12.03/responses/text_log.rb
@@ -3,30 +3,28 @@
 module Dcmgr::Endpoints::V1203::Responses
   class TextLog < Dcmgr::Endpoints::ResponseGenerator
     def initialize(text_log)
-      raise ArgumentError if !text_log.is_a?(Dcmgr::Models::TextLog)
       @text_log = text_log
+      @id = 0
     end
 
     def generate
-      @text_log.instance_exec {
-        to_hash.merge(
-          :id => id,
-          :resource_type => resource_type,
-          :payload => payload,
-          :created_at => Time.at(created_at)
-        )
+      @id += 1
+      h = {
+        :id => @text_log[:id],
+        :payload => @text_log[:message],
+        :created_at => Time.now
       }
+      h
     end
   end
 
   class TextLogCollection < Dcmgr::Endpoints::ResponseGenerator
     def initialize(ds)
-      raise ArgumentError if !ds.is_a?(Sequel::Dataset)
       @ds = ds
     end
 
     def generate()
-      @ds.all.map { |i|
+      @ds.map { |i|
         TextLog.new(i).generate
       }
     end
--- dcmgr/lib/dcmgr/endpoints/12.03/text_logs.rb
+++ dcmgr/lib/dcmgr/endpoints/12.03/text_logs.rb
@@ -3,12 +3,69 @@
 require 'dcmgr/endpoints/12.03/responses/text_log'
 
 Dcmgr::Endpoints::V1203::CoreAPI.namespace '/text_logs' do
-  get do
-    ds = M::TextLog.dataset
 
-    ds = datetime_range_params_filter(:created, ds)
-    collection_respond_with(ds) do |paging_ds|
-      R::TextLogCollection.new(paging_ds).generate
+  get do
+
+    # If you want to provide to the user, it is much better to apply access restrictions.
+    config = {
+      :account_id => params[:account_id],
+      :instance_id => params[:instance_id],
+      :application_id => params[:application_id]
+    }
+
+    time = params[:start_time]
+    limit = params[:limit]
+    text_log = Dcmgr::TextLog.new(log_storage, config)
+    last_id = params[:id]
+
+    if(last_id)
+      config = {
+        :time => time
+      }
+      ds = text_log.position_search(last_id, limit, config)
+    else
+      ds = text_log.timeseries_search(time, limit)
     end
+
+    collection_respond_with(ds) do |ds|
+      R::TextLogCollection.new(ds).generate
+    end
+
+  end
+
+  get '/keys' do
+
+
+    config = {
+      :account_id => params[:account_id],
+      :instance_id => params[:instance_id],
+      :application_id => params[:application_id]
+    }
+
+    results = log_storage.get_keys
+
+    respond_with([{
+                :total => results.size,
+                :results=> results
+              }])
+  end
+
+  private
+  def collection_respond_with(ds, &blk)
+    results = blk.call(ds)
+    respond_with([{
+                    :total => results.size,
+                    :start => params[:start] || 0,
+                    :limit => params[:limit] || 0,
+                    :results=> results
+                  }])
+  end
+
+  def log_storage
+    Dcmgr::Models::LogStorage.create(:cassandra, {
+      :keyspace => Dcmgr.conf.cassandra_keyspace,
+      :cf => Dcmgr.conf.cassandra_cf,
+      :uri => Dcmgr.conf.cassandra_uri
+    })
   end
 end
