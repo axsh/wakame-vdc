0289095325a47cb889eeff1bc2d30c05257981d2
Masahito Yoshida <masahito@axsh.net>
2013-02-12 03:30:24 +0900

    mussel,test: split each step functions.

--- client/mussel/test/acceptance/v12.03/1shot/helper_shunit2.sh
+++ client/mussel/test/acceptance/v12.03/1shot/helper_shunit2.sh
@@ -17,6 +17,21 @@ declare ssh_keypair_path=${BASH_SOURCE[0]%/*}/keypair.$$
 declare rule_path=${BASH_SOURCE[0]%/*}/rule.$$
 declare vifs_path=${BASH_SOURCE[0]%/*}/vifs.$$
 
+## configuarable variables
+
+image_id=${image_id:-wmi-centos1d}
+hypervisor=${hypervisor:-openvz}
+cpu_cores=${cpu_cores:-1}
+memory_size=${memory_size:-256}
+network_id=${network_id:-nw-demo1}
+
+## test local variables
+
+inst_id=
+ipaddr=
+secg_id=
+ssh_key_id=
+
 ## group functions
 
 function ssh() {
@@ -40,4 +55,74 @@ function check_network_connection() {
   local ipaddr=$1
 
   ping -c 1 -W 1 ${ipaddr}
+}
+
+##
+
+function render_secg_rule() {
+  :
+}
+
+function render_vif_table() {
+  :
+}
+
+function generate_ssh_key_pair() {
+  ssh-keygen -N "" -f ${ssh_keypair_path} -C shunit2.$$ >/dev/null
+}
+
+function remove_ssh_known_host_entry() {
+  ipaddr=$(get_instance_ipaddr)
+  ssh-keygen -R ${ipaddr} >/dev/null 2>&1
+}
+
+function create_ssh_key_pair() {
+  public_key=${ssh_keypair_path}.pub
+  ssh_key_id=$(run_cmd ssh_key_pair create | hash_value id)
+}
+
+function create_security_group() {
+  render_secg_rule > ${rule_path}
+  rule=${rule_path}
+  secg_id=$(run_cmd security_group create | hash_value id)
+}
+
+function create_instance() {
+  render_vif_table > ${vifs_path}
+  vifs=${vifs_path}
+  inst_id=$(run_cmd instance create | hash_value id)
+}
+
+function get_instance_ipaddr() {
+  run_cmd instance show ${inst_id} | hash_value address
+}
+
+function wait_for_instance_state_is_running() {
+  retry_until ${wait_sec} "check_document_pair instance ${inst_id} state running"
+}
+
+function wait_for_instance_network_is_ready() {
+  ipaddr=$(get_instance_ipaddr)
+  retry_until ${wait_sec} "check_network_connection ${ipaddr}" >/dev/null
+}
+
+function wait_for_instance_sshd_is_ready() {
+  ipaddr=$(get_instance_ipaddr)
+  retry_until ${wait_sec} "check_port ${ipaddr} tcp 22" >/dev/null
+}
+
+function wait_for_instance_state_is_terminated() {
+  retry_until ${wait_sec} "check_document_pair instance ${inst_id} state terminated"
+}
+
+function destroy_instance() {
+  run_cmd instance destroy ${inst_id}
+}
+
+function destroy_ssh_key_pair() {
+  run_cmd ssh_key_pair destroy ${ssh_key_id}
+}
+
+function destroy_security_group() {
+  run_cmd security_group destroy ${secg_id}
 }
--- client/mussel/test/acceptance/v12.03/1shot/t.instance.sh
+++ client/mussel/test/acceptance/v12.03/1shot/t.instance.sh
@@ -11,20 +11,6 @@
 
 ## variables
 
-# configuarable variables
-
-image_id=${image_id:-wmi-centos1d}
-hypervisor=${hypervisor:-openvz}
-cpu_cores=${cpu_cores:-1}
-memory_size=${memory_size:-256}
-network_id=${network_id:-nw-demo1}
-
-# test local variables
-
-declare inst_id=
-declare inst_hash=
-declare ipaddr=
-
 ## functions
 
 function oneTimeTearDown() {
@@ -45,48 +31,6 @@ function render_vif_table() {
   cat <<-EOS
 	{"eth0":{"index":"0","network":"${network_id}","security_groups":"${secg_id}"}}
 	EOS
-}
-
-function generate_ssh_key_pair() {
-  ssh-keygen -N "" -f ${ssh_keypair_path} -C shunit2.$$ >/dev/null
-}
-
-function create_ssh_key_pair() {
-  public_key=${ssh_keypair_path}.pub
-  ssh_key_id=$(run_cmd ssh_key_pair create | hash_value id)
-}
-
-function create_security_group() {
-  render_secg_rule > ${rule_path}
-  rule=${rule_path}
-  secg_id=$(run_cmd security_group create | hash_value id)
-}
-
-function create_instance() {
-  render_vif_table > ${vifs_path}
-  vifs=${vifs_path}
-
-  inst_id=$(run_cmd instance create | hash_value id)
-}
-
-function wait_for_instance_state_is_running() {
-  retry_until ${wait_sec} "check_document_pair instance ${inst_id} state running"
-}
-
-function wait_for_instance_state_is_terminated() {
-  retry_until ${wait_sec} "check_document_pair instance ${inst_id} state terminated"
-}
-
-function destroy_instance() {
-  run_cmd instance destroy ${inst_id}
-}
-
-function destroy_ssh_key_pair() {
-  run_cmd ssh_key_pair destroy ${ssh_key_id}
-}
-
-function destroy_security_group() {
-  run_cmd security_group destroy ${secg_id}
 }
 
 ### step
@@ -117,32 +61,29 @@ function test_wait_for_instance_state_is_running() {
 }
 
 function test_wait_for_instance_network_is_ready() {
-  ipaddr=$(run_cmd instance show ${inst_id} | hash_value address)
-  retry_until ${wait_sec} "check_network_connection ${ipaddr}" >/dev/null
+  wait_for_instance_network_is_ready
   assertEquals $? 0
 }
 
 function test_wait_for_instance_sshd_is_ready() {
-  ipaddr=$(run_cmd instance show ${inst_id} | hash_value address)
-  retry_until ${wait_sec} "check_port ${ipaddr} tcp 22" >/dev/null
+  wait_for_instance_sshd_is_ready
   assertEquals $? 0
 }
 
 function test_remove_ssh_known_host_entry() {
-  ipaddr=$(run_cmd instance show ${inst_id} | hash_value address)
-  ssh-keygen -R ${ipaddr} >/dev/null 2>&1
+  remove_ssh_known_host_entry >/dev/null 2>&1
   assertEquals $? 0
 }
 
 function test_compare_instance_hostname() {
-  ipaddr=$(run_cmd instance show ${inst_id} | hash_value address)
+  ipaddr=$(get_instance_ipaddr)
   assertEquals \
     "$(run_cmd instance show ${inst_id} | hash_value hostname)" \
     "$(ssh root@${ipaddr} -i ${ssh_keypair_path} hostname)"
 }
 
 function test_compare_instance_ipaddr() {
-  ipaddr=$(run_cmd instance show ${inst_id} | hash_value address)
+  ipaddr=$(get_instance_ipaddr)
   ssh root@${ipaddr} -i ${ssh_keypair_path} ip addr show eth0 | egrep -q ${ipaddr}
   assertEquals $? 0
 }
