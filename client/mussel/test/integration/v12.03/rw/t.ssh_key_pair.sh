a7f64f17b04cee66b5700ae789ccb3b070f87751
Masahito Yoshida <masahito@axsh.net>
2013-02-12 18:02:08 +0900

    mussel,test: split ssh_key_pair rw test.

--- client/mussel/test/integration/v12.03/rw/t.ssh_key_pair.sh
+++ client/mussel/test/integration/v12.03/rw/t.ssh_key_pair.sh
@@ -13,32 +13,38 @@
 declare namespace=$(namespace ${BASH_SOURCE[0]})
 declare ssh_keypair_path=${BASH_SOURCE[0]%/*}/keypair.$$
 
+declare uuid
+declare public_key=${ssh_keypair_path}.pub
+
 ## functions
 
-function setUp() {
+function oneTimeSetUp() {
   ssh-keygen -N "" -f ${ssh_keypair_path} -C shunit2.$$ >/dev/null
 }
 
-function tearDown() {
+function oneTimeTearDown() {
   rm -f ${ssh_keypair_path}*
 }
 
 ###
 
-function test_crud() {
-  local uuid
-  local public_key=${ssh_keypair_path}.pub
-
+function test_create_ssh_key_pair() {
   uuid=$(run_cmd ${namespace} create | awk '$1 == ":id:" {print $2}')
   assertEquals $? 0
+}
 
-  run_cmd ${namespace} show ${uuid}
+function test_show_ssh_key_pair() {
+  run_cmd ${namespace} show ${uuid} >/dev/null
   assertEquals $? 0
+}
 
-  run_cmd ${namespace} update ${uuid}
+function test_update_ssh_key_pair() {
+  run_cmd ${namespace} update ${uuid} >/dev/null
   assertEquals $? 0
+}
 
-  run_cmd ${namespace} destroy ${uuid}
+function test_destroy_ssh_key_pair() {
+  run_cmd ${namespace} destroy ${uuid} >/dev/null
   assertEquals $? 0
 }
 
