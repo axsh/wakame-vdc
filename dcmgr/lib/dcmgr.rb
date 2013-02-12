9881b4a256702b84f414e63654c8703c55967f87
saicologic <s-mikami@axsh.net>
2013-02-12 08:28:44 +0000

    Remove text_log model

--- dcmgr/lib/dcmgr.rb
+++ dcmgr/lib/dcmgr.rb
@@ -90,7 +90,6 @@ module Dcmgr
     autoload :MacRange, 'dcmgr/models/mac_range'
     autoload :NetworkVifMonitor, 'dcmgr/models/network_vif_monitor'
     autoload :InstanceMonitorAttr, 'dcmgr/models/instance_monitor_attr'
-    autoload :TextLog, 'dcmgr/models/text_log'
     autoload :QueuedJob, 'dcmgr/models/queued_job'
 
     require 'dcmgr/models/log_storage/base'
