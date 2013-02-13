172e915eedde526c966254ec0c0ef9c5e1096589
Masahito Yoshida <masahito@axsh.net>
2013-02-13 15:56:38 +0900

    mussel,test: add t.instance.vifs.multi.

---
+++ client/mussel/test/integration/v12.03/rw/t.instance.vifs.multi.null-net.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. ${BASH_SOURCE[0]%/*}/helper_shunit2.sh
+. ${BASH_SOURCE[0]%/*}/helper_instance.sh
+. ${BASH_SOURCE[0]%/*}/helper_instance_vifs.sh
+
+## variables
+
+## functions
+
+function render_vif_table() {
+  cat <<-EOS
+	{
+	"eth0":{"index":"0","network":"","security_groups":""},
+	"eth1":{"index":"1","network":"","security_groups":""}
+	}
+	EOS
+}
+
+###
+
+function test_show_instance_vifs_multi_null_net() {
+  run_cmd ${namespace} show ${instance_uuid}
+}
+
+## shunit2
+
+. ${shunit2_file}
