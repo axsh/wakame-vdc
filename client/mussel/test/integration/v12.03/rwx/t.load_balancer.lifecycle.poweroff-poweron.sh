ec46235417203beef86e6ad1f8d9a483567cff20
Masahito Yoshida <masahito@axsh.net>
2013-02-15 17:25:46 +0900

    mussel,test: add basic load_balancer poweroff-poweron test.

---
+++ client/mussel/test/integration/v12.03/rwx/t.load_balancer.lifecycle.poweroff-poweron.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. ${BASH_SOURCE[0]%/*}/helper_shunit2.sh
+. ${BASH_SOURCE[0]%/*}/helper_load_balancer.sh
+
+## variables
+
+## functions
+
+function test_poweroff_load_balancer() {
+  # :state: halting
+  # :status: online
+  run_cmd load_balancer poweroff ${load_balancer_uuid} >/dev/null
+  assertEquals $? 0
+
+  # :state: halted
+  # :status: online
+  retry_until "check_document_pair load_balancer ${load_balancer_uuid} state halted"
+  assertEquals $? 0
+}
+
+function test_poweron_load_balancer() {
+  # :state: starting
+  # :status: online
+  run_cmd load_balancer poweron ${load_balancer_uuid} >/dev/null
+  assertEquals $? 0
+
+  # :state: running
+  # :status: online
+  retry_until "check_document_pair load_balancer ${load_balancer_uuid} state running"
+  assertEquals $? 0
+}
+
+## shunit2
+
+. ${shunit2_file}
