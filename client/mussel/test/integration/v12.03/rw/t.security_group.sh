e95cbc8307cc152f08b48763e467fcb68fa5d125
Masahito Yoshida <masahito@axsh.net>
2013-02-17 13:27:20 +0900

    mussel,test: update rule generation way.

--- client/mussel/test/integration/v12.03/rw/t.security_group.sh
+++ client/mussel/test/integration/v12.03/rw/t.security_group.sh
@@ -10,37 +10,51 @@
 
 ## variables
 
-declare security_group_uuid= rule=
+security_group_uuid=
+rule_path=${BASH_SOURCE[0]%/*}/rule.$$
+rule=${rule_path}
 
 ## functions
+
+function oneTimeTearDown() {
+  rm -f ${rule_path}
+}
 
 ###
 
 function test_create_security_group() {
-  rule=tcp:22,22,ip4:0.0.0.0/0
+  cat <<-EOS > ${rule_path}
+	icmp:-1,-1,ip4:0.0.0.0/0
+	tcp:22,22,ip4:0.0.0.0/0
+	tcp:80,80,ip4:0.0.0.0/0
+	EOS
   security_group_uuid=$(run_cmd security_group create | hash_value id)
   assertEquals $? 0
 }
 
 function test_show_security_group() {
-  run_cmd security_group show ${security_group_uuid} >/dev/null
+  run_cmd security_group show ${security_group_uuid}
   assertEquals $? 0
 }
 
 function test_update_security_group_icmp() {
-  rule=icmp:-1,-1,ip4:0.0.0.0/0
-  run_cmd security_group update ${security_group_uuid} >/dev/null
+  cat <<-EOS > ${rule_path}
+	icmp:-1,-1,ip4:0.0.0.0/0
+	EOS
+  run_cmd security_group update ${security_group_uuid}
   assertEquals $? 0
 }
 
 function test_update_security_group_udp() {
-  rule=udp:53,53,ip4:0.0.0.0/0
-  run_cmd security_group update ${security_group_uuid} >/dev/null
+  cat <<-EOS > ${rule_path}
+	udp:53,53,ip4:0.0.0.0/0
+	EOS
+  run_cmd security_group update ${security_group_uuid}
   assertEquals $? 0
 }
 
 function test_destroy_security_group() {
-  run_cmd security_group destroy ${security_group_uuid} >/dev/null
+  run_cmd security_group destroy ${security_group_uuid}
   assertEquals $? 0
 }
 
