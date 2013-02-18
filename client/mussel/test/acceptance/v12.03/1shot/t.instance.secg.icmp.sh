201ee630e9cd7d8aba340db9f3dc17c348a6e4b6
Masahito Yoshida <masahito@axsh.net>
2013-02-18 14:48:50 +0900

    mussel,test: split t.instance.secg.sh to t.instance.secg.icmp.sh and t.instance.secg.tcp.sh.

---
+++ client/mussel/test/acceptance/v12.03/1shot/t.instance.secg.icmp.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#  cat, ssh-keygen, ping, rm
+#
+
+## include files
+
+. ${BASH_SOURCE[0]%/*}/helper_shunit2.sh
+. ${BASH_SOURCE[0]%/*}/helper_instance.sh
+. ${BASH_SOURCE[0]%/*}/helper_instance_vifs.sh
+. ${BASH_SOURCE[0]%/*}/helper_instance_vifs_single.sh
+. ${BASH_SOURCE[0]%/*}/helper_instance_login.sh
+
+## variables
+
+declare instance_ipaddr=
+
+function needs_secg() { true; }
+
+## functions
+
+function render_secg_rule() {
+  cat <<-EOS
+	icmp:-1,-1,ip4:0.0.0.0/0
+	EOS
+}
+
+function after_create_instance() {
+  instance_ipaddr=$(run_cmd instance show ${instance_uuid} | hash_value address)
+  wait_for_network_to_be_ready ${instance_ipaddr}
+}
+
+### step
+
+function test_drop_icmp() {
+  cat <<-EOS > ${rule_path}
+	#
+	EOS
+  run_cmd security_group update ${security_group_uuid}
+  sleep 3
+
+  wait_for_network_not_to_be_ready ${instance_ipaddr}
+  assertEquals $? 0
+}
+
+function test_accept_icmp() {
+  render_secg_rule > ${rule_path}
+  run_cmd security_group update ${security_group_uuid}
+  sleep 3
+
+  wait_for_network_to_be_ready ${instance_ipaddr}
+  assertEquals $? 0
+}
+
+## shunit2
+
+. ${shunit2_file}
