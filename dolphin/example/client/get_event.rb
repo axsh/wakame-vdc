a5276b9256f4195583e701308ea8764aa3a1e6f6
saicologic <s-mikami@axsh.net>
2013-03-05 19:07:54 +0900

    Added sample code for event api

---
+++ dolphin/example/client/get_event.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+require 'uri'
+require 'multi_json'
+require 'pry'
+
+path = 'http://127.0.0.1:3000/events?start=0&limit=10'
+uri = URI.parse(path)
+
+headers = {
+  'Content-Type' =>'application/json',
+  'X-NOTIFICATION-ID' => 'system',
+}
+
+request = Net::HTTP::Get.new(uri.request_uri, headers)
+http = Net::HTTP.new(uri.host, uri.port)
+http.set_debug_output $stderr
+http.start do |h|
+  response = h.request(request)
+  res = MultiJson.load(response.body)
+  p res
+end
