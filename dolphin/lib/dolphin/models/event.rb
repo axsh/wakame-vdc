bd6600c04ae95ada545c212afa2f4d65f0b12b49
saicologic <s-mikami@axsh.net>
2013-03-05 19:23:18 +0900

    Changed row key name.

--- dolphin/lib/dolphin/models/event.rb
+++ dolphin/lib/dolphin/models/event.rb
@@ -23,8 +23,7 @@ module Dolphin
 
         notification_id = event[:notification_id]
         column_name = SimpleUUID::UUID.new(Time.now).to_guid
-        time = Time.now.strftime("%Y%m%d%m%d")
-        row_key = [notification_id, time].join(':')
+        row_key = notification_id
         value = MultiJson.dump(event[:messages])
 
         db.insert('events', row_key, {column_name => value})
