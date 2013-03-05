3c90569c3b73c9e8b99b0f9ce4792ef565c6213d
saicologic <s-mikami@axsh.net>
2013-03-05 19:21:55 +0900

    Request content type was changed  to json.

--- dolphin/lib/dolphin/helpers/request_helper.rb
+++ dolphin/lib/dolphin/helpers/request_helper.rb
@@ -4,24 +4,15 @@ require 'multi_json'
 module Dolphin
   module Helpers
     module RequestHelper
+
       def attach_request_params(request)
         @params = {}
+        @notification_id ||= request.env['X-Notification-Id']
+        @message_type ||= request.env['X-Message-Type']
         case request.method
           when "POST"
             v = request.input.to_a[0]
-            case request.env["Content-Type"]
-              when 'application/x-www-form-urlencoded'
-                v.split("&").each {|val|
-                  key, value = val.split("=")
-                  @params[key] = URI.decode(value)
-                }
-                @notification_id ||= @params["notification_id"]
-                @message_type ||= @params["message_type"]
-              when 'application/json'
-                @notification_id ||= request.env['X-Notification-Id']
-                @message_type ||= request.env['X-Message-Type']
-                @params = MultiJson.load(v)
-            end
+            @params = MultiJson.load(v)
           when "GET"
             @params = request.path.to_hash
         end
