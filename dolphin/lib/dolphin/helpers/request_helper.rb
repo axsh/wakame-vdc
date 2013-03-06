bbd920475ad8c0c888bf49adca1ce13aa4642708
saicologic <s-mikami@axsh.net>
2013-03-06 17:17:10 +0900

    Checked Content-Type

--- dolphin/lib/dolphin/helpers/request_helper.rb
+++ dolphin/lib/dolphin/helpers/request_helper.rb
@@ -6,6 +6,7 @@ module Dolphin
     module RequestHelper
 
       def attach_request_params(request)
+        raise 'Unsuppoted Content-Type' unless request.env['Content-Type'] === 'application/json'
         @params = {}
         @notification_id ||= request.env['X-Notification-Id']
         @message_type ||= request.env['X-Message-Type']
