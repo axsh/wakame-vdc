54c98724adb97eb78c7b2da8b6f28088c12eb440
Masahiro Fujiwara <m-fujiwara@axsh.net>
2013-03-11 11:00:58 +0900

    dcmgr: add data typed column and model change for Resource Label.

--- dcmgr/config/db/migrations/0008_add_resource_label.rb
+++ dcmgr/config/db/migrations/0008_add_resource_label.rb
@@ -6,12 +6,15 @@ Sequel.migration do
       primary_key :id, :type=>"int(11)"
       column :resource_uuid, "varchar(255)", :null=>true
       column :name, "varchar(255)", :null=>false
-      column :value, "varchar(255)", :null=>false
+      column :value_type, "int(11)", :null=>false
+      column :string_value, "varchar(255)", :null=>true
+      column :blob_value, "text", :null=>true
 
       column :created_at, "datetime", :null=>false
       column :updated_at, "datetime", :null=>false
 
-      index [:resource_uuid, :name]
+      index [:resource_uuid, :name], :unique=>true
+      index [:string_value]
     end
   end
 
