9de6c30d524c7c1362af0998f5d098341f9852e1
Masahito Yoshida <masahito@axsh.net>
2013-03-13 13:32:38 +0900

    mussel,test: add lbnode acceptance test.

---
+++ client/mussel/test/acceptance/v12.03/1shot/t.instance.lbnode.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#  cat, ssh-keygen, ping, rm
+#
+
+## include files
+
+. ${BASH_SOURCE[0]%/*}/helper_shunit2.sh
+. ${BASH_SOURCE[0]%/*}/helper_instance.sh
+
+## variables
+
+image_id=${image_id_lbnode:-wmi-lbnode}
+
+declare instance_ipaddr=
+
+function needs_vif() { true; }
+function needs_secg() { true; }
+
+## functions
+
+function render_secg_rule() {
+  cat <<-EOS
+	icmp:-1,-1,ip4:0.0.0.0/0
+	tcp:80,80,ip4:0.0.0.0/0
+	EOS
+}
+
+### step
+
+function test_get_instance_ipaddr() {
+  instance_ipaddr=$(run_cmd instance show ${instance_uuid} | hash_value address)
+  wait_for_network_to_be_ready ${instance_ipaddr}
+}
+
+function test_wait_for_httpd_to_be_ready() {
+  wait_for_httpd_to_be_ready ${instance_ipaddr}
+  assertEquals $? 0
+}
+
+function test_compare_instance_hostname_using_http_get() {
+  assertEquals \
+    "${instance_uuid}" \
+    "$(curl -fsSkL http://${instance_ipaddr}/)"
+}
+
+## shunit2
+
+. ${shunit2_file}
