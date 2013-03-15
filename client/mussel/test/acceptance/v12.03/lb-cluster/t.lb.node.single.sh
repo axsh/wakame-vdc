dff92e6dbff99c62107bea25bfc9725e7b8c777f
Masahito Yoshida <masahito@axsh.net>
2013-03-15 16:21:08 +0900

    mussel,test: enable to register multi node.

--- client/mussel/test/acceptance/v12.03/lb-cluster/t.lb.node.single.sh
+++ client/mussel/test/acceptance/v12.03/lb-cluster/t.lb.node.single.sh
@@ -13,56 +13,40 @@
 
 ## variables
 
+target_instance_num=${target_instance_num:-1}
+
 ## functions
-
-### shunit2 setup
-
-function oneTimeSetUp() {
-  create_instance
-  create_load_balancer
-
-  instance_ipaddr=$(run_cmd instance show ${instance_uuid} | hash_value address)
-  load_balancer_ipaddr=$(run_cmd load_balancer show ${load_balancer_uuid} | hash_value address | head -1)
-  instance_vifs="$(run_cmd instance show ${instance_uuid} | hash_value vif_id)"
-
-  wait_for_network_to_be_ready ${instance_ipaddr}
-  wait_for_network_to_be_ready ${load_balancer_ipaddr}
-
-  wait_for_httpd_to_be_ready ${instance_ipaddr}
-  wait_for_httpd_to_be_ready ${load_balancer_ipaddr}
-}
-
-function oneTimeTearDown() {
-  destroy_instance
-  destroy_load_balancer
-}
 
 ### step
 
 function test_register_instances_to_load_balancer() {
-  vifs=${instance_vifs} run_cmd load_balancer register ${load_balancer_uuid}
+  vifs="$(cat ${instance_vifs_path})" run_cmd load_balancer register ${load_balancer_uuid}
   assertEquals $? 0
 
   retry_until "curl -fsSkL http://${load_balancer_ipaddr}/"
+  assertEquals $? 0
 }
 
 function test_http_get_for_registerd_lb() {
-  assertEquals \
-    "${instance_uuid}" \
-    "$(curl -fsSkL http://${load_balancer_ipaddr}/)"
+  for instance_uuid in $(cat ${instance_uuids_path}); do
+    retry_until [[ '"$(curl -fsSkL http://${load_balancer_ipaddr}/)"' == "${instance_uuid}" ]]
+    assertEquals $? 0
+  done
 }
 
 function test_unregister_instances_from_load_balancer() {
-  vifs=${instance_vifs} run_cmd load_balancer unregister ${load_balancer_uuid}
+  vifs="$(cat ${instance_vifs_path})" run_cmd load_balancer unregister ${load_balancer_uuid}
   assertEquals $? 0
 
   retry_while "curl -fsSkL http://${load_balancer_ipaddr}/"
+  assertEquals $? 0
 }
 
 function test_http_get_for_unregisterd_lb() {
-  assertNotEquals \
-    "${instance_uuid}" \
-    "$(curl -fsSkL http://${load_balancer_ipaddr}/)"
+  for instance_uuid in $(cat ${instance_uuids_path}); do
+    retry_while [[ '"$(curl -fsSkL http://${load_balancer_ipaddr}/)"' == "${instance_uuid}" ]]
+    assertEquals $? 0
+  done
 }
 
 ## shunit2
