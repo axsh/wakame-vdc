f495e8eb8b5f27fa7a1e28806ff98492e2395f12
Manabu Ejima <jemiam@gmail.com>
2013-03-25 20:48:08 +0900

    natbox: changed dc_network settings and applied it to the filter when finding network_routes

--- dcmgr/lib/dcmgr/configurations/natbox.rb
+++ dcmgr/lib/dcmgr/configurations/natbox.rb
@@ -9,7 +9,6 @@ module Dcmgr
       class DcNetwork < Configuration
         param :interface
         param :bridge
-        param :bridge_type
 
         def initialize(network_name)
           super()
@@ -18,32 +17,22 @@ module Dcmgr
 
         def validate(errors)
           errors << "Missing interface parameter for the network #{@config[:name]}" unless @config[:interface]
-          errors << "Missing bridge_type parameter for the network #{@config[:name]}" unless @config[:bridge_type]
-
-          case @config[:bridge_type]
-          when 'ovs'
-            # bridge name is needed in this case.
-            errors << "Missing bridge parameter for the network #{@config[:name]}" unless @config[:bridge]
-          when 'linux'
-          when 'macvlan'
-          when 'private'
-          else
-            errors << "Unknown type value for bridge_type: #{@config[:bridge_type]}"
-          end
+          errors << "Missing bridge parameter for the network #{@config[:name]}" unless @config[:bridge]
         end
       end
 
       DSL do
-        def dc_network(name, &blk)
+        def outside_dc_network(name, &blk)
           abort "" unless blk
-
           conf = DcNetwork.new(name)
-          @config[:dc_networks][name] = conf.parse_dsl(&blk)
+          @config[:outside_dc_network] = conf.parse_dsl(&blk)
         end
-      end
 
-      on_initialize_hook do
-        @config[:dc_networks] = {}
+        def inside_dc_network(name, &blk)
+          abort "" unless blk
+          conf = DcNetwork.new(name)
+          @config[:inside_dc_network] = conf.parse_dsl(&blk)
+        end
       end
 
       param :ovs_ofctl_path, :default => "/usr/bin/ovs-ofctl"
@@ -52,6 +41,9 @@ module Dcmgr
       param :ovs_flow_priority, :default => 100
 
       def validate(errors)
+        [:outside_dc_network, :inside_dc_network].each do |dc_network|
+          errors << "Missing #{dc_network} parameter" unless @config[dc_network]
+        end
       end
 
     end
