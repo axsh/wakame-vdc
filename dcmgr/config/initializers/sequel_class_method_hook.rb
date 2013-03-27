98f792cb8e8f1f191e2854bd510e8a5a6fdd3485
Manabu Ejima <jemiam@gmail.com>
2013-03-27 21:14:12 +0900

    added hook_class_methods plugin of sequel to write method hooks that deal jobs not related with model itself.

---
+++ dcmgr/config/initializers/sequel_class_method_hook.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+require 'pry'
+Dcmgr::Models::BaseNew.plugin :hook_class_methods
+
+Dir.glob(File.join(Dcmgr::DCMGR_ROOT, "lib/dcmgr/models/hooks/*_hook.rb")).each do |hook|
+  require "dcmgr/models/hooks/#{File.basename(hook).sub(/\.rb/, "")}"
+end
