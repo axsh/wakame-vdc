f073540c23265fca111bc0255f0500d218727dce
saicologic <s-mikami@axsh.net>
2013-03-27 15:01:44 +0000

    Fixed a critical bug where load balancers didn't get security groups　assigned to their public interface

--- dcmgr/lib/dcmgr/models/instance.rb
+++ dcmgr/lib/dcmgr/models/instance.rb
@@ -294,14 +294,14 @@ module Dcmgr::Models
       sched.schedule(nic)
       nic.save
 
-      if !request_params.has_key?('security_groups') && !vif_template[:security_groups].empty?
-        groups = vif_template[:security_groups]
+      if !request_params.has_key?('security_groups') && !request_params.has_key?(request_params[:security_groups])
+        groups = vif_template["security_groups"] || vif_template[:security_groups]
       else
         # TODO: this code will delete. it's remained for compatibility.
         groups = self.request_params["security_groups"]
       end
 
-      if !groups.nil?
+      unless groups.nil? || (groups.respond_to?(:empty?) && groups.empty?)
         groups = [groups] unless groups.is_a? Array
         groups.each { |group_id|
           nic.add_security_group(SecurityGroup[group_id])
