ec46e11eef7825abaa52f9da10915c76c81cd719
Manabu Ejima <jemiam@gmail.com>
2013-03-27 21:26:50 +0900

    added network_route_hook and removed messaging code from api

---
+++ dcmgr/lib/dcmgr/models/hooks/network_route_hook.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+Dcmgr::Models::NetworkRoute.after_create do |network_route|
+  network_route.db.after_commit do
+    Dcmgr.messaging.event_publish("vnet/network_route/created", :args=>[network_route.id])
+  end
+end
+
+Dcmgr::Models::NetworkRoute.after_destroy do |network_route|
+  network_route.db.after_commit do
+    Dcmgr.messaging.event_publish("vnet/network_route/deleted", :args =>[network_route.id])
+  end
+end
