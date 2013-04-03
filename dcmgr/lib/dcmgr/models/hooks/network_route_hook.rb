05b6f59396109596459fd3a525cf74740265fb33
Manabu Ejima <jemiam@gmail.com>
2013-04-03 16:42:46 +0900

    change coding style of class method hooks

--- dcmgr/lib/dcmgr/models/hooks/network_route_hook.rb
+++ dcmgr/lib/dcmgr/models/hooks/network_route_hook.rb
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
 
-module Dcmgr
-  module Models
-    class NetworkRoute
-      after_create do |network_route|
-        network_route.db.after_commit do
-          ::Dcmgr.messaging.event_publish("vnet/network_route/created", :args=>[network_route.id])
-          true
-        end
-      end
-      after_destroy do |network_route|
-        network_route.db.after_commit do
-          ::Dcmgr.messaging.event_publish("vnet/network_route/deleted", :args =>[network_route.id])
-          true
-        end
-      end
-    end
+Dcmgr::Models::NetworkRoute.after_create do |network_route|
+  network_route.db.after_commit do
+    Dcmgr.messaging.event_publish("vnet/network_route/created", :args=>[network_route.id])
   end
+  true
+end
+
+Dcmgr::Models::NetworkRoute.after_destroy do |network_route|
+  network_route.db.after_commit do
+    Dcmgr.messaging.event_publish("vnet/network_route/deleted", :args =>[network_route.id])
+  end
+  true
 end
