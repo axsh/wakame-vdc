8f1080a286c0e55bc33f5256a61fa3562f017144
saicologic <s-mikami@axsh.net>
2013-04-08 17:19:22 +0900

    Remove log message

--- dolphin/lib/dolphin/helpers/request_helper.rb
+++ dolphin/lib/dolphin/helpers/request_helper.rb
@@ -14,7 +14,6 @@ module Dolphin
         case request.method
           when "POST"
             v = request.input.to_a[0]
-            logger :info, v
             @params = MultiJson.load(v)
           when "GET"
             @params = parse_query_string(request.env["QUERY_STRING"])
