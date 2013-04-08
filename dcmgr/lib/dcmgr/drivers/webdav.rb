779f6ab079f74240cd0c3909cb73c194b5f06bbb
Masahiro Fujiwara <m-fujiwara@axsh.net>
2013-04-08 13:42:10 +0900

    webdav: allow onliner mode since Apache supports PUT with chunked transfer.

--- dcmgr/lib/dcmgr/drivers/webdav.rb
+++ dcmgr/lib/dcmgr/drivers/webdav.rb
@@ -16,6 +16,17 @@ module Dcmgr::Drivers
     # data is transformed to different format, i.e. gzip,  then the
     # generated data size is vary while goes through the shell pipeline.
     # Therefore, this driver no longer supports shell onliner API.
+    include CommandAPI
+
+    attr_accessor :upload_base_uri
+    
+    def download_command(src_bo, dst_path)
+      ["curl -s -o %s", [abs_uri(src_bo)]]
+    end
+
+    def upload_command(src_path, dst_bo)
+      ["curl -s -H 'Transfer-Encoding: chunked' --upload-file - '%s'", [abs_uri(dst_bo)]]
+    end
 
     def upload(src_path, dst_bo)
       sh("curl -T %s %s", [src_path, abs_uri(dst_bo)])
@@ -31,7 +42,9 @@ module Dcmgr::Drivers
 
     private
     def abs_uri(bo)
-      bo[:backup_storage][:base_uri] + bo[:object_key]
+      base = (@upload_base_uri || bo[:backup_storage][:base_uri]).to_s
+      base += '/' if base !~ /\/$/
+      base + bo[:object_key]
     end
   end
 end
