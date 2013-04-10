5bc0f5e201d820dc6f1bc6f62f031fcc7e4da79e
saicologic <s-mikami@axsh.net>
2013-04-10 19:29:55 +0900

    Fixed reconnecting to cassandra

--- dolphin/lib/dolphin/database.rb
+++ dolphin/lib/dolphin/database.rb
@@ -36,17 +36,23 @@ module Dolphin
     end
 
     def self.create(adapter, options)
+
+      config = {
+        :hosts => Dolphin.settings['database']['hosts'],
+        :port => Dolphin.settings['database']['port'],
+        :max_retry_count => Dolphin.settings['database']['max_retry_count'].to_i,
+        :retry_interval => Dolphin.settings['database']['retry_interval'].to_i
+      }
+
       case adapter
         when :cassandra
           # TODO: more better code
           column_family = options[:class].class.name.split('::')[2].downcase + 's'
           klass = Dolphin::DataBase::Cassandra
-          config = {
-            :hosts => Dolphin.settings['database']['hosts'],
-            :port => Dolphin.settings['database']['port'],
+          config.merge!({
             :keyspace => Dolphin::DataBase::Cassandra::KEYSPACE,
             :cf => column_family
-          }
+          })
         else
           raise NotImplementedError
       end
@@ -76,42 +82,42 @@ module Dolphin
         @keyspace = config[:keyspace]
         @cf = config[:cf]
         raise "database hosts is blank" if config[:hosts].empty?
-        @seeds = config[:hosts].split(',')
-      end
-
-      def uri
-        [available_node_ip, Dolphin.settings['database']['port']].join(':')
-      end
-
-      def available_node_ip
-        if @seeds.empty?
-          raise UnAvailableNodeException, "No live servers"
-        end
-        @seeds.shift
+        @hosts = config[:hosts].split(',')
+        @port = config[:port]
+        @max_retry_count = config[:max_retry_count]
+        @retry_interval = config[:retry_interval]
+        @retry_count = 0
       end
 
       def connect
         begin
           if @connection.nil?
-            u = uri
-            logger :info, "Connect to #{u}"
-            @connection = ::Cassandra.new(@keyspace, u)
+            @connection = ::Cassandra.new(@keyspace, servers)
 
             # test connecting..
             @connection.ring
             return @connection
           end
         rescue ThriftClient::NoServersAvailable => e
-          @connection = nil
           logger :error, e
-          logger :error, "retry connection.."
-          retry
+          if @retry_count < @max_retry_count
+            @connection = nil
+            @retry_count += 1
+            logger :error, "retry connection..#{@retry_count}"
+            sleep @retry_interval
+            retry
+          end
         rescue UnAvailableNodeException => e
           logger :error, e
         rescue CassandraThrift::InvalidRequestException => e
           logger :error, e
         end
         nil
+      end
+
+      private
+      def servers
+        @hosts.collect{|host| "#{host}:#{@port}"}
       end
     end
   end
