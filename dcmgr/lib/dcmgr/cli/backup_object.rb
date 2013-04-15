5a3a5b234032f34875fafa4425b7b1bdc669c4e0
Metallion <akieckens@axsh.net>
2013-04-15 17:33:52 +0900

    Fixed a bug where it wasn't possible to modify backup storage through vdc-manage

--- dcmgr/lib/dcmgr/cli/backup_object.rb
+++ dcmgr/lib/dcmgr/cli/backup_object.rb
@@ -49,7 +49,7 @@ module Dcmgr::Cli
       fields = options.dup
       fields.delete(:storage_id)
 
-      if options[options[:storage_id]]
+      if options[:storage_id]
         bkst = M::BackupStorage[options[:storage_id]]
         Error.raise("Backup storage '#{options[:storage_id]}' does not exist.",100) if bkst.nil?
         fields[:backup_storage_id] = bkst.id
