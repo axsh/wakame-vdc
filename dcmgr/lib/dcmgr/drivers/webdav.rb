190a72fd5fb121c63de476f1fb8364ad8905b672
Masahiro Fujiwara <m-fujiwara@axsh.net>
2013-04-24 17:35:03 +0900

    webdav: add common curl options to get error exit code.

--- dcmgr/lib/dcmgr/drivers/webdav.rb
+++ dcmgr/lib/dcmgr/drivers/webdav.rb
@@ -21,23 +21,23 @@ module Dcmgr::Drivers
     attr_accessor :upload_base_uri
     
     def download_command(src_bo, dst_path)
-      ["curl -s %s", [abs_uri(src_bo)]]
+      ["curl -fsS %s", [abs_uri(src_bo)]]
     end
 
     def upload_command(src_path, dst_bo)
-      ["curl -s -H 'Transfer-Encoding: chunked' --upload-file - '%s'", [abs_uri(dst_bo)]]
+      ["curl -fsS -H 'Transfer-Encoding: chunked' --upload-file - '%s'", [abs_uri(dst_bo)]]
     end
 
     def upload(src_path, dst_bo)
-      sh("curl -T %s %s", [src_path, abs_uri(dst_bo)])
+      sh("curl -fsS -T %s %s", [src_path, abs_uri(dst_bo)])
     end
 
     def download(src_bo, dst_path)
-      sh("curl -o %s %s", [dst_path, abs_uri(src_bo)])
+      sh("curl -fsS -o %s %s", [dst_path, abs_uri(src_bo)])
     end
 
     def delete(bo)
-      sh("curl -s -X DELETE %s", [abs_uri(bo)])
+      sh("curl -fsS -X DELETE %s", [abs_uri(bo)])
     end
 
     private
