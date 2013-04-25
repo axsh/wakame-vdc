5aae1f17933347119c1401e12c7f96f3b35ddbab
Masahiro Fujiwara <m-fujiwara@axsh.net>
2013-04-25 12:42:39 +0900

    hva: make snapshot_path object variable to share during session.

--- dcmgr/lib/dcmgr/drivers/linux_local_store.rb
+++ dcmgr/lib/dcmgr/drivers/linux_local_store.rb
@@ -88,15 +88,17 @@ module Dcmgr
 
       def upload_image(inst, ctx, bo, evcb)
         @ctx = ctx
-        take_snapshot_for_backup()
-        snapshot_path = ctx.os_devpath
+        @bo = bo
 
         Task::TaskSession.current[:backup_storage] = bo[:backup_storage]
         @bkst_drv_class = BackupStorage.driver_class(bo[:backup_storage][:storage_type])
 
+        @snapshot_path = take_snapshot_for_backup(@ctx.os_dev_path)
+        logger.info("#{@snapshot_path}")
+        
         # upload image file
         if @bkst_drv_class.include?(BackupStorage::CommandAPI)
-          archive_from_snapshot(ctx, snapshot_path) do |cmd_tuple, chksum_path, size_path|
+          archive_from_snapshot(ctx, @snapshot_path) do |cmd_tuple, chksum_path, size_path|
             cmd_tuple2 = invoke_task(@bkst_drv_class,
                                      :upload_command, [nil, bo])
 
@@ -129,7 +131,7 @@ module Dcmgr
             evcb.setattr(chksum, alloc_size.to_i)
           end
         else
-          archive_from_snapshot(ctx, snapshot_path) do |cmd_tuple, chksum_path, size_path|
+          archive_from_snapshot(ctx, @snapshot_path) do |cmd_tuple, chksum_path, size_path|
             bkup_tmp = Tempfile.new(inst[:uuid], download_tmp_dir)
             begin
               bkup_tmp.close(false)
@@ -275,8 +277,10 @@ module Dcmgr
         end
       end
 
-      def take_snapshot_for_backup()
+      def take_snapshot_for_backup(image_path)
+        image_path
       end
+
       def clean_snapshot_for_backup()
       end
 
