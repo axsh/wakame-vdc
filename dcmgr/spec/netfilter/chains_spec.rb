c7720eb90a482aa404be80808fc7e78024c00fbc
Metallion <akieckens@axsh.net>
2013-06-24 17:11:43 +0900

    Added another rspec test scenario

--- dcmgr/spec/netfilter/chains_spec.rb
+++ dcmgr/spec/netfilter/chains_spec.rb
@@ -76,7 +76,6 @@ end
 describe "SGHandler and NetfilterAgent" do
   context "with 1 vnic, 1 host node, 1 security group" do
     let(:secg) { Fabricate(:secg) }
-
     let(:host) { Fabricate(:host_node) }
     let(:vnic) do
       Fabricate(:vnic, mac_addr: "525400033c48").tap do |n|
@@ -86,11 +85,9 @@ describe "SGHandler and NetfilterAgent" do
       end
     end
 
-    let(:handler) {SGHandlerTest.new}
+    let(:handler) {SGHandlerTest.new.tap{|sgh| sgh.add_host(host)}}
 
     it "should create and delete chains" do
-      host = vnic.instance.host_node
-      handler.add_host(host)
       handler.init_vnic(vnic.canonical_uuid)
 
       handler.get_netfilter_agent(host).chains[:l2].should =~ (
@@ -109,10 +106,8 @@ describe "SGHandler and NetfilterAgent" do
 
   context "with 2 vnics, 1 host node, 1 security group" do
     let(:secg) { Fabricate(:secg) }
-
     let(:host) { Fabricate(:host_node) }
-
-    let!(:vnicA) do
+    let(:vnicA) do
       Fabricate(:vnic, mac_addr: "525400033c48").tap do |n|
         n.add_security_group(secg)
         n.instance.host_node = host
@@ -128,12 +123,10 @@ describe "SGHandler and NetfilterAgent" do
       end
     end
 
-    let(:handler) {SGHandlerTest.new}
+    let(:handler) {SGHandlerTest.new.tap{|sgh| sgh.add_host(host)}}
 
     it "should create and delete chains" do
       # Create vnic A
-      host = vnicA.instance.host_node
-      handler.add_host(host)
       handler.init_vnic(vnicA.canonical_uuid)
 
       handler.get_netfilter_agent(host).chains[:l2].should =~ (
@@ -171,6 +164,63 @@ describe "SGHandler and NetfilterAgent" do
       # Destroy vnic B
       handler.destroy_vnic(vnicB.canonical_uuid)
       vnicB.destroy
+      handler.get_netfilter_agent(host).chains[:l2].should == []
+      handler.get_netfilter_agent(host).chains[:l3].should == []
+    end
+  end
+
+  context "with 2 vnics, 1 host node, 2 security groups" do
+    let(:host) { Fabricate(:host_node) }
+    let(:groupA) { Fabricate(:secg) }
+    let(:groupB) { Fabricate(:secg) }
+
+    let(:vnicA) do
+      Fabricate(:vnic, mac_addr: "525400033c48").tap do |n|
+        n.add_security_group(groupA)
+        n.instance.host_node = host
+        n.instance.save
+      end
+    end
+    let(:vnicB) do
+      Fabricate(:vnic, mac_addr: "525400033c49").tap do |n|
+        n.add_security_group(groupB)
+        n.instance.host_node = host
+        n.instance.save
+      end
+    end
+
+    let(:handler) {SGHandlerTest.new.tap{|sgh| sgh.add_host(host)}}
+
+    it "should create and delete chains" do
+      handler.init_vnic(vnicA.canonical_uuid)
+      handler.init_vnic(vnicB.canonical_uuid)
+
+      handler.get_netfilter_agent(host).chains[:l2].should =~ (
+        l2_chains_for_vnic(vnicA.canonical_uuid) +
+        l2_chains_for_vnic(vnicB.canonical_uuid) +
+        l2_chains_for_secg(groupA.canonical_uuid) +
+        l2_chains_for_secg(groupB.canonical_uuid)
+      )
+      handler.get_netfilter_agent(host).chains[:l3].should =~ (
+        l3_chains_for_vnic(vnicA.canonical_uuid) +
+        l3_chains_for_vnic(vnicB.canonical_uuid) +
+        l3_chains_for_secg(groupA.canonical_uuid) +
+        l3_chains_for_secg(groupB.canonical_uuid)
+      )
+
+      handler.destroy_vnic(vnicB.canonical_uuid)
+      vnicB.destroy
+      handler.get_netfilter_agent(host).chains[:l2].should =~ (
+        l2_chains_for_vnic(vnicA.canonical_uuid) +
+        l2_chains_for_secg(groupA.canonical_uuid)
+      )
+      handler.get_netfilter_agent(host).chains[:l3].should =~ (
+        l3_chains_for_vnic(vnicA.canonical_uuid) +
+        l3_chains_for_secg(groupA.canonical_uuid)
+      )
+
+      handler.destroy_vnic(vnicA.canonical_uuid)
+      vnicA.destroy
       handler.get_netfilter_agent(host).chains[:l2].should == []
       handler.get_netfilter_agent(host).chains[:l3].should == []
     end
