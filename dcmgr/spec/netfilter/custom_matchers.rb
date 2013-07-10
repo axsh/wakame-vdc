fbb0ffc4b5b0fc5d39d764f765138e9cdb683bf1
Metallion <akieckens@axsh.net>
2013-07-10 14:24:37 +0900

    Added rspec tests for netfilter standard rules

--- dcmgr/spec/netfilter/custom_matchers.rb
+++ dcmgr/spec/netfilter/custom_matchers.rb
@@ -78,16 +78,47 @@ RSpec::Matchers.define :have_applied_vnic do |vnic|
     @groups.map {|g| "vdc_#{g.canonical_uuid}_#{suffix}" }
   end
 
+  def l2_stnd_rules_for_vnic
+    vnic_ip = @vnic.direct_ip_lease.first.ipv4
+    gw_ip = @vnic.network && @vnic.network.ipv4_gw
+    dns_server = @vnic.network && @vnic.network.dns_server
+    rules = []
+
+    rules << "--protocol arp --arp-opcode Request --arp-ip-src=#{gw_ip} --arp-ip-dst=#{vnic_ip} -j ACCEPT" if gw_ip
+    rules << "--protocol arp --arp-opcode Request --arp-ip-src=#{dns_server} --arp-ip-dst=#{vnic_ip} -j ACCEPT" if dns_server
+    rules << "--protocol arp --arp-gratuitous --arp-ip-src=#{gw_ip} -j ACCEPT" if gw_ip
+    rules << "--protocol arp --arp-opcode Reply --arp-ip-dst=#{vnic_ip} --arp-mac-dst=#{@vnic.pretty_mac_addr} -j ACCEPT"
+    rules << "--protocol IPv4 -j ACCEPT"
+
+    rules
+  end
+
+  def l3_stnd_rules_for_vnic
+    dns_server = @vnic.network && @vnic.network.dns_server
+    dhcp_server = @vnic.network && @vnic.network.dhcp_server
+    rules = []
+
+    rules << "-m state --state RELATED,ESTABLISHED -j ACCEPT"
+    rules << "-p udp -d #{dns_server} --dport 53 -j ACCEPT" if dns_server
+    rules << "-p udp ! -s #{dhcp_server} --sport 67:68 -j DROP" if dhcp_server
+    rules << "-p udp -s #{dhcp_server} --sport 67:68 -j ACCEPT" if dhcp_server
+
+    rules
+  end
+
   chain :with_secgs do |secg_array|
     @groups = secg_array
   end
 
   match do |nfa|
     @nfa = nfa
+    @vnic = vnic
     @vnic_id = vnic.canonical_uuid
 
     expect_chains("ebtables", l2_chains_for_vnic) &&
     expect_chains("iptables", l3_chains_for_vnic) &&
+    expect_rules("ebtables", "vdc_#{@vnic_id}_d_standard", l2_stnd_rules_for_vnic) &&
+    expect_rules("iptables", "vdc_#{@vnic_id}_d_standard", l3_stnd_rules_for_vnic) &&
     ( @groups.nil? || (
       expect_jumps("ebtables", "vdc_#{@vnic_id}_d_isolation", group_chains("isolation")) &&
       expect_jumps("ebtables", "vdc_#{@vnic_id}_d_reffers", group_chains("reffers")) &&
