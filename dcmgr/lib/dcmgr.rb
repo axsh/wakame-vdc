2cd04cf9e578f3d92a019ead8afb4a38385bb584
t-iwano <t-iwano@axsh.net>
2013-07-11 20:42:42 +0900

    Add metric_collector and metric_monitor.

--- dcmgr/lib/dcmgr.rb
+++ dcmgr/lib/dcmgr.rb
@@ -138,6 +138,8 @@ module Dcmgr
     autoload :EventHook, 'dcmgr/node_modules/event_hook'
     autoload :JobQueueProxy, 'dcmgr/node_modules/job_queue_proxy'
     autoload :JobQueueWorker, 'dcmgr/node_modules/job_queue_worker'
+    autoload :MetricCollector, 'dcmgr/node_modules/metric_collector'
+    autoload :MetricMonitor, 'dcmgr/node_modules/metric_monitor'
   end
 
   module Helpers
