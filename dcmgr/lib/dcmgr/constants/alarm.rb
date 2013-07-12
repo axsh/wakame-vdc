6c43c8eb6d7a86d131a613d8ea93eeedb9f63c19
saicologic <s-mikami@axsh.net>
2013-07-12 18:33:44 +0900

    Add constants for alarms

--- dcmgr/lib/dcmgr/constants/alarm.rb
+++ dcmgr/lib/dcmgr/constants/alarm.rb
@@ -2,11 +2,28 @@
 
 module Dcmgr::Constants
   module Alarm
-    METRIC_NAMES = [
+    RESOURCE_METRICS = [
       'cpu.usage',
       'disk.usage',
       'memory.usage',
+    ].freeze
+
+    LOG_METRICS = [
       'log'
     ].freeze
+
+    SUPPORT_METRICS = (RESOURCE_METRICS + LOG_METRICS).freeze
+
+    SUPPORT_STATISTICS = [
+      'avg'
+    ].freeze
+
+    SUPPORT_COMPARISON_OPERATOR = [
+      'ge',
+      'gt',
+      'le',
+      'lt',
+    ].freeze
+
   end
 end
