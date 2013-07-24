b8ea08362834becb3b3be941637384ed1d2df611
t-iwano <t-iwano@axsh.net>
2013-07-24 13:15:13 +0900

    Added cpu evaluation.

--- dcmgr/lib/dcmgr/resource_monitor/metric_evaluation.rb
+++ dcmgr/lib/dcmgr/resource_monitor/metric_evaluation.rb
@@ -4,6 +4,13 @@ require 'metric_libs'
 module Dcmgr
   module ResourceMonitor
     class MetricEvaluation
+      COMPARISON_OPERATOR = {
+        :gt => :>,
+        :ge => :>=,
+        :lt => :<,
+        :le => :<=,
+        }
+
       def initialize
         @cache = {}
       end
@@ -17,8 +24,8 @@ module Dcmgr
         hash.values.each{ |h|
           h.each{ |k,v|
             ts = MetricLibs::TimeSeries.new
-            @cache[resource_id]["#{metric}.#{k}"] ||= {}
-            @cache[resource_id]["#{metric}.#{k}"] = ts.push(v, Time.at(h["time"].to_i))
+            @cache[resource_id]["#{metric}.#{k}"] ||= MetricLibs::TimeSeries.new
+            @cache[resource_id]["#{metric}.#{k}"].push(v, Time.at(h["time"].to_i))
           }
         }
       end
@@ -26,8 +33,27 @@ module Dcmgr
       def delete
       end
 
-      def evaluate
+      def evaluate(alarm)
+        raise ArgumentError unless alarm.is_a?(Hash)
+        
+        case alarm[:metric_name]
+        when 'cpu.usage'
+          end_time = Time.now.to_i
+          start_time = end_time - alarm[:params]["period"]
+          metric_value = @cache[alarm[:resource_id]]["cpu.total_usage"]
+
+          values = metric_value.find(Time.at(start_time), Time.at(end_time)).map {|v|
+            v.value
+          }
+          cpu_usage = values.inject{|sum, n| sum.to_f + n.to_f} / values.size
+
+          cpu_usage.method(COMPARISON_OPERATOR[alarm[:params]["comparison_operator"].to_sym]).call(alarm[:params]["threshold"])
+        when 'memory.usage'
+        else
+          raise "Unknown metric name: #{alarm[:metric_name]}"
+        end
       end
+
     end
   end
 end
