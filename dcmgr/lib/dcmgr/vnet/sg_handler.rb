21ce374979802743376ca6e59be3af9ad7c2e7a9
Metallion <akieckens@axsh.net>
2013-08-01 16:21:44 +0900

    Some more minor code cleanup to improve readability of sg_handler

--- dcmgr/lib/dcmgr/vnet/sg_handler.rb
+++ dcmgr/lib/dcmgr/vnet/sg_handler.rb
@@ -39,11 +39,11 @@ module Dcmgr::VNet::SGHandler
     logger.info "Telling host '#{host_node.canonical_uuid}' to initialize vnic '#{vnic_id}'."
     init_vnic_on_host(host_node, vnic)
 
-    vnic.security_groups.each {|group|
-      group.host_nodes.each {|host_node|
-        didnt_have_secg_yet = group.network_vif_dataset.filter(:instance => host_node.instances_dataset).exclude(:instance => vnic.instance).empty?
+    vnic.security_groups.each { |group|
+      group.host_nodes.each { |host_node|
+        host_didnt_have_secg_yet = group.network_vif_dataset.filter(:instance => host_node.instances_dataset).exclude(:instance => vnic.instance).empty?
 
-        if didnt_have_secg_yet
+        if host_didnt_have_secg_yet
           logger.debug "Host '#{host_node.canonical_uuid}' doesn't have security group '#{group.canonical_uuid}' yet. Initialize it."
           init_security_group(host_node, group)
         else
@@ -65,7 +65,7 @@ module Dcmgr::VNet::SGHandler
     vnic = M::NetworkVif[vnic_id]
     vnic_host = vnic.instance.host_node
 
-    current_sg_uuids = vnic.security_groups.map {|g| g.canonical_uuid }
+    current_sg_uuids = vnic.security_groups.map { |g| g.canonical_uuid }
     new_sg_uuids = sg_uuids - current_sg_uuids
 
     new_sg_uuids.each { |group_id|
@@ -73,7 +73,7 @@ module Dcmgr::VNet::SGHandler
       hosts_before_change = group.host_nodes
       vnic.add_security_group(group)
 
-      hosts_before_change.each {|host_node|
+      hosts_before_change.each { |host_node|
         update_isolation_group(host_node, group)
       }
 
@@ -92,14 +92,14 @@ module Dcmgr::VNet::SGHandler
     vnic = M::NetworkVif[vnic_id]
     vnic_host = vnic.instance.host_node
 
-    current_sg_ids = vnic.security_groups.map {|g| g.canonical_uuid }
-    set_vnic_security_groups(vnic_host, vnic, (current_sg_ids - sg_ids_to_remove))
+    current_sg_ids = vnic.security_groups.map { |g| g.canonical_uuid }
+    set_vnic_security_groups(vnic_host, vnic, current_sg_ids - sg_ids_to_remove)
 
     (sg_ids_to_remove & current_sg_ids).each { |group_id|
       group = M::SecurityGroup[group_id]
       vnic.remove_security_group(group)
 
-      group.host_nodes.each {|host_node|
+      group.host_nodes.each { |host_node|
         update_isolation_group(host_node, group)
         handle_referencees(host_node, group)
       }
@@ -123,20 +123,18 @@ module Dcmgr::VNet::SGHandler
       group_id = group.canonical_uuid
 
       refresh_referencers(group)
-      group.host_nodes.each {|host_node|
-        no_more_instances_left_in_group = group.network_vif_dataset.filter(:instance => host_node.instances_dataset).exclude(:instance => vnic.instance).empty?
+      group.host_nodes.each { |host_node|
+        no_more_vnics_left_in_group = group.network_vif_dataset.filter(:instance => host_node.instances_dataset).exclude(:instance => vnic.instance).empty?
 
-        if no_more_instances_left_in_group
+        if no_more_vnics_left_in_group
           logger.debug "Host '#{host_node.canonical_uuid}' no longer has security group '#{group.canonical_uuid}' yet. Destroy it."
-
           destroy_security_group(host_node, group_id)
         else
           logger.debug "Host '#{host_node.canonical_uuid}' still has security group '#{group.canonical_uuid}'. Update its isolation."
           # The vnic isn't destroyed in the database until after this method is called.
           # Therefore we delete it from the ips we pass to the isolation group.
-          # Hence the third argument to this method
-          self_ips = vnic.direct_ip_lease.map {|lease| lease.ipv4_s }
-          update_isolation_group(host_node, group, self_ips)
+          self_ips = vnic.direct_ip_lease.map { |lease| lease.ipv4_s }
+          update_isolation_group(host_node, group, group.vnic_ips - self_ips)
         end
       }
 
@@ -149,7 +147,7 @@ module Dcmgr::VNet::SGHandler
     group = M::SecurityGroup[secg_id]
     rules = group.rules_array_no_ref
 
-    group.host_nodes.each {|host_node|
+    group.host_nodes.each { |host_node|
       logger.info "Updating rules of group '#{secg_id}' on host '#{host_node.canonical_uuid}'"
       call_packetfilter_service(host_node, "update_sg_rules", secg_id, rules)
       handle_referencees(host_node, group)
@@ -179,8 +177,8 @@ module Dcmgr::VNet::SGHandler
     call_packetfilter_service(host, "destroy_isolation_group", group_id)
   end
 
-  def update_isolation_group(host, group, exclude_friend_ips = [])
-    friend_ips = group.vnic_ips - exclude_friend_ips
+  def update_isolation_group(host, group, friend_ips = nil)
+    friend_ips ||= group.vnic_ips
     call_packetfilter_service(host, "update_isolation_group", group.canonical_uuid, friend_ips)
   end
 
@@ -189,7 +187,7 @@ module Dcmgr::VNet::SGHandler
   end
 
   def set_vnic_security_groups(host, vnic, group_ids = nil)
-    group_ids ||= vnic.security_groups.map {|sg| sg.canonical_uuid}
+    group_ids ||= vnic.security_groups.map { |sg| sg.canonical_uuid}
     call_packetfilter_service(host, "set_vnic_security_groups", vnic.canonical_uuid, group_ids)
   end
 
@@ -197,8 +195,8 @@ module Dcmgr::VNet::SGHandler
     #TODO: Right now all of this is updated every time a single referencee is changed
     # It would be better if we could handle it per referencee group somehow
     rules = group.rules_array_only_ref
-    parsed_rules = group.referencees.map {|reffee|
-      rules.map {|r|
+    parsed_rules = group.referencees.map { |reffee|
+      rules.map { |r|
         if r[:ip_source] == reffee.canonical_uuid
           reffee.vnic_ips.map { |ref_ip|
             parsed_rule = r.dup
@@ -209,14 +207,14 @@ module Dcmgr::VNet::SGHandler
       }
     }.flatten.compact
 
-    ref_ips = group.referencees.map {|ref| ref.vnic_ips}.flatten.uniq
+    ref_ips = group.referencees.map { |ref| ref.vnic_ips}.flatten.uniq
 
     call_packetfilter_service(host, "set_sg_referencers", group.canonical_uuid, ref_ips, parsed_rules)
   end
 
   def refresh_referencers(group)
-    group.referencers.each {|ref_g|
-      ref_g.host_nodes.each {|ref_h|
+    group.referencers.each { |ref_g|
+      ref_g.host_nodes.each { |ref_h|
         logger.info "Telling host '#{ref_h.canonical_uuid}' to update referencees for group '#{ref_g.canonical_uuid}'."
         handle_referencees(ref_h, ref_g)
       }
