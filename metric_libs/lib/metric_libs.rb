b4065fa564381094eeb7e22e0e1fe069ab972222
t-iwano <t-iwano@axsh.net>
2013-08-02 14:11:03 +0900

    added alarm_manager, alarm, timer and constants.

--- metric_libs/lib/metric_libs.rb
+++ metric_libs/lib/metric_libs.rb
@@ -6,4 +6,11 @@ module MetricLibs
   autoload :MetricValue, 'metric_libs/metric_value'
   autoload :TimeSeries, 'metric_libs/time_series'
   autoload :VERSION, 'metric_libs/version'
+  autoload :AlarmManager, 'metric_libs/alarm_manager'
+  autoload :Alarm, 'metric_libs/alarm'
+  autoload :Timer, 'metric_libs/timer'
+
+  module Constants
+    autoload :Alarm, 'metric_libs/constants/alarm'
+  end
 end
---
+++ metric_libs/lib/metric_libs/constants/alarm.rb
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+
+module MetricLibs::Constants
+  module Alarm
+    RESOURCE_METRICS = [
+      'cpu.usage',
+      'disk.usage',
+      'memory.usage',
+    ].freeze
+
+    LOG_METRICS = [
+      'log'
+    ].freeze
+
+    SUPPORT_METRICS = (RESOURCE_METRICS + LOG_METRICS).freeze
+
+    SUPPORT_STATISTICS = [
+      'avg'
+    ].freeze
+
+    SUPPORT_COMPARISON_OPERATOR = {
+      'ge' => :>=,
+      'gt' => :>,
+      'le' => :<=,
+      'lt' => :<,
+    }.freeze
+
+  end
+end
