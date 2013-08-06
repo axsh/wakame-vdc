7543599c3317d19b0693408cb72f7d53ba2443bd
t-iwano <t-iwano@axsh.net>
2013-08-06 16:36:06 +0900

    added evaluation count.

--- metric_libs/lib/metric_libs/alarm.rb
+++ metric_libs/lib/metric_libs/alarm.rb
@@ -4,6 +4,8 @@ module MetricLibs
   class Alarm
     include MetricLibs::Constants::Alarm
 
+    attr_reader :uuid, :resource_id, :metric_name
+
     def initialize(alm, manager)
       set_variable(alm)
       @manager = manager
@@ -16,37 +18,45 @@ module MetricLibs
 
     def feed(data)
       raise ArgumentError unless data.is_a?(Hash)
-      metric_name = @metric_name.split('.')
-      @resource.push(data[@resource_id][metric_name[0]][metric_name[1]],
-        Time.at(data[@resource_id][metric_name[0]]["time"].to_i))
+      @resource.push(data[@metric_name], Time.at(data["time"].to_i))
     end
 
     def evaluate
-      time = Time.now
-      ev = case @metric_name
-           when 'cpu.usage'
-             end_time = time.to_i
-             start_time = end_time - @evaluation_periods
-             values = @resource.find(Time.at(start_time), Time.at(end_time)).map {|v|
+      case @metric_name
+      when 'cpu.usage'
+        end_time = Time.now.to_i
+        start_time = end_time - @evaluation_periods
+        values = @resource.find(Time.at(start_time), Time.at(end_time)).map {|v|
           v.value
         }
-             usage = values.inject{|sum, n| sum.to_f + n.to_f} / values.size
-             usage.method(SUPPORT_COMPARISON_OPERATOR[@params["comparison_operator"]]).call(@params["threshold"])
-           when 'memory.usage'
-             memory_usage = @resource.last.value.to_f
-             memory_usage.method(SUPPORT_COMPARISON_OPERATOR[@params["comparison_operator"]]).call(@params["threshold"])
-           else
-             raise "Unknown metric name: #{@metric_name}"
-           end
+        usage = values.inject{|sum, n| sum.to_f + n.to_f} / values.size
+        ev = usage.method(SUPPORT_COMPARISON_OPERATOR[@params["comparison_operator"]]).call(@params["threshold"])
+        @resource.delete_first
+        ev
+      when 'memory.usage'
+        memory_usage = @resource.last.value.to_f
+        ev = memory_usage.method(SUPPORT_COMPARISON_OPERATOR[@params["comparison_operator"]]).call(@params["threshold"])
+        @resource.delete_first
+        ev
+      else
+        raise "Unknown metric name: #{@metric_name}"
+      end
+    end
 
-      delete_resource(time)
-      ev
+    def evaluate?
+      case @metric_name
+      when "cpu.usage"
+        return false if @resource.length <= 1
+      when "memory.usage"
+      end
+      @resource.length >= @evaluation_count ? true : false
     end
 
     private
     def set_variable(alm)
       raise ArgumentError unless alm.is_a?(Hash)
       alm.each {|k, v| instance_variable_set("@#{k}", v) }
+      @evaluation_count = @evaluation_periods.to_i / @capture_periods.to_i unless @capture_periods.nil?
     end
 
     def delete_resource(time)
