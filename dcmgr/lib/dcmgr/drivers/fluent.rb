0d713f90e5292720a3657cae9b87041ce8f356fb
saicologic <s-mikami@axsh.net>
2013-08-09 20:52:07 +0900

    Modify path to config file

--- dcmgr/lib/dcmgr/drivers/fluent.rb
+++ dcmgr/lib/dcmgr/drivers/fluent.rb
@@ -6,13 +6,11 @@ module Dcmgr
       include Dcmgr::Helpers::TemplateHelper
       include Dcmgr::Helpers::CliHelper
 
-      CONFIG_FILENAME = 'fluent.conf'.freeze
-
       @template_base_dir = "fluent"
 
       def initialize
-        @template_file_name = CONFIG_FILENAME
-        @output_file_name = CONFIG_FILENAME
+        @template_file_name = 'fluent.conf'
+        @output_file = Dcmgr.conf.logging_service_conf
       end
 
       def set_alarms(alarms)
@@ -29,17 +27,16 @@ module Dcmgr
       end
 
       def generate_config
-        output_file_path = File.join(Dcmgr.conf.logging_service_tmp, @output_file_name)
-        if File.exists?(output_file_path)
-          FileUtils.rm(output_file_path)
+        if File.exists?(@output_file)
+          FileUtils.rm(@output_file)
         end
-        render_template(@template_file_name, output_file_path, binding)
+        render_template(@template_file_name, @output_file, binding)
       end
 
       def reload
         if Dcmgr.conf.logging_service_reload
           sh("#{Dcmgr.conf.logging_service_reload} reload")
-          logger.info("Reload fluent with #{File.join(Dcmgr.conf.logging_service_tmp, @output_file_name)}")
+          logger.info("Reload fluent with #{@output_file}")
         else
           logger.error("Failed to reload fluent. Not found #{Dcmgr.conf.logging_service_reload}")
         end
