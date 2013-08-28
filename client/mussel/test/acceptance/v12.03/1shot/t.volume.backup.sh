47715e160429b05f4c8af24cd041695642fed80d
Masahiro Fujiwara <m-fujiwara@axsh.net>
2013-08-28 12:18:42 +0900

    put last response result file to the shunit2's shared tmp folder.

--- client/mussel/test/acceptance/v12.03/1shot/t.volume.backup.sh
+++ client/mussel/test/acceptance/v12.03/1shot/t.volume.backup.sh
@@ -17,11 +17,17 @@ display_name=${display_name:-}
 is_cacheable=${is_cacheable:-}
 is_public=${is_public:-}
 
-## functions
+## hook functions
 
 function after_create_instance() {
   run_cmd instance poweroff ${instance_uuid} >/dev/null
   retry_until "document_pair? instance ${instance_uuid} state halted"
+}
+
+last_result_path=""
+
+function setUp() {
+  last_result_path=$(mktemp --tmpdir=${SHUNIT_TMPDIR})
 }
 
 ### step
@@ -34,24 +40,22 @@ function test_get_volume() {
 
 # PUT $base_uri/instances/i-xxxxx/volumes/vol-xxxxxx
 function test_backup_volume() {
-  local result_path=$(mktemp)
-  run_cmd instance show_volumes "${instance_uuid}" > $result_path
+  run_cmd instance show_volumes "${instance_uuid}" > $last_result_path
   assertEquals $? 0
   
-  local boot_volume_uuid=$(yaml_find_first 'uuid' < $result_path)
+  local boot_volume_uuid=$(yaml_find_first 'uuid' < $last_result_path)
   test -n "$boot_volume_uuid"
   assertEquals $? 0
 
-  run_cmd instance backup_volume ${instance_uuid} $boot_volume_uuid > $result_path
+  run_cmd instance backup_volume ${instance_uuid} $boot_volume_uuid > $last_result_path
   assertEquals $? 0
 
 
-  local backup_obj_uuid=$(yaml_find_first 'backup_object_id' < $result_path)
+  local backup_obj_uuid=$(yaml_find_first 'backup_object_id' < $last_result_path)
   test -n "$backup_obj_uuid"
   assertEquals $? 0
 
   retry_until "document_pair? backup_object ${backup_obj_uuid} state available"
-  rm -f $result_path
 }
 
 ## shunit2
