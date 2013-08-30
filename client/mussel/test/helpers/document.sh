07c8fa7287e18869240d9c96910295b625494529
Masahiro Fujiwara <m-fujiwara@axsh.net>
2013-08-30 17:40:35 +0900

    add yaml convert functions.

--- client/mussel/test/helpers/document.sh
+++ client/mussel/test/helpers/document.sh
@@ -33,3 +33,46 @@ function yaml_find_last() {
   local key="$1"
   awk '$0 ~ key_pattern { last_item=$(NF) } END { print $last_item }' key_pattern=":$key:"
 }
+
+# Convert YAML text to "/" delimitered format for retrieving from shell script.
+#cat <<EOF | ydump
+#---
+#:xxxx:
+#  :yyyy: 2
+#  :zzzz: 3
+#:oooo:
+#- a
+#- b
+#EOF
+#
+#:xxxx:/:yyyy:=2
+#:xxxx:/:zzzz:=3
+#:oooo:/0=a
+#:oooo:/1=b
+#
+function ydump() {
+  # nm: namespace array
+  # lv:  namespace level
+  # cidt: current indent depth
+  # lidt: last indent depth
+  # ac: array counter
+  awk -v lv=1 -v ac=0 -v cidt=1 -v lidt=1 'BEGIN{ }
+function path_join(ary, _i,_r,_s) { _r = ary[1]; for(_i = 2; _i <= length(ary); _i++){ _r = _r "/" ary[_i];}; return _r; }
+{ match($0, /[^ ]+/); cidt=RSTART; }
+$1 == "-" { cidt = cidt + 2; }
+cidt < lidt { delete nm[lv]; lv--; }
+cidt > lidt { lv++; ac=0; }
+$1 ~ /^:?[[:alnum:]_]+:/ && NF == 1 { nm[lv]=$1; }
+$1 ~ /^:?[[:alnum:]_]+:/ && NF == 2 { print path_join(nm) "/" $1 "=" $2; }
+# 
+$1 == "-" { nm[lv]=ac++; }
+$1 == "-" && NF == 2 { print path_join(nm) "=" $2; }
+$1 == "-" && $2 ~ /^:?[[:alnum:]_]+:/ && NF == 3 { print path_join(nm) "/" $2 "=" $3; }
+{ lidt=cidt; }'
+}
+
+# Return value part from ydump() format.
+# ydump | yfind '/0/:uuid:'
+function yfind() {
+  awk -v key="$1" -F= '$1 == key { print $2; exit; }'
+}
