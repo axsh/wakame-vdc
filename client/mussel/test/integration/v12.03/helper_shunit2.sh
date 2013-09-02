bde5be205b3a90a3410a90e58988e3da5a231dec
t-iwano <t-iwano@axsh.net>
2013-09-02 14:59:41 +0900

    add alarm helper.

--- client/mussel/test/integration/v12.03/helper_shunit2.sh
+++ client/mussel/test/integration/v12.03/helper_shunit2.sh
@@ -18,6 +18,7 @@ readonly shunit2_file=${BASH_SOURCE[0]%/*}/../../shunit2
 . ${BASH_SOURCE[0]%/*}/../../helpers/ssl.sh
 . ${BASH_SOURCE[0]%/*}/../../helpers/load_balancer.sh
 . ${BASH_SOURCE[0]%/*}/../../helpers/external_ip.sh
+. ${BASH_SOURCE[0]%/*}/../../helpers/alarm.sh
 
 ## environment-specific configuration
 
