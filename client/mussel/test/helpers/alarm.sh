9a779bf75592a1c5e392b431763d8063ca155dca
t-iwano <t-iwano@axsh.net>
2013-09-03 18:22:13 +0900

    update alarm initial parameter.

--- client/mussel/test/helpers/alarm.sh
+++ client/mussel/test/helpers/alarm.sh
@@ -14,14 +14,14 @@ declare alarm_uuid
 
 resource_id=${resource_id:-i-demo0001}
 metric_name=${metric_name:-log}
-# enabled=${enabled:-true}
+enabled=${enabled:-true}
 
 ### params
 params=${params:-}
 
 ### optional
 evaluation_periods=${evaluation_periods:-60}
-notification_periods=${notification_periods:-60}
+notification_periods=${notification_periods:-180}
 ok_actions=${ok_actions:-}
 alarm_actions=${alarm_actions:-}
 insufficient_data_actions=${insufficient_data_actions:-}
