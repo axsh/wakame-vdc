f985eb1b0ea93c0ab305055075f9abb089f5c3f0
Masahiro Fujiwara <m-fujiwara@axsh.net>
2013-09-19 18:05:18 +0900

    mussel: add scenario for image backup.

--- client/mussel/test/acceptance/v12.03/1shot/t.instance.backup.sh
+++ client/mussel/test/acceptance/v12.03/1shot/t.instance.backup.sh
@@ -107,6 +107,52 @@ function test_backup_second_volume_from_backup() {
   assertEquals 0 $?
 }
 
+# API test for image backup.
+#
+# 1. boot instance with second blank volume.
+# 2. poweroff the instance.
+# 3. take backup of OS image.
+# 4. shutdown everything.
+function test_backup_second_blank_volume() {
+  # boot instance with second blank volume.
+  if is_container_hypervisor; then
+    volumes_args="volumes[0][size]=1G volumes[0][volume_type]=local volumes[0][guest_device_name]=/mnt/tmp"
+  else
+    volumes_args="volumes[0][size]=1G volumes[0][volume_type]=local"
+  fi
+  create_instance
+
+  run_cmd instance poweroff ${instance_uuid} >/dev/null
+  retry_until "document_pair? instance ${instance_uuid} state halted"
+
+  run_cmd instance show_volumes ${instance_uuid} | ydump > $last_result_path
+  assertEquals 0 $?
+
+  local ex_volume_uuid=$(yfind '1/:uuid:' < $last_result_path)
+  test -n "$ex_volume_uuid"
+  assertEquals 0 $?
+
+  run_cmd instance backup ${instance_uuid} | ydump > $last_result_path
+  assertEquals 0 $?
+
+  local image_uuid=$(yfind ':image_id:' < $last_result_path)
+  test -n "$image_uuid"
+  assertEquals 0 $?
+
+  local backup_object_uuid=$(yfind ':backup_object_id:' < $last_result_path)
+  test -n "$backup_object_uuid"
+  assertEquals 0 $?
+
+  retry_until "document_pair? image ${image_uuid} state available"
+
+  run_cmd image destroy ${image_uuid}
+  assertEquals 0 $?
+
+  run_cmd instance destroy ${instance_uuid} >/dev/null
+  assertEquals 0 $?
+}
+
+
 
 ## shunit2
 
