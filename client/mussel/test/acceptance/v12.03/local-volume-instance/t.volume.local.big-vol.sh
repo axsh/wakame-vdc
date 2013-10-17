628130c3857dd28f75b122a3fbf6f733783c45d5
Masahito Yoshida <masahito@axsh.net>
2013-10-18 01:09:13 +0900

    mussel,test: fix sourced type sciprt.

--- client/mussel/test/acceptance/v12.03/local-volume-instance/t.volume.local.big-vol.sh
+++ client/mussel/test/acceptance/v12.03/local-volume-instance/t.volume.local.big-vol.sh
@@ -8,4 +8,6 @@
 ## wrapper
 
 blank_volume_size=${blank_volume_size:-10G}
-. ${BASH_SOURCE[0]%/*}/t.volume.local.base.sh
+# set SHUNIT_PARENT to sourced file to make sure to run test_xx functions in sourced file.
+SHUNIT_PARENT=${BASH_SOURCE[0]%/*}/t.volume.local.base.sh
+. ${SHUNIT_PARENT}
