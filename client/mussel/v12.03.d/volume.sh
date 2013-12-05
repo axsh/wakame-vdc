459f7fe1ccee1185f70df3b0e4bda1546015d82d
Masahiro Fujiwara <m-fujiwara@axsh.net>
2013-12-05 18:56:28 +0900

    mussel: add "volume create" sub command.

--- client/mussel/v12.03.d/volume.sh
+++ client/mussel/v12.03.d/volume.sh
@@ -19,3 +19,19 @@ task_backup() {
    ) \
    $(base_uri)/${namespace}s/${uuid}/backup.$(suffix)
 }
+
+
+task_create() {
+  local namespace=$1 cmd=$2 uuid=$3
+  [[ -n "${namespace}" ]] || { echo "[ERROR] 'namespace' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${cmd}"       ]] || { echo "[ERROR] 'cmd' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+
+  call_api -X POST $(urlencode_data \
+    $(add_param volume_size         string) \
+    $(add_param storage_node_id     string) \
+    $(add_param backup_object_id    string) \
+   ) \
+   $(add_args_param volumes) \
+   $(base_uri)/${namespace}s.$(suffix)
+}
+
