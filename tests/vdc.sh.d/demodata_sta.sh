7b5fda820091ac10cd5fa383109215f8cc86aebf
Masahiro Fujiwara <m-fujiwara@axsh.net>
2013-12-05 14:08:52 +0900

    add db migration to drop columns will be moved to sta.conf.

--- tests/vdc.sh.d/demodata_sta.sh
+++ tests/vdc.sh.d/demodata_sta.sh
@@ -14,17 +14,13 @@ case ${sta_server} in
   ${ipaddr})
   shlog ./bin/vdc-manage storage iscsi add sta.${sta_id} \
     --uuid sn-${sta_id} \
-    --base-path ${vdc_data}/volumes \
     --disk-space $((1024 * 1024)) \
     --ipaddr ${sta_server} \
-    --snapshot-base-path ${vdc_data}/snap
  ;;
 *)
   shlog ./bin/vdc-manage storage iscsi add sta.${sta_id} \
    --uuid sn-${sta_id} \
-   --base-path xpool \
    --disk-space $((1024 * 1024)) \
    --ipaddr ${sta_server} \
-   --snapshot-base-path /export/home/wakame/vdc/sta/snap
  ;;
 esac
