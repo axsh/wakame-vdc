db7bc13bc2e4c443fdc327de48dcf9351230e8df
Masahiro Fujiwara <m-fujiwara@axsh.net>
2014-02-03 18:31:14 +0900

    mussel: add "host_nodes evacuate" operation.

--- client/mussel/v12.03.d/host_node.sh
+++ client/mussel/v12.03.d/host_node.sh
@@ -4,3 +4,15 @@
 #
 
 . ${BASH_SOURCE[0]%/*}/base.sh
+
+task_evacuate() {
+  local namespace=$1 cmd=$2 uuid=$3
+  [[ -n "${namespace}" ]] || { echo "[ERROR] 'namespace' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${cmd}"       ]] || { echo "[ERROR] 'cmd' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${uuid}"      ]] || { echo "[ERROR] 'uuid' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+
+  call_api -X PUT $(urlencode_data \
+    $(add_param host_node_id  string) \
+   ) \
+   $(base_uri)/${namespace}s/${uuid}/evacuate.$(suffix)
+}
