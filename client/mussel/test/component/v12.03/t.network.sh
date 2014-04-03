e864588e8a85ac13f420be8b57bc97c60c2ffde1
akry <a-yokokawa@axsh.net>
2014-04-03 20:27:32 +0900

    Added network_create component test

--- client/mussel/test/component/v12.03/t.network.sh
+++ client/mussel/test/component/v12.03/t.network.sh
@@ -14,6 +14,33 @@ declare namespace=network
 
 ## functions
 
+function test_network_create() {
+  local cmd=create
+
+  local opts="
+    --account_id=a-shpoolxx
+    --display_name=virtual
+    --network_mode=virtual
+    --description=vnet
+    --domain_name=vnet
+    --prefix=24
+    --service_dhcp=10.100.0.1
+  "
+
+  local params="
+    description=vnet
+    display_name=virtual
+    domain_name=vnet
+    network_mode=virtual
+    prefix=24
+    service_dhcp=10.100.0.1
+    account_id=a-shpoolxx
+  "
+
+  assertEquals "$(cli_wrapper ${namespace} ${cmd} ${opts})" \
+               "curl -X POST $(urlencode_data ${params}) $(base_uri)/${namespace}s"
+}
+
 ## shunit2
 
 . ${shunit2_file}
