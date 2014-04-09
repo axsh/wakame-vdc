8b460d4df171053b9a536574230cd5090a9677d8
akry <a-yokokawa@axsh.net>
2014-04-10 07:04:30 +0900

    Created task_index and task_create for dc_network

--- client/mussel/v12.03.d/dc_network.sh
+++ client/mussel/v12.03.d/dc_network.sh
@@ -4,3 +4,16 @@
 #
 
 . ${BASH_SOURCE[0]%/*}/base.sh
+
+task_index() {
+  xquery="name=vnet"
+  cmd_index $*
+}
+
+task_create() {
+  call_api -X POST $(urlencode_data \
+    $(add_param name string) \
+    $(add_param offering_network_modes string) \
+   ) \
+   $(base_uri)/${namespace}s.$(suffix)
+}
