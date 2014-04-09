234383353a497c01d4c7bc5fec42e4185d2a5559
akry <a-yokokawa@axsh.net>
2014-04-10 07:07:46 +0900

    Added test_create_network to see if a coresponded network entry is created on vnet

---
+++ client/mussel/test/integration/v12.03/rw/t.network.sh
@@ -0,0 +1,33 @@
+
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. ${BASH_SOURCE[0]%/*}/helper_shunit2.sh
+
+## functions
+
+function extract_uuid() {
+  local key=$1
+  awk '{if(match($0,/'${key}'\-[0-9a-zA-Z]+/)){print substr($0, RSTART, RLENGTH);}}'
+}
+
+function test_network_create() {
+  dc_network=$(run_cmd dc_network index | hash_value id)
+  vdc_network_uuid=$(run_cmd network create | extract_uuid nw)
+  vnet_network_uuid=$(curl -fsSkL -X GET http://${DCMGR_HOST}:9090/api/networks/${vdc_network_uuid} | extract_uuid nw)
+
+  echo "vdc_network_uuid = ${vdc_network_uuid}"
+  echo "vnet_network_uuid = ${vnet_network_uuid}"
+
+  assertEquals ${vnet_network_uuid} ${vdc_network_uuid}
+}
+
+
+## shunit2
+
+. ${shunit2_file}
