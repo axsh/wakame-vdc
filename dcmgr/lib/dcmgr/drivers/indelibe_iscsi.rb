f349d86390476f3f18bef884167bd8432c1b56cc
Metallion <akieckens@axsh.net>
2014-04-16 16:26:01 +0900

    Updated the indelibe iscsi driver to use Ruby's http library and indelibe FS' new command syntax

--- dcmgr/lib/dcmgr/drivers/indelibe_iscsi.rb
+++ dcmgr/lib/dcmgr/drivers/indelibe_iscsi.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#require 'yaml'
+require 'net/http'
 
 module Dcmgr::Drivers
   class IndelibeIscsi < IscsiTarget
@@ -16,26 +16,33 @@ module Dcmgr::Drivers
     end
 
     def create(ctx)
-      @volume_id = ctx.volume_id
-      @volume = ctx.volume
-      @vol_path = @volume[:volume_device][:iscsi_storage_node][:export_path]
-      @snap_path = @volume[:volume_device][:iscsi_storage_node][:snap_path]
-      @ip = @volume[:volume_device][:iscsi_storage_node][:ip_address]
+      @ip = ctx.volume[:volume_device][:iscsi_storage_node][:ip_address]
 
-      iqn = "#{IQN_PREFIX}:#{@volume_id}"
+      iqn = "#{IQN_PREFIX}:#{ctx.volume_id}"
+      vol_path = ctx.volume[:volume_device][:iscsi_storage_node][:export_path]
 
-      sh("curl -s http://#{@ip}:#{@web_ui_port}/iscsi/#{@vol_path}/#{@volume_id}?export=#{iqn}")
+      #TODO: Error handling
+      ifs_iscsi("#{vol_path}/#{ctx.volume_id}", :export, target: iqn)
 
       { :iqn => iqn, :lun => 0 }
     end
 
     def delete(ctx)
-      @volume_id = ctx.volume_id
-      @volume = ctx.volume
-      @ip = @volume[:volume_device][:iscsi_storage_node][:ip_address]
+      @ip = ctx.volume[:volume_device][:iscsi_storage_node][:ip_address]
 
-      sh("curl -s http://#{@ip}:#{@web_ui_port}/iscsi?unexport=#{IQN_PREFIX}:#{@volume_id}")
+      ifs_iscsi("", :unexport, target: "#{IQN_PREFIX}:#{ctx.volume_id}")
     end
 
+    private
+    def ifs_iscsi(uri_suffix, cmd, params = {}, &blk)
+      uri = "http://#{@ip}:#{@web_ui_port}/iscsi/#{uri_suffix}?"
+      params[:cmd] = cmd
+      uri.concat params.to_a.map { |i| "#{i.first}=#{i.last}" }.join("&")
+      logger.debug "Calling Indelibe FS server: " + uri
+
+      JSON.parse(Net::HTTP.get(URI(uri))).tap { |output|
+        blk.call(output) if block_given?
+      }
+    end
   end
 end
