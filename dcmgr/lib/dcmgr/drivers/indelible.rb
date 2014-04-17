a64dabd57c45d4aeecc1c77784a2ea27c19b6122
Metallion <akieckens@axsh.net>
2014-04-17 13:56:16 +0900

    Some code cleanup

--- dcmgr/lib/dcmgr/drivers/indelible.rb
+++ dcmgr/lib/dcmgr/drivers/indelible.rb
@@ -21,34 +21,29 @@ module Dcmgr::Drivers
     end
 
     def create_volume(ctx, snap_file = nil)
-      @volume_id   = ctx.volume_id
-      @volume      = ctx.volume
+      volume_id = ctx.volume_id
       #TODO: Nilchecks... how many do we need here?
-      @webapi_ip = @volume[:volume_device][:iscsi_storage_node][:ip_address]
-      @vol_path = @volume[:volume_device][:iscsi_storage_node][:export_path]
+      vol_path = ctx.volume[:volume_device][:iscsi_storage_node][:export_path]
 
-      ifsutils(@vol_path, :mkdir) unless directory_exists?(@vol_path)
+      ifsutils(vol_path, :mkdir) unless directory_exists?(vol_path)
 
-      if @snapshot
-        snap_path = @snapshot[:destination_key].split(":").last
-        new_vol_path = @vol_path.split("/",2).last
+      if ctx.snapshot
+        snap_path = ctx.snapshot[:destination_key].split(":").last
+        new_vol_path = vol_path.split("/",2).last
 
-        ifsutils(snap_path, :duplicate, dest: "#{new_vol_path}/#{@volume_id}")
+        ifsutils(snap_path, :duplicate, dest: "#{new_vol_path}/#{volume_id}")
       else
-        path = "#{@vol_path}/#{@volume_id}"
-        ifsutils(path, :allocate, size: "#{@volume[:size]}") { |result|
-          e = result["error"]
-          if e
-            raise "Indelibe FS error code #{e["code"]}. Long reason:\n#{e["longReason"]}"
-          end
+        path = "#{vol_path}/#{volume_id}"
+        ifsutils(path, :allocate, size: "#{ctx.volume[:size]}") { |result|
+          result["error"] && raise "Indelibe FS error code %s. Long reason:\n%s}" %
+              [result["error"]["code"], result["error"]["longReason"]]
         }
       end
 
-      logger.info("created new volume: #{@volume_id}")
+      logger.info("created new volume: #{volume_id}")
     end
 
     def delete_volume(ctx)
-      @webapi_ip = ctx.volume[:volume_device][:iscsi_storage_node][:ip_address]
       vol_path   = ctx.volume[:volume_device][:iscsi_storage_node][:export_path]
 
       logger.info("Deleting volume: #{ctx.volume_id}")
@@ -56,13 +51,10 @@ module Dcmgr::Drivers
     end
 
     def create_snapshot(ctx)
-      @volume    = ctx.volume
-      @vol_path  = @volume[:volume_device][:iscsi_storage_node][:export_path]
-      @webapi_ip = @volume[:volume_device][:iscsi_storage_node][:ip_address]
-
+      vol_path  = ctx.volume[:volume_device][:iscsi_storage_node][:export_path]
       new_snap_path = snapshot_path(ctx)
 
-      ifsutils("#{@vol_path}/#{@volume[:uuid]}", :duplicate, new_snap_path)
+      ifsutils("#{vol_path}/#{volume[:uuid]}", :duplicate, new_snap_path)
 
       logger.info("created new snapshot: #{new_snap_path}")
     end
