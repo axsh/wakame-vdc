78d7919425452ecf8c8af79ca7ff621fccc2ac05
Metallion <akieckens@axsh.net>
2014-04-22 16:00:48 +0900

    Set the indelible FS volumes path in sta.conf instead of in the database

--- dcmgr/lib/dcmgr/drivers/indelible.rb
+++ dcmgr/lib/dcmgr/drivers/indelible.rb
@@ -12,61 +12,40 @@ module Dcmgr::Drivers
     def_configuration do
       param :webapi_port, default: 8091
       param :webapi_ip, default: "127.0.0.1"
+      #TODO: Raise error when not provided in the config file
+      param :indelible_volume
+      param :wakame_volumes_dir, default: "volumes"
     end
 
     def initialize()
       super
       @webapi_port = Dcmgr.conf.backing_store.webapi_port
       @webapi_ip   = Dcmgr.conf.backing_store.webapi_ip
+      indelible_volume = Dcmgr.conf.backing_store.indelible_volume
+      wakame_volumes_dir = Dcmgr.conf.backing_store.wakame_volumes_dir
+
+      @vol_path = "#{indelible_volume}/#{wakame_volumes_dir}"
     end
 
     def create_volume(ctx, snap_file = nil)
       volume_id = ctx.volume_id
-      #TODO: Nilchecks... how many do we need here?
-      vol_path = ctx.volume[:volume_device][:iscsi_storage_node][:export_path]
 
-      ifsutils(vol_path, :mkdir) unless directory_exists?(vol_path)
+      ifsutils(@vol_path, :mkdir) unless directory_exists?(@vol_path)
 
-      if ctx.snapshot
-        snap_path = ctx.snapshot[:destination_key].split(":").last
-        new_vol_path = vol_path.split("/",2).last
-
-        ifsutils(snap_path, :duplicate, dest: "#{new_vol_path}/#{volume_id}")
-      else
-        path = "#{vol_path}/#{volume_id}"
-        ifsutils(path, :allocate, size: "#{ctx.volume[:size]}") { |result|
-          if result["error"]
-            raise "Indelibe FS error code %s. Long reason:\n%s}" %
-              [result["error"]["code"], result["error"]["longReason"]]
-          end
-        }
-      end
+      path = "#{@vol_path}/#{volume_id}"
+      ifsutils(path, :allocate, size: "#{ctx.volume[:size]}") { |result|
+        if result["error"]
+          raise "Indelibe FS error code %s. Long reason:\n%s}" %
+            [result["error"]["code"], result["error"]["longReason"]]
+        end
+      }
 
       logger.info("created new volume: #{volume_id}")
     end
 
     def delete_volume(ctx)
-      vol_path   = ctx.volume[:volume_device][:iscsi_storage_node][:export_path]
-
       logger.info("Deleting volume: #{ctx.volume_id}")
-      ifsutils("#{vol_path}/#{ctx.volume_id}", :delete)
-    end
-
-    def create_snapshot(ctx)
-      vol_path  = ctx.volume[:volume_device][:iscsi_storage_node][:export_path]
-      new_snap_path = snapshot_path(ctx)
-
-      ifsutils("#{vol_path}/#{volume[:uuid]}", :duplicate, new_snap_path)
-
-      logger.info("created new snapshot: #{new_snap_path}")
-    end
-
-    # do nothing because IFS's snapshot is as same as the backup object.
-    def delete_snapshot(ctx)
-    end
-
-    def snapshot_path(ctx)
-      ctx.backup_object[:object_key]
+      ifsutils("#{@vol_path}/#{ctx.volume_id}", :delete)
     end
   end
 end
--- dcmgr/lib/dcmgr/drivers/indelible_iscsi.rb
+++ dcmgr/lib/dcmgr/drivers/indelible_iscsi.rb
@@ -10,6 +10,9 @@ module Dcmgr::Drivers
     def_configuration do
       param :webapi_ip, default: "127.0.0.1"
       param :webapi_port, default: 8090
+      #TODO: Raise error when not provided in the config file
+      param :indelible_volume
+      param :wakame_volumes_dir, default: "volumes"
     end
 
     def initialize()
@@ -17,14 +20,16 @@ module Dcmgr::Drivers
       @iqn_prefix  = Dcmgr.conf.iscsi_target.iqn_prefix
       @webapi_ip   = Dcmgr.conf.iscsi_target.webapi_ip
       @webapi_port = Dcmgr.conf.iscsi_target.webapi_port
+      indelible_volume = Dcmgr.conf.backing_store.indelible_volume
+      wakame_volumes_dir = Dcmgr.conf.backing_store.wakame_volumes_dir
+
+      @vol_path = "#{indelible_volume}/#{wakame_volumes_dir}"
     end
 
     def create(ctx)
       iqn = "#{@iqn_prefix}:#{ctx.volume_id}"
-      vol_path = ctx.volume[:volume_device][:iscsi_storage_node][:export_path]
-
       #TODO: Error handling
-      ifs_iscsi("#{vol_path}/#{ctx.volume_id}", :export, target: iqn)
+      ifs_iscsi("#{@vol_path}/#{ctx.volume_id}", :export, target: iqn)
 
       { :iqn => iqn, :lun => 0 }
     end
