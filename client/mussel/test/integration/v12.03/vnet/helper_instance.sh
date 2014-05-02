908b22fdfa8c4f4e85c0afb6cdf38563b05fcf01
akry <a-yokokawa@axsh.net>
2014-05-02 20:08:52 +0900

    Configure eth0 after instance gets ready.

--- client/mussel/test/integration/v12.03/vnet/helper_instance.sh
+++ client/mussel/test/integration/v12.03/vnet/helper_instance.sh
@@ -8,7 +8,10 @@
 
 ## variables
 
+declare instance_ipaddr=
 declare instance_uuids_path=$(generate_cache_file_path instance_uuids)
+
+function needs_vif() { true; }
 
 ## functions
 
@@ -17,16 +20,26 @@ declare instance_uuids_path=$(generate_cache_file_path instance_uuids)
 function oneTimeSetUp() {
   # launching instances
   for i in $(eval echo "{1..${target_instance_num:-1}}"); do
-    instance_uuid=i-vm${i}
     create_instance
     echo ${instance_uuid} >> ${instance_uuids_path}
   done
 
-  # wait until the instance be ready
   for instance_uuid in $(cat ${instance_uuids_path}); do
     instance_ipaddr="$(cached_instance_param ${instance_uuid} | hash_value address)"
+
+    # wait until the instance be ready
     wait_for_network_to_be_ready ${instance_ipaddr}
     wait_for_port_to_be_ready    ${instance_ipaddr} tcp ${instance_port}
+
+    # configure eth0
+    ssh root@${instance_ipaddr} -i ${ssh_key_pair_path} "
+      rm -rf /etc/sysconfig/network-scripts/ifcfg-eth0
+      cat 'DEVICE=eth0' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+      cat 'TYPE=Ethernet' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+      cat 'BOOTPROTO=dhcp' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+      cat 'ONBOOT=yes' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+      service network restart
+    "
   done
 }
 
