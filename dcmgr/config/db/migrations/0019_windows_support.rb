44a0ef323ebcd9be7b5984e6a70e81a97eb4623a
Metallion <akieckens@axsh.net>
2014-05-22 18:13:56 +0900

    Added a column to instances to keep the encrypted windows administrator password

--- dcmgr/config/db/migrations/0019_windows_support.rb
+++ dcmgr/config/db/migrations/0019_windows_support.rb
@@ -5,11 +5,19 @@ Sequel.migration do
     alter_table(:images) do
       add_column :os_type, "varchar(255)", null: false, default: "generic"
     end
+
+    alter_table(:instances) do
+      add_column :encrypted_password, "varchar(255)"
+    end
   end
 
   down do
     alter_table(:images) do
       drop_column :os_type
+    end
+
+    alter_table(:instances) do
+      drop_column :encrypted_password
     end
   end
 end
