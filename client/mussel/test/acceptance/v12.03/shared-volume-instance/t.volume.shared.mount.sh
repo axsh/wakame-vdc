b606be11bb91bd07bd099980f3ff43379aebc808
t-iwano <t-iwano@axsh.net>
2014-05-26 14:38:41 +0900

    shared-volume-instance:t.volume.shared.mount.sh add test_mount_shared_volume_halted_instance

--- client/mussel/test/acceptance/v12.03/shared-volume-instance/t.volume.shared.mount.sh
+++ client/mussel/test/acceptance/v12.03/shared-volume-instance/t.volume.shared.mount.sh
@@ -81,6 +81,99 @@ function test_mount_shared_volume(){
   assertEquals 0 $?
 }
 
+function test_mount_shared_volume_halted_instance(){
+  remote_sudo=$(remote_sudo)
+
+  # poweroff instance
+  run_cmd instance poweroff ${instance_uuid} >/dev/null
+  retry_untill "document_pair? instance ${instance_uuid} state halted"
+  assertEquals 0 $?
+
+  # create new volume
+  volume_uuid=$(volume_size=${volume_size} run_cmd volume create | hash_value uuid)
+  retry_until "document_pair? volume ${volume_uuid} state available"
+  assertEquals 0 $?
+
+  # attach volume to instance
+  instance_id=${instance_uuid} run_cmd volume attach ${volume_uuid}
+  retry_until "document_pair? volume ${volume_uuid} state attached"
+  assertEquals 0 $?
+
+  # poweron instance
+  run_cmd instance poweron ${instance_uuid} >/dev/null
+  retry_until "document_pair? instance ${instance_uuid} state running"
+  assertEquals 0 $?
+
+  # wait for network to be ready
+  wait_for_network_to_be_ready ${instance_ipaddr}
+
+  # wait for sshd to be ready
+  wait_for_sshd_to_be_ready    ${instance_ipaddr}
+
+  # TODO: temporarily using device /dev/sdc, /dev/vdc
+  # blank device path
+  blank_dev_path=$(blank_dev_path)
+  [[ -n "${blank_dev_path}" ]]
+  assertEquals 0 $?
+  [[ -n "${blank_dev_path}" ]] || return
+
+  # device check
+  ssh -t  ${ssh_user}@${instance_ipaddr} -i ${ssh_key_pair_path} <<-EOS
+	${remote_sudo} lsblk -d ${blank_dev_path}
+	EOS
+  assertEquals 0 $?
+
+  # format
+  ssh -t ${ssh_user}@${instance_ipaddr} -i ${ssh_key_pair_path} <<-EOS
+	${remote_sudo} mkfs.ext3 -F -I 128 ${blank_dev_path}
+	EOS
+  assertEquals 0 $?
+
+  # mount
+  ssh -t ${ssh_user}@${instance_ipaddr} -i ${ssh_key_pair_path} <<-EOS
+	${remote_sudo} mount ${blank_dev_path} /mnt
+	EOS
+  assertEquals 0 $?
+
+  # disk-usage
+  ssh -t ${ssh_user}@${instance_ipaddr} -i ${ssh_key_pair_path} <<-EOS
+	df -P -h
+	EOS
+  assertEquals 0 $?
+
+  # poweroff instance
+  run_cmd instance poweroff ${instance_uuid} >/dev/null
+  retry_untill "document_pair? instance ${instance_uuid} state halted"
+  assertEquals 0 $?
+
+  # detach volume to instance
+  instance_id=${instance_uuid} run_cmd volume detach ${volume_uuid}
+  retry_until "document_pair? volume ${volume_uuid} state available"
+  assertEquals 0 $?
+
+  # poweron instance
+  run_cmd instance poweron ${instance_uuid} >/dev/null
+  retry_until "document_pair? instance ${instance_uuid} state running"
+  assertEquals 0 $?
+
+  # wait for network to be ready
+  wait_for_network_to_be_ready ${instance_ipaddr}
+
+  # wait for sshd to be ready
+  wait_for_sshd_to_be_ready    ${instance_ipaddr}
+
+  # device-check
+  ssh -t  ${ssh_user}@${instance_ipaddr} -i ${ssh_key_pair_path} <<-EOS
+	${remote_sudo} lsblk -d ${blank_dev_path}
+	EOS
+  assertNotEquals 0 $?
+
+  # delete volume
+  run_cmd volume destroy ${volume_uuid}
+  retry_until "document_pair? volume ${volume_uuid} state deleted"
+  assertEquals 0 $?
+}
+
 ## shunit2
 . ${shunit2_file}
 
