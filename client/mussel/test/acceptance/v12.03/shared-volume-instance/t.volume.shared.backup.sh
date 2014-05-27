2f2c0fec19e60344adb5c209aa07c4b8dff9c9cc
t-iwano <t-iwano@axsh.net>
2014-05-27 19:16:32 +0900

    shared-volume-instance:t.volume.shared.backup.sh add volume backup second blank volume

--- client/mussel/test/acceptance/v12.03/shared-volume-instance/t.volume.shared.backup.sh
+++ client/mussel/test/acceptance/v12.03/shared-volume-instance/t.volume.shared.backup.sh
@@ -135,6 +135,40 @@ function test_image_backup_just_for_boot_volume() {
 #   wait_for_sshd_to_be_ready    ${instance_ipaddr}
 # }
 
+function test_volume_backup_second_blank_volume(){
+  # poweroff instance
+  run_cmd instance poweroff ${instance_uuid} >/dev/null
+  retry_until "document_pair? instance ${instance_uuid} state halted"
+  assertEquals 0 $?
+
+  run_cmd instance show_volumes ${instance_uuid} | ydump > $last_result_path
+  assertEquals 0 $?
+
+  local ex_volume_uuid=$(yfind '1/:uuid:' < $last_result_path)
+  test -n "$ex_volume_uuid"
+  assertEquals 0 $?
+
+  # backup second blank volume
+  run_cmd instance backup_volume ${instance_uuid} $ex_volume_uuid | ydump > $last_result_path
+  assertEquals 0 $?
+
+  local backup_obj_uuid=$(yfind ':backup_object_id:' < $last_result_path)
+  test -n "$backup_obj_uuid"
+  assertEquals 0 $?
+
+  # assert that poweron should fail until backup task completes.
+  run_cmd instance poweron ${instance_uuid} >/dev/null
+  assertNotEquals 0 $?
+
+  retry_until "document_pair? backup_object ${backup_obj_uuid} state available"
+  assertEquals 0 $?
+
+  # delete backup_object
+  run_cmd backup_object destroy ${backup_obj_uuid}
+  assertEquals 0 $?
+
+}
+
 ## shunit2
 . ${shunit2_file}
 
