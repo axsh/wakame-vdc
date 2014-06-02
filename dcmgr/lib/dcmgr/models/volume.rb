b6dbf6c8cd5aa93c3b3516b7efde94f2cd740fea
Masahiro Fujiwara <m-fujiwara@axsh.net>
2014-06-02 22:41:47 +0900

    Revert "fix validation error at detaching volume from the halted instance."
    This reverts commit 5f385e228aa5fe27ff03dd9fe0c11d66bdb3697d.

--- dcmgr/lib/dcmgr/models/volume.rb
+++ dcmgr/lib/dcmgr/models/volume.rb
@@ -61,8 +61,7 @@ module Dcmgr::Models
 
       errors.add(:size, "Invalid volume size: #{self.size}") if self.size < 0
 
-      # test column value directly because associated model object might be cached.
-      if !self.instance_id.nil?
+      if self.instance
         # check if volume parameters are conformant for hypervisor.
         hypervisor_class = Dcmgr::Drivers::Hypervisor.driver_class(self.instance.hypervisor.to_sym)
         hypervisor_class.policy.validate_volume_model(self)
