d7b8be01cb1a10adf5387e9baf61d5f884fbce22
t-iwano <t-iwano@axsh.net>
2014-06-10 15:18:31 +0900

    feature-shared-volume-instance: commentout backup tasks

--- client/mussel/test/acceptance/v12.03/shared-volume-instance/t.volume.shared.base.sh
+++ client/mussel/test/acceptance/v12.03/shared-volume-instance/t.volume.shared.base.sh
@@ -38,50 +38,50 @@ function test_create_new_volume_from_image(){
   assertEquals 0 $?
 }
 
-function test_volume_backup_from_new_volume(){
-  # create volume
-  volume_uuid=$(volume_size=${blank_volume_size} run_cmd volume create | hash_value uuid)
-  retry_until "document_pair? volume ${volume_uuid} state available"
-  assertEquals 0 $?
+# function test_volume_backup_from_new_volume(){
+#   # create volume
+#   volume_uuid=$(volume_size=${blank_volume_size} run_cmd volume create | hash_value uuid)
+#   retry_until "document_pair? volume ${volume_uuid} state available"
+#   assertEquals 0 $?
 
-  # create backup object from new volume
-  backup_obj_uuid=$(run_cmd volume backup ${volume_uuid} | hash_value backup_object_id)
-  retry_until "document_pair? backup_object ${backup_obj_uuid} state available"
-  assertEquals 0 $?
+#   # create backup object from new volume
+#   backup_obj_uuid=$(run_cmd volume backup ${volume_uuid} | hash_value backup_object_id)
+#   retry_until "document_pair? backup_object ${backup_obj_uuid} state available"
+#   assertEquals 0 $?
 
-  # delete backup_object
-  run_cmd backup_object destroy ${backup_obj_uuid}
-  assertEquals 0 $?
+#   # delete backup_object
+#   run_cmd backup_object destroy ${backup_obj_uuid}
+#   assertEquals 0 $?
 
-  # delete volume
-  run_cmd volume destroy ${volume_uuid}
-  retry_until "document_pair? volume ${volume_uuid} state deleted"
-  assertEquals 0 $?
-}
+#   # delete volume
+#   run_cmd volume destroy ${volume_uuid}
+#   retry_until "document_pair? volume ${volume_uuid} state deleted"
+#   assertEquals 0 $?
+# }
 
-function test_volume_backup_from_image(){
-  # image show
-  backup_obj_uuid=$(run_cmd image show ${image_id} | hash_value backup_object_id)
+# function test_volume_backup_from_image(){
+#   # image show
+#   backup_obj_uuid=$(run_cmd image show ${image_id} | hash_value backup_object_id)
 
-  # create volume
-  volume_uuid=$(backup_object_id=${backup_obj_uuid} run_cmd volume create | hash_value uuid)
-  retry_until "document_pair? volume ${volume_uuid} state available"
-  assertEquals 0 $?
+#   # create volume
+#   volume_uuid=$(backup_object_id=${backup_obj_uuid} run_cmd volume create | hash_value uuid)
+#   retry_until "document_pair? volume ${volume_uuid} state available"
+#   assertEquals 0 $?
 
-  # create backup object from image
-  backup_obj_uuid=$(run_cmd volume backup ${volume_uuid} | hash_value backup_object_id)
-  retry_until "document_pair? backup_object ${backup_obj_uuid} state available"
-  assertEquals 0 $?
+#   # create backup object from image
+#   backup_obj_uuid=$(run_cmd volume backup ${volume_uuid} | hash_value backup_object_id)
+#   retry_until "document_pair? backup_object ${backup_obj_uuid} state available"
+#   assertEquals 0 $?
 
-  # delete backup_object
-  run_cmd backup_object destroy ${backup_obj_uuid}
-  assertEquals 0 $?
+#   # delete backup_object
+#   run_cmd backup_object destroy ${backup_obj_uuid}
+#   assertEquals 0 $?
 
-  # delete volume
-  run_cmd volume destroy ${volume_uuid}
-  retry_until "document_pair? volume ${volume_uuid} state deleted"
-  assertEquals 0 $?
-}
+#   # delete volume
+#   run_cmd volume destroy ${volume_uuid}
+#   retry_until "document_pair? volume ${volume_uuid} state deleted"
+#   assertEquals 0 $?
+# }
 
 ## shunit2
 
