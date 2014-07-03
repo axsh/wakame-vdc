f5eeb5ac8660480b03a96542c232c304c4938b5d
Metallion <akieckens@axsh.net>
2014-07-03 14:06:23 +0900

    Took care of some code duplication in the netfilter related rspec matchers

--- dcmgr/spec/netfilter/matchers/chain_methods.rb
+++ dcmgr/spec/netfilter/matchers/chain_methods.rb
@@ -14,13 +14,11 @@ module ChainMethods
   def expect_chains(bin, chains)
     actual_chains = @nfa.send(bin).all_chain_names
     if (actual_chains & chains).sort == chains.sort
-      succeed_with "There were chains applied that we expected not to.\n
-      expected: [#{chains.join(", ")}]\n
-      got: [#{actual_chains.join(", ")}]"
+      succeed_with "There were chains applied that we expected not to.\n%s" %
+        find_unexpected(chains, actual_chains)
     else
-      fail_with "The chains we expected weren't applied.\n
-      expected: [#{chains.join(", ")}]\n
-      got: [#{actual_chains.join(", ")}]"
+      fail_with "The chains we expected weren't applied.\n%s" %
+        miss_expected(chains, actual_chains)
     end
   end
 
@@ -29,13 +27,11 @@ module ChainMethods
     expected = rules.sort
 
     if (actual & expected).sort == expected
-      succeed_with "#{bin} chain #{chain} had rules applied that we expected not to be.\n
-      expected: [#{expected.join(", ")}]\n
-      got: [#{actual.join(", ")}]"
+      succeed_with "#{bin} chain '%s' had rules applied that we expected not to be.\n%s" %
+        [chain, find_unexpected(expected, actual)]
     else
-      fail_with "#{bin} chain #{chain} didn't apply the rules we expected.\n
-      expected: [#{expected.join(", ")}]\n
-      got: [#{actual.join(", ")}]"
+      fail_with "#{bin} chain '%s' didn't apply the rules we expected.\n%s" %
+        [chain, miss_expected(expected, actual)]
     end
   end
 
@@ -44,12 +40,11 @@ module ChainMethods
     expected = rules.sort
 
     if actual == expected
-      succeed_with "#{bin} chain '#{chain}' had rules we expected it not to have.\n
-      rules: [#{actual.join(", ")}]"
+      succeed_with "#{bin} chain '%s' had rules we expected it not to have.\n%s" %
+        [chain, find_unexpected(expected, actual)]
     else
-      fail_with "#{bin} chain '#{chain}' didn't have the rules we expected.\n
-      expected: [#{expected.join(", ")}]\n
-      got: [#{actual.join(", ")}]"
+      fail_with "#{bin} chain '%s' didn't have the rules we expected.\n%s" %
+        [chain, miss_expected(expected, actual)]
     end
   end
 
@@ -58,12 +53,11 @@ module ChainMethods
     expected = rules.sort
 
     if actual == expected
-      succeed_with "iptables nat chain '#{chain}' had rules we expected it not to have.\n
-      rules: [#{actual.join(", ")}]"
+      succeed_with "iptables nat chain '%s' had rules we expected it not to have.\n%s" %
+        [chain, find_unexpected(expected, actual)]
     else
-      fail_with "iptables nat chain '#{chain}' didn't have the rules we expected.\n
-      expected: [#{expected.join(", ")}]\n
-      got: [#{actual.join(", ")}]"
+      fail_with "iptables nat chain '%s' didn't have the rules we expected.\n%s" %
+        [chain, miss_expected(expected, actual)]
     end
   end
 
@@ -72,12 +66,23 @@ module ChainMethods
     expected = targets.sort
 
     if actual == expected
-      succeed_with "#{bin} chain '#{chain}' had jumps we expected it not to have.\n
-      jumps: [#{actual.join(", ")}]"
+      succeed_with "#{bin} chain '%s' had jumps we expected it not to have.\n%s" %
+        [chain, find_unexpected(expected, actual)]
     else
-      fail_with "#{bin} chain '#{chain}' didn't have the jumps we expected.\n
-      expected: [#{expected.join(", ")}]\n
-      got: [#{actual.join(", ")}]"
+      fail_with "#{bin} chain '%s' didn't have the jumps we expected.\n%s" %
+        [chain, miss_expected(expected, actual)]
     end
+  end
+
+  def miss_expected(expected, got)
+    "expected:\n  [#{expected.join(", ")}]\n" +
+    "got:\n  [#{got.join(", ")}]\n" +
+    "missed:\n  [#{(expected - got).join(", ")}]"
+  end
+
+  def find_unexpected(expected, got)
+    "expected:\n  [#{expected.join(", ")}]\n" +
+    "got:\n  [#{got.join(", ")}]\n" +
+    "unexpected:\n  [#{(got - expected).join(", ")}]"
   end
 end
