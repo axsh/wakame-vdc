29d2c9836b25605be4f5c1d97bc1b5e4d4ded105
Metallion <akieckens@axsh.net>
2014-07-30 17:23:48 +0900

    Created the first actual working api test

--- dcmgr/spec/dcmgr/endpoints/12.03/helper.rb
+++ dcmgr/spec/dcmgr/endpoints/12.03/helper.rb
@@ -5,6 +5,10 @@ require 'spec_helper'
 require "rack"
 require "rack/test"
 
+def body
+  JSON.parse(last_response.body)
+end
+
 RSpec.configure do |c|
   c.include Rack::Test::Methods
 end
--- dcmgr/spec/dcmgr/endpoints/12.03/instances_spec.rb
+++ dcmgr/spec/dcmgr/endpoints/12.03/instances_spec.rb
@@ -2,9 +2,23 @@
 require_relative 'helper'
 
 describe "instances" do
-  it "gets us some instances" do
-    get "instances"
-    puts last_response.errors
+  describe "GET" do
+    before(:each)  { get("instances", params) }
+
+    context "with no parameters" do
+      let(:params) { Hash.new }
+
+      context "with no instances in the database" do
+        it "shows that there are indeed no instances in the database" do
+           expect(body).to eq [{
+            "total" => 0,
+            "start" => 0,
+            "limit" => 250,
+            "results" => []
+           }]
+        end
+      end
+    end
   end
 end
 
