3a7b9087c0ce191cf929af549d76757f9902a3e8
Metallion <akieckens@axsh.net>
2014-07-31 16:53:32 +0900

    Renamed instance to instance_hash in the metadata type class to better reflect what it's expecting

--- dcmgr/lib/dcmgr/metadata.rb
+++ dcmgr/lib/dcmgr/metadata.rb
@@ -3,17 +3,17 @@
 module Dcmgr::Metadata
 
   # Factory method
-  def self.md_type(instance)
+  def self.md_type(instance_hash)
     # In the future we can use this to provide different types of metadata items
-    # depending on the instance.
+    # depending on the instance_hash.
 
     # For now we have only one type of metadata items so we're making one of those.
-    AWS.new(instance)
+    AWS.new(instance_hash)
   end
 
   class MetadataType
-    def initialize(instance)
-      @inst = instance
+    def initialize(instance_hash)
+      @inst = instance_hash
     end
 
     def get_items
