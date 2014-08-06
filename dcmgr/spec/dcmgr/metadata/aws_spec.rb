344f508c08407c18854503c9ce8aa9c9ec653758
Metallion <akieckens@axsh.net>
2014-08-06 18:11:37 +0900

    Cleaned up aws_spec a bit

--- dcmgr/spec/dcmgr/metadata/aws_spec.rb
+++ dcmgr/spec/dcmgr/metadata/aws_spec.rb
@@ -3,31 +3,28 @@
 require 'spec_helper'
 require 'yaml'
 
-def it_does_not_crash
-  it 'doesn\'t crash' do
-    items
-  end
-end
-
-describe Dcmgr::Metadata do
-  describe Dcmgr::Metadata::AWS do
-    let(:inst) { Fabricate(:instance) }
-    let(:items) { Dcmgr::Metadata::AWS.new(inst.to_hash).get_items }
+describe Dcmgr::Metadata::AWS do
+  describe "#get_items" do
+    subject(:items) { Dcmgr::Metadata::AWS.new(inst.to_hash).get_items }
 
     context 'with all settings to make it work' do
       let(:inst) do
-        Fabricate(:instance,
-          request_params: {}
-        )
+        Fabricate(:instance, request_params: {})
       end
 
       it 'sets metadata items that mimic the aws metadata layout' do
         expect(items['ami-id']).to eq inst.image.canonical_uuid
+        expect(items['hostname']).to eq inst.hostname
+        expect(items['instance-action']).to eq inst.state
       end
     end
 
     context "without request_params set" do
-      it_does_not_crash
+      let(:inst) { Fabricate(:instance) }
+
+      it 'doesn\'t crash' do
+        items
+      end
     end
   end
 end
