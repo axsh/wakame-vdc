46fc48b9d5bcd01a7787d7a02a6d492a9a700fb8
Metallion <akieckens@axsh.net>
2014-08-07 14:53:04 +0900

    Added the fabricators needed to simulate instance with ip leases

--- dcmgr/spec/fabricators/network_vif.rb
+++ dcmgr/spec/fabricators/network_vif.rb
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+#TODO: Investigate invalid mac error that comes up some time
+
 Fabricator(:network_vif, class_name: Dcmgr::Models::NetworkVif) do
   mac_addr { Fabricate(:mac_lease).mac_addr.to_s(16) }
+end
+
+Fabricator(:network_vif_with_ip, class_name: Dcmgr::Models::NetworkVif) do
+  mac_addr { Fabricate(:mac_lease).mac_addr.to_s(16) }
+  ip(count: 1) { Fabricate(:network_vif_ip_lease) }
 end
