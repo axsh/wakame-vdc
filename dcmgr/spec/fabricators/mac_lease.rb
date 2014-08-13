be6001089f9046014018e202d09909fd1a1ae527
Metallion <akieckens@axsh.net>
2014-08-13 15:13:26 +0900

    Made mac address fabrication sequential to work around network_vif's problem with mac_addr string length

--- dcmgr/spec/fabricators/mac_lease.rb
+++ dcmgr/spec/fabricators/mac_lease.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 Fabricator(:mac_lease, class_name: Dcmgr::Models::MacLease) do
-  mac_addr { rand(0xffffffffffff) }
+  mac_addr { sequence(:mac_addr, 0x525400000001) }
 end
