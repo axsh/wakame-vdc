788c8446f323cc997b84cd58b46c5facdf997b96
Metallion <akieckens@axsh.net>
2014-08-18 18:24:44 +0900

    Added a test for adding interfaces with no networks set

--- dcmgr/spec/dcmgr/scheduler/network/vifs_request_param_spec.rb
+++ dcmgr/spec/dcmgr/scheduler/network/vifs_request_param_spec.rb
@@ -69,5 +69,19 @@ describe "Dcmgr::Scheduler::Network::VifsRequestParam" do
       end
     end
 
+    context "with a single entry with no network in the vifs parameter" do
+      let(:vifs_parameter) do
+        { "eth0" => {"index" => 0 } }
+      end
+
+      it "schedules a single network interface for the instance" do
+        expect(inst.network_vif.size).to eq 1
+      end
+
+      it "doesn't schedule a network for the interface" do
+        expect(inst.network_vif.first.network).to be nil
+      end
+    end
+
   end
 end
