b68e3dbb3323c591a972b6fb7ab32c02ca680511
Metallion <akieckens@axsh.net>
2014-08-18 15:35:12 +0900

    First tests for VifsRequestParam scheduler... still red so far

---
+++ dcmgr/spec/dcmgr/scheduler/network/vifs_request_param_spec.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+require 'spec_helper'
+
+describe "Dcmgr::Scheduler::Network::VifsRequestParam" do
+  describe "#schedule" do
+    subject(:inst) do
+      Fabricate(:instance, request_params: {vifs: vifs_parameter}).tap do |i|
+        Dcmgr::Scheduler::Network::VifsRequestParam.new.schedule(i)
+      end
+    end
+
+    context "with a malformed vifs parameter" do
+      let(:vifs_parameter) { "JOSSEFIEN!" }
+
+      it { is_expected.to raise_error Dcmgr::Scheduler::NetworkSchedulingError }
+    end
+
+    context "with a single entry in the vifs parameter" do
+      let(:network) { Fabricate(:network) }
+
+      let(:vifs_parameter) do
+        { "eth0" => {"index" => 0, "network" => network.canonical_uuid } }
+      end
+
+      it "schedules a single network interface for the instance" do
+        expect(inst.network_vif.size).to eq 1
+        expect(inst.network_vif.first.network).to be network
+      end
+
+    end
+  end
+end
