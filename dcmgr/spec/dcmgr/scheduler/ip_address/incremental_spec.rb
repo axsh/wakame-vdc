6d2783e31c9abfce90c953b7cfb70c9814703aa1
Metallion <akieckens@axsh.net>
2014-08-19 15:51:49 +0900

    Moved examples into their own files and added a test for exhausted dhcp range

--- dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
+++ dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
@@ -2,13 +2,14 @@
 
 require 'spec_helper'
 
+Dir["#{File.dirname(__FILE__)}/incremental_examples/*.rb"].each {|f| require f }
+
 describe Dcmgr::Scheduler::IPAddress::Incremental do
   include NetworkHelper
 
   describe "#schedule" do
     subject { network_vif }
 
-    let(:network) { Fabricate(:network).tap {|n| set_dhcp_range(n)} }
     let(:network_vif) { Fabricate(:network_vif, network: network) }
 
     describe "sad paths" do
@@ -16,29 +17,8 @@ describe Dcmgr::Scheduler::IPAddress::Incremental do
         lambda { Dcmgr::Scheduler::IPAddress::Incremental.new.schedule(options) }
       end
 
-      context "when options is not a Hash nor a NetworkVif" do
-        let(:options) { "BURN!" }
-
-        it { is_expected.to raise_error ArgumentError }
-      end
-
-      context "when options[:network] isn't a Network" do
-        let(:options) { { network: "me not network"} }
-
-        it { is_expected.to raise_error ArgumentError }
-      end
-
-      context "when options[:network_vif] isn't a NetworkVif" do
-        let(:options) { { network: network, network_vif: "me not network vif"} }
-
-        it { is_expected.to raise_error ArgumentError }
-      end
-
-      context "when options[:ip_pool] isn't a IpPool" do
-        let(:options) { { network: network, ip_pool: "me not ip pool"} }
-
-        it { is_expected.to raise_error ArgumentError }
-      end
+      include_examples "fail argument checks"
+      include_examples "dhcp range exhausted"
     end
   end
 end
