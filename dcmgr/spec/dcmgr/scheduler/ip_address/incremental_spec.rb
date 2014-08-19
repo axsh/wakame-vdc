ee522470c69e59abd269cd3ba31e71d6e64821c0
Metallion <akieckens@axsh.net>
2014-08-19 18:31:42 +0900

    Use single quotes when we're not doing any string manipulation

--- dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
+++ dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
@@ -7,29 +7,29 @@ Dir["#{File.dirname(__FILE__)}/incremental_examples/*.rb"].each {|f| require f }
 describe Dcmgr::Scheduler::IPAddress::Incremental do
   include NetworkHelper
 
-  describe "#schedule" do
+  describe '#schedule' do
     let(:network_vif) { Fabricate(:network_vif, network: network) }
     let(:incremental) { Dcmgr::Scheduler::IPAddress::Incremental.new }
 
-    describe "sad paths" do
+    describe 'sad paths' do
       subject { lambda { incremental.schedule(options) } }
 
-      include_examples "fail argument checks"
-      include_examples "dhcp range exhausted"
+      include_examples 'fail argument checks'
+      include_examples 'dhcp range exhausted'
     end
 
-    describe "happy paths" do
+    describe 'happy paths' do
       subject do
         incremental.schedule(network_vif)
 
         network_vif.direct_ip_lease.first.ipv4
       end
 
-      include_examples "first ip in range"
-      include_examples "one range full, one empty"
-      include_examples "gateway in dhcp range"
-      include_examples "wraparound dhcp range"
-      include_examples "reassign released addresses"
+      include_examples 'first ip in range'
+      include_examples 'one range full, one empty'
+      include_examples 'gateway in dhcp range'
+      include_examples 'wraparound dhcp range'
+      include_examples 'reassign released addresses'
     end
   end
 end
