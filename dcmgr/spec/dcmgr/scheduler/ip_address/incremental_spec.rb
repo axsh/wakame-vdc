9274b88e0d719349b6d80fbd6a9251d335bb3021
Metallion <akieckens@axsh.net>
2014-08-19 16:14:49 +0900

    Created the first simple happy path test for the incremental scheduler

--- dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
+++ dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
@@ -8,8 +8,6 @@ describe Dcmgr::Scheduler::IPAddress::Incremental do
   include NetworkHelper
 
   describe "#schedule" do
-    subject { network_vif }
-
     let(:network_vif) { Fabricate(:network_vif, network: network) }
 
     describe "sad paths" do
@@ -19,6 +17,26 @@ describe Dcmgr::Scheduler::IPAddress::Incremental do
 
       include_examples "fail argument checks"
       include_examples "dhcp range exhausted"
+    end
+
+    describe "happy paths" do
+      subject do
+        Dcmgr::Scheduler::IPAddress::Incremental.new.schedule(options)
+        network_vif
+      end
+
+      context "when options is a NetworkVif with its network set" do
+        let(:network) do
+          n = Fabricate(:network, ipv4_network: "192.168.0.0")
+          set_dhcp_range(n)
+          n
+        end
+        let(:options) { network_vif }
+
+        it "assigns the first address in the network" do
+          expect(subject.direct_ip_lease.first.ipv4).to eq "192.168.0.1"
+        end
+      end
     end
   end
 end
--- dcmgr/spec/helper_methods.rb
+++ dcmgr/spec/helper_methods.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+require 'ipaddress'
+
 module NetworkHelper
   def set_dhcp_range(network)
     nw_ipv4 = IPAddress::IPv4.new("#{network.ipv4_network}/#{network.prefix}")
@@ -7,5 +9,9 @@ module NetworkHelper
     Fabricate(:dhcp_range, network: network,
                            range_begin: nw_ipv4.first,
                            range_end: nw_ipv4.last)
+  end
+
+  def ipv4_to_u32(ipv4)
+    IPAddress::IPv4.new(ipv4).to_u32
   end
 end
