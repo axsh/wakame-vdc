d3feb436635286a6c7191db196a1c5f2fea23985
Metallion <akieckens@axsh.net>
2014-08-19 18:09:10 +0900

    Figured this particular part looks better when using tap

--- dcmgr/spec/dcmgr/scheduler/ip_address/incremental_examples/wraparound_dhcp_range.rb
+++ dcmgr/spec/dcmgr/scheduler/ip_address/incremental_examples/wraparound_dhcp_range.rb
@@ -3,12 +3,10 @@
 shared_examples "wraparound dhcp range" do
   context "when reaching the end of all dhcp ranges while past ip addresses have been released" do
     let(:network) do
-      n = Fabricate(:network, ipv4_network: "192.168.0.0")
-
-      set_dhcp_range(n, "192.168.0.1", "192.168.0.5")
-      set_dhcp_range(n, "192.168.0.11", "192.168.0.15")
-
-      n
+      Fabricate(:network, ipv4_network: "192.168.0.0").tap do |n|
+        set_dhcp_range(n, "192.168.0.1", "192.168.0.5")
+        set_dhcp_range(n, "192.168.0.11", "192.168.0.15")
+      end
     end
 
     before do
