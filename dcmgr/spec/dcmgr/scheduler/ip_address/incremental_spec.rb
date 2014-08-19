b570110a554e23129d32127e12ff48bd3101cdc4
Metallion <akieckens@axsh.net>
2014-08-19 16:44:37 +0900

    Some more refactoring and wrote a test for jumping between dhcp ranges

--- dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
+++ dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
@@ -9,31 +9,52 @@ describe Dcmgr::Scheduler::IPAddress::Incremental do
 
   describe "#schedule" do
     let(:network_vif) { Fabricate(:network_vif, network: network) }
+    let(:incremental) { Dcmgr::Scheduler::IPAddress::Incremental.new }
 
     describe "sad paths" do
-      subject do
-        lambda { Dcmgr::Scheduler::IPAddress::Incremental.new.schedule(options) }
-      end
+      subject { lambda { incremental.new.schedule(options) } }
 
       include_examples "fail argument checks"
       include_examples "dhcp range exhausted"
     end
 
     describe "happy paths" do
-      subject { network_vif }
-      before { Dcmgr::Scheduler::IPAddress::Incremental.new.schedule(options) }
+      subject do
+        incremental.schedule(options)
 
-      let(:network) do
-        n = Fabricate(:network, ipv4_network: "192.168.0.0")
-        set_dhcp_range(n)
-        n
+        network_vif
       end
 
+      let(:options) { network_vif }
+
       context "when options is a NetworkVif with its network set" do
-        let(:options) { network_vif }
+        let(:network) do
+          n = Fabricate(:network, ipv4_network: "192.168.0.0")
+          set_dhcp_range(n)
+          n
+        end
 
         it "assigns the first address in the network" do
           expect(subject.direct_ip_lease.first.ipv4).to eq "192.168.0.1"
+        end
+      end
+
+      context "when one dhcp range is exhausted but the next one is empty" do
+        let(:network) do
+          n = Fabricate(:network, ipv4_network: "192.168.0.0")
+
+          set_dhcp_range(n, "192.168.0.3", "192.168.0.5")
+          set_dhcp_range(n, "192.168.0.10", "192.168.0.12")
+
+          n
+        end
+
+        before do
+          3.times { incremental.schedule Fabricate(:network_vif, network: network) }
+        end
+
+        it "assigns the first available address in the empty range" do
+          expect(subject.direct_ip_lease.first.ipv4).to eq "192.168.0.10"
         end
       end
     end
--- dcmgr/spec/helper_methods.rb
+++ dcmgr/spec/helper_methods.rb
@@ -3,12 +3,20 @@
 require 'ipaddress'
 
 module NetworkHelper
-  def set_dhcp_range(network)
-    nw_ipv4 = IPAddress::IPv4.new("#{network.ipv4_network}/#{network.prefix}")
+  def set_dhcp_range(network, r_begin = nil, r_end = nil)
+    if r_begin.nil? || r_end.nil?
+      nw_ipv4 = IPAddress::IPv4.new("#{network.ipv4_network}/#{network.prefix}")
+
+      r_begin ||= nw_ipv4.first
+      r_end ||= nw_ipv4.last
+    end
+
+    r_begin = ipv4_to_u32(r_begin) if r_begin.is_a?(String)
+    r_end = ipv4_to_u32(r_end) if r_end.is_a?(String)
 
     Fabricate(:dhcp_range, network: network,
-                           range_begin: nw_ipv4.first,
-                           range_end: nw_ipv4.last)
+                           range_begin: r_begin,
+                           range_end: r_end)
   end
 
   def ipv4_to_u32(ipv4)
