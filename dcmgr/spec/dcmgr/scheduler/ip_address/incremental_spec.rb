b6805d08ca1047d39fd04a9c37ac5b845e992bc1
Metallion <akieckens@axsh.net>
2014-08-19 15:41:11 +0900

    Added sad path tests for Incremental's argument checks

--- dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
+++ dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
@@ -21,6 +21,24 @@ describe Dcmgr::Scheduler::IPAddress::Incremental do
 
         it { is_expected.to raise_error ArgumentError }
       end
+
+      context "when options[:network] isn't a Network" do
+        let(:options) { { network: "me not network"} }
+
+        it { is_expected.to raise_error ArgumentError }
+      end
+
+      context "when options[:network_vif] isn't a NetworkVif" do
+        let(:options) { { network: network, network_vif: "me not network vif"} }
+
+        it { is_expected.to raise_error ArgumentError }
+      end
+
+      context "when options[:ip_pool] isn't a IpPool" do
+        let(:options) { { network: network, ip_pool: "me not ip pool"} }
+
+        it { is_expected.to raise_error ArgumentError }
+      end
     end
   end
 end
