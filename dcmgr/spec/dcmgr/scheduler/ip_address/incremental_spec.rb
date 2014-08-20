f18dc834a5652d806cf29ccaad04680650af1aa1
Metallion <akieckens@axsh.net>
2014-08-20 16:21:38 +0900

    Test if the incremental scheduler still works when dhcp ranges change

--- dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
+++ dcmgr/spec/dcmgr/scheduler/ip_address/incremental_spec.rb
@@ -30,6 +30,28 @@ describe Dcmgr::Scheduler::IPAddress::Incremental do
       include_examples 'gateway in dhcp range'
       include_examples 'wraparound dhcp range'
       include_examples 'reassign released addresses'
+
+      context "when dhcp range changes" do
+        let(:network) do
+          Fabricate(:network, ipv4_network: "192.168.0.0").tap do |n|
+            set_dhcp_range(n, "192.168.0.10", "192.168.0.15")
+          end
+        end
+
+        before do
+          3.times do
+            incremental.schedule Fabricate(:network_vif, network: network)
+          end
+
+          destroy_dhcp_range(network, "192.168.0.10", "192.168.0.15")
+
+          set_dhcp_range(network, "192.168.0.4", "192.168.0.6")
+        end
+
+        it "assigns the lowest available address in the new range" do
+          expect(subject).to eq "192.168.0.4"
+        end
+      end
     end
   end
 end
