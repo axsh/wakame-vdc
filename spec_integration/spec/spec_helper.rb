73fbb94496127fd8475fb38d145ccf63ea7f71e5
akry <a-yokokawa@axsh.net>
2014-08-21 15:32:02 +0900

    'describe' and 'it' are now aliased as 'feature' and 'scenario'.

--- spec_integration/spec/spec_helper.rb
+++ spec_integration/spec/spec_helper.rb
@@ -8,6 +8,16 @@ Bundler.require(:test)
 $LOAD_PATH << File.expand_path("./helpers", File.dirname(__FILE__))
 require 'vdc_vnet_spec'
 
+def self.feature(*args, &blk)
+  describe(*args, &blk)
+end
+
+class << RSpec::Core::ExampleGroup
+  def scenario(*args, &blk)
+    it(*args, &blk)
+  end
+end
+
 RSpec.configure do |c|
   c.formatter = :documentation
   c.color     = true
