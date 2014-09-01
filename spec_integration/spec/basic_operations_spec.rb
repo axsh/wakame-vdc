3725a15fcf434338668981ad6fadd0143a91cf6f
akry <a-yokokawa@axsh.net>
2014-09-01 18:57:53 +0900

    Added empty scenarios.

---
+++ spec_integration/spec/basic_operations_spec.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+require 'spec_helper'
+
+feature 'Basic Virtual Network Operations' do
+  scenario 'Create and Delete virtual network' do
+  end
+
+  scenario 'Confirm that set once parameters can not be changed' do
+  end
+
+  scenario 'Create instance to virtual network (Dynamic IP address)' do
+  end
+
+  scenario 'Create instance to virtual network (Static IP address)' do
+  end
+
+  scenario 'Virtual network can not be deleted if there is instance' do
+  end
+
+  scenario 'Instance works properly after restart (PowerOff/PowerOn)' do
+  end
+
+  scenario 'Change instance IP address of primary insterface' do
+  end
+end
