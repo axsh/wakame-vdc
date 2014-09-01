9e15da08be2d0b618164e95d4dc088046e256226
akry <a-yokokawa@axsh.net>
2014-09-01 19:38:23 +0900

    Scenarios are pending.

--- spec_integration/spec/basic_operations_spec.rb
+++ spec_integration/spec/basic_operations_spec.rb
@@ -4,23 +4,37 @@ require 'spec_helper'
 
 feature 'Basic Virtual Network Operations' do
   scenario 'Create and Delete virtual network' do
+    pending 'not implemented, yet.'
+    fail
   end
 
   scenario 'Confirm that set once parameters can not be changed' do
+    pending 'not implemented, yet.'
+    fail
   end
 
   scenario 'Create instance to virtual network (Dynamic IP address)' do
+    pending 'not implemented, yet.'
+    fail
   end
 
   scenario 'Create instance to virtual network (Static IP address)' do
+    pending 'not implemented, yet.'
+    fail
   end
 
   scenario 'Virtual network can not be deleted if there is instance' do
+    pending 'not implemented, yet.'
+    fail
   end
 
   scenario 'Instance works properly after restart (PowerOff/PowerOn)' do
+    pending 'not implemented, yet.'
+    fail
   end
 
   scenario 'Change instance IP address of primary insterface' do
+    pending 'not implemented, yet.'
+    fail
   end
 end
