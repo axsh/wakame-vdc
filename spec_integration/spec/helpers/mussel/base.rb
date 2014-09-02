c5607bfbe24d87c89d33fe5c74f91e8128bf2d65
akry <a-yokokawa@axsh.net>
2014-09-02 15:17:47 +0900

    Added index method to return collection

--- spec_integration/spec/helpers/mussel/base.rb
+++ spec_integration/spec/helpers/mussel/base.rb
@@ -30,6 +30,11 @@ module Mussel
         Responses.const_get(class_name.camelize).new(http_response)
       end
 
+      def index
+        http_response = JSON.parse(`#{response_format} #{MUSSEL} #{class_name} index`)
+        Responses::Collection.generate(class_name.camelize, http_response.first['results'])
+      end
+
       def parse_params(params)
         str = response_format
         params.keys.each do |k|
@@ -52,6 +57,12 @@ module Mussel
 
   module Responses
     class Base < OpenStruct
+    end
+
+    class Collection
+      def self.generate(klass, array)
+        array.map { |i| Responses.const_get(klass).new(i) }
+      end
     end
 
     class SshKeyPair < Base
