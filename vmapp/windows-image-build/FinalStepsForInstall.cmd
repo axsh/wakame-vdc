766af4d69ad8a4c2c4eb0f9cf254b20b93d734ea
Richard Potter <potter@axsh.net>
2014-09-05 00:04:47 +0900

    Remove code to install product key where first-boot no longer uses it

--- vmapp/windows-image-build/FinalStepsForInstall.cmd
+++ vmapp/windows-image-build/FinalStepsForInstall.cmd
@@ -13,8 +13,6 @@ REM a:run-sysprep.cmd will rename this to SetupComplete.cmd so it runs after fir
 copy a:\SetupComplete-firstboot.cmd c:\Windows\Setup\Scripts\
 REM the renamed SetupComplete-firstboot.cmd will run this
 copy a:\wakame-init-first-boot.ps1 c:\Windows\Setup\Scripts\
-REM wakame-init-first-boot.ps1 will read this key and overwrite file
-copy a:\keyfile c:\Windows\Setup\Scripts\
 
 REM EVERY BOOT PHASE
 REM wakame-init-first-boot.ps1 installs these with schtasks.exe
