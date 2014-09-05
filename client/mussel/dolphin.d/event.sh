19f6b915d594a76c85c90dbf4ea4817c0917bd6c
t-iwano <t-iwano@axsh.net>
2014-09-05 19:09:27 +0900

    update task show for dolphin event api.

--- client/mussel/dolphin.d/event.sh
+++ client/mussel/dolphin.d/event.sh
@@ -6,8 +6,14 @@
 . ${BASH_SOURCE[0]%/*}/base.sh
 
 function task_show() {
-  echo "[ERROR] no such cmd '${cmd}'" >&2
-  exit 1
+  local namespace=$1 cmd=$2 start_id=$3 
+
+  [[ -n "${namespace}" ]] || { echo "[ERROR] 'namespace' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${cmd}"       ]] || { echo "[ERROR] 'cmd' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${start_id}"      ]] || { echo "[ERROR] 'start_id' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+
+  xquery="limit=1\&start_id=${start_id}"
+  call_api -X GET $(base_uri)/${namespace}s?${xquery}
 }
 
 function task_create() {
