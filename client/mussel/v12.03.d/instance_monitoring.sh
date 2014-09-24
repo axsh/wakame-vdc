80d2282e1f61d7333de4424f9fff0c878e081700
t-iwano <t-iwano@axsh.net>
2014-09-24 21:11:30 +0900

    Add task set_enable.

--- client/mussel/v12.03.d/instance_monitoring.sh
+++ client/mussel/v12.03.d/instance_monitoring.sh
@@ -66,3 +66,14 @@ task_destroy() {
   $(base_uri)/instances/${uuid}/monitoring/${monitor_id}.$(suffix)
 }
 
+task_set_enable() {
+  local namespace=$1 cmd=$2 uuid=$3
+  [[ -n "${namespace}" ]] || { echo "[ERROR] 'namespace' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${cmd}"       ]] || { echo "[ERROR] 'cmd' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${uuid}"      ]] || { echo "[ERROR] 'uuid' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+
+  call_api -X PUT $(urlencode_data \
+    $(add_param monitoring hash) \
+   ) \
+   $(base_uri)/instances/${uuid}.$(suffix)
+}
