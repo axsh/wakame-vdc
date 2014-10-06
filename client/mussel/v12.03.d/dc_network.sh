2ae319e7a416825421ae86a71cf59b1c13156a4e
akry <a-yokokawa@axsh.net>
2014-10-07 01:39:14 +0900

    mussel - Added dc_network tasks: create, update, add_offering_modes

--- client/mussel/v12.03.d/dc_network.sh
+++ client/mussel/v12.03.d/dc_network.sh
@@ -4,3 +4,31 @@
 #
 
 . ${BASH_SOURCE[0]%/*}/base.sh
+
+task_create() {
+  call_api -X POST $(urlencode_data \
+    $(add_param name) \
+    $(add_param description) \
+  ) \
+  $(base_uri)/${namespace}s.$(suffix)
+}
+
+task_update() {
+  local namespace=$1 cmd=$2 uuid=$3
+
+  call_api -X PUT $(urlencode_data \
+    $(add_param name) \
+    $(add_param description) \
+    $(add_param allow_new_networks) \
+   ) \
+   $(base_uri)/${namespace}s/${uuid}.$(suffix)
+}
+
+task_add_offering_modes() {
+  local namespace=$1 cmd=$2 uuid=$3
+
+  call_api -X PUT $(urlencode_data \
+    $(add_param mode) \
+   ) \
+   $(base_uri)/${namespace}s/${uuid}/offering_modes/add.$(suffix)
+}
