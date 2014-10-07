99e66fee0d93710b14756930be52eefb0673e93b
Metallion <akieckens@axsh.net>
2014-10-07 14:14:30 +0900

    Commented and indented the *-manage commands

--- scripts/install_guide_demo_data.sh
+++ scripts/install_guide_demo_data.sh
@@ -41,65 +41,78 @@ cd /opt/axsh/wakame-vdc/dcmgr
 rake db:up
 
 # Fill up the backend database
-cat <<CMDSET | grep -v '^#' | /opt/axsh/wakame-vdc/dcmgr/bin/vdc-manage -e
-host add hva.demo1 \
-  --uuid hn-demo1 \
-  --display-name "demo hva 1" \
-  --cpu-cores 100 \
-  --memory-size 10240 \
-  --hypervisor openvz \
-  --arch x86_64 \
-  --disk-space 102400 \
-  --force
+cat<<CMDSET | grep -v '\s*^#' | /opt/axsh/wakame-vdc/dcmgr/bin/vdc-manage -e
+  # Register the HVA
+  host add hva.demo1 \
+    --uuid hn-demo1 \
+    --display-name "demo hva 1" \
+    --cpu-cores 100 \
+    --memory-size 10240 \
+    --hypervisor openvz \
+    --arch x86_64 \
+    --disk-space 102400 \
+    --force
 
-backupstorage add \
-  --uuid bkst-local \
-  --display-name "local storage" \
-  --base-uri "file:///var/lib/wakame-vdc/images/" \
-  --storage-type local \
-  --description "storage on the local filesystem"
+  # Tell Wakame-vdc that we're storing images on the local file system
+  backupstorage add \
+    --uuid bkst-local \
+    --display-name "local storage" \
+    --base-uri "file:///var/lib/wakame-vdc/images/" \
+    --storage-type local \
+    --description "storage on the local filesystem"
 
-backupobject add \
-  --uuid bo-lucid5d \
-  --display-name "Ubuntu 10.04 (Lucid Lynx) root partition" \
-  --storage-id bkst-local \
-  --object-key ubuntu-lucid-kvm-md-32.raw.gz \
-  --size 149084 \
-  --allocation-size 359940 \
-  --container-format gz \
-  --checksum 55dcc87838af4aa14eb3eb986ea756d3
+  # Add the image's backup object (hard drive image)
+  backupobject add \
+    --uuid bo-lucid5d \
+    --display-name "Ubuntu 10.04 (Lucid Lynx) root partition" \
+    --storage-id bkst-local \
+    --object-key ubuntu-lucid-kvm-md-32.raw.gz \
+    --size 149084 \
+    --allocation-size 359940 \
+    --container-format gz \
+    --checksum 55dcc87838af4aa14eb3eb986ea756d3
 
-image add local bo-lucid5d \
-  --account-id a-shpoolxx \
-  --uuid wmi-lucid5d \
-  --root-device uuid:148bc5df-3fc5-4e93-8a16-7328907cb1c0 \
-  --display-name "Ubuntu 10.04 (Lucid Lynx)"
+  # Tell Wakame-vdc that this backup object is a bootable machine image
+  image add local bo-lucid5d \
+    --account-id a-shpoolxx \
+    --uuid wmi-lucid5d \
+    --root-device uuid:148bc5df-3fc5-4e93-8a16-7328907cb1c0 \
+    --display-name "Ubuntu 10.04 (Lucid Lynx)"
 
-network add \
-  --uuid nw-demo1 \
-  --ipv4-network 192.168.3.0 \
-  --prefix 24 \
-  --ipv4-gw 192.168.3.1 \
-  --account-id a-shpoolxx \
-  --display-name "demo network"
+  # Give Wakame-vdc a network to start instances in
+  network add \
+    --uuid nw-demo1 \
+    --ipv4-network 192.168.3.0 \
+    --prefix 24 \
+    --ipv4-gw 192.168.3.1 \
+    --account-id a-shpoolxx \
+    --display-name "demo network"
 
-network dhcp addrange nw-demo1 192.168.3.1 192.168.3.254
-macrange add 525400 1 ffffff --uuid mr-demomacs
-network dc add public --uuid dcn-public --description "the network instances are started in"
-network dc add-network-mode public securitygroup
-network forward nw-demo1 public
+  # Tell Wakame-vdc which ip addresses from the network it can use
+  network dhcp addrange nw-demo1 192.168.3.1 192.168.3.254
+
+  # Tell Wakame-vdc which mac addresses it can use
+  macrange add 525400 1 ffffff --uuid mr-demomacs
+
+  # Tell Wakame-vdc which bridge to attach nw-demo1's vnics to
+  # See /etc/wakame-vdc/hva.conf to see which bridge dcn-public is mapped to
+  network dc add public --uuid dcn-public --description "the network instances are started in"
+  network forward nw-demo1 public
+
+  # Enable security groups on the public bridge
+  network dc add-network-mode public securitygroup
 CMDSET
 
-# Set up the frontend GUI
+# Set up the frontend GUI database
 mysqladmin -uroot create wakame_dcmgr_gui
 cd /opt/axsh/wakame-vdc/frontend/dcmgr_gui/
 rake db:init
 
 # Fill it up
-cat <<CMDSET | grep -v '^#' | /opt/axsh/wakame-vdc/frontend/dcmgr_gui/bin/gui-manage -e
-account add --name default --uuid a-shpoolxx
-user add --name "demo user" --uuid u-demo --password demo --login-id demo
-user associate u-demo --account-ids a-shpoolxx
+cat <<CMDSET | /opt/axsh/wakame-vdc/frontend/dcmgr_gui/bin/gui-manage -e
+  account add --name default --uuid a-shpoolxx
+  user add --name "demo user" --uuid u-demo --password demo --login-id demo
+  user associate u-demo --account-ids a-shpoolxx
 CMDSET
 
 # add newline
