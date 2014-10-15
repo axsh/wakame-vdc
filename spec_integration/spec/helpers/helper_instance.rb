71b48e6749b24a04cbaa452ff7d92578886fbe74
akry <a-yokokawa@axsh.net>
2014-10-15 17:21:47 +0900

    Moved the methods related to launching instance to a separated file.

---
+++ spec_integration/spec/helpers/helper_instance.rb
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+def launch_instance
+  instance_params[:vifs] = {
+    'eth0' => {'index'=>'0', 'network'=>@network.id}
+  }
+  n = instance_params[:vifs].size
+  instance_params[:vifs].merge!({
+    "eth#{n}" => {"index" => n, "network" => config[:nw_management_uuid]}
+  })
+
+  setup_vif(instance_params)
+  output_keyfile = create_ssh_key_pair(instance_params)
+
+  @instance = wait_instance(Mussel::Instance.create(instance_params), config[:nw_management_uuid])
+  @key_files[@instance.id] = output_keyfile
+end
+
+def wait_instance(instance, network_uuid)
+  instance = wait_until_vif_ready(instance.id)
+  ip = extract_management_ip_address(instance.vif)
+  ping_until_vif_ready(ip)
+  instance
+
+  # ipp_uuid = config[:ip_pool_uuid]
+  # ipp_params = {
+  #   :network_id => config[:nw_global_uuid]
+  # }
+  # ip_handle = Mussel::IpPool.acquire(ipp_uuid, ipp_params)
+
+  # eip_params = {
+  #   :ip_handle_id => ip_handle.ip_handle_id
+  # }
+  # vif = Mussel::NetworkVif.attach_external_ip(eip_params, instance.vif.first['vif_id'])
+end
+
+def wait_until_vif_ready(instance_uuid)
+  loop do
+    sleep(1)
+    ret = Mussel::Instance.show(instance_uuid)
+    break if not ret.vif.empty?
+  end
+  instance = Mussel::Instance.show(instance_uuid)
+  p instance.inspect
+  instance
+end
+
+def extract_management_ip_address(vifs)
+  vifs.select { |vif|
+    vif['network_id'] == config[:nw_management_uuid]
+  }['ipv4']['address']
+end
+
+def ping_until_vif_ready(ip)
+  trial = 0
+  loop do
+    sleep(1)
+    ping_success = system("ping -c 1 #{ip}")
+    trial = trial + 1
+    break if ping_success || (trial >= config[:trial_limit])
+  end
+end
+
+def terminate_instance
+  ret = Mussel::Instance.destroy(@instance)
+  expect(ret.first).to eq @instance.id
+end
+
+def setup_vif(params)
+  output_vifsfile="#{File.dirname(__FILE__)}/vifs.#{$$}.#{Time.now.strftime("%Y%m%d%H%M%S")}"
+  File.write(output_vifsfile, params[:vifs].to_s.gsub("=>",":"))
+  params.delete(:vifs)
+  params[:vifs] = output_vifsfile
+end
+
+def create_ssh_key_pair(params)
+  output_keyfile="#{File.dirname(__FILE__)}/key_pair.#{$$}.#{Time.now.strftime("%Y%m%d%H%M%S")}"
+  system("ssh-keygen -N '' -f #{output_keyfile} -C #{output_keyfile}")
+  ssh_key_pair = Mussel::SshKeyPair.create({
+    :description => output_keyfile,
+    :public_key => "#{output_keyfile}.pub"
+  })
+  params[:ssh_key_id] = ssh_key_pair.id
+  output_keyfile
+end
--- spec_integration/spec/helpers/helper_methods.rb
+++ spec_integration/spec/helpers/helper_methods.rb
@@ -2,54 +2,6 @@
 
 SSH_OPTS = "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-def setup_vif(params)
-  output_vifsfile="#{File.dirname(__FILE__)}/vifs.#{$$}.#{Time.now.strftime("%Y%m%d%H%M%S")}"
-  File.write(output_vifsfile, params[:vifs].to_s.gsub("=>",":"))
-  params.delete(:vifs)
-  params[:vifs] = output_vifsfile
-end
-
-def create_ssh_key_pair(params)
-  output_keyfile="#{File.dirname(__FILE__)}/key_pair.#{$$}.#{Time.now.strftime("%Y%m%d%H%M%S")}"
-  system("ssh-keygen -N '' -f #{output_keyfile} -C #{output_keyfile}")
-  ssh_key_pair = Mussel::SshKeyPair.create({
-    :description => output_keyfile,
-    :public_key => "#{output_keyfile}.pub"
-  })
-  params[:ssh_key_id] = ssh_key_pair.id
-  output_keyfile
-end
-
-def wait_instance(instance, network_uuid)
-  trial_limit = 40
-  trial = 0
-
-  loop do
-    sleep(1)
-    ret = Mussel::Instance.show(instance.id)
-    break if not ret.vif.empty?
-  end
-
-  instance = Mussel::Instance.show(instance.id)
-  p instance.inspect
-  ip = extract_ip_address(instance, network_uuid)
-
-  loop do
-    sleep(1)
-    ping_success = system("ping -c 1 #{ip}")
-    trial = trial + 1
-    break if ping_success || (trial >= trial_limit)
-  end
-
-  instance
-end
-
-def extract_ip_address(instance, network_uuid)
-  instance.vif.select do |v|
-    v['network_id'] == network_uuid
-  end.first['ipv4']['address']
-end
-
 def recursive_symbolize_keys(hash)
   hash.each_with_object({}){|(k,v), m|
     m[k.to_s.to_sym] = (v.is_a?(Hash) ? recursive_symbolize_keys(v) : v)
@@ -58,4 +10,8 @@ end
 
 def config
   @config ||= recursive_symbolize_keys(YAML.load_file(File.expand_path("../../../config/config.yml", __FILE__)))
+end
+
+def create_virtual_network_nw_demo1
+  @network = Mussel::Network.create(nw_demo1_params)
 end
