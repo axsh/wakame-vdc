e4da6f0a1cb0099dcf81770c563aed8167a66eca
Richard Potter <potter@axsh.net>
2014-12-16 12:16:52 +0900

    A little refactoring for Windows image build

--- vmapp/windows-image-build/windows-image-smoke-test.sh
+++ vmapp/windows-image-build/windows-image-smoke-test.sh
@@ -13,32 +13,28 @@ evalcheck()
 
 export SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd -P)" || reportfail
 
+cleanup-code()
+{
+    setsid "$SCRIPT_DIR/build-w-answerfile-floppy.sh" "$BDIR/" -cleanup
+}
+
+trap 'echo "Doing post-test cleanup" ; cleanup-code' EXIT
+
+
 WIN_VERSION="$1"
 [ "$1" = 2008 ] || [ "$1" = 2012 ] || reportfail "first parameter must be 2008 or 2012"
-
 
 # Assume Jenkins puts us in a suitable part of the disk hierarchy to create a build directory.
 BDIR="./builddirs/smoketest-$WIN_VERSION/"
 evalcheck mkdir -p ./builddirs
 
-if [ -d "$BDIR" ]; then
-    setsid "$SCRIPT_DIR/build-w-answerfile-floppy.sh" "$BDIR/" -cleanup
-    rm "$BDIR" -fr
-fi
-
-cleanup-code()
-{
-    echo "Doing post-test cleanup"
-    setsid "$SCRIPT_DIR/build-w-answerfile-floppy.sh" "$BDIR/" -cleanup
-}
-
-trap 'cleanup-code' EXIT
-
-
-# Make sure resource files are in place
+# Make sure KVMs, processes, and build dir from previous jobs are deleted
+[ -d "$BDIR" ] && { cleanup-code ; rm "$BDIR" -fr ; }
 
 ensure-file-is-in-place()
 {
+    # Make sure the file "$1" is in "$TARGETDIR", and if not, try do
+    # download it from each download path in the list "$DLSOURCES".
     [ -f "$TARGETDIR/$1" ] && return 0
     for asource in $DLSOURCES; do
 	case "$asource" in
@@ -66,12 +62,12 @@ ensure-file-is-in-place()
     reportfail "Could not install $1"
 }
 
-set -x
+# Make sure resource files are in place.
 
 localsource="$(echo /home/*/for-jenkins-windows-image-smoke-test)"
 [ -d "$localsource" ] || localsource=""
 
-TARGETDIR="$SCRIPT_DIR"
+TARGETDIR="$SCRIPT_DIR" # Currently, all these resource files go in the same directory as this script.
 
 DLSOURCES="$localsource http://alt.fedoraproject.org/pub/alt/virtio-win/archives/virtio-win-0.1-74"
 ensure-file-is-in-place virtio-win-0.1-74.iso
@@ -95,7 +91,7 @@ ensure-file-is-in-place metadata.img.tar.gz
 
 
 
-# All the needed files should be in place. Start the build.
+# All the needed files should now be in place. Start the build.
 
 KILLPGOK=yes "$SCRIPT_DIR/build-w-answerfile-floppy.sh" "$BDIR" 0-init "$WIN_VERSION"
 
@@ -116,8 +112,8 @@ for i in $(seq 1 30); do
     sleep 60
 done
 
-# after-gen0-sysprep.tar.gz is the set of Windows log files after sysprep is run for the
-# initial time, which for image building is the final step.
+# after-gen0-sysprep.tar.gz is the set of Windows log files after sysprep is run as
+# the final step of installing Windows.  Extract just one file from this tar archive.
 tar xzvOf "./$BDIR/after-gen0-sysprep.tar.gz"   Windows/Setup/State/State.ini >State.ini
 
 [[ "$(< State.ini)" == *IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE* ]] || reportfail "sysprep did not update State.ini file"
