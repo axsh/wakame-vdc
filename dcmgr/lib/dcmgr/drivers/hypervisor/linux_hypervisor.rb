e000bae8c03c4dddc0e6f7af9ae52eedceaf7ff8
toros11 <tobias.rosen@axsh.net>
2015-02-03 14:03:58 +0900

    fix: hva crash when dc network not set

--- dcmgr/lib/dcmgr/drivers/hypervisor/linux_hypervisor.rb
+++ dcmgr/lib/dcmgr/drivers/hypervisor/linux_hypervisor.rb
@@ -60,7 +60,7 @@ module Dcmgr
 
           network = hc.rpc.request('hva-collector', 'get_network', vnic[:network_id])
           
-          raise "Dc network missing in database." if network[:dc_network][:name].nil?              
+          raise "Dc network not found in database." if network[:dc_network].nil?              
 
           network_name = network[:dc_network][:name]
           dcn = Dcmgr::Configurations.hva.dc_networks[network_name]
