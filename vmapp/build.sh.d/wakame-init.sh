ada8bdcc8b92a40a566d383f7d797acdb14e4ac6
Masahito Yoshida <masahito@axsh.net>
2015-04-08 12:00:36 +0900

    add proper indents.

--- vmapp/build.sh.d/wakame-init.sh
+++ vmapp/build.sh.d/wakame-init.sh
@@ -28,37 +28,38 @@ function install_wakame_init() {
   [[ -d "${chroot_dir}" ]] || { echo "[ERROR] directory not found: ${chroot_dir} (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
 
   case "${metadata_type}" in
-  md)
-    prevent_interfaces_booting ${chroot_dir} eth*
-    ;;
-  ms|mcd)
-    ;;
-  *)
-    echo "[ERROR] unknown metadata_type:${metadata_type} ${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1;
-    ;;
+    md)
+      prevent_interfaces_booting ${chroot_dir} eth*
+      ;;
+    ms|mcd)
+      ;;
+    *)
+      echo "[ERROR] unknown metadata_type:${metadata_type} ${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1;
+      ;;
   esac
+
   local wakame_init_path
   case "${distro}" in
-  centos|rhel)
-    rsync -a $(cd ${BASH_SOURCE[0]%/*} && pwd)/../../rpmbuild/wakame-vdc.repo ${chroot_dir}/etc/yum.repos.d/wakame-vdc.repo
-    run_in_target ${chroot_dir} yum install -y wakame-init
-    ;;
-  ubuntu|debian)
-    wakame_init_path=${wakame_init_ubuntu_path}
+    centos|rhel)
+      rsync -a $(cd ${BASH_SOURCE[0]%/*} && pwd)/../../rpmbuild/wakame-vdc.repo ${chroot_dir}/etc/yum.repos.d/wakame-vdc.repo
+      run_in_target ${chroot_dir} yum install -y wakame-init
+      ;;
+    ubuntu|debian)
+      wakame_init_path=${wakame_init_ubuntu_path}
 
-    printf "[DEBUG] Installing wakame-init script\n"
-    cat <<-EOS >> ${chroot_dir}/etc/rc.local
+      printf "[DEBUG] Installing wakame-init script\n"
+      cat <<-EOS >> ${chroot_dir}/etc/rc.local
 	/etc/wakame-init ${metadata_type}
 	EOS
-    cat ${chroot_dir}/etc/rc.local
+      cat ${chroot_dir}/etc/rc.local
 
-    rsync -a ${wakame_init_path} ${chroot_dir}/etc/wakame-init
-    chmod 755 ${chroot_dir}/etc/wakame-init
-    chown 0:0 ${chroot_dir}/etc/wakame-init
-    ;;
-  *)
-    echo "[ERROR] not supported distro:${distro} ${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1;
-    ;;
+      rsync -a ${wakame_init_path} ${chroot_dir}/etc/wakame-init
+      chmod 755 ${chroot_dir}/etc/wakame-init
+      chown 0:0 ${chroot_dir}/etc/wakame-init
+      ;;
+    *)
+      echo "[ERROR] not supported distro:${distro} ${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1;
+      ;;
   esac
 }
 
