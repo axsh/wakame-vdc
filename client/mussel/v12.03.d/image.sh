a5b814e9f9d0e2fa59e4a7d54ac725d41a2b0cad
Masahito Yoshida <masahito@axsh.net>
2015-04-09 19:42:24 +0900

    add --service-type option.

--- client/mussel/v12.03.d/image.sh
+++ client/mussel/v12.03.d/image.sh
@@ -10,5 +10,9 @@ task_index() {
   if [[ -n "${is_public}" ]]; then
       xquery="is_public=${is_public}"
   fi
+  # --service-type=(std|lb)
+  if [[ -n "${service_type}" ]]; then
+    xquery="${xquery}\&service_type=${service_type}"
+  fi
   cmd_index $*
 }
