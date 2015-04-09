db0d774f5223f0e23dc59500f981004316e58204
Masahito Yoshida <masahito@axsh.net>
2015-04-09 19:16:05 +0900

    enable to set "--is-public" option."

--- client/mussel/v12.03.d/image.sh
+++ client/mussel/v12.03.d/image.sh
@@ -4,3 +4,11 @@
 #
 
 . ${BASH_SOURCE[0]%/*}/base.sh
+
+task_index() {
+  # --is-public=(true|false|0|1)
+  if [[ -n "${is_public}" ]]; then
+      xquery="is_public=${is_public}"
+  fi
+  cmd_index $*
+}
