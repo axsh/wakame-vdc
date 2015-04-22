953b1f6a25ad1f12cb473efbe217364d84e6afca
t-iwano <t-iwano@axsh.net>
2015-04-22 20:10:30 +0900

    Fixed Zabbix API Response log message.

--- dcmgr/lib/dcmgr/drivers/network_monitoring/zabbix.rb
+++ dcmgr/lib/dcmgr/drivers/network_monitoring/zabbix.rb
@@ -39,12 +39,10 @@ module Dcmgr::Drivers
     def rpc_request(method, params)
       logger.debug("JSON-RPC Request: #{method}: #{params}")
       res = connection.request(method, params)
-      if logger.debug?
-        logger.debug("JSON-RPC Response: #{method}: #{res.json_body}")
+      if res.error?
+        logger.error("JSON-RPC Response: #{method}: #{res.json_body}")
       else
-        if res.error?
-          logger.error("JSON-RPC Response: #{method}: #{res.json_body}")
-        end
+        logger.debug("JSON-RPC Response: #{method}: #{res.json_body}")
       end
       raise res.error_message if res.error?
 
