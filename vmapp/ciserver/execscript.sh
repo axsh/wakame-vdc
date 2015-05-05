b70e63408c576a610b5ed2531e0ea928e6d70596
Masahito Yoshida <masahito@axsh.net>
2015-05-06 00:00:43 +0900

    remove tail "/".

--- vmapp/ciserver/execscript.sh
+++ vmapp/ciserver/execscript.sh
@@ -113,7 +113,7 @@ script
   sleep 3
   su - wakame-vdc -c /bin/bash <<EOS >>/var/log/hubot.log 2>&1
     [ -f /etc/default/hubot ] && . /etc/default/hubot
-    export HUBOT_JENKINS_URL="${HUBOT_JENKINS_URL:-"http://127.0.0.1:8080/"}"
+    export HUBOT_JENKINS_URL="${HUBOT_JENKINS_URL:-"http://127.0.0.1:8080"}"
     cd /home/wakame-vdc/myhubot
     ./bin/hubot -a hipchat
 EOS
