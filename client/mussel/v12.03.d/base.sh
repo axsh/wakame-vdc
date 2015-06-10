f98ea2e7211ffdc044efd49699f25254d515251d
Masahito Yoshida <masahito@axsh.net>
2015-06-10 16:31:11 +0900

    output_format -> mussel_output_format

--- client/mussel/v12.03.d/base.sh
+++ client/mussel/v12.03.d/base.sh
@@ -24,14 +24,14 @@ task_default() {
 }
 
 piped_task_create() {
-  case "${output_format:-""}" in
+  case "${mussel_output_format:-""}" in
     id) egrep '^:id:' </dev/stdin | awk '{print $2}' ;;
      *) cat ;;
   esac
 }
 
 piped_task_index() {
-  case "${output_format:-""}" in
+  case "${mussel_output_format:-""}" in
     id) egrep -- '- :id:' </dev/stdin | awk '{print $3}' ;;
      *) cat ;;
   esac
