f73c72800cbe09e9ae53ca9264a6872ff66edcb0
Masahito Yoshida <masahito@axsh.net>
2015-06-10 17:01:43 +0900

    add piped_task_update.

--- client/mussel/v12.03.d/base.sh
+++ client/mussel/v12.03.d/base.sh
@@ -30,6 +30,13 @@ piped_task_create() {
   esac
 }
 
+piped_task_update() {
+  case "${mussel_output_format:-""}" in
+    id) egrep '^:id:' </dev/stdin | awk '{print $2}' ;;
+     *) cat ;;
+  esac
+}
+
 piped_task_index() {
   case "${mussel_output_format:-""}" in
     id) egrep -- '- :id:' </dev/stdin | awk '{print $3}' ;;
