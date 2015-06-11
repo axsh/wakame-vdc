80dda8dcc087a903f07f6a3d7f91d0f822534da5
Masahito Yoshida <masahito@axsh.net>
2015-06-11 18:23:09 +0900

    add piped_task_reboot/poweroff/poweron for load_balancer.

--- client/mussel/v12.03.d/load_balancer.sh
+++ client/mussel/v12.03.d/load_balancer.sh
@@ -88,3 +88,23 @@ task_update() {
    ) \
    $(base_uri)/${namespace}s/${uuid}.$(suffix)
 }
+
+# piped_task
+
+piped_task_reboot() {
+  piped_task_destroy
+}
+
+piped_task_poweroff() {
+  case "${mussel_output_format:-""}" in
+    id) egrep '^:load_balancer_id:' </dev/stdin | awk '{print $2}' ;;
+     *) cat ;;
+  esac
+}
+
+piped_task_poweron() {
+  case "${mussel_output_format:-""}" in
+    id) egrep '^:load_balancer_id:' </dev/stdin | awk '{print $2}' ;;
+     *) cat ;;
+  esac
+}
