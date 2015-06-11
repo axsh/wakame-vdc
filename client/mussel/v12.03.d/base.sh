9a429c87fe236413f6442dd655492bd34e774caf
Masahito Yoshida <masahito@axsh.net>
2015-06-11 18:01:57 +0900

    add piped_task_destroy.

--- client/mussel/v12.03.d/base.sh
+++ client/mussel/v12.03.d/base.sh
@@ -30,6 +30,13 @@ piped_task_index() {
   esac
 }
 
+piped_task_destroy() {
+  case "${mussel_output_format:-""}" in
+    id) tail -n 1 </dev/stdin | awk '{print $2}' ;;
+     *) cat ;;
+  esac
+}
+
 piped_task_create() {
   case "${mussel_output_format:-""}" in
     id) egrep '^:id:' </dev/stdin | awk '{print $2}' ;;
