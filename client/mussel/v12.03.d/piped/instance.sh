b15bd109ddb25d0a3e4f9a084116c68dcd636806
Masahito Yoshida <masahito@axsh.net>
2015-06-11 18:46:39 +0900

    use piped_task_poweroff because of same format.

--- client/mussel/v12.03.d/piped/instance.sh
+++ client/mussel/v12.03.d/piped/instance.sh
@@ -25,8 +25,5 @@ piped_task_poweron() {
 }
 
 piped_task_move() {
-  case "${mussel_output_format:-""}" in
-    id) egrep '^:instance_id:' </dev/stdin | awk '{print $2}' ;;
-     *) cat ;;
-  esac
+  piped_task_poweroff
 }
