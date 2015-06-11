22568f58b7bca8a7ae21999c5492b14b32726af8
Masahito Yoshida <masahito@axsh.net>
2015-06-11 19:25:56 +0900

    add piped/network_vif.sh.

--- client/mussel/v12.03.d/network_vif.sh
+++ client/mussel/v12.03.d/network_vif.sh
@@ -4,6 +4,7 @@
 #
 
 . ${BASH_SOURCE[0]%/*}/base.sh
+. ${BASH_SOURCE[0]%/*}/piped/${BASH_SOURCE[0]##*/}
 
 task_show_external_ip() {
   local namespace=$1 cmd=$2 uuid=$3
---
+++ client/mussel/v12.03.d/piped/network_vif.sh
@@ -0,0 +1,19 @@
+# -*-Shell-script-*-
+#
+#
+
+piped_add_security_group() {
+  piped_task_update
+}
+
+piped_remove_security_group() {
+  piped_task_update
+}
+
+piped_attach_external_ip() {
+  piped_task_update
+}
+
+piped_detach_external_ip() {
+  piped_task_update
+}
