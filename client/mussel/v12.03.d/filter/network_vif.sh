54380af54a97a0fa3677e85cefb8ba7f6adddad3
Masahito Yoshida <masahito@axsh.net>
2015-06-13 16:05:43 +0900

    fix filter task name.

--- client/mussel/v12.03.d/filter/network_vif.sh
+++ client/mussel/v12.03.d/filter/network_vif.sh
@@ -2,18 +2,18 @@
 #
 #
 
-filter_add_security_group() {
+filter_task_add_security_group() {
   filter_task_update
 }
 
-filter_remove_security_group() {
+filter_task_remove_security_group() {
   filter_task_update
 }
 
-filter_attach_external_ip() {
+filter_task_attach_external_ip() {
   filter_task_update
 }
 
-filter_detach_external_ip() {
+filter_task_detach_external_ip() {
   filter_task_update
 }
--- client/mussel/v12.03.d/filter/volume.sh
+++ client/mussel/v12.03.d/filter/volume.sh
@@ -2,17 +2,17 @@
 #
 #
 
-filter_backup() {
+filter_task_backup() {
   case "${mussel_output_format:-""}" in
     id) egrep '^:volume_id:' </dev/stdin | awk '{print $2}' ;;
      *) cat ;;
   esac
 }
 
-filter_attach() {
+filter_task_attach() {
   filter_task_update
 }
 
-filter_detach() {
+filter_task_detach() {
   filter_task_update
 }
