77b6d9cb5cba28a183f175ea3a4043c70a9d405f
t-iwano <t-iwano@axsh.net>
2015-07-30 18:08:50 +0900

    Fixed required params for dolphin notification.

--- client/mussel/dolphin.d/notification.sh
+++ client/mussel/dolphin.d/notification.sh
@@ -16,6 +16,13 @@ function task_show() {
 }
 
 function task_create() {
+  local namespace=$1 cmd=$2 notification_id=$3
+
+  [[ -n "${namespace}"       ]] || { echo "[ERROR] 'namespace' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${cmd}"             ]] || { echo "[ERROR] 'cmd' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${notification_id}" ]] || { echo "[ERROR] 'notification_id' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${email}"           ]] || { echo "[ERROR] 'notification_id' is empty (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+
   call_api -X POST -d '$(cat ${email})' \
    $(base_uri)/${namespace}s
 }
