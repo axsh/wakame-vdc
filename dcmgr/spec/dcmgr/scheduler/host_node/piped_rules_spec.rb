107a61a3caa1419c4700b63457a321cd450a15b4
t-iwano <t-iwano@axsh.net>
2015-08-13 15:46:36 +0900

    Added 2 host group unit test.

--- dcmgr/spec/dcmgr/scheduler/host_node/piped_rules_spec.rb
+++ dcmgr/spec/dcmgr/scheduler/host_node/piped_rules_spec.rb
@@ -33,10 +33,8 @@ describe Dcmgr::Scheduler::HostNode::PipedRules do
         end
       end
 
-      host_ids.each do |h|
-        ds = Dcmgr::Models::HostNode.where(id: h)
-        allow(Isono::Models::NodeState).to receive(:filter).with(state: 'online').and_return(ds)
-      end
+      ds = Dcmgr::Models::HostNode.where(id: host_ids.flatten!)
+      allow(Isono::Models::NodeState).to receive(:filter).with(state: 'online').and_return(ds)
 
       instances.each do |i|
         svc_type = Dcmgr::Scheduler.service_type(i)
@@ -79,6 +77,42 @@ describe Dcmgr::Scheduler::HostNode::PipedRules do
           2.times.map.with_index(1) do |n, i|
             Fabricate(:host_node, uuid: "kvm#{i}", hypervisor: 'kvm', node_id: "hva.kvm#{i}")
           end
+        end
+
+        it 'assigns one instance per host node' do
+          expect(instances.first.host_node).to eq(hosts.first)
+          expect(instances.last.host_node).to eq(hosts.last)
+        end
+      end
+    end
+
+    context 'with two group' do
+      context 'with two hosts and two instances' do
+        let(:instances) do
+          2.times.map.with_index(1) do |n, i|
+            Fabricate(:instance, hypervisor: 'kvm', request_params: {"host_node_group" => "local#{i}"})
+          end
+        end
+
+        let(:hosts) do
+          2.times.map.with_index(1) do |n, i|
+            Fabricate(:host_node, uuid: "kvm#{i}", hypervisor: 'kvm', node_id: "hva.kvm#{i}")
+          end
+        end
+
+        let(:host_groups) do
+          hngs = 2.times.map.with_index(1) do |n, i|
+            hng = Fabricate(:host_node_group, uuid: "hng-local#{i}", name: "hng-local#{i}")
+          end
+          hngs.zip(hosts) do |hng, h|
+            hng.map_resource(h, 0)
+          end
+          hngs
+        end
+
+        it 'assigns one host node per host node group' do
+          expect(hosts.first).to eq(host_groups.first.mapped_resources.first)
+          expect(hosts.last).to eq(host_groups.last.mapped_resources.first)
         end
 
         it 'assigns one instance per host node' do
