feb8c4359cc55fe875b7b508bf24235df039a7bc
t-iwano <t-iwano@axsh.net>
2015-08-13 17:31:10 +0900

    Added virtual data center catalog test.

--- dcmgr/spec/dcmgr/catalogs_spec.rb
+++ dcmgr/spec/dcmgr/catalogs_spec.rb
@@ -40,5 +40,39 @@ end
 
 describe Dcmgr::Catalogs::VirtualDataCenter do
   describe '#load' do
+    let(:virtual_data_center_catalog) do
+      Dcmgr::Catalogs.load Dcmgr::Catalogs::VirtualDataCenter
+      Dcmgr::Catalogs.virtualdatacenter
+    end
+
+    subject { lambda { virtual_data_center_catalog } }
+
+    context 'None of the usual_paths existed' do
+      it { is_expected.to raise_error RuntimeError }
+    end
+
+    context 'use minimal_virtual_data_center.yml' do
+      let(:virtual_data_center_catalog) do
+        Dcmgr::Catalogs.load Dcmgr::Catalogs::VirtualDataCenter,
+        ["#{Dcmgr::DCMGR_ROOT}/spec/minimal_virtual_data_center.yml"]
+        Dcmgr::Catalogs.virtualdatacenter
+      end
+
+      it 'use instance_spec small' do
+        expect(virtual_data_center_catalog.instance_spec["small"]).to eq({"cpu_cores"=>1})
+      end
+
+      it 'use vdc_spec docker' do
+        expect(virtual_data_center_catalog.vdc_spec["docker"]).to eq({"instance_spec"=>"small"})
+      end
+
+      it 'use unknown instance_spec' do
+        expect(virtual_data_center_catalog.instance_spec["medium"]).to eq(nil)
+      end
+
+      it 'use unknown vdc_spec' do
+        expect(virtual_data_center_catalog.vdc_spec["openstack"]).to eq(nil)
+      end
+    end
   end
 end
