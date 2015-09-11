15663e1381f5ee4557eaf2ba162d7e3e43d04c50
Masahito Yoshida <masahito@axsh.net>
2015-09-11 16:41:54 +0900

    remove trema.

--- tests/vdc.sh.d/install.d/tmp_dirs.sh
+++ tests/vdc.sh.d/install.d/tmp_dirs.sh
@@ -1,5 +1,5 @@
 # prepare direcotries under $VDC_ROOT/tmp.
-for i in "${VDC_ROOT}/tmp/instances" "${VDC_ROOT}/tmp/snap/" "${VDC_ROOT}/tmp/images/" "${VDC_ROOT}/tmp/volumes/"  "${VDC_ROOT}/tmp/trema/log/" "${VDC_ROOT}/tmp/trema/pid/" "${VDC_ROOT}/tmp/trema/sock/"; do
+for i in "${VDC_ROOT}/tmp/instances" "${VDC_ROOT}/tmp/snap/" "${VDC_ROOT}/tmp/images/" "${VDC_ROOT}/tmp/volumes/"; do
   [[ -d "$i" ]] || mkdir -p $i
 done
 
