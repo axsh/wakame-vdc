a34cf141fce7b29f1e35478d896482b8baffe77d
Akira Yokokawa <a-yokokawa@axsh.net>
2015-09-12 02:10:22 -0700

    Fix mistakes in instance fabricator

--- dcmgr/spec/fabricators/instance.rb
+++ dcmgr/spec/fabricators/instance.rb
@@ -7,10 +7,10 @@ Fabricator(:instance, class_name: Dcmgr::Models::Instance) do
   memory_size 256
 end
 
-Fabricate(:instance_with_network_vif, class_name: Dcmgr::Models::Instance) do
+Fabricator(:instance_with_network_vif, class_name: Dcmgr::Models::Instance) do
   account_id 'a-shpoolxx'
   image { Fabricate(:image) }
   cpu_cores 1
   memory_size 256
-  network_vifs { [Fabricate(:network_vif)] }
+  network_vif { [Fabricate(:network_vif)] }
 end
