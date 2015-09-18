447403d87c7dce254006034d57b83bb136837894
Richard Potter <potter@axsh.net>
2015-09-18 18:00:05 +0900

    Moved code to ensure required Windows files are in place to a new script

--- vmapp/windows-image-build/utils/windows-image-smoke-test.sh
+++ vmapp/windows-image-build/utils/windows-image-smoke-test.sh
@@ -20,18 +20,6 @@ cleanup-code()
 
 trap 'echo "Doing post-test cleanup" ; cleanup-code >>"$SCRIPT_DIR/post-test-cleanup.out" 2>&1' EXIT
 
-# Check for missing dependencies here so that time is not wasted
-# running a test that will inevitably fail.
-for i in s3cmd mkfs.vfat ntfslabel mount.ntfs
-do
-    which "$i" >/dev/null || reportfail "Must install package for $i"
-done
-
-# FYI:
-# yum install s3cmd
-# yum install dosfstools
-# yum install ntfs-3g.x86_64 ntfsprogs.x86_64
-
 WIN_VERSION="$1"
 [ "$1" = 2008 ] || [ "$1" = 2012 ] || reportfail "first parameter must be 2008 or 2012"
 
@@ -48,62 +36,7 @@ if [ -d "$BDIR" ]; then
     rm "$BDIR" -fr
 fi
 
-ensure-file-is-in-place()
-{
-    # Make sure the file "$1" is in "$TARGETDIR", and if not, try do
-    # download it from each download path in the list "$DLSOURCES".
-    [ -f "$TARGETDIR/$1" ] && return 0
-    for asource in $DLSOURCES; do
-	case "$asource" in
-	    /*)
-		cp -al "$asource/$1" "$TARGETDIR/$1" 2>/dev/null || \
-		    cp "$asource/$1" "$TARGETDIR/$1" 2>/dev/null || \
-		    echo "Attempt to copy $1 from local source failed" 1>&2
-		;;
-	    JenkinsENV)
-		# try to grab an environment variable set by Jenkins, e.g.: JenkinsENV-key2008
-		attempt="$(eval echo "\$${asource}_$1")"
-		[ "$attempt" != "" ] && echo "$attempt" >"$TARGETDIR/$1"
-		;;
-	    http://* | https://*)
-		curl --fail "$asource/$1" -o "$TARGETDIR/$1" || echo "Download attempt of $1 failed" 1>&2
-		;;
-	    s3://*)
-		s3cmd get "$asource/$1" "$TARGETDIR/$1" || echo "s3cmd download attempt of $1 failed" 1>&2
-		;;
-	    *)
-		reportfail "unknown source: $asource"
-		;;
-	esac
-	[ -f "$TARGETDIR/$1" ] && return 0
-    done
-    reportfail "Could not install $1"
-}
-
-# Make sure resource files are in place.
-
-localsource="$(echo /home/*/for-jenkins-windows-image-smoke-test)"
-[ -d "$localsource" ] || localsource=""
-
-TARGETDIR="$SCRIPT_DIR/../resources"
-
-DLSOURCES="$localsource https://fedorapeople.org/groups/virt/virtio-win/deprecated-isos/archives/virtio-win-0.1-74"
-ensure-file-is-in-place virtio-win-0.1-74.iso
-
-DLSOURCES="$localsource http://repo.zabbix.jp/zabbix/zabbix-1.8/windows"
-ensure-file-is-in-place zabbix_agent-1.8.15-1.JP_installer.exe
-
-DLSOURCES="$localsource $S3URL JenkinsENV"  # S3URL is set in Jenkins
-[ "$WIN_VERSION" = "2008" ] && \
-    ensure-file-is-in-place key2008
-[ "$WIN_VERSION" = "2012" ] && \
-    ensure-file-is-in-place key2012
-
-DLSOURCES="$localsource $S3URL"  # S3URL is set in Jenkins
-isoname="$(eval echo \$ISO${WIN_VERSION})"
-[ "$isoname" != "" ] || reportfail "Environment variable ISO${WIN_VERSION} must be set"
-ensure-file-is-in-place "$isoname"
-
+"$SCRIPT_DIR/check-download-resources.sh" "$WIN_VERSION" || exit
 # All the needed files should now be in place. Start the build.
 
 KILLPGOK=yes "$SCRIPT_DIR/../build-w-answerfile-floppy.sh" "$BDIR" 0-init "$WIN_VERSION"
