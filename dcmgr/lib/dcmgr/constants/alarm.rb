57662f0f7e947547646fe333c85d6d61dbef4abb
Tsunehisa Iwano <t-iwano@axsh.net>
2015-09-25 17:44:30 +0900

    Remove resource alarm.

--- dcmgr/lib/dcmgr/constants/alarm.rb
+++ dcmgr/lib/dcmgr/constants/alarm.rb
@@ -2,17 +2,11 @@
 
 module Dcmgr::Constants
   module Alarm
-    RESOURCE_METRICS = [
-      'cpu.usage',
-      'disk.usage',
-      'memory.usage',
-    ].freeze
-
     LOG_METRICS = [
       'log'
     ].freeze
 
-    SUPPORT_METRICS = (RESOURCE_METRICS + LOG_METRICS).freeze
+    SUPPORT_METRICS = (LOG_METRICS).freeze
 
     SUPPORT_COMPARISON_OPERATOR = {
       'ge' => :>=,
@@ -23,13 +17,6 @@ module Dcmgr::Constants
 
     SUPPORT_NOTIFICATION_TYPE = [
       'dolphin'
-    ].freeze
-
-
-    RESOURCE_NOTIFICATION_ACTIONS = [
-      'ok',
-      'alarm',
-      'insufficient_data'
     ].freeze
 
     LOG_NOTIFICATION_ACTIONS = [
