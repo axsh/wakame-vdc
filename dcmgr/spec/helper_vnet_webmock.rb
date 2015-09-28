12efdd9fe7dad0943d5c789697ad31e0ed7e7599
Akira Yokokawa <a-yokokawa@axsh.net>
2015-09-27 21:14:48 -0700

    Vnet webmock expects only required keys and ignores values.

--- dcmgr/spec/helper_vnet_webmock.rb
+++ dcmgr/spec/helper_vnet_webmock.rb
@@ -4,17 +4,8 @@ require 'webmock/rspec'
 
 WebMock.disable_net_connect!(allow_localhost: true)
 
-def request_params(params)
-  ret = ""
-  params.each do |k,v|
-    next if k == :uuid
-    next if k == :display_name
-    ret = "#{ret}#{k.to_sym}=#{v}&"
-  end
-  ret.chop!
-end
-
 def stub_vnet_request(api_suffix, params)
-  uri = Addressable::Template.new("localhost:9090/api/1.0/#{api_suffix}.json?#{request_params(params)}{&other*}")
+  expected_param_list = params.keys.map(&:to_s).join(",")
+  uri = Addressable::Template.new("localhost:9090/api/1.0/#{api_suffix}.json{?#{expected_param_list}}")
   stub_request(:post, uri).to_return(:body => "{\"uuid\":\"#{params[:uuid]}\"}")
 end
