ce1b8ab9f29187ef1d45b2357142536c4fe15578
Tsunehisa Iwano <t-iwano@axsh.net>
2015-09-28 18:51:25 +0900

    Changed default parameter for logging_service_reload.

--- dcmgr/lib/dcmgr/configurations/hva.rb
+++ dcmgr/lib/dcmgr/configurations/hva.rb
@@ -228,7 +228,7 @@ module Dcmgr
       param :logging_service_ip, :default =>'169.254.169.253'
       param :logging_service_port, :default => 8888
       param :logging_service_conf, :default => '/var/lib/wakame-vdc/fluent.conf'
-      param :logging_service_reload, :default => '/etc/init.d/td-agent reload'
+      param :logging_service_reload, :default => 'sudo /etc/init.d/td-agent restart'
       param :logging_service_max_read_message_bytes, :default => -1
       param :logging_service_max_match_count, :default => -1
       param :enable_gre, :default=>false
