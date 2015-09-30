0be66ff190efe5d4bb8b50a1f856e9036c956da5
Akira Yokokawa <a-yokokawa@axsh.net>
2015-09-30 14:43:37 -0700

    Create network and associated db entries when launching an instance.

--- client/mussel/test/acceptance-vnet/v12.03/single-vnet/helper_instance.sh
+++ client/mussel/test/acceptance-vnet/v12.03/single-vnet/helper_instance.sh
@@ -18,8 +18,18 @@ prefix=24
 network_mode=l2overlay
 service_dhcp="10.197.0.1"
 dhcp_range="default"
-
 network_uuid=$(run_cmd network create | hash_value id | grep "nw-")
+
+interfaces=$(curl -s -X GET http://${VNMGR_HOST}:${VNMGR_PORT}/api/1.0/interfaces.yaml | hash_value uuid | grep "if-")
+interface_uuid=""
+for i in ${interfaces}; do
+  ii=$(curl -s -X GET http://${VNMGR_HOST}:${VNMGR_PORT}/api/1.0/interfaces/${i}.yaml | grep host)
+  if [[ ! -z ${ii} ]]; then
+    interface_uuid=${i}
+  fi
+done
+datapath_network_uuid=$(curl -s -X POST --data-urlencode interface_uuid=${interface_uuid} http://${VNMGR_HOST}:${VNMGR_PORT}/api/1.0/datapaths/${datapath_uuid}/networks/${network_uuid}.yaml | hash_value uuid)
+
 
 ## functions
 
@@ -28,7 +38,8 @@ function needs_vif() { true; }
 function render_vif_table() {
   cat <<-EOS
 	{
-	"eth0":{"index":"0","network":"${network_uuid}"}
+	"eth0":{"index":"0","network":"${network_uuid}"},
+	"eth1":{"index":"1","network":"nw-demo8"}
 	}
 	EOS
 }
