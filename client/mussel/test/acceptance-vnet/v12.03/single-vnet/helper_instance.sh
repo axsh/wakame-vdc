3488b4b51b071b17ef5c080017219766ed27bfcc
Akira Yokokawa <a-yokokawa@axsh.net>
2015-10-01 05:32:07 -0700

    No need to create datapath_network by WebAPI.

--- client/mussel/test/acceptance-vnet/v12.03/single-vnet/helper_instance.sh
+++ client/mussel/test/acceptance-vnet/v12.03/single-vnet/helper_instance.sh
@@ -20,15 +20,15 @@ service_dhcp="10.197.0.1"
 dhcp_range="default"
 network_uuid=$(run_cmd network create | hash_value id | grep "nw-")
 
-interfaces=$(curl -s -X GET http://${VNMGR_HOST}:${VNMGR_PORT}/api/1.0/interfaces.yaml | hash_value uuid | grep "if-")
-interface_uuid=""
-for i in ${interfaces}; do
-  ii=$(curl -s -X GET http://${VNMGR_HOST}:${VNMGR_PORT}/api/1.0/interfaces/${i}.yaml | grep host)
-  if [[ ! -z ${ii} ]]; then
-    interface_uuid=${i}
-  fi
-done
-datapath_network_uuid=$(curl -s -X POST --data-urlencode interface_uuid=${interface_uuid} http://${VNMGR_HOST}:${VNMGR_PORT}/api/1.0/datapaths/${datapath_uuid}/networks/${network_uuid}.yaml | hash_value uuid)
+# interfaces=$(curl -s -X GET http://${VNMGR_HOST}:${VNMGR_PORT}/api/1.0/interfaces.yaml | hash_value uuid | grep "if-")
+# interface_uuid=""
+# for i in ${interfaces}; do
+#   ii=$(curl -s -X GET http://${VNMGR_HOST}:${VNMGR_PORT}/api/1.0/interfaces/${i}.yaml | grep host)
+#   if [[ ! -z ${ii} ]]; then
+#     interface_uuid=${i}
+#   fi
+# done
+# datapath_network_uuid=$(curl -s -X POST --data-urlencode interface_uuid=${interface_uuid} http://${VNMGR_HOST}:${VNMGR_PORT}/api/1.0/datapaths/${datapath_uuid}/networks/${network_uuid}.yaml | hash_value uuid)
 
 
 ## functions
