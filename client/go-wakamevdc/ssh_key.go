43f83ef0775b555356778f8d14de6899e970b09b
Masahiro Fujiwara <m-fujiwara@axsh.net>
2015-11-20 17:08:53 +0900

    Add DELETE & GET for ssh_key_pairs.

--- client/go-wakamevdc/ssh_key.go
+++ client/go-wakamevdc/ssh_key.go
@@ -1,34 +1,42 @@
 package wakamevdc
 
 import (
+  "fmt"
   "net/http"
 )
-
-type SshKeyAPI interface {
-  Create(*SshKeyCreateRequest) (*SshKey, *http.Response, error)
-}
 
 const SshKeyPath = "ssh_key_pairs"
 
 type SshKey struct {
   ID          string `json:"uuid"`
   PublicKey   string `json:"public_key"`
-  Fingerprint string `json:"fingerprint"`
+  Fingerprint string `json:"finger_print"`
   CreatedAt   string `json:"created_at"`
+  Description string `json:"description"`
 }
 
 type SshKeyService struct {
   client *Client
 }
 
-type SshKeyCreateRequest struct {
+type SshKeyCreateParams struct {
 	DisplayName  string `url:"display_name,omitempty"`
 	PublicKey    string `url:"public_key"`
 }
 
-// Create a key using a KeyCreateRequest
-func (s *SshKeyService) Create(req *SshKeyCreateRequest) (*SshKey, *http.Response, error) {
+func (s *SshKeyService) Create(req *SshKeyCreateParams) (*SshKey, *http.Response, error) {
   ssh_key := new(SshKey)
   resp, err := s.client.Sling().Post(SshKeyPath).BodyForm(req).ReceiveSuccess(ssh_key)
+  return ssh_key, resp, err
+}
+
+func (s *SshKeyService) Delete(id string) (*http.Response, error) {
+  resp, err := s.client.Sling().Delete(fmt.Sprintf(SshKeyPath + "/%s", id)).Receive(nil, nil)
+  return resp, err
+}
+
+func (s *SshKeyService) GetByID(id string) (*SshKey, *http.Response, error) {
+  ssh_key := new(SshKey)
+  resp, err := s.client.Sling().Get(fmt.Sprintf(SshKeyPath + "/%s", id)).ReceiveSuccess(ssh_key)
   return ssh_key, resp, err
 }
