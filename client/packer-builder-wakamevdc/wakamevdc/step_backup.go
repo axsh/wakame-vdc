3bd4608ab0a861a0f192b0d77edbc93dd4fc95a8
Masahiro Fujiwara <m-fujiwara@axsh.net>
2015-12-01 23:07:42 +0900

    Call real API in poweroff and backup steps.

--- client/packer-builder-wakamevdc/wakamevdc/step_backup.go
+++ client/packer-builder-wakamevdc/wakamevdc/step_backup.go
@@ -1,9 +1,9 @@
 package wakamevdc
 
 import (
-	//"fmt"
+	"fmt"
 
-	//"github.com/digitalocean/godo"
+	goclient "github.com/axsh/wakame-vdc/client/go-wakamevdc"
 	"github.com/mitchellh/multistep"
 	"github.com/mitchellh/packer/packer"
 )
@@ -11,8 +11,34 @@ import (
 type stepBackup struct {}
 
 func (s *stepBackup) Run(state multistep.StateBag) multistep.StepAction {
-  state.Get("ui").(packer.Ui).Say("Take instance backup...")
+	client := state.Get("client").(*goclient.Client)
+	ui := state.Get("ui").(packer.Ui)
+	conf := state.Get("config").(Config)
+	inst_id := state.Get("instance_id").(string)
 
+  ui.Say("Take instance backup...")
+	image_id, _, err := client.Instance.Backup(inst_id, &goclient.InstanceBackupParams {
+		All: false,
+	})
+	if err != nil {
+		err := fmt.Errorf("Error turning instance off: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+	ui.Say("New image: " + image_id)
+
+	err = waitForResourceState("available", image_id, client.Image, conf.StateTimeout)
+	if err != nil {
+		err := fmt.Errorf("Error waiting for image to become available: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+	ui.Say("Image created...")
+
+
+	state.Put("image_id", image_id)
   return multistep.ActionContinue
 }
 
--- client/packer-builder-wakamevdc/wakamevdc/step_power_off.go
+++ client/packer-builder-wakamevdc/wakamevdc/step_power_off.go
@@ -1,9 +1,9 @@
 package wakamevdc
 
 import (
-	//"fmt"
+	"fmt"
 
-	//"github.com/digitalocean/godo"
+	goclient "github.com/axsh/wakame-vdc/client/go-wakamevdc"
 	"github.com/mitchellh/multistep"
 	"github.com/mitchellh/packer/packer"
 )
@@ -11,7 +11,27 @@ import (
 type stepPowerOff struct {}
 
 func (s *stepPowerOff) Run(state multistep.StateBag) multistep.StepAction {
-  state.Get("ui").(packer.Ui).Say("Power off instance...")
+	client := state.Get("client").(*goclient.Client)
+	ui := state.Get("ui").(packer.Ui)
+	conf := state.Get("config").(Config)
+	inst_id := state.Get("instance_id").(string)
+
+	ui.Say("Power off instance...")
+	_, err := client.Instance.PowerOff(inst_id)
+	if err != nil {
+		err := fmt.Errorf("Error turning instance off: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+	err = waitForResourceState("halted", inst_id, client.Instance, conf.StateTimeout)
+	if err != nil {
+		err := fmt.Errorf("Error waiting for instance to become running: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+	ui.Say("Instance halted...")
 
   return multistep.ActionContinue
 }
