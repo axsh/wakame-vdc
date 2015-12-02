1117a859b5712a518d06158cb1db321faf7fb253
Masahiro Fujiwara <m-fujiwara@axsh.net>
2015-12-02 19:45:53 +0900

    Apply gofmt.

--- client/go-wakamevdc/ssh_key.go
+++ client/go-wakamevdc/ssh_key.go
@@ -1,43 +1,43 @@
 package wakamevdc
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
 const SshKeyPath = "ssh_key_pairs"
 
 type SshKey struct {
-  ID          string `json:"id"`
-  PublicKey   string `json:"public_key"`
-  Fingerprint string `json:"finger_print"`
-  CreatedAt   string `json:"created_at"`
-  Description string `json:"description"`
-  PrivateKey  string `json:"private_key"`
+	ID          string `json:"id"`
+	PublicKey   string `json:"public_key"`
+	Fingerprint string `json:"finger_print"`
+	CreatedAt   string `json:"created_at"`
+	Description string `json:"description"`
+	PrivateKey  string `json:"private_key"`
 }
 
 type SshKeyService struct {
-  client *Client
+	client *Client
 }
 
 type SshKeyCreateParams struct {
-  DisplayName  string `url:"display_name,omitempty"`
-  PublicKey    string `url:"public_key"`
+	DisplayName string `url:"display_name,omitempty"`
+	PublicKey   string `url:"public_key"`
 }
 
 func (s *SshKeyService) Create(req *SshKeyCreateParams) (*SshKey, *http.Response, error) {
-  ssh_key := new(SshKey)
-  resp, err := s.client.Sling().Post(SshKeyPath).BodyForm(req).ReceiveSuccess(ssh_key)
-  return ssh_key, resp, err
+	sshKey := new(SshKey)
+	resp, err := s.client.Sling().Post(SshKeyPath).BodyForm(req).ReceiveSuccess(sshKey)
+	return sshKey, resp, err
 }
 
 func (s *SshKeyService) Delete(id string) (*http.Response, error) {
-  resp, err := s.client.Sling().Delete(fmt.Sprintf(SshKeyPath + "/%s", id)).Receive(nil, nil)
-  return resp, err
+	resp, err := s.client.Sling().Delete(fmt.Sprintf(SshKeyPath+"/%s", id)).Receive(nil, nil)
+	return resp, err
 }
 
 func (s *SshKeyService) GetByID(id string) (*SshKey, *http.Response, error) {
-  ssh_key := new(SshKey)
-  resp, err := s.client.Sling().Get(fmt.Sprintf(SshKeyPath + "/%s", id)).ReceiveSuccess(ssh_key)
-  return ssh_key, resp, err
+	sshKey := new(SshKey)
+	resp, err := s.client.Sling().Get(fmt.Sprintf(SshKeyPath+"/%s", id)).ReceiveSuccess(sshKey)
+	return sshKey, resp, err
 }
