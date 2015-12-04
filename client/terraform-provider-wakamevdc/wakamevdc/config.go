0cb2597815ffeea5ef39662bbfedfda80533a98b
Masahiro Fujiwara <m-fujiwara@axsh.net>
2015-12-04 21:08:50 +0900

    Apply gofmt.

--- client/terraform-provider-wakamevdc/wakamevdc/config.go
+++ client/terraform-provider-wakamevdc/wakamevdc/config.go
@@ -1,18 +1,19 @@
 package wakamevdc
 
 import (
-  "github.com/axsh/wakame-vdc/client/go-wakamevdc"
-  "net/url"
+	"net/url"
+
+	"github.com/axsh/wakame-vdc/client/go-wakamevdc"
 )
 
 type Config struct {
-  APIEndpoint string
+	APIEndpoint string
 }
 
 func (c *Config) Client() (*wakamevdc.Client, error) {
-  //TODO: Don't force the user to include /api/12.03
-  baseURL, err := url.Parse(c.APIEndpoint)
-  client :=  wakamevdc.NewClient(baseURL, nil)
+	//TODO: Don't force the user to include /api/12.03
+	baseURL, err := url.Parse(c.APIEndpoint)
+	client := wakamevdc.NewClient(baseURL, nil)
 
-  return client, err
+	return client, err
 }
