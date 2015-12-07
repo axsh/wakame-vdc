26a9e0a89cbe81fbac95811eca70798869a83f86
Metallion <akieckens@axsh.net>
2015-12-07 19:10:15 +0900

    Ran go fmt on the code

--- client/terraform-provider-wakamevdc/wakamevdc/resource_wakamevdc_ssh_key.go
+++ client/terraform-provider-wakamevdc/wakamevdc/resource_wakamevdc_ssh_key.go
@@ -1,55 +1,55 @@
 package wakamevdc
 
 import (
-  "github.com/axsh/wakame-vdc/client/go-wakamevdc"
-  "github.com/hashicorp/terraform/helper/schema"
+	"github.com/axsh/wakame-vdc/client/go-wakamevdc"
+	"github.com/hashicorp/terraform/helper/schema"
 )
 
 func resourceWakamevdcSSHKey() *schema.Resource {
-  return &schema.Resource{
-    Create: resourceWakamevdcSSHKeyCreate,
-    Read: resourceWakamevdcSSHKeyRead,
-    Update: resourceWakamevdcSSHKeyUpdate,
-    Delete: resourceWakamevdcSSHKeyDelete,
+	return &schema.Resource{
+		Create: resourceWakamevdcSSHKeyCreate,
+		Read:   resourceWakamevdcSSHKeyRead,
+		Update: resourceWakamevdcSSHKeyUpdate,
+		Delete: resourceWakamevdcSSHKeyDelete,
 
-    Schema: map[string]*schema.Schema{
-      "id": &schema.Schema{
-        Type:     schema.TypeString,
-        Computed: true,
-      },
+		Schema: map[string]*schema.Schema{
+			"id": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 
-      "display_name": &schema.Schema{
-        Type:     schema.TypeString,
-        Required: true,
-      },
+			"display_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
 
-      "public_key": &schema.Schema{
-        Type:     schema.TypeString,
-        Optional: true,
-        ForceNew: true,
-      },
+			"public_key": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
 
-      "fingerprint": &schema.Schema{
-        Type:     schema.TypeString,
-        Computed: true,
-      },
-    },
-  }
+			"fingerprint": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
 }
 
 func resourceWakamevdcSSHKeyCreate(d *schema.ResourceData, m interface{}) error {
-  client := m.(*wakamevdc.Client)
+	client := m.(*wakamevdc.Client)
 
-  params := wakamevdc.SshKeyCreateParams{
-    DisplayName: d.Get("display_name").(string),
-    PublicKey: d.Get("public_key").(string),
-  }
+	params := wakamevdc.SshKeyCreateParams{
+		DisplayName: d.Get("display_name").(string),
+		PublicKey:   d.Get("public_key").(string),
+	}
 
-  key, _, err := client.SshKey.Create(&params)
+	key, _, err := client.SshKey.Create(&params)
 
-  d.SetId(key.ID)
+	d.SetId(key.ID)
 
-  return err
+	return err
 }
 
 func resourceWakamevdcSSHKeyRead(d *schema.ResourceData, m interface{}) error {
@@ -67,9 +67,9 @@ func resourceWakamevdcSSHKeyRead(d *schema.ResourceData, m interface{}) error {
 }
 
 func resourceWakamevdcSSHKeyUpdate(d *schema.ResourceData, m interface{}) error {
-  return nil
+	return nil
 }
 
 func resourceWakamevdcSSHKeyDelete(d *schema.ResourceData, m interface{}) error {
-  return nil
+	return nil
 }
