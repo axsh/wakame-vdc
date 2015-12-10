e2a614a45f1a4d9a550a774a0332e99b71628fac
Richard Potter <potter@axsh.net>
2015-12-10 20:02:43 +0900

    Simplified header for independent scripts and related refactoring

--- vmimages/centos-6.7-x86_64/build.sh
+++ vmimages/centos-6.7-x86_64/build.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-######################################################################
-## Directory Paths
-######################################################################
-
-export CODEDIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd -P)" || reportfailed
-export DATADIR="$CODEDIR/output"
-
-# put the current directory someplace unwritable to force use
-# of the above variables
-cd -P /proc/self
-
-######################################################################
-## Inline Steps
-######################################################################
-
-# set reportfailed, $skip_rest_if_already_done, etc.
-source "$CODEDIR/bin/simple-defaults-for-bashsteps.source"
+# Because this script can be called independently,
+# it must set up its bashsteps environment, e.g.
+# reportfailed(), $skip_rest_if_already_done, etc.
+source "$(dirname "$0")/bin/simple-defaults-for-bashsteps.source" || exit
 
 (
     $starting_dependents "Build centos-6.7-x86_64 image"
