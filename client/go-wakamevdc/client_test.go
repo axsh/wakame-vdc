532b14286745f64359a5fe34a4017b2bd34808d0
Masahiro Fujiwara <m-fujiwara@axsh.net>
2015-12-11 19:42:22 +0900

    Allow arbitrary Account ID to be set.

--- client/go-wakamevdc/client_test.go
+++ client/go-wakamevdc/client_test.go
@@ -10,3 +10,15 @@ func TestNewClient(t *testing.T) {
 		t.Errorf("NewClient() failed")
 	}
 }
+
+func TestSetAccount(t *testing.T) {
+	c := NewClient(nil, nil)
+	if c.accountID != defaultAccountID {
+		t.Errorf("accountID does not have default value:")
+	}
+
+	c.AccountID("a-000001")
+	if c.accountID != "a-000001" {
+		t.Errorf("AccountID() failed to overwrite")
+	}
+}
