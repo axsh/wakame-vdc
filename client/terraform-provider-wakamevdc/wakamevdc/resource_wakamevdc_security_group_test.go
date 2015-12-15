bb6e21f1f769322eb6df3b3c9c0fe3e9ec9cc848
Masahiro Fujiwara <m-fujiwara@axsh.net>
2015-12-15 21:40:55 +0900

    Implement check destroy func.

--- client/terraform-provider-wakamevdc/wakamevdc/resource_wakamevdc_security_group_test.go
+++ client/terraform-provider-wakamevdc/wakamevdc/resource_wakamevdc_security_group_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/axsh/wakame-vdc/client/go-wakamevdc"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -16,16 +17,23 @@ resource "wakamevdc_security_group" "sg1" {
 `
 
 func TestResourceWakamevdcSecurityGroupCreate(t *testing.T) {
+	var resourceID string
+
 	testCheck := func(s *terraform.State) error {
 		rs, _ := s.RootModule().Resources["wakamevdc_security_group.sg1"]
 		if rs.Primary.ID == "" {
 			return fmt.Errorf("No ID is set")
 		}
+		resourceID = rs.Primary.ID
 		return nil
 	}
 
 	testCheckDestroy := func(s *terraform.State) error {
-
+		client := testVdcProvider.Meta().(*wakamevdc.Client)
+		_, _, err := client.SecurityGroup.GetByID(resourceID)
+		if _, ok := err.(*wakamevdc.APIError); ok == false {
+			return fmt.Errorf("APIError is expected")
+		}
 		return nil
 	}
 
