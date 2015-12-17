cf41275a95f8b744ee020eefde43d9d6bb2bdf3a
Masahiro Fujiwara <m-fujiwara@axsh.net>
2015-12-17 23:23:11 +0900

    Add test for creating network.

--- client/go-wakamevdc/network_test.go
+++ client/go-wakamevdc/network_test.go
@@ -27,3 +27,29 @@ func TestNetworkList(t *testing.T) {
 		t.Errorf("Results contain more items than specifed in limit")
 	}
 }
+
+func TestNetworkCreate(t *testing.T) {
+	c := NewClient(nil, nil)
+	// 1box image sets "allow_new_networks" true
+	dcnList, _, err := c.DCNetwork.List(nil, "vnet")
+	if err != nil {
+		t.Fatalf("Failed DCNetwork.List: %v", err)
+	}
+	if dcnList.Total != 1 {
+		t.Fatalf("Unknown DCNetwork name=\"vnet\"")
+	}
+	nw, _, err := c.Network.Create(&NetworkCreateParams{
+		IPv4Network: "10.0.0.0",
+		Prefix:      24,
+		NetworkMode: "l2overlay",
+		DCNetworkID: dcnList.Results[0].ID,
+	})
+	if err != nil {
+		t.Fatalf("Failed to create network: %v", err)
+	}
+
+	_, err = c.Network.Delete(nw.ID)
+	if err != nil {
+		t.Fatalf("Failed to delete network: %s", nw.ID)
+	}
+}
