621e24d3ac71e50094f249b02e96edf5233bdaa1
Metallion <akieckens@axsh.net>
2016-03-17 15:46:53 +0900

    Added boilerplate code for more thorough security group tests

--- client/terraform-provider-wakamevdc/wakamevdc/resource_wakamevdc_security_group_test.go
+++ client/terraform-provider-wakamevdc/wakamevdc/resource_wakamevdc_security_group_test.go
@@ -40,6 +40,39 @@ func TestResourceWakamevdcSecurityGroupMinimal(t *testing.T) {
 	})
 }
 
+const testConfigCreate = `
+resource "wakame_vdc_security_group" "sg2" {
+	display_name = "sg2"
+	account_id = "a-shpoolxx"
+	service_type = "std"
+	discritpion = "The second group in our test"
+	rules = <<EOS
+tcp:22,22,ip4:0.0.0.0
+icmp:-1,-1,ip4:0.0.0.0
+EOS
+}
+`
+
+func TestResourceWakamevdcSecurityGroupFull(t *testing.T) {
+	var resourceID string
+
+	testCreated := func(s *terraform.State) error {
+		return nil
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     nil,
+		Providers:    testVdcProviders,
+		CheckDestroy: testCheckDestroy(resourceID),
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testConfigCreate,
+				Check:  testCreated,
+			},
+		},
+	})
+}
+
 //helpers
 func testCheckDestroy(uuid string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
