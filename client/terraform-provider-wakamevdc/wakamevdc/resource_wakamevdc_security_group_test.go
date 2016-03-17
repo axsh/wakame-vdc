8650f7d70d3e1106fa6f7505a87dd814d06f5f34
Metallion <akieckens@axsh.net>
2016-03-17 15:10:34 +0900

    Refactored security group test to make the checkdestroy function reusable

--- client/terraform-provider-wakamevdc/wakamevdc/resource_wakamevdc_security_group_test.go
+++ client/terraform-provider-wakamevdc/wakamevdc/resource_wakamevdc_security_group_test.go
@@ -9,14 +9,13 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
-const testEmptyRuleConfig = `
+const testConfigMinimal = `
 resource "wakamevdc_security_group" "sg1" {
   display_name = "sg1"
-  rules = ""
 }
 `
 
-func TestResourceWakamevdcSecurityGroupCreate(t *testing.T) {
+func TestResourceWakamevdcSecurityGroupMinimal(t *testing.T) {
 	var resourceID string
 
 	testCheck := func(s *terraform.State) error {
@@ -28,24 +27,29 @@ func TestResourceWakamevdcSecurityGroupCreate(t *testing.T) {
 		return nil
 	}
 
-	testCheckDestroy := func(s *terraform.State) error {
-		client := testVdcProvider.Meta().(*wakamevdc.Client)
-		_, _, err := client.SecurityGroup.GetByID(resourceID)
-		if _, ok := err.(*wakamevdc.APIError); ok == false {
-			return fmt.Errorf("APIError is expected")
-		}
-		return nil
-	}
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:     nil,
 		Providers:    testVdcProviders,
-		CheckDestroy: testCheckDestroy,
+		CheckDestroy: testCheckDestroy(resourceID),
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: testEmptyRuleConfig,
+				Config: testConfigMinimal,
 				Check:  testCheck,
 			},
 		},
 	})
+}
+
+//helpers
+func testCheckDestroy(uuid string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testVdcProvider.Meta().(*wakamevdc.Client)
+
+		_, _, err := client.SecurityGroup.GetByID(uuid)
+		if _, ok := err.(*wakamevdc.APIError); ok == false {
+			return fmt.Errorf("The security group resource wasn't deleted when expected")
+		}
+
+		return nil
+	}
 }
