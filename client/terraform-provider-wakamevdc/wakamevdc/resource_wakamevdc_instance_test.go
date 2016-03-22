c275c221334d63da37357d0cc4d2ce89f85565f1
Metallion <akieckens@axsh.net>
2016-03-22 15:24:30 +0900

    Started putting in tests that make sure terraform and wakame-vdc are in sync when starting instances

--- client/terraform-provider-wakamevdc/wakamevdc/resource_wakamevdc_instance_test.go
+++ client/terraform-provider-wakamevdc/wakamevdc/resource_wakamevdc_instance_test.go
@@ -2,6 +2,7 @@ package wakamevdc
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/axsh/wakame-vdc/client/go-wakamevdc"
@@ -28,13 +29,20 @@ func TestResourceWakamevdcInstanceCreate(t *testing.T) {
 	var resourceID string
 
 	testCheck := func(s *terraform.State) error {
-		rs, _ := s.RootModule().Resources["wakamevdc_instance.inst1"]
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("No ID is set")
+		rs, inst, err := getTerraformResourceAndWakameInstance(s, "wakamevdc_instance.inst1")
+		if err != nil {
+			return err
 		}
-		if rs.Primary.Attributes["state"] != "running" {
-			return fmt.Errorf("instance is in unexpected state: %s", rs.Primary.ID)
+
+		if inst.ID != rs.Primary.ID {
+			return parameterCheckFailed("id", inst.ID, rs.Primary.ID)
 		}
+
+		instCPUCoresS := strconv.Itoa(inst.CPUCores)
+		if instCPUCoresS != rs.Primary.Attributes["cpu_cores"] {
+			return parameterCheckFailed("cpu_cores", instCPUCoresS, rs.Primary.Attributes["cpu_cores"])
+		}
+
 		resourceID = rs.Primary.ID
 		return nil
 	}
@@ -59,19 +67,20 @@ func TestResourceWakamevdcInstanceCreate(t *testing.T) {
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testInstanceConfig1,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("wakamevdc_instance.inst1", "cpu_cores", "1"),
-					resource.TestCheckResourceAttr("wakamevdc_instance.inst1", "memory_size", "512"),
-					resource.TestCheckResourceAttr("wakamevdc_instance.inst1", "image_id", "wmi-centos1d64"),
-					resource.TestCheckResourceAttr("wakamevdc_instance.inst1", "hypervisor", "openvz"),
-					resource.TestCheckResourceAttr("wakamevdc_instance.inst1", "display_name", "inst1"),
-					resource.TestCheckResourceAttr("wakamevdc_instance.inst1", "ssh_key_id", "ssh-demo"),
-				),
-			},
-			resource.TestStep{
-				Config: testInstanceConfig1,
 				Check:  testCheck,
 			},
 		},
 	})
+}
+
+func getTerraformResourceAndWakameInstance(s *terraform.State, resourceName string) (*terraform.ResourceState, *wakamevdc.Instance, error) {
+	rs, ok := s.RootModule().Resources[resourceName]
+	if !ok {
+		return nil, nil, fmt.Errorf("Not found: %s", resourceName)
+	}
+
+	client := testVdcProvider.Meta().(*wakamevdc.Client)
+
+	key, _, err := client.Instance.GetByID(rs.Primary.ID)
+	return rs, key, err
 }
