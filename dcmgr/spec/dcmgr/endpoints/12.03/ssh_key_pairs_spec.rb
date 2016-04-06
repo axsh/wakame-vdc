74dbd1039b2fdceb511e7a872c5cf7f04b7313f2
Metallion <akieckens@axsh.net>
2016-04-06 19:08:08 +0900

    Added some tests for the ssh keys endpoint

--- dcmgr/spec/dcmgr/endpoints/12.03/ssh_key_pairs_spec.rb
+++ dcmgr/spec/dcmgr/endpoints/12.03/ssh_key_pairs_spec.rb
@@ -2,12 +2,12 @@
 require_relative 'helper'
 
 describe "ssh_key_pairs" do
+  before(:each) { stub_dcmgr_syncronized_message_ready }
+
+  let(:account) { Fabricate(:account) }
+
   describe "POST" do
-    let(:account) { Fabricate(:account) }
-
     before(:each) do
-      stub_dcmgr_syncronized_message_ready
-
       post("ssh_key_pairs",
            params,
            Dcmgr::Endpoints::HTTP_X_VDC_ACCOUNT_UUID => account.canonical_uuid)
@@ -19,6 +19,63 @@ describe "ssh_key_pairs" do
       it "doesn't crash" do
         if !last_response.errors.empty?
           raise "The API call crashed.\n#{last_response.errors}"
+        end
+      end
+    end
+  end
+
+  describe "GET" do
+    context "with no parameters" do
+      let(:params) { Hash.new }
+
+      context "with no ssh keys in the database" do
+        it "returns an empty list of results" do
+          get("ssh_key_pairs",
+               params,
+               Dcmgr::Endpoints::HTTP_X_VDC_ACCOUNT_UUID => account.canonical_uuid)
+
+          expect(body).to eq [{
+           "total" => 0,
+           "start" => 0,
+           "limit" => 250,
+           "results" => []
+          }]
+        end
+      end
+
+      context "with ssh keys in the database" do
+        before(:each) do
+          3.times { Fabricate(:ssh_key_pair, account_id: account.canonical_uuid) }
+
+          Fabricate(:ssh_key_pair, account_id: Fabricate(:account).canonical_uuid)
+        end
+
+        it "shows only the ones belonging the the requester's account" do
+          get("ssh_key_pairs",
+               params,
+               Dcmgr::Endpoints::HTTP_X_VDC_ACCOUNT_UUID => account.canonical_uuid)
+
+          expect(body.first["total"]).to eq 3
+          expect(body.first["results"].size).to eq 3
+        end
+      end
+
+      context "with deleted ssh keys in the database" do
+        before(:each) do
+          3.times { Fabricate(:ssh_key_pair, account_id: account.canonical_uuid).destroy }
+        end
+
+        it "does not show the deleted keys" do
+          get("ssh_key_pairs",
+               params,
+               Dcmgr::Endpoints::HTTP_X_VDC_ACCOUNT_UUID => account.canonical_uuid)
+
+          expect(body).to eq [{
+           "total" => 0,
+           "start" => 0,
+           "limit" => 250,
+           "results" => []
+          }]
         end
       end
     end
