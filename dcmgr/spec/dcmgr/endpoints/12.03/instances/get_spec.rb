b737ce4052580140c073923c8f3876fa2808542b
Metallion <akieckens@axsh.net>
2016-04-06 16:26:11 +0900

    Fixed the tests for GET instances

--- dcmgr/spec/dcmgr/endpoints/12.03/instances/get_spec.rb
+++ dcmgr/spec/dcmgr/endpoints/12.03/instances/get_spec.rb
@@ -4,25 +4,36 @@ require_relative '../helper'
 #Dir["#{File.dirname(__FILE__)}/instances/*.rb"].each {|f| require f }
 
 describe Dcmgr::Endpoints::V1203::CoreAPI, "GET instances" do
-  before(:each)  { get("instances", params) }
+  let(:account) { Fabricate(:account) }
+
+  let(:headers) do
+    { Dcmgr::Endpoints::HTTP_X_VDC_ACCOUNT_UUID => account.canonical_uuid }
+  end
+
+  before(:each)  {
+    stub_dcmgr_syncronized_message_ready
+  }
 
   context "with no parameters" do
     let(:params) { Hash.new }
 
     context "with no instances in the database" do
       it "shows that there are indeed no instances in the database" do
-         expect(body).to eq [{
-          "total" => 0,
-          "start" => 0,
-          "limit" => 250,
-          "results" => []
-         }]
+        get("instances", params, headers)
+
+        expect(body).to eq [{
+         "total" => 0,
+         "start" => 0,
+         "limit" => 250,
+         "results" => []
+        }]
       end
     end
 
     context "with 3 instances in the database" do
-      before(:all) do
-        3.times { Fabricate(:instance) }
+      before(:each) do
+        3.times { Fabricate(:instance, account_id: account.canonical_uuid) }
+        get("instances", params, headers)
       end
 
       it "shows all 3 instances in the database" do
