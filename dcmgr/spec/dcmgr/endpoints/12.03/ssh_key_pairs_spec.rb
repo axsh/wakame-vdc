8812b4e84fbcdadc4a8ac5f1b9d4170c448481b3
Metallion <akieckens@axsh.net>
2016-04-08 17:11:26 +0900

    Wrote a test for ssh key pairs get id

--- dcmgr/spec/dcmgr/endpoints/12.03/ssh_key_pairs_spec.rb
+++ dcmgr/spec/dcmgr/endpoints/12.03/ssh_key_pairs_spec.rb
@@ -5,6 +5,9 @@ describe "ssh_key_pairs" do
   before(:each) { stub_dcmgr_syncronized_message_ready }
 
   let(:account) { Fabricate(:account) }
+  let(:headers) do
+    { Dcmgr::Endpoints::HTTP_X_VDC_ACCOUNT_UUID => account.canonical_uuid }
+  end
   # Contexts can override this let to execute code before the api calls
   let(:before_api_call) {}
 
@@ -12,9 +15,7 @@ describe "ssh_key_pairs" do
     before(:each) do
       before_api_call
 
-      post("ssh_key_pairs",
-           params,
-           Dcmgr::Endpoints::HTTP_X_VDC_ACCOUNT_UUID => account.canonical_uuid)
+      post("ssh_key_pairs", params, headers)
     end
 
     context "with no parameters" do
@@ -28,13 +29,41 @@ describe "ssh_key_pairs" do
     end
   end
 
+  describe "GET /:id" do
+    before(:each) do
+      before_api_call
+
+      get("ssh_key_pairs/#{key_pair_id}", {}, headers)
+    end
+
+    context "with an existing key pair id" do
+      let(:key_pair) { Fabricate(:ssh_key_pair, account_id: account.canonical_uuid) }
+      let(:key_pair_id) { key_pair.canonical_uuid }
+
+      it "shows the key pair" do
+        expect(body).to eq({
+          "id" => key_pair.canonical_uuid,
+          "account_id" => key_pair.account_id,
+          "uuid" => key_pair.canonical_uuid,
+          "finger_print" => key_pair.finger_print,
+          "public_key" => key_pair.public_key,
+          "display_name" => key_pair.display_name,
+          "description" => key_pair.description.to_s,
+          "created_at" => key_pair.created_at.iso8601,
+          "updated_at" => key_pair.updated_at.iso8601,
+          "service_type" => key_pair.service_type,
+          "deleted_at" => key_pair.deleted_at,
+          "labels" => []
+        })
+      end
+    end
+  end
+
   describe "GET" do
     before(:each) do
       before_api_call
 
-      get("ssh_key_pairs",
-           params,
-           Dcmgr::Endpoints::HTTP_X_VDC_ACCOUNT_UUID => account.canonical_uuid)
+      get("ssh_key_pairs", params, headers)
     end
 
     context "with no parameters" do
